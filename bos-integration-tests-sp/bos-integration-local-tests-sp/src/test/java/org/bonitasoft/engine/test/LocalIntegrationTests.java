769659fc9e3c192a7ed47e3d9d321cac24cd4c32
Elias Ricken de Medeiros <elias.ricken@bonitasoft.com>
2011-11-04 09:20:08 +0000

    feature: 6543, first implementation of sp modules

---
+++ bos-integration-tests-sp/bos-integration-local-tests-sp/src/test/java/org/bonitasoft/engine/test/LocalIntegrationTests.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2011 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bonitasoft.engine.test;
+
+
+import org.bonitasoft.engine.BPMTests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+/**
+ * @author Elias Ricken de Medeiros
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+  BPMTests.class
+})
+public class LocalIntegrationTests {
+
+}
