1252fa017691b77bc94b761e42a218654c0e03ed
Elias Ricken de Medeiros <elias.ricken@bonitasoft.com>
2011-11-07 14:57:26 +0000

    feature: 6543, add missing dependency, update spring configurations

--- bpm/bos-integration-tests/bos-integration-local-tests/src/test/java/org/bonitasoft/engine/test/LocalIntegrationTestsSP.java
+++ bpm/bos-integration-tests/bos-integration-local-tests/src/test/java/org/bonitasoft/engine/test/LocalIntegrationTestsSP.java
@@ -5,32 +5,27 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.bonitasoft.engine.test;
-
 
 import org.bonitasoft.engine.BPMTestsSP;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-
 /**
  * @author Elias Ricken de Medeiros
- *
  */
 @RunWith(Suite.class)
 @SuiteClasses({
-  BPMTestsSP.class
-})
-public class LocalIntegrationTests {
+    BPMTestsSP.class
+    })
+public class LocalIntegrationTestsSP {
 
 }
