8b8fcd48a71ccee5d361fb518c0ddf9e902c8da0
Matthieu Chaffotte <matthieu.chaffotte@bonitasoft.com>
2012-02-24 16:25:53 +0000

    feature 8320, added userId in Session (Emmanuel's patch :p)

--- bpm/bos-core/bos-login/bos-login-api-impl/src/main/java/org/bonitasoft/engine/core/login/impl/SecuredLoginServiceImpl.java
+++ bpm/bos-core/bos-login/bos-login-api-impl/src/main/java/org/bonitasoft/engine/core/login/impl/SecuredLoginServiceImpl.java
@@ -1,19 +1,16 @@
 /**
  * Copyright (C) 2011 BonitaSoft S.A.
- * BonitaSoft, 31 rue Gustave Eiffel - 38000 Grenoble
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2.0 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
 package org.bonitasoft.engine.core.login.impl;
 
 import java.io.File;
@@ -31,23 +28,24 @@ import org.bonitasoft.engine.core.login.LoginService;
 import org.bonitasoft.engine.core.login.LoginServiceImpl;
 import org.bonitasoft.engine.exception.BonitaHomeNotSetException;
 import org.bonitasoft.engine.home.BonitaHomeServer;
+import org.bonitasoft.engine.identity.IdentityService;
 import org.bonitasoft.engine.session.SessionException;
 import org.bonitasoft.engine.session.SessionService;
 import org.bonitasoft.engine.session.model.SSession;
 import org.bonitasoft.engine.sessionaccessor.SessionAccessor;
 
 /**
- * 
  * @author Matthieu Chaffotte
- *
  */
 public class SecuredLoginServiceImpl extends LoginServiceImpl implements LoginService {
 
     private final AuthenticationService authenticationService;
+
     private final SessionAccessor sessionAccesor;
 
-    public SecuredLoginServiceImpl(final AuthenticationService authenticationService, final SessionService sessionService, final SessionAccessor sessionAccessor) {
-        super(sessionService);
+    public SecuredLoginServiceImpl(final AuthenticationService authenticationService, final SessionService sessionService,
+            final SessionAccessor sessionAccessor, final IdentityService identityService) {
+        super(sessionService, identityService);
         this.authenticationService = authenticationService;
         this.sessionAccesor = sessionAccessor;
     }
@@ -55,21 +53,23 @@ public class SecuredLoginServiceImpl extends LoginServiceImpl implements LoginSe
     @Override
     public SSession login(final long tenantId, final String userName, final String password) throws LoginException {
         sessionAccesor.setSessionInfo(-1, tenantId); // necessary to check user credentials
+        long userId = -1;
         try {
-            authenticationService.checkUserCredentials(userName, password);
+            userId = authenticationService.checkUserCredentials(userName, password).getId();
         } catch (final UserNotFoundException e) {
             handleInvalidUser(tenantId, userName, password, e);
         } catch (final InvalidPasswordException e) {
             handleInvalidUser(tenantId, userName, password, e);
-        }  
+        }
         try {
-            return getSessionService().createSession(tenantId, userName);
+            return getSessionService().createSession(tenantId, userName, userId);
         } catch (final SessionException se) {
             throw new LoginException(se);
         }
     }
 
-    private void handleInvalidUser(final long tenantId, final String userName, final String password, final SBonitaException originalException) throws LoginException {
+    private void handleInvalidUser(final long tenantId, final String userName, final String password, final SBonitaException originalException)
+            throws LoginException {
         try {
             final boolean isTechnicalUser = checkTechinicalUserCredentials(tenantId, userName, password);
             if (!isTechnicalUser) {
@@ -79,10 +79,11 @@ public class SecuredLoginServiceImpl extends LoginServiceImpl implements LoginSe
             throw new LoginException(bhnse);
         } catch (final FileNotFoundException fnfe) {
             throw new LoginException(fnfe);
-        } 
+        }
     }
 
-    private boolean checkTechinicalUserCredentials(final long tenantId, final String userName, final String password) throws BonitaHomeNotSetException, FileNotFoundException, LoginException {
+    private boolean checkTechinicalUserCredentials(final long tenantId, final String userName, final String password) throws BonitaHomeNotSetException,
+            FileNotFoundException, LoginException {
         final String technicalUserPropertiesPath = BonitaHomeServer.getInstance().getTenantConfFolder(tenantId) + File.separator + "technical-user.properties";
         final Properties techProp = new Properties();
         final FileInputStream fis = new FileInputStream(technicalUserPropertiesPath);
