75e85753bec616698b7cef1890b31add11d1fd5e
Yanyan Liu <yanyan.liu@bonitasoft.com>
2012-03-26 06:44:33 +0000

    change BPMTests to BPMLocalTests

--- bpm/bos-integration-tests/bos-integration-client-tests/src/main/java/org/bonitasoft/engine/BPMTestsSP.java
+++ bpm/bos-integration-tests/bos-integration-client-tests/src/main/java/org/bonitasoft/engine/BPMTestsSP.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2011 BonitaSoft S.A.
+ * Copyright (C) 2011-2012 BonitaSoft S.A.
  * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -28,7 +28,7 @@ import org.junit.runners.Suite.SuiteClasses;
  */
 @RunWith(Suite.class)
 @SuiteClasses({
-  BPMTests.class,
+  BPMLocalTests.class,
   UserTestSP.class,
   LogTestSP.class
 })
