5e4957e395342d6cbc3e835928868ccf896f524c
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2012-03-27 14:27:59 +0000

    fix compilation failure

--- bpm/bos-core/bos-login/bos-login-api-impl/src/main/java/org/bonitasoft/engine/core/login/impl/SecuredLoginServiceImpl.java
+++ bpm/bos-core/bos-login/bos-login-api-impl/src/main/java/org/bonitasoft/engine/core/login/impl/SecuredLoginServiceImpl.java
@@ -21,14 +21,14 @@ import org.bonitasoft.engine.authentication.AuthenticationService;
 import org.bonitasoft.engine.authentication.InvalidPasswordException;
 import org.bonitasoft.engine.authentication.UserNotFoundException;
 import org.bonitasoft.engine.commons.exceptions.SBonitaException;
-import org.bonitasoft.engine.core.login.LoginException;
 import org.bonitasoft.engine.core.login.LoginService;
 import org.bonitasoft.engine.core.login.LoginServiceImpl;
+import org.bonitasoft.engine.core.login.SLoginException;
 import org.bonitasoft.engine.exception.BonitaHomeNotSetException;
 import org.bonitasoft.engine.home.BonitaHomeServer;
 import org.bonitasoft.engine.identity.IdentityService;
 import org.bonitasoft.engine.io.PropertiesManager;
-import org.bonitasoft.engine.session.SessionException;
+import org.bonitasoft.engine.session.SSessionException;
 import org.bonitasoft.engine.session.SessionService;
 import org.bonitasoft.engine.session.model.SSession;
 import org.bonitasoft.engine.sessionaccessor.SessionAccessor;
@@ -42,15 +42,15 @@ public class SecuredLoginServiceImpl extends LoginServiceImpl implements LoginSe
 
     private final SessionAccessor sessionAccesor;
 
-    public SecuredLoginServiceImpl(final AuthenticationService authenticationService, final SessionService sessionService,
+    public SecuredLoginServiceImpl(final AuthenticationService authenticationService, final SessionService SSessionService,
             final SessionAccessor sessionAccessor, final IdentityService identityService) {
-        super(sessionService, identityService);
+        super(SSessionService, identityService);
         this.authenticationService = authenticationService;
         sessionAccesor = sessionAccessor;
     }
 
     @Override
-    public SSession login(final long tenantId, final String userName, final String password) throws LoginException {
+    public SSession login(final long tenantId, final String userName, final String password) throws SLoginException {
         sessionAccesor.setSessionInfo(-1, tenantId); // necessary to check user credentials
         long userId = -1;
         try {
@@ -64,27 +64,27 @@ public class SecuredLoginServiceImpl extends LoginServiceImpl implements LoginSe
             final SSession createSession = getSessionService().createSession(tenantId, userName);
             getSessionService().setCurrentUserId(createSession, userId);
             return createSession;
-        } catch (final SessionException se) {
-            throw new LoginException(se);
+        } catch (final SSessionException se) {
+            throw new SLoginException(se);
         }
     }
 
     private void handleInvalidUser(final long tenantId, final String userName, final String password, final SBonitaException originalException)
-            throws LoginException {
+            throws SLoginException {
         try {
-            final boolean isTechnicalUser = checkTechinicalUserCredentials(tenantId, userName, password);
+            final boolean isTechnicalUser = checkTechnicalUserCredentials(tenantId, userName, password);
             if (!isTechnicalUser) {
-                throw new LoginException(originalException);
+                throw new SLoginException(originalException);
             }
         } catch (final BonitaHomeNotSetException bhnse) {
-            throw new LoginException(bhnse);
+            throw new SLoginException(bhnse);
         } catch (final IOException ioe) {
-            throw new LoginException(ioe);
+            throw new SLoginException(ioe);
         }
     }
 
-    private boolean checkTechinicalUserCredentials(final long tenantId, final String userName, final String password) throws BonitaHomeNotSetException,
-            LoginException, IOException {
+    private boolean checkTechnicalUserCredentials(final long tenantId, final String userName, final String password) throws BonitaHomeNotSetException,
+            SLoginException, IOException {
         final String technicalUserPropertiesPath = BonitaHomeServer.getInstance().getTenantConfFolder(tenantId) + File.separator + "technical-user.properties";
         final Properties properties = PropertiesManager.getPropertiesFromFile(new String(technicalUserPropertiesPath));
         final String techinicalUser = (String) properties.get("userName");
