463b63c7b69bc46c64e49462f18191e3bffa48c8
Elias Ricken de Medeiros <elias.ricken@bonitasoft.com>
2012-04-04 08:08:00 +0000

    fix log tests

--- bpm/bos-integration-tests/bos-integration-client-tests/src/main/java/org/bonitasoft/engine/log/LogTestSP.java
+++ bpm/bos-integration-tests/bos-integration-client-tests/src/main/java/org/bonitasoft/engine/log/LogTestSP.java
@@ -5,63 +5,51 @@ import static org.junit.Assert.assertNull;
 
 import java.util.List;
 
-import org.bonitasoft.engine.BPMTestUtil;
 import org.bonitasoft.engine.CommonAPITest;
-import org.bonitasoft.engine.api.IdentityAPI;
-import org.bonitasoft.engine.api.LogAPI;
-import org.bonitasoft.engine.api.TenantAPIAccessor;
 import org.bonitasoft.engine.exception.BonitaException;
+import org.bonitasoft.engine.identity.Group;
+import org.bonitasoft.engine.identity.GroupBuilder;
 import org.bonitasoft.engine.identity.User;
 import org.bonitasoft.engine.identity.UserUpdateDescriptor;
-import org.bonitasoft.engine.session.APISession;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class LogTestSP extends CommonAPITest {
 
-    private static LogAPI logAPI;
-
-    private static APISession session;
-
-    private static IdentityAPI identityAPI;
-
-    @BeforeClass
-    public static void beforeTest() throws BonitaException {
-        session = BPMTestUtil.loginDefaultTenant();
-        identityAPI = TenantAPIAccessor.getIdentityAPI(session);
-        logAPI = TenantAPIAccessor.getLogAPI(session);
-    }
-
-    @AfterClass
-    public static void afterTest() throws BonitaException {
-        BPMTestUtil.logoutTenant(session);
-    }
-
     @Test
     public void log() throws BonitaException {
-        User userOld = identityAPI.createUser("old", "oldPassword");
-        UserUpdateDescriptor updateDescriptor = new UserUpdateDescriptor();
+        login();
+        final Group group = getIdentityAPI().createGroup(new GroupBuilder().createNewInstance("group1").done());
+        getIdentityAPI().deleteGroup(group.getId());
+
+        assertEquals(0, getLogAPI().getNumberOfLogs());
+
+        final User userOld = getIdentityAPI().createUser("old", "oldPassword");
+        assertEquals(1, getLogAPI().getNumberOfLogs());
+
+        final UserUpdateDescriptor updateDescriptor = new UserUpdateDescriptor();
         updateDescriptor.updateUserName("new");
         updateDescriptor.updatePassword("newPassword");
+        getIdentityAPI().updateUser(userOld.getId(), updateDescriptor);
+        assertEquals(2, getLogAPI().getNumberOfLogs());
 
-        assertEquals(0, logAPI.getNumberOfLogs());
+        getIdentityAPI().deleteUser(userOld.getId());
+        assertEquals(3, getLogAPI().getNumberOfLogs());
 
-        identityAPI.updateUser(userOld.getId(), updateDescriptor);
-        assertEquals(1, logAPI.getNumberOfLogs());
-        identityAPI.deleteUser(userOld.getId());
-        assertEquals(2, logAPI.getNumberOfLogs());
-
-        final List<Log> logs = logAPI.getLogs(0, 2, LogCriterion.DEFAULT);
-        assertEquals("IDENTITY_USER_UPDATED", logs.get(0).getActionType());
+        final List<Log> logs = getLogAPI().getLogs(0, 3, LogCriterion.DEFAULT);
+        assertEquals("IDENTITY_USER_DELETED", logs.get(0).getActionType());
         assertEquals(SeverityLevel.INTERNAL, logs.get(0).getSeverity());
         assertNull(logs.get(0).getCallerClassName());
         assertNull(logs.get(0).getCallerMethodName());
 
-        assertEquals("IDENTITY_USER_DELETED", logs.get(1).getActionType());
+        assertEquals("IDENTITY_USER_UPDATED", logs.get(1).getActionType());
         assertEquals(SeverityLevel.INTERNAL, logs.get(1).getSeverity());
         assertNull(logs.get(1).getCallerClassName());
         assertNull(logs.get(1).getCallerMethodName());
-    }
 
+        assertEquals("IDENTITY_USER_CREATED", logs.get(2).getActionType());
+        assertEquals(SeverityLevel.INTERNAL, logs.get(2).getSeverity());
+        assertNull(logs.get(2).getCallerClassName());
+        assertNull(logs.get(2).getCallerMethodName());
+        logout();
+    }
 }
