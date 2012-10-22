fdce7a85ec1b6bb3154f7813d626551ffc15c370
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2012-10-22 16:41:45 +0000

    ENGINE-20, integrated in execution

--- services/bonita-work-sp/bonita-work-parallel/src/main/java/com/bonitasoft/engine/work/ParalellWorkSynchronization.java
+++ services/bonita-work-sp/bonita-work-parallel/src/main/java/com/bonitasoft/engine/work/ParalellWorkSynchronization.java
@@ -3,7 +3,12 @@ package com.bonitasoft.engine.work;
 import java.util.Collection;
 import java.util.concurrent.ThreadPoolExecutor;
 
+import org.bonitasoft.engine.log.technical.TechnicalLoggerService;
+import org.bonitasoft.engine.session.SessionService;
+import org.bonitasoft.engine.sessionaccessor.SessionAccessor;
+import org.bonitasoft.engine.transaction.TransactionService;
 import org.bonitasoft.engine.work.AbstractWorkSynchronization;
+import org.bonitasoft.engine.work.BonitaWork;
 
 /**
  * @author Charles Souillard
@@ -11,14 +16,15 @@ import org.bonitasoft.engine.work.AbstractWorkSynchronization;
  */
 public class ParalellWorkSynchronization extends AbstractWorkSynchronization {
 
-    public ParalellWorkSynchronization(final ThreadPoolExecutor threadPoolExecutor) {
-        super(threadPoolExecutor);
+    public ParalellWorkSynchronization(final ThreadPoolExecutor threadPoolExecutor, final TechnicalLoggerService loggerService,
+            final SessionAccessor sessionAccessor, final SessionService sessionService, final TransactionService transactionService) {
+        super(threadPoolExecutor, loggerService, sessionAccessor, sessionService, transactionService);
     }
 
     @Override
-    protected void executeRunnables(final Collection<Runnable> runnables) {
-        for (final Runnable runnable : runnables) {
-            threadPoolExecutor.submit(runnable);
+    protected void executeRunnables(final Collection<BonitaWork> works) {
+        for (final BonitaWork work : works) {
+            threadPoolExecutor.submit(work);
         }
     }
 }
--- services/bonita-work-sp/bonita-work-parallel/src/main/java/com/bonitasoft/engine/work/ParalellWorkSynchronizationFactory.java
+++ services/bonita-work-sp/bonita-work-parallel/src/main/java/com/bonitasoft/engine/work/ParalellWorkSynchronizationFactory.java
@@ -2,6 +2,10 @@ package com.bonitasoft.engine.work;
 
 import java.util.concurrent.ThreadPoolExecutor;
 
+import org.bonitasoft.engine.log.technical.TechnicalLoggerService;
+import org.bonitasoft.engine.session.SessionService;
+import org.bonitasoft.engine.sessionaccessor.SessionAccessor;
+import org.bonitasoft.engine.transaction.TransactionService;
 import org.bonitasoft.engine.work.AbstractWorkSynchronization;
 import org.bonitasoft.engine.work.WorkSynchronizationFactory;
 
@@ -11,8 +15,9 @@ import org.bonitasoft.engine.work.WorkSynchronizationFactory;
  */
 public class ParalellWorkSynchronizationFactory implements WorkSynchronizationFactory {
 
-    public AbstractWorkSynchronization getWorkSynchronization(final ThreadPoolExecutor threadPoolExecutor) {
-        return new ParalellWorkSynchronization(threadPoolExecutor);
+    public AbstractWorkSynchronization getWorkSynchronization(final ThreadPoolExecutor threadPoolExecutor, final TechnicalLoggerService loggerService,
+            final SessionAccessor sessionAccessor, final SessionService sessionService, final TransactionService transactionService) {
+        return new ParalellWorkSynchronization(threadPoolExecutor, loggerService, sessionAccessor, sessionService, transactionService);
     }
 
 }
