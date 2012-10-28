467f00f79c4a72ba083b31bea95be2ce9cd00dfc
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2012-10-28 16:40:40 +0000

    ENGINE-238,  delete archive when deleting processes

--- bpm/bonita-integration-tests/bonita-integration-local-tests/src/test/java/org/bonitasoft/engine/test/LocalIntegrationTestsSP.java
+++ bpm/bonita-integration-tests/bonita-integration-local-tests/src/test/java/org/bonitasoft/engine/test/LocalIntegrationTestsSP.java
@@ -12,7 +12,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
     BPMTestsSP.class,
-    BPMLocalTests.class,
+    BPMLocalSuiteTests.class,
     BPMRemoteTests.class
 })
 public class LocalIntegrationTestsSP {
