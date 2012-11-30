dfa568e93c18ffa5e29e275b62adf6e6773ffb2c
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2012-11-30 13:30:30 +0000

    ENGINE-467, added bonita home sp for hibernate

--- bpm/bonita-server/shade-generator.sh
+++ bpm/bonita-server/shade-generator.sh
@@ -1,2 +1,15 @@
-#!/bin/sh
-mvn dependency:list | grep org.bonitasoft | cut --delimiter="]" --fields=2- | sed "s/^[ |+\\-]*//" | sed "s/:jar:6.0-SNAPSHOT:compile//" | sed "s/^/\t\t\t\t\t\t\t\t\t\<include\>/" | sed "s/$/\<\/include\>/"|  grep -Ev 'bos-common|bos-client'| sort > shade.txt
+
+
+
+#generate the shade
+mvn dependency:list | grep org.bonitasoft | cut --delimiter="]" --fields=2- | sed "s/^[ |+\\-]*//" | sed "s/:jar:.*:compile//" | sed "s/^/\t\t\t\t\t\t\t\t\t\<include\>/" | sed "s/$/\<\/include\>/"|  grep -Ev 'bonita-common|bonita-client'| sort > shade.txt
+
+
+#compare with old shade:
+cat shade.txt | sed "s/\s*<include>//" | sed "s/<\/include>//" | sort > shade2.txt
+
+#put the old shade (with the includes) in orgshade.txt
+cat orgshade.txt | sed "s/\s*<include>//" | sed "s/<\/include>//" | sort > orgshade2.txt
+
+
+#compare file shade2.txt and orgshade2.txt
