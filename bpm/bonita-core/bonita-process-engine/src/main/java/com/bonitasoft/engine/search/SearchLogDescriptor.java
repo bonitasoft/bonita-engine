fbd2c2ff4b6a1ff5732fe4398274818d90b862a7
Matthieu Chaffotte <matthieu.chaffotte@bonitasoft.com>
2013-01-18 08:35:14 +0000

    ENGINE-649: remove unused columns

--- bpm/bonita-core/bonita-process-engine/src/main/java/com/bonitasoft/engine/search/SearchLogDescriptor.java
+++ bpm/bonita-core/bonita-process-engine/src/main/java/com/bonitasoft/engine/search/SearchLogDescriptor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 BonitaSoft S.A.
+ * Copyright (C) 2012-2013 BonitaSoft S.A.
  * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
  */
 package com.bonitasoft.engine.search;
@@ -9,15 +9,16 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.bonitasoft.engine.persistence.PersistentObject;
 import org.bonitasoft.engine.queriablelogger.model.SQueriableLog;
 import org.bonitasoft.engine.queriablelogger.model.builder.SIndexedLogBuilder;
-import org.bonitasoft.engine.persistence.PersistentObject;
 import org.bonitasoft.engine.search.FieldDescriptor;
 import org.bonitasoft.engine.search.LogSearchDescriptor;
 import org.bonitasoft.engine.search.SearchEntityDescriptor;
 
 /**
  * @author Zhang Bole
+ * @author Matthieu Chaffotte
  */
 public class SearchLogDescriptor extends SearchEntityDescriptor {
 
@@ -26,7 +27,7 @@ public class SearchLogDescriptor extends SearchEntityDescriptor {
     private final Map<Class<? extends PersistentObject>, Set<String>> logAllFields;
 
     public SearchLogDescriptor(final SIndexedLogBuilder sIndexedLogBuilder) {
-        searchEntityKeys = new HashMap<String, FieldDescriptor>(3);
+        searchEntityKeys = new HashMap<String, FieldDescriptor>(5);
         searchEntityKeys.put(LogSearchDescriptor.ACTION_SCOPE, new FieldDescriptor(SQueriableLog.class, sIndexedLogBuilder.getActionScopeKey()));
         searchEntityKeys.put(LogSearchDescriptor.ACTION_TYPE, new FieldDescriptor(SQueriableLog.class, sIndexedLogBuilder.getActionTypeKey()));
         searchEntityKeys.put(LogSearchDescriptor.CREATED_BY, new FieldDescriptor(SQueriableLog.class, sIndexedLogBuilder.getUserIdKey()));
@@ -36,11 +37,7 @@ public class SearchLogDescriptor extends SearchEntityDescriptor {
         logAllFields = new HashMap<Class<? extends PersistentObject>, Set<String>>(2);
         final Set<String> logFields = new HashSet<String>();
         logFields.add(sIndexedLogBuilder.getUserIdKey());
-        logFields.add(sIndexedLogBuilder.getClientIpKey());
         logFields.add(sIndexedLogBuilder.getClusterNodeKey());
-        logFields.add(sIndexedLogBuilder.getApplicationNameKey());
-        logFields.add(sIndexedLogBuilder.getClientApplicationNameKey());
-        logFields.add(sIndexedLogBuilder.getDomainKey());
         logFields.add(sIndexedLogBuilder.getProductVersionKey());
         logFields.add(sIndexedLogBuilder.getActionTypeKey());
         logFields.add(sIndexedLogBuilder.getActionScopeKey());
