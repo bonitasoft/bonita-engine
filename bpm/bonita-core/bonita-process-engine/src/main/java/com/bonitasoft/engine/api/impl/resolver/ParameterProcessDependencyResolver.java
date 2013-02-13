b3625de5927f00b4baca5e90df7d7315baabbb61
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2013-02-13 17:20:55 +0000

    ENGINE-851, deploy now don't throw exceptions when dependencies or not resolved

--- bpm/bonita-core/bonita-process-engine/src/main/java/com/bonitasoft/engine/api/impl/resolver/ParameterProcessDependencyResolver.java
+++ bpm/bonita-core/bonita-process-engine/src/main/java/com/bonitasoft/engine/api/impl/resolver/ParameterProcessDependencyResolver.java
@@ -4,16 +4,25 @@
  */
 package com.bonitasoft.engine.api.impl.resolver;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.bonitasoft.engine.api.ProcessAPI;
 import org.bonitasoft.engine.api.impl.resolver.ProcessDependencyResolver;
 import org.bonitasoft.engine.bpm.bar.BusinessArchive;
+import org.bonitasoft.engine.bpm.model.Problem;
+import org.bonitasoft.engine.bpm.model.Problem.Level;
+import org.bonitasoft.engine.bpm.model.ProblemImpl;
+import org.bonitasoft.engine.commons.exceptions.SBonitaException;
 import org.bonitasoft.engine.core.process.definition.model.SParameterDefinition;
 import org.bonitasoft.engine.core.process.definition.model.SProcessDefinition;
+import org.bonitasoft.engine.parameter.OrderBy;
 import org.bonitasoft.engine.parameter.ParameterService;
+import org.bonitasoft.engine.parameter.SParameter;
 import org.bonitasoft.engine.parameter.SParameterProcessNotFoundException;
 import org.bonitasoft.engine.service.TenantServiceAccessor;
 
@@ -55,4 +64,31 @@ public class ParameterProcessDependencyResolver extends ProcessDependencyResolve
         return resolved;
     }
 
+    @Override
+    public List<Problem> checkResolution(final ProcessAPI processApi, final TenantServiceAccessor tenantAccessor, final SProcessDefinition processDefinition) {
+
+        if (processDefinition.getParameters().isEmpty()) {
+            return Collections.emptyList();
+        }
+        final ParameterService parameterService = ((com.bonitasoft.engine.service.TenantServiceAccessor) tenantAccessor).getParameterService();
+
+        List<SParameter> parameters;
+        final ArrayList<Problem> problems = new ArrayList<Problem>();
+        int i = 0;
+        do {
+            try {
+                parameters = parameterService.getNullValues(processDefinition.getId(), i, 100, OrderBy.NAME_ASC);
+            } catch (final SBonitaException e) {
+                return Collections.singletonList((Problem) new ProblemImpl(Level.ERROR, null, "parameter", "unable to get parameter"));
+            }
+            i += 100;
+            for (final SParameter parameter : parameters) {
+                if (parameter.getValue() == null) {
+                    final Problem problem = new ProblemImpl(Level.ERROR, null, "parameter", "Parameter" + parameter.getName() + " is not set");
+                    problems.add(problem);
+                }
+            }
+        } while (parameters.size() == 100);
+        return problems;
+    }
 }
