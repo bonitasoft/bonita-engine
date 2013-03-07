c3e42148f65a5d80bb78b7c83c091103d57944a6
Laurent Vaills <laurent.vaills@bonitasoft.com>
2013-03-07 10:04:21 +0000

    bonita-home does not depend anymore of the JNDI implementation.

--- bpm/bonita-integration-tests/bonita-integration-local-tests/src/test/java/org/bonitasoft/engine/test/LocalIntegrationTestsSP.java
+++ bpm/bonita-integration-tests/bonita-integration-local-tests/src/test/java/org/bonitasoft/engine/test/LocalIntegrationTestsSP.java
@@ -1,13 +1,18 @@
 package org.bonitasoft.engine.test;
 
+import java.sql.SQLException;
+
 import org.bonitasoft.engine.api.PlatformLoginAPI;
 import org.bonitasoft.engine.exception.BonitaException;
 import org.bonitasoft.engine.session.PlatformSession;
+import org.h2.tools.Server;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.bonitasoft.engine.BPMSPTests;
 import com.bonitasoft.engine.api.PlatformAPI;
@@ -15,34 +20,67 @@ import com.bonitasoft.engine.api.PlatformAPIAccessor;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-    BPMSPTests.class,
-    BPMLocalSuiteTests.class,
-    LocalLogTest.class,
-    APIMethodSPTest.class,
-    ConnectorExecutionTimeOutTest.class
+        BPMSPTests.class,
+        BPMLocalSuiteTests.class,
+        LocalLogTest.class,
+        APIMethodSPTest.class,
+        ConnectorExecutionTimeOutTest.class
 })
-public class LocalIntegrationTestsSP  {
+public class LocalIntegrationTestsSP {
 
-        @BeforeClass
-        public static void beforeClass() throws BonitaException {
-            System.err.println("=================== LocalIntegrationTestsSP.beforeClass()");
-            PlatformLoginAPI platformLoginAPI = PlatformAPIAccessor.getPlatformLoginAPI();
-            PlatformSession session = platformLoginAPI.login("platformAdmin", "platform");
-            PlatformAPI platformAPI = PlatformAPIAccessor.getPlatformAPI(session);
-            platformAPI.createPlatform();
-            platformLoginAPI.logout(session);
+    static ConfigurableApplicationContext contextJNDI;
+    static Server server;
 
-            System.setProperty("delete.job.frequency", "0/30 * * * * ?");
+    @BeforeClass
+    public static void beforeClass() throws BonitaException {
+        System.err.println("=================== LocalIntegrationTestsSP.beforeClass()");
+
+        setupH2();
+        setupJNDI();
+
+        PlatformLoginAPI platformLoginAPI = PlatformAPIAccessor.getPlatformLoginAPI();
+        PlatformSession session = platformLoginAPI.login("platformAdmin", "platform");
+        PlatformAPI platformAPI = PlatformAPIAccessor.getPlatformAPI(session);
+        platformAPI.createPlatform();
+        platformLoginAPI.logout(session);
+
+        System.setProperty("delete.job.frequency", "0/30 * * * * ?");
+    }
+
+    @AfterClass
+    public static void afterClass() throws BonitaException {
+        System.err.println("=================== LocalIntegrationTestsSP.afterClass()");
+        PlatformLoginAPI platformLoginAPI = PlatformAPIAccessor.getPlatformLoginAPI();
+        PlatformSession session = platformLoginAPI.login("platformAdmin", "platform");
+        PlatformAPI platformAPI = PlatformAPIAccessor.getPlatformAPI(session);
+        platformAPI.deletePlaftorm();
+        platformLoginAPI.logout(session);
+
+        shutdownJNDI();
+        shutdownH2();
+    }
+
+    private static void setupH2() throws BonitaException {
+        // start the TCP Server
+        try {
+            server = Server.createTcpServer(new String[] { "-tcpAllowOthers" }).start();
+        } catch (SQLException e) {
+            throw new BonitaException(e);
         }
+    }
+    
+    private static void shutdownH2() {
+        server.stop();
+    }
+    
+    
+    private static void setupJNDI() {
+        System.setProperty("java.naming.factory.initial", "org.ow2.carol.jndi.spi.URLInitialContextFactory");
+        System.setProperty("java.naming.provider.url", "rmi://localhost:1099");
+        contextJNDI = new ClassPathXmlApplicationContext("jndi-setup.xml");
+    }
 
-        @AfterClass
-        public static void afterClass() throws BonitaException {
-            System.err.println("=================== LocalIntegrationTestsSP.afterClass()");
-            PlatformLoginAPI platformLoginAPI = PlatformAPIAccessor.getPlatformLoginAPI();
-            PlatformSession session = platformLoginAPI.login("platformAdmin", "platform");
-            PlatformAPI platformAPI = PlatformAPIAccessor.getPlatformAPI(session);
-            platformAPI.deletePlaftorm();
-            platformLoginAPI.logout(session);
-        }
-
+    private static void shutdownJNDI() {
+        contextJNDI.close();
+    }
 }
