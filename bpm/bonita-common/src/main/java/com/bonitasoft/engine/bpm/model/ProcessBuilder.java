d0dda7ff2b86d6c42d8a5f4a42c1b7a2fca958cf
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2013-03-08 16:07:56 +0000

    updated copyright for SP

--- bpm/bonita-common/src/main/java/com/bonitasoft/engine/bpm/model/ProcessBuilder.java
+++ bpm/bonita-common/src/main/java/com/bonitasoft/engine/bpm/model/ProcessBuilder.java
@@ -1,7 +1,11 @@
-/*
- * Copyright (C) 2012 BonitaSoft S.A.
- * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
- */
+/*******************************************************************************
+ * Copyright (C) 2009, 2013 BonitaSoft S.A.
+ * BonitaSoft is a trademark of BonitaSoft SA.
+ * This software file is BONITASOFT CONFIDENTIAL. Not For Distribution.
+ * For commercial licensing information, contact:
+ * BonitaSoft, 32 rue Gustave Eiffel – 38000 Grenoble
+ * or BonitaSoft US, 51 Federal Street, Suite 305, San Francisco, CA 94107
+ *******************************************************************************/
 package com.bonitasoft.engine.bpm.model;
 
 import org.bonitasoft.engine.bpm.model.impl.DesignProcessDefinitionImpl;
