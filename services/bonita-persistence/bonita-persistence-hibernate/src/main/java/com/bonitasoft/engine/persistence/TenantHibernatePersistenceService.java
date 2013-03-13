8d3f7aade719a721c5a1383ebce0b89f979f5f1c
Charles Souillard <charles.souillard@bonitasoft.com>
2013-03-13 08:23:05 +0000

    try to get back to performance

---
+++ services/bonita-persistence/bonita-persistence-hibernate/src/main/java/com/bonitasoft/engine/persistence/TenantHibernatePersistenceService.java
@@ -0,0 +1,48 @@
+package com.bonitasoft.engine.persistence;
+
+import org.bonitasoft.engine.log.technical.TechnicalLoggerService;
+import org.bonitasoft.engine.persistence.DBConfigurationsProvider;
+import org.bonitasoft.engine.persistence.HibernateConfigurationProvider;
+import org.bonitasoft.engine.persistence.PersistentObject;
+import org.bonitasoft.engine.persistence.PersistentObjectWithFlag;
+import org.bonitasoft.engine.persistence.SBonitaReadException;
+import org.bonitasoft.engine.persistence.SelectByIdDescriptor;
+import org.bonitasoft.engine.sequence.SequenceManager;
+import org.bonitasoft.engine.services.SPersistenceException;
+import org.bonitasoft.engine.services.UpdateDescriptor;
+import org.bonitasoft.engine.sessionaccessor.ReadSessionAccessor;
+import org.bonitasoft.engine.transaction.TransactionService;
+
+public class TenantHibernatePersistenceService extends org.bonitasoft.engine.persistence.TenantHibernatePersistenceService {
+
+    private static final String DELETED_KEY = "deleted";
+    
+    public TenantHibernatePersistenceService(final String name, final TransactionService txService, final ReadSessionAccessor sessionAccessor,
+            final HibernateConfigurationProvider hbmConfigurationProvider, final DBConfigurationsProvider tenantConfigurationsProvider,
+            final String statementDelimiter, final TechnicalLoggerService logger, final SequenceManager sequenceManager) throws SPersistenceException {
+        super(name, txService, sessionAccessor, hbmConfigurationProvider, tenantConfigurationsProvider, statementDelimiter, logger, sequenceManager);
+    }
+    
+    @Override
+    public void delete(PersistentObject entity) throws SPersistenceException {
+        if (entity instanceof PersistentObjectWithFlag) {
+            final UpdateDescriptor buildSetField = UpdateDescriptor.buildSetField(entity, DELETED_KEY, true);
+            super.update(buildSetField);
+            DeleteBatchJobRegister.getInstance().registerJobIfNotRegistered();
+        } else {
+            super.delete(entity);
+        }
+    }
+    
+    @Override
+    public <T extends PersistentObject> T selectById(SelectByIdDescriptor<T> selectDescriptor) throws SBonitaReadException {
+        final T entity = super.selectById(selectDescriptor);
+        if (entity instanceof PersistentObjectWithFlag) {
+            final PersistentObjectWithFlag entityWithFlag = (PersistentObjectWithFlag) entity;
+            if (entityWithFlag.isDeleted()) {
+                return null;
+            }
+        }
+        return entity;
+    }
+}
