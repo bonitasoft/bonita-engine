57ed876921b72fa2a0c6a9029cb2616fb5441744
Matthieu Chaffotte <matthieu.chaffotte@bonitasoft.com>
2013-04-23 11:56:37 +0000

    ENGINE-1106: manual test

---
+++ bpm/bonita-integration-tests/bonita-integration-local-tests/src/test/java/com/bonitasoft/engine/LicenseTest.java
@@ -0,0 +1,41 @@
+package com.bonitasoft.engine;
+
+import org.bonitasoft.engine.exception.BonitaException;
+import org.bonitasoft.engine.exception.LoginException;
+import org.bonitasoft.engine.session.PlatformSession;
+import org.junit.Test;
+
+import com.bonitasoft.engine.api.PlatformAPI;
+import com.bonitasoft.engine.api.PlatformAPIAccessor;
+
+// @RunWith(PowerMockRunner.class)
+// @PrepareForTest(Manager.class)
+public class LicenseTest extends CommonAPISPTest {
+
+    @Test
+    public void invalidLicense() throws BonitaException {
+        final PlatformSession session = loginPlatform();
+        final PlatformAPI platformAPI = PlatformAPIAccessor.getPlatformAPI(session);
+        try {
+            // License invalid == not exists
+            platformAPI.createTenant("lumi corp", "The snow maker", null, null, "lumi", "lumi");
+        } catch (final Exception e) {
+            logoutPlatform(session);
+        }
+        try {
+            login();
+        } catch (final LoginException le) {
+            startNode();
+            login();
+            logout();
+        }
+    }
+
+    private void startNode() throws BonitaException {
+        final PlatformSession session2 = loginPlatform();
+        final PlatformAPI platformAPI2 = PlatformAPIAccessor.getPlatformAPI(session2);
+        platformAPI2.startNode();
+        logoutPlatform(session2);
+    }
+
+}
