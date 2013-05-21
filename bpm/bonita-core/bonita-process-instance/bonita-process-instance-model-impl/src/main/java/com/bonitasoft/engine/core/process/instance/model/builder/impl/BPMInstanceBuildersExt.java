d64c718f2429be5e432205b2858d1c4aa9215abb
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2013-05-21 15:46:31 +0000

    Fixed invalid javadoc special character + renamed class BPMInstanceBuildersExt

--- bpm/bonita-core/bonita-process-instance/bonita-process-instance-model-impl/src/main/java/com/bonitasoft/engine/core/process/instance/model/builder/impl/BPMInstanceBuildersExt.java
+++ bpm/bonita-core/bonita-process-instance/bonita-process-instance-model-impl/src/main/java/com/bonitasoft/engine/core/process/instance/model/builder/impl/BPMInstanceBuildersExt.java
@@ -3,7 +3,7 @@
  * BonitaSoft is a trademark of BonitaSoft SA.
  * This software file is BONITASOFT CONFIDENTIAL. Not For Distribution.
  * For commercial licensing information, contact:
- * BonitaSoft, 32 rue Gustave Eiffel – 38000 Grenoble
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
  * or BonitaSoft US, 51 Federal Street, Suite 305, San Francisco, CA 94107
  *******************************************************************************/
 package com.bonitasoft.engine.core.process.instance.model.builder.impl;
@@ -21,7 +21,7 @@ import com.bonitasoft.engine.core.process.instance.model.builder.SProcessInstanc
 /**
  * @author Celine Souchet
  */
-public class BPMinstanceBuildersExt extends BPMinstanceBuildersImpl implements BPMInstanceBuilders {
+public class BPMInstanceBuildersExt extends BPMinstanceBuildersImpl implements BPMInstanceBuilders {
 
     @Override
     public SBreakpointBuilder getSBreakpointBuilder() {
