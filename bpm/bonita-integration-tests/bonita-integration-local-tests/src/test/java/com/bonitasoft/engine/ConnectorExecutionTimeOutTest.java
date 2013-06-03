1a14e3983e6c4ffd4d2a3633bbcd85fa6f0dec4b
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2013-06-03 10:52:08 +0000

    ENGINE-1423: Switch ConnectorExecutor BOS implementation in SP for 6.0.0 version

--- bpm/bonita-integration-tests/bonita-integration-local-tests/src/test/java/com/bonitasoft/engine/ConnectorExecutionTimeOutTest.java
+++ bpm/bonita-integration-tests/bonita-integration-local-tests/src/test/java/com/bonitasoft/engine/ConnectorExecutionTimeOutTest.java
@@ -8,6 +8,8 @@
  *******************************************************************************/
 package com.bonitasoft.engine;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -31,6 +33,7 @@ import org.bonitasoft.engine.operation.OperatorType;
 import org.bonitasoft.engine.sessionaccessor.SessionAccessor;
 import org.bonitasoft.engine.test.annotation.Cover;
 import org.bonitasoft.engine.test.annotation.Cover.BPMNConcept;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.bonitasoft.engine.bpm.process.impl.ProcessDefinitionBuilderExt;
@@ -39,8 +42,6 @@ import com.bonitasoft.engine.connector.impl.ConnectorExecutorTimedOut;
 import com.bonitasoft.engine.service.TenantServiceAccessor;
 import com.bonitasoft.engine.service.impl.ServiceAccessorFactory;
 import com.bonitasoft.engine.service.impl.TenantServiceSingleton;
-
-import static org.junit.Assert.assertEquals;
 
 /**
  * @author Baptiste Mesta
@@ -59,6 +60,7 @@ public class ConnectorExecutionTimeOutTest extends ConnectorExecutionTest {
 
     @Cover(classes = Connector.class, concept = BPMNConcept.PROCESS, keywords = { "Connector", "Execution too long" }, jira = "ENGINE-472", story = "Test if connector fails when connector execution is too long.")
     @Test
+    @Ignore("Using BOS implementation for Now (6.0.0)")
     public void testExecuteConnectorWithExecutionTooLong() throws Exception {
         final String delivery = "Delivery men";
         final ProcessDefinitionBuilderExt designProcessDefinition = new ProcessDefinitionBuilderExt().createNewInstance("testConnectorWithExecutionTooLong",
@@ -95,8 +97,8 @@ public class ConnectorExecutionTimeOutTest extends ConnectorExecutionTest {
     @Test
     public void testExecuteConnectorWithCustomOutputTypeWithCommands() throws Exception {
         final String delivery = "Delivery men";
-        final ProcessDefinitionBuilderExt designProcessDefinition = new ProcessDefinitionBuilderExt().createNewInstance("testConnectorWithExecutionTooLong",
-                "1.0");
+        final ProcessDefinitionBuilderExt designProcessDefinition = new ProcessDefinitionBuilderExt().createNewInstance(
+                "testExecuteConnectorWithCustomOutputTypeWithCommands", "1.0");
         designProcessDefinition.addActor(delivery).addDescription("Delivery all day and night long");
         designProcessDefinition.addShortTextData("value", null);
         designProcessDefinition.addUserTask("step1", delivery);
