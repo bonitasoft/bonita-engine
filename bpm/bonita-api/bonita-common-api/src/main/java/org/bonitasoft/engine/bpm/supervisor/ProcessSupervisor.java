2d4aa05b9b4c09580828c3ce1440d9167095bb29
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2013-06-19 15:01:07 +0000

    updated javadoc on ProcessSupervisor

--- bpm/bonita-api/bonita-common-api/src/main/java/org/bonitasoft/engine/bpm/supervisor/ProcessSupervisor.java
+++ bpm/bonita-api/bonita-common-api/src/main/java/org/bonitasoft/engine/bpm/supervisor/ProcessSupervisor.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2012 BonitaSoft S.A.
+ * Copyright (C) 2012-2013 BonitaSoft S.A.
  * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
  * This library is free software; you can redistribute it and/or modify it under the terms
  * of the GNU Lesser General Public License as published by the Free Software Foundation
@@ -16,11 +16,14 @@ package org.bonitasoft.engine.bpm.supervisor;
 import org.bonitasoft.engine.bpm.BonitaObject;
 
 /**
- * A process supervisor is able to see task of the process and do administration of the process.
+ * A process supervisor is the responsible for what happens to a specific process. A process supervisor can see the tasks of the process and do administration
+ * of the process.
+ * As for actors, process supervisors map people to processes through user / group / role associations.
  * 
  * @author Yanyan Liu
  * @author Elias Ricken de Medeiros
  * @author Celine Souchet
+ * @author Emmanuel Duchastenier
  */
 public interface ProcessSupervisor extends BonitaObject {
 
