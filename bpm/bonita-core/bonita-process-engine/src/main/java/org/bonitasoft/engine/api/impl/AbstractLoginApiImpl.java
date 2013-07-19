851e8afb984e7c1e2a6daab2adb026f302de4c2b
Laurent Vaills <laurent.vaills@bonitasoft.com>
2013-07-19 18:45:54 +0200

    Added comments and stopped using the TransactionExecutor.

--- bpm/bonita-core/bonita-process-engine/src/main/java/org/bonitasoft/engine/api/impl/AbstractLoginApiImpl.java
+++ bpm/bonita-core/bonita-process-engine/src/main/java/org/bonitasoft/engine/api/impl/AbstractLoginApiImpl.java
@@ -1,12 +1,12 @@
 package org.bonitasoft.engine.api.impl;
 
-import org.bonitasoft.engine.commons.transaction.TransactionExecutor;
 import org.bonitasoft.engine.exception.BonitaRuntimeException;
 import org.bonitasoft.engine.log.technical.TechnicalLogSeverity;
 import org.bonitasoft.engine.platform.PlatformService;
 import org.bonitasoft.engine.platform.SPlatformNotFoundException;
 import org.bonitasoft.engine.service.PlatformServiceAccessor;
 import org.bonitasoft.engine.transaction.STransactionException;
+import org.bonitasoft.engine.transaction.TransactionService;
 
 public class AbstractLoginApiImpl {
 
@@ -17,18 +17,20 @@ public class AbstractLoginApiImpl {
     protected void putPlatformInCacheIfNecessary(PlatformServiceAccessor platformAccessor, PlatformService platformService) {
         // Get the platform from cache:
         try {
+            // First call that will look into the cache and tries to fetch it from the DB 
+            // if not in cache : but there is no transaction open yet so that will fail
             platformService.getPlatform();
         } catch (SPlatformNotFoundException e1) {
             try {
-                TransactionExecutor transactionExecutor = platformAccessor.getTransactionExecutor();
-                boolean openTransaction = false;
+                TransactionService transactionService = platformAccessor.getTransactionService();
+                transactionService.begin();
                 try {
-                    openTransaction = transactionExecutor.openTransaction();
+                    // Second call that will look into the cache and fetches it from the DB but this time the 
                     platformService.getPlatform();
                 } catch (SPlatformNotFoundException e) {
                     platformAccessor.getTechnicalLoggerService().log(getClass(), TechnicalLogSeverity.INFO, "Platform not yet created");
                 } finally {
-                    transactionExecutor.completeTransaction(openTransaction);
+                    transactionService.complete();
                 }
             } catch (STransactionException e) {
                 throw new BonitaRuntimeException("Transaction error on getting platform at login", e);
