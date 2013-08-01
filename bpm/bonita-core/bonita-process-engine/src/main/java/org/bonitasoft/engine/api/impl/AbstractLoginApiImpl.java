b2fc41037559d63348072cf12bd5bd044736727d
Matthieu Chaffotte <matthieu.chaffotte@bonitasoft.com>
2013-08-01 17:18:24 +0200

    Add license

--- bpm/bonita-core/bonita-process-engine/src/main/java/org/bonitasoft/engine/api/impl/AbstractLoginApiImpl.java
+++ bpm/bonita-core/bonita-process-engine/src/main/java/org/bonitasoft/engine/api/impl/AbstractLoginApiImpl.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright (C) 2013 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
 package org.bonitasoft.engine.api.impl;
 
 import org.bonitasoft.engine.exception.BonitaRuntimeException;
@@ -8,31 +21,34 @@ import org.bonitasoft.engine.service.PlatformServiceAccessor;
 import org.bonitasoft.engine.transaction.STransactionException;
 import org.bonitasoft.engine.transaction.TransactionService;
 
+/**
+ * @author Baptiste Mesta
+ */
 public class AbstractLoginApiImpl {
 
     public AbstractLoginApiImpl() {
         super();
     }
 
-    protected void putPlatformInCacheIfNecessary(PlatformServiceAccessor platformAccessor, PlatformService platformService) {
+    protected void putPlatformInCacheIfNecessary(final PlatformServiceAccessor platformAccessor, final PlatformService platformService) {
         // Get the platform from cache:
         try {
-            // First call that will look into the cache and tries to fetch it from the DB 
+            // First call that will look into the cache and tries to fetch it from the DB
             // if not in cache : but there is no transaction open yet so that will fail
             platformService.getPlatform();
-        } catch (SPlatformNotFoundException e1) {
+        } catch (final SPlatformNotFoundException e1) {
             try {
-                TransactionService transactionService = platformAccessor.getTransactionService();
+                final TransactionService transactionService = platformAccessor.getTransactionService();
                 transactionService.begin();
                 try {
-                    // Second call that will look into the cache and fetches it from the DB but this time the 
+                    // Second call that will look into the cache and fetches it from the DB but this time the
                     platformService.getPlatform();
-                } catch (SPlatformNotFoundException e) {
+                } catch (final SPlatformNotFoundException e) {
                     platformAccessor.getTechnicalLoggerService().log(getClass(), TechnicalLogSeverity.INFO, "Platform not yet created");
                 } finally {
                     transactionService.complete();
                 }
-            } catch (STransactionException e) {
+            } catch (final STransactionException e) {
                 throw new BonitaRuntimeException("Transaction error on getting platform at login", e);
             }
         }
