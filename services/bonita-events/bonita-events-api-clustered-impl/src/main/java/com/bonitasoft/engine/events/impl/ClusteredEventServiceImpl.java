4bc01f133f88d4592dafcddaad9fe8df1c083067
Laurent Vaills <laurent.vaills@bonitasoft.com>
2013-08-12 11:08:45 +0200

    The EventService is also used at the platform level.

--- services/bonita-events/bonita-events-api-clustered-impl/src/main/java/com/bonitasoft/engine/events/impl/ClusteredEventServiceImpl.java
+++ services/bonita-events/bonita-events-api-clustered-impl/src/main/java/com/bonitasoft/engine/events/impl/ClusteredEventServiceImpl.java
@@ -25,13 +25,24 @@ import com.hazelcast.core.HazelcastInstance;
 
 public class ClusteredEventServiceImpl extends ConfigurableEventServiceImpl {
 
-    private final String eventServiceHandlerMapName;
+    public ClusteredEventServiceImpl(final SEventBuilders eventBuilders, final Map<String, SHandler<SEvent>> handlers, final TechnicalLoggerService logger, final HazelcastInstance hazelcastInstance) throws HandlerRegistrationException {
+        this("PLATFORM", eventBuilders, handlers, logger, hazelcastInstance);
+    }
 
     public ClusteredEventServiceImpl(final SEventBuilders eventBuilders, final Map<String, SHandler<SEvent>> handlers, final TechnicalLoggerService logger, final HazelcastInstance hazelcastInstance, final long tenantId) throws HandlerRegistrationException {
+        this("TENANT@" + tenantId, eventBuilders, handlers, logger, hazelcastInstance);
+    }
+
+    private ClusteredEventServiceImpl(final String eventServiceHandlerMapNameSuffix, final SEventBuilders eventBuilders, final Map<String, SHandler<SEvent>> handlers, final TechnicalLoggerService logger, final HazelcastInstance hazelcastInstance) throws HandlerRegistrationException {
         super(eventBuilders, handlers, logger);
-        eventServiceHandlerMapName = tenantId + "-EVENT_SERVICE_HANDLERS";
-        // Create a Map that is shared accross the cluster.
-        registeredHandlers = hazelcastInstance.getMap(eventServiceHandlerMapName);
+
+        // Create a Map that is shared across the cluster.
+        registeredHandlers = hazelcastInstance.getMap("EVENT_SERVICE_HANDLERS-" + eventServiceHandlerMapNameSuffix);
+
+        // register the handlers with their associated event type
+        for (final Map.Entry<String, SHandler<SEvent>> entry : handlers.entrySet()) {
+            addHandler(entry.getKey(), entry.getValue());
+        }
     }
 
 }
