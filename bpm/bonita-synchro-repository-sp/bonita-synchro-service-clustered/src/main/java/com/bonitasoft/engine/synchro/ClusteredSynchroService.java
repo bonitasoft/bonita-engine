b078a2c8af4761170fe4e57b58526ed5e3485e26
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2013-08-30 19:15:42 +0200

    ENGINE-1707: Implement Clustered SynchroRepository + ENGINE-1689 test implementation of Clustered EventService

--- bpm/bonita-synchro-repository-sp/bonita-synchro-service-clustered/src/main/java/com/bonitasoft/engine/synchro/ClusteredSynchroService.java
+++ bpm/bonita-synchro-repository-sp/bonita-synchro-service-clustered/src/main/java/com/bonitasoft/engine/synchro/ClusteredSynchroService.java
@@ -15,27 +15,30 @@ package com.bonitasoft.engine.synchro;
 
 import java.io.Serializable;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.locks.Lock;
 
 import org.bonitasoft.engine.cache.CommonCacheService;
+import org.bonitasoft.engine.log.technical.TechnicalLogSeverity;
 import org.bonitasoft.engine.log.technical.TechnicalLoggerService;
 import org.bonitasoft.engine.synchro.AbstractSynchroService;
-import org.bonitasoft.engine.synchro.SynchroObject;
 import org.bonitasoft.engine.synchro.SynchroService;
 
 import com.bonitasoft.manager.Features;
 import com.bonitasoft.manager.Manager;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ISemaphore;
 
 /**
  * @author Emmanuel Duchastenier
+ * @author Baptiste Mesta
  */
 public class ClusteredSynchroService extends AbstractSynchroService implements SynchroService {
 
-    private final static String CLUSTERED_SYNCHRO_SERVICE_FIRED = "CLUSTERED_SYNCHRO_SERVICE_FIRED";
-
     private final static String CLUSTERED_SYNCHRO_SERVICE_WAITERS = "CLUSTERED_SYNCHRO_SERVICE_WAITERS";
 
-    private final static String CLUSTERED_SYNCHRO_SERVICE_MUTEX = "CLUSTERED_SYNCHRO_SERVICE_MUTEX";
+    private final static String CLUSTERED_SYNCHRO_SERVICE_LOCK = "CLUSTERED_SYNCHRO_SERVICE_LOCK";
 
     private final HazelcastInstance hazelcastInstance;
 
@@ -45,19 +48,72 @@ public class ClusteredSynchroService extends AbstractSynchroService implements S
             throw new IllegalStateException("The clustering is not an active feature.");
         }
         this.hazelcastInstance = hazelcastInstance;
-        this.hazelcastInstance.getMap(CLUSTERED_SYNCHRO_SERVICE_MUTEX).put(CLUSTERED_SYNCHRO_SERVICE_MUTEX, new Object());
-
     }
 
+    /**
+     * String value is an identifier of the sempaphore for the current event (event.hashCode())
+     */
     @Override
-    protected Map<Map<String, Serializable>, SynchroObject> getWaitersMap() {
+    protected Map<Map<String, Serializable>, String> getWaitersMap() {
         return hazelcastInstance.getMap(CLUSTERED_SYNCHRO_SERVICE_WAITERS);
     }
 
     @Override
-    protected Object getMutex() {
-        // FIXME: Can we use something else than a Map to have a simple shared object?:
-        return hazelcastInstance.getMap(CLUSTERED_SYNCHRO_SERVICE_MUTEX).get(CLUSTERED_SYNCHRO_SERVICE_MUTEX);
+    protected Map<String, Serializable> getEventKeyAndIdMap() {
+        return hazelcastInstance.getMap("CLUSTERED_SYNCHRO_SERVICE_EVENT");
+    }
+
+    @Override
+    protected Lock getServiceLock() {
+        return hazelcastInstance.getLock(CLUSTERED_SYNCHRO_SERVICE_LOCK);
+    }
+
+    /**
+     * @param semaphoreKey
+     */
+    @Override
+    protected void releaseWaiter(final String semaphoreKey) {
+        ISemaphore semaphore = hazelcastInstance.getSemaphore(semaphoreKey);
+        semaphore.release();
+    }
+
+    @Override
+    public Serializable waitForEvent(final Map<String, Serializable> event, final long timeout) throws InterruptedException, TimeoutException {
+        Serializable id = null;
+        String semaphoreKey = null;
+        ISemaphore semaphore = null;
+        getServiceLock().lock();
+        try {
+            id = getFiredAndRemoveIt(event);
+            if (id != null) {
+                logger.log(this.getClass(), TechnicalLogSeverity.DEBUG, "waiting for event " + event + " already fired, returning directly");
+                // Event has already been fired
+            } else {
+                logger.log(this.getClass(), TechnicalLogSeverity.DEBUG, "waiting for event " + event + " NOT fired yet, waiting for " + timeout + " ms max.");
+                semaphoreKey = String.valueOf(event.hashCode());
+                semaphore = hazelcastInstance.getSemaphore(semaphoreKey);
+                semaphore.init(1);
+                semaphore.acquire(1);
+                getWaitersMap().put(event, semaphoreKey);
+            }
+        } finally {
+            getServiceLock().unlock();
+        }
+        if (semaphore != null) {
+            try {
+                if (!semaphore.tryAcquire(timeout, TimeUnit.MILLISECONDS)) {
+                    throwTimeout(event, timeout);
+                }
+            } catch (final InterruptedException e) {
+                throwTimeout(event, timeout);
+            } finally {
+                getWaitersMap().remove(event);
+            }
+            return getEventKeyAndIdMap().get(semaphoreKey);
+        } else {
+            logger.log(this.getClass(), TechnicalLogSeverity.DEBUG, "Event arrived. Returning ID " + id);
+            return id;
+        }
     }
 
 }
