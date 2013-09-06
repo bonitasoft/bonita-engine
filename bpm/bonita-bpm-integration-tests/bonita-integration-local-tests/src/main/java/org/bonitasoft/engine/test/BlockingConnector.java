386e77aedc9bb143ba9ed5c998092af0ef93273c
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2013-09-06 10:51:20 +0200

    ENGINE-1814, added the test in ignore

---
+++ bpm/bonita-bpm-integration-tests/bonita-integration-local-tests/src/main/java/org/bonitasoft/engine/test/BlockingConnector.java
@@ -0,0 +1,26 @@
+package org.bonitasoft.engine.test;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+import org.bonitasoft.engine.connector.AbstractConnector;
+import org.bonitasoft.engine.connector.ConnectorException;
+import org.bonitasoft.engine.connector.ConnectorValidationException;
+
+public class BlockingConnector extends AbstractConnector {
+
+    public static Semaphore semaphore = new Semaphore(1);
+
+    @Override
+    public void validateInputParameters() throws ConnectorValidationException {
+    }
+
+    @Override
+    protected void executeBusinessLogic() throws ConnectorException {
+        try {
+            semaphore.tryAcquire(15, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
