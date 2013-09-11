db0d7e23b3cdc6b6d73610437051e0f7895d1228
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2013-09-11 15:12:41 +0200

    ENGINE-1844, added more logs + avoid a single thread to lock 2 times

--- services/bonita-lock/bonita-lock-api-clustered-impl/src/main/java/com/bonitasoft/engine/lock/impl/ClusteredLockService.java
+++ services/bonita-lock/bonita-lock-api-clustered-impl/src/main/java/com/bonitasoft/engine/lock/impl/ClusteredLockService.java
@@ -28,44 +28,50 @@ import com.hazelcast.core.MultiMap;
  */
 public class ClusteredLockService extends AbstractLockService {
 
-	private final HazelcastInstance hazelcastInstance;
-	private final MultiMap<String, RejectedLockHandler> rejectedLockHandlers;
+    private final HazelcastInstance hazelcastInstance;
 
-	public ClusteredLockService(final HazelcastInstance hazelcastInstance, final TechnicalLoggerService logger, ReadSessionAccessor sessionAccessor,
-			int lockTimeout) {
-		super(logger, sessionAccessor, lockTimeout);
-		this.hazelcastInstance = hazelcastInstance;
-		if (!Manager.getInstance().isFeatureActive(Features.ENGINE_CLUSTERING)) {
-			throw new IllegalStateException("The clustering is not an active feature.");
-		}
-		this.rejectedLockHandlers = this.hazelcastInstance.getMultiMap("rejectedLockHandlers");
-	}
+    private final MultiMap<String, RejectedLockHandler> rejectedLockHandlers;
 
-	@Override
-	protected Lock getLock(String key) {
-		return hazelcastInstance.getLock(key);
-	}
+    public ClusteredLockService(final HazelcastInstance hazelcastInstance, final TechnicalLoggerService logger, final ReadSessionAccessor sessionAccessor,
+            final int lockTimeout) {
+        super(logger, sessionAccessor, lockTimeout);
+        this.hazelcastInstance = hazelcastInstance;
+        if (!Manager.getInstance().isFeatureActive(Features.ENGINE_CLUSTERING)) {
+            throw new IllegalStateException("The clustering is not an active feature.");
+        }
+        this.rejectedLockHandlers = this.hazelcastInstance.getMultiMap("rejectedLockHandlers");
+    }
 
-	@Override
-	protected void removeLockFromMapIfnotUsed(String key) {
-	}
+    @Override
+    protected Lock getLock(final String key) {
+        return hazelcastInstance.getLock(key);
+    }
 
-	@Override
-	protected RejectedLockHandler getOneRejectedHandler(final String key) {
-		if (rejectedLockHandlers.containsKey(key)) {
-			final Collection<RejectedLockHandler> handlers = this.rejectedLockHandlers.get(key);
-			final RejectedLockHandler handler = handlers.iterator().next();
-			rejectedLockHandlers.remove(key, handler);
-			if (handlers.size() == 0) {
-				rejectedLockHandlers.remove(key);
-			}
-			return handler;
-		}
-		return null;
-	}
+    @Override
+    protected void removeLockFromMapIfnotUsed(final String key) {
+    }
 
-	@Override
-	protected void storeRejectedLock(final String key, final RejectedLockHandler handler) {
-		rejectedLockHandlers.put(key, handler);
-	}
+    @Override
+    protected RejectedLockHandler getOneRejectedHandler(final String key) {
+        if (rejectedLockHandlers.containsKey(key)) {
+            final Collection<RejectedLockHandler> handlers = this.rejectedLockHandlers.get(key);
+            final RejectedLockHandler handler = handlers.iterator().next();
+            rejectedLockHandlers.remove(key, handler);
+            if (handlers.size() == 0) {
+                rejectedLockHandlers.remove(key);
+            }
+            return handler;
+        }
+        return null;
+    }
+
+    @Override
+    protected void storeRejectedLock(final String key, final RejectedLockHandler handler) {
+        rejectedLockHandlers.put(key, handler);
+    }
+
+    @Override
+    protected boolean isOwnedByCurrentThread(final Lock lock, final String key) {
+        return false;
+    }
 }
