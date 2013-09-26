12355a3fecd114b2b6d02dbc0143e0fe360bd3f2
Charles Souillard <charles.souillard@bonitasoft.com>
2013-09-26 11:55:32 +0200

    use direct activemq connection instead of using JNDI to access the JMS Server

--- bpm/bonita-synchro-repository/bonita-synchro-jms/src/main/java/org/bonitasoft/engine/synchro/jms/JMSProducer.java
+++ bpm/bonita-synchro-repository/bonita-synchro-jms/src/main/java/org/bonitasoft/engine/synchro/jms/JMSProducer.java
@@ -10,30 +10,36 @@ import javax.jms.Session;
 import javax.jms.Topic;
 import javax.jms.TopicConnection;
 import javax.jms.TopicConnectionFactory;
-import javax.naming.InitialContext;
 import javax.naming.NamingException;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
 
 public class JMSProducer {
 
 	public static final String TOPIC_CONNECTION_FACTORY = "bonita/jms/TopicConnectionFactory";
 	public static final String TOPIC_NAME = "synchroServiceTopic";
 
-	private final MessageProducer producer;
-	private final Session session;
+	private TopicConnectionFactory topicConnectionFactory;
+	private TopicConnection topicConnection;
+	private Session session;
+	private Topic topic;
+	private MessageProducer producer;
+	
 
 	private final long timeout;
 
 	private static JMSProducer jmsProducer;
 	
 	private JMSProducer(final long timeout) throws NamingException, JMSException {
-		final InitialContext context = new InitialContext();
-		final TopicConnectionFactory topicConnectionFactory = (TopicConnectionFactory) context.lookup(TOPIC_CONNECTION_FACTORY);
-		final Topic topic = (Topic) context.lookup(TOPIC_NAME);
+		final String brokerURL = System.getProperty("broker.url");
 		
-		final TopicConnection topicConnection = topicConnectionFactory.createTopicConnection();
+		// Create a ConnectionFactory
+        this.topicConnectionFactory = new ActiveMQConnectionFactory(brokerURL);
+        this.topicConnection = topicConnectionFactory.createTopicConnection();
+        this.session = topicConnection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		this.topic = session.createTopic(TOPIC_NAME);
+		
 		topicConnection.start();
-
-		this.session = topicConnection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
 		this.producer = session.createProducer(topic);
 
@@ -42,9 +48,10 @@ public class JMSProducer {
 			public void run() {
 				try {
 					topicConnection.stop();
-					topicConnection.close();
 					producer.close();
 					session.close();
+					topicConnection.close();
+					
 				} catch (JMSException e) {
 					e.printStackTrace();
 				}
