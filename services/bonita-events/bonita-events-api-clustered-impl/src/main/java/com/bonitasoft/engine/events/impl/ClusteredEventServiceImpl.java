d82a50ec580db85ee81d60cbee52c9d08df1ff19
Laurent Vaills <laurent.vaills@bonitasoft.com>
2013-10-09 14:02:24 +0200

    EventService in a clustered environment
    - Do not add the default handlers if we are not the first one in the cluster
    - Ensure the handler is not already registered for an event before registering it

--- services/bonita-events/bonita-events-api-clustered-impl/src/main/java/com/bonitasoft/engine/events/impl/ClusteredEventServiceImpl.java
+++ services/bonita-events/bonita-events-api-clustered-impl/src/main/java/com/bonitasoft/engine/events/impl/ClusteredEventServiceImpl.java
@@ -17,6 +17,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -28,10 +29,13 @@ import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.bonitasoft.engine.commons.exceptions.SBonitaRuntimeException;
+import org.bonitasoft.engine.events.impl.AbstractEventServiceImpl;
 import org.bonitasoft.engine.events.model.HandlerRegistrationException;
+import org.bonitasoft.engine.events.model.HandlerUnregistrationException;
 import org.bonitasoft.engine.events.model.SEvent;
 import org.bonitasoft.engine.events.model.SHandler;
 import org.bonitasoft.engine.events.model.builders.SEventBuilders;
+import org.bonitasoft.engine.log.technical.TechnicalLogSeverity;
 import org.bonitasoft.engine.log.technical.TechnicalLoggerService;
 
 import com.bonitasoft.manager.Features;
@@ -51,7 +55,7 @@ import com.hazelcast.core.Member;
  * @author Laurent Vaills
  * 
  */
-public class ClusteredEventServiceImpl extends ConfigurableEventServiceImpl {
+public class ClusteredEventServiceImpl extends AbstractEventServiceImpl {
 
     private final IMap<String, List<SHandler<SEvent>>> eventHandlers;
 
@@ -76,7 +80,7 @@ public class ClusteredEventServiceImpl extends ConfigurableEventServiceImpl {
     ClusteredEventServiceImpl(final String eventServiceHandlerMapNameSuffix, final SEventBuilders eventBuilders,
             final Map<String, SHandler<SEvent>> handlers, final TechnicalLoggerService logger, final HazelcastInstance hazelcastInstance, final Manager manager)
             throws HandlerRegistrationException {
-        super(eventBuilders, handlers, logger);
+        super(eventBuilders, logger);
         if (!manager.isFeatureActive(Features.ENGINE_CLUSTERING)) {
             throw new IllegalStateException("The clustering is not an active feature.");
         }
@@ -90,69 +94,105 @@ public class ClusteredEventServiceImpl extends ConfigurableEventServiceImpl {
         config.addMapConfig(new MapConfig(mapName));
         // ---
         this.hazelcastInstance = hazelcastInstance;
+
+        // We will need this service in the tasks defined below.
         hazelcastInstance.getUserContext().put("ClusteredEventService", this);
+
         eventHandlers = hazelcastInstance.getMap(mapName);
-
-        // TODO
-        // See to fill the Set localEventTypes from another cluster's member
-        // Execute a Callable on a member that returns a copy of its own Set ?
-        // What if another handler is added while was executed but not yet returned ?
-
-        // We have created a Map that is shared across the cluster, so we do not need this one.
-        registeredHandlers = null;
+        String prefix = "---- ";
+        System.out.println(prefix + "Cluster : " + hazelcastInstance.getCluster().getMembers());
+        System.out.println(prefix + "EventHandlers : " + eventHandlers.size() + " " + eventHandlers);
+        for (Map.Entry<String, List<SHandler<SEvent>>> entry : eventHandlers.entrySet()) {
+            System.out.println(prefix + "Entry : " + entry.getKey() + " -> " + entry.getValue());
+        }
+        System.out.println(prefix + "EventHandlers end");
+        if (eventHandlers.isEmpty()) {
+            // it seems we are the first one in the cluster so let's add the default handlers
+            // There is another way, maybe cleaner, to know if we are the first one in the cluster :
+            // hazelcastInstance.getCluster().getMembers().size() == 1
+            // but that's not as easy in the tests to setup 2 hazelcast instances.
+            // Furthermore, we have other constraints that prevents from starting 2 nodes at the same time
+            // so the isEmpty() should be enough to guess we are the first one
+            for(Map.Entry<String, SHandler<SEvent>> entry : handlers.entrySet()) {
+                addHandler(entry.getKey(), entry.getValue());
+            }
+            System.out.println(prefix + "Added the default handlers : " + handlers);
+        } else {
+            // If we are not the first one we have to initialize a local copy of the already registered events.
+            localEventTypes.addAll(eventHandlers.keySet());
+        }
     }
 
     @Override
     protected boolean containsHandlerFor(final String eventType) {
+        boolean result = false;
         Lock readLock = localEventTypesLock.readLock();
         readLock.lock();
         try {
-            return localEventTypes.contains(eventType);
+            return (result = localEventTypes.contains(eventType));
         } finally {
+            if (logger.isLoggable(getClass(), TechnicalLogSeverity.TRACE)) {
+                logger.log(getClass(), TechnicalLogSeverity.TRACE, "containsHandlerFor : " + eventType + " " + result);
+            }
             readLock.unlock();
         }
     }
 
     @Override
     protected Collection<SHandler<SEvent>> getHandlersFor(final String eventType) {
-        return eventHandlers.get(eventType);
+        List<SHandler<SEvent>> result = eventHandlers.get(eventType);
+
+        if (logger.isLoggable(getClass(), TechnicalLogSeverity.TRACE)) {
+            logger.log(getClass(), TechnicalLogSeverity.TRACE, "getHandlersFor : " + eventType + " " + result);
+        }
+
+        return result;
     }
 
     @Override
     protected void addHandlerFor(final String eventType, final SHandler<SEvent> handler) throws HandlerRegistrationException {
-        // check if the given event type is already registered in the Event Service
-        if (containsHandlerFor(eventType)) {
-            // if the handler already exists for the same eventType, an Exception is thrown
-            final List<SHandler<SEvent>> handlers = eventHandlers.get(eventType);
-            // the add method returns false if the given element already exists in the Set, and does nothing.
-            if (!handlers.add(handler)) {
-                throw new HandlerRegistrationException("This handler is already registered for this event type");
-            }
+        eventHandlers.lock(eventType);
+        try {
+            // check if the given event type is already registered in the Event Service
+            if (containsHandlerFor(eventType)) {
+                // if the handler already exists for the same eventType, an Exception is thrown
+                final List<SHandler<SEvent>> handlers = eventHandlers.get(eventType);
 
-            eventHandlers.replace(eventType, handlers);
-        } else {
-            // if the given type doesn't already exist in the eventFilters list, we create it
-            final List<SHandler<SEvent>> newHandlerSet = new ArrayList<SHandler<SEvent>>(3);
-            newHandlerSet.add(handler);
-            eventHandlers.put(eventType, newHandlerSet);
-            EventTypeCallable task = new AddEventTypeCallable(eventType);
-            informOtherMembers(task);
+                // Check if another handler of the same class is already registered
+                for (SHandler<SEvent> tmpHandler : handlers) {
+                    if (tmpHandler.getIdentifier().equals(handler.getIdentifier())) {
+                        throw new HandlerRegistrationException("The handler with identifier " + tmpHandler.getIdentifier() + " is already registered for the event " + eventType);
+                    }
+                }
+                handlers.add(handler);
+
+                eventHandlers.replace(eventType, handlers);
+            } else {
+                // if the given type doesn't already exist in the eventFilters list, we create it
+                final List<SHandler<SEvent>> newHandlerList = new ArrayList<SHandler<SEvent>>(3);
+                newHandlerList.add(handler);
+                eventHandlers.put(eventType, newHandlerList);
+                EventTypeCallable task = new AddEventTypeCallable(eventType);
+                informAllMembers(task);
+            }
+        } finally {
+            eventHandlers.unlock(eventType);
         }
     }
 
     /**
      * @param task
      */
-    private void informOtherMembers(final LocalClusteredServiceTask task) {
+    private void informAllMembers(final LocalClusteredServiceTask task) {
         IExecutorService executorService = hazelcastInstance.getExecutorService("ClusteredEventServiceHandlerManagement");
         Map<Member, Future<Void>> results = executorService.submitToAllMembers(task);
         for (Map.Entry<Member, Future<Void>> entry : results.entrySet()) {
             try {
                 entry.getValue().get();
             } catch (InterruptedException e) {
-                throw new SBonitaRuntimeException(e);
+                throw new SBonitaRuntimeException("There was an error on the member " + entry.getKey() + " for the task " + task.getClass().getName(), e);
             } catch (ExecutionException e) {
-                throw new SBonitaRuntimeException(e);
+                throw new SBonitaRuntimeException("There was an error on the member " + entry.getKey() + " for the task " + task.getClass().getName(), e);
             }
         }
     }
@@ -166,7 +206,7 @@ public class ClusteredEventServiceImpl extends ConfigurableEventServiceImpl {
             if (handlers.isEmpty()) {
                 eventHandlers.remove(eventType);
                 EventTypeCallable task = new RemoveEventTypeCallable(eventType);
-                informOtherMembers(task);
+                informAllMembers(task);
             } else {
                 eventHandlers.replace(eventType, handlers);
             }
@@ -176,7 +216,7 @@ public class ClusteredEventServiceImpl extends ConfigurableEventServiceImpl {
     // It should be used only for the tests
     /* package */synchronized void removeAllHandlers() {
         eventHandlers.clear();
-        informOtherMembers(new ClearLocalEventType());
+        informAllMembers(new ClearLocalEventType());
     }
 
     private static abstract class LocalClusteredServiceTask implements Serializable, Callable<Void>, HazelcastInstanceAware {
@@ -205,6 +245,8 @@ public class ClusteredEventServiceImpl extends ConfigurableEventServiceImpl {
     }
 
     private static abstract class WriteLockLocalClusteredServiceTask extends LocalClusteredServiceTask {
+
+        private static final long serialVersionUID = 1L;
 
         @Override
         final protected void doWithClusteredEventService(final ClusteredEventServiceImpl clusteredEventService) {
@@ -264,6 +306,8 @@ public class ClusteredEventServiceImpl extends ConfigurableEventServiceImpl {
 
     private static class RemoveEventTypeCallable extends EventTypeCallable {
 
+        private static final long serialVersionUID = 1L;
+
         public RemoveEventTypeCallable(final String eventType) {
             super(eventType);
         }
@@ -273,6 +317,32 @@ public class ClusteredEventServiceImpl extends ConfigurableEventServiceImpl {
             clusteredEventService.localEventTypes.remove(eventType);
         }
 
+    }
+
+    @Override
+    protected void removeHandlerFor(final String eventType, final SHandler<SEvent> h) throws HandlerUnregistrationException {
+        boolean removed = false;
+        eventHandlers.lock(eventType);
+        try {
+            List<SHandler<SEvent>> handlers = eventHandlers.get(eventType);
+            if (handlers != null) {
+                Iterator<SHandler<SEvent>> it = handlers.iterator();
+                while (!removed && it.hasNext()) {
+                    SHandler<SEvent> handler = it.next();
+                    if (h.getIdentifier().equals(handler.getIdentifier())) {
+                        it.remove();
+                        removed = true;
+                    }
+                }
+            }
+            if (!removed) {
+                throw new HandlerUnregistrationException();
+            } else {
+                eventHandlers.replace(eventType, handlers);
+            }
+        } finally {
+            eventHandlers.lock(eventType);
+        }
     }
 
 }
--- services/bonita-events/bonita-events-api-clustered-impl/src/test/java/com/bonitasoft/engine/events/impl/ClusteredEventServiceImplTest.java
+++ services/bonita-events/bonita-events-api-clustered-impl/src/test/java/com/bonitasoft/engine/events/impl/ClusteredEventServiceImplTest.java
@@ -1,9 +1,12 @@
 package com.bonitasoft.engine.events.impl;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,15 +15,20 @@ import org.bonitasoft.engine.events.EventServiceTest;
 import org.bonitasoft.engine.events.model.HandlerRegistrationException;
 import org.bonitasoft.engine.events.model.SEvent;
 import org.bonitasoft.engine.events.model.SHandler;
+import org.bonitasoft.engine.events.model.SHandlerExecutionException;
 import org.bonitasoft.engine.events.model.builders.SEventBuilders;
 import org.bonitasoft.engine.events.model.builders.impl.SEventBuildersImpl;
 import org.bonitasoft.engine.log.technical.TechnicalLogSeverity;
 import org.bonitasoft.engine.log.technical.TechnicalLoggerService;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 import com.bonitasoft.manager.Features;
 import com.bonitasoft.manager.Manager;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 
@@ -31,8 +39,20 @@ public class ClusteredEventServiceImplTest extends EventServiceTest {
     private ClusteredEventServiceImpl eventService;
 
     @BeforeClass
-    public static void setupBeforeClass() {
-        hazelcastInstance = Hazelcast.newHazelcastInstance();
+    public static void beforeClass() {
+        Config config = new Config();
+        //disable all networking
+        JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+        joinConfig.getMulticastConfig().setEnabled(false);
+        joinConfig.getTcpIpConfig().setEnabled(false);
+        joinConfig.getAwsConfig().setEnabled(false);
+
+        hazelcastInstance1 = Hazelcast.newHazelcastInstance(config);
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        Hazelcast.shutdownAll();
     }
 
     @After
@@ -42,8 +62,11 @@ public class ClusteredEventServiceImplTest extends EventServiceTest {
 
     @Override
     protected EventService instantiateEventServiceImplementation() {
+        return instantiateEventServiceImplementation(new HashMap<String, SHandler<SEvent>>());
+    }
+
+    protected EventService instantiateEventServiceImplementation(final Map<String, SHandler<SEvent>> handlers) {
         SEventBuilders eventBuilders = new SEventBuildersImpl();
-        Map<String, SHandler<SEvent>> handlers = new HashMap<String, SHandler<SEvent>>();
         TechnicalLoggerService logger = mockTechnicalLoggerService();
         Manager manager = mock(Manager.class);
         when(manager.isFeatureActive(Features.ENGINE_CLUSTERING)).thenReturn(true);
@@ -62,6 +85,115 @@ public class ClusteredEventServiceImplTest extends EventServiceTest {
         TechnicalLoggerService logger = mock(TechnicalLoggerService.class);
         when(logger.isLoggable(any(Class.class), any(TechnicalLogSeverity.class))).thenReturn(false);
         return logger;
+    }
+
+
+    @Test
+    public void constructInstanceWithDefaultHandlers() throws Exception {
+        SEvent event = new DummyEvent("DUMMY");
+        SHandler<SEvent> handler = new DummyHandler(event.getType());
+        Map<String, SHandler<SEvent>> defaultHandlers = Collections.singletonMap(event.getType(), handler);
+
+        ClusteredEventServiceImpl eventService1 = (ClusteredEventServiceImpl) instantiateEventServiceImplementation(defaultHandlers);
+        assertEquals(1, eventService1.getHandlers(event.getType()).size());
+    }
+
+    @Test
+    public void constructTwoInstances() throws Exception {
+        SEvent event = new DummyEvent("DUMMY");
+        // Add the handler on #1
+        ClusteredEventServiceImpl eventService1 = (ClusteredEventServiceImpl) instantiateEventServiceImplementation();
+        SHandler<SEvent> handler = new DummyHandler(event.getType());
+        eventService1.addHandler(event.getType(), handler);
+
+        // Fire the event on #2
+        ClusteredEventServiceImpl eventService2 = (ClusteredEventServiceImpl) instantiateEventServiceImplementation();
+        assertTrue(eventService2.containsHandlerFor(event.getType()));
+        //        eventService2.fireEvent(event);
+        // How to assert the handler was executed correctly ?
+        // (-> Write on the filesystem in the handler then read on the assert ?)
+    }
+
+    @Test
+    public void constructTwoInstancesWithDefaultHandlers() throws Exception {
+        SEvent event = new DummyEvent("DUMMY");
+        SHandler<SEvent> handler = new DummyHandler(event.getType());
+        Map<String, SHandler<SEvent>> defaultHandlers = Collections.singletonMap(event.getType(), handler);
+
+        ClusteredEventServiceImpl eventService1 = (ClusteredEventServiceImpl) instantiateEventServiceImplementation(defaultHandlers);
+        assertEquals(1, eventService1.getHandlers(event.getType()).size());
+
+        ClusteredEventServiceImpl eventService2 = (ClusteredEventServiceImpl) instantiateEventServiceImplementation(defaultHandlers);
+        assertEquals(1, eventService2.getHandlers(event.getType()).size());
+    }
+
+    @Test
+    public void fireEventWithTwoInstances() throws Exception {
+        SEvent event = new DummyEvent("DUMMY");
+        // Add the handler on #1
+        ClusteredEventServiceImpl eventService1 = (ClusteredEventServiceImpl) instantiateEventServiceImplementation();
+        SHandler<SEvent> handler = new DummyHandler(event.getType());
+        eventService1.addHandler(event.getType(), handler);
+
+        // Fire the event on #2
+        ClusteredEventServiceImpl eventService2 = (ClusteredEventServiceImpl) instantiateEventServiceImplementation();
+        assertTrue(eventService2.containsHandlerFor(event.getType()));
+        //        eventService2.fireEvent(event);
+        // How to assert the handler was executed correctly ?
+        // (-> Write on the filesystem in the handler then read on the assert ?)
+    }
+
+    private static class DummyHandler implements SHandler<SEvent> {
+
+        private static final long serialVersionUID = 1L;
+
+        private final String interestedEventType;
+
+        public DummyHandler(final String eventType) {
+            this.interestedEventType = eventType;
+        }
+
+        @Override
+        public void execute(final SEvent event) throws SHandlerExecutionException {
+            System.out.println("Executing on event type : " + event.getType() + " " + event);
+        }
+
+        @Override
+        public boolean isInterested(final SEvent event) {
+            return interestedEventType.equals(event.getType());
+        }
+
+        @Override
+        public String getIdentifier() {
+            return getClass().getName();
+        }
+
+    }
+
+    private static class DummyEvent implements SEvent {
+
+        private final String type;
+        private Object object;
+
+        public DummyEvent(final String type) {
+            this.type = type;
+        }
+
+        @Override
+        public String getType() {
+            return type;
+        }
+
+        @Override
+        public Object getObject() {
+            return object;
+        }
+
+        @Override
+        public void setObject(final Object object) {
+            this.object = object;
+        }
+
     }
 
 }
