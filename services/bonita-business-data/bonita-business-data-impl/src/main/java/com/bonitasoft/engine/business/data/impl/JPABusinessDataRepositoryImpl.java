fb7323d4fd47af1f8fa45edea6026bbacf247fe0
Romain Bioteau <romain.bioteau@bonitasoft.com>
2013-12-16 11:43:35 +0100

    BS-2698 Create a PersistenceUnitBuilder

--- services/bonita-business-data/bonita-business-data-impl/src/main/java/com/bonitasoft/engine/business/data/impl/JPABusinessDataRepositoryImpl.java
+++ services/bonita-business-data/bonita-business-data-impl/src/main/java/com/bonitasoft/engine/business/data/impl/JPABusinessDataRepositoryImpl.java
@@ -8,9 +8,7 @@
  *******************************************************************************/
 package com.bonitasoft.engine.business.data.impl;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
@@ -22,23 +20,12 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.TransformerFactoryConfigurationError;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.bonitasoft.engine.commons.exceptions.SBonitaRuntimeException;
 import org.bonitasoft.engine.commons.io.IOUtil;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import com.bonitasoft.engine.business.data.BusinessDataNotFoundException;
@@ -50,131 +37,109 @@ import com.bonitasoft.engine.business.data.NonUniqueResultException;
  */
 public class JPABusinessDataRepositoryImpl implements BusinessDataRespository {
 
-    private EntityManagerFactory entityManagerFactory;
+	private EntityManagerFactory entityManagerFactory;
 
-    @Override
-    public void deploy(final byte[] bdrArchive) {
-        List<String> classNameList = null;
-        try {
-            classNameList = IOUtil.getClassNameList(bdrArchive);
-        } catch (IOException e) {
-            throw new SBonitaRuntimeException(e);
-        }
+	@Override
+	public void deploy(final byte[] bdrArchive) {
+		transformBDRArchive(bdrArchive);
 
-        if (classNameList == null || classNameList.isEmpty()) {
-            throw new IllegalStateException("No entity found in bdr archive");
-        }
-        byte[] persistenceFileContent = null;
-        try {
-            persistenceFileContent = getPersistenceFileContentFor(classNameList);
-        } catch (ParserConfigurationException e) {
-            throw new SBonitaRuntimeException(e);
-        } catch (SAXException e) {
-            throw new SBonitaRuntimeException(e);
-        } catch (IOException e) {
-            throw new SBonitaRuntimeException(e);
-        } catch (TransformerFactoryConfigurationError e) {
-            throw new SBonitaRuntimeException(e);
-        } catch (TransformerException e) {
-            throw new SBonitaRuntimeException(e);
-        }
+	}
 
-    }
+	protected void transformBDRArchive(final byte[] bdrArchive) {
+		List<String> classNameList = null;
+		try {
+			classNameList = IOUtil.getClassNameList(bdrArchive);
+		} catch (IOException e) {
+			throw new SBonitaRuntimeException(e);
+		}
 
-    protected byte[] getPersistenceFileContentFor(final List<String> classNames) throws ParserConfigurationException, SAXException, IOException,
-            TransformerFactoryConfigurationError, TransformerException {
-        final DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        documentBuilderFactory.setValidating(false);
-        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		if (classNameList == null || classNameList.isEmpty()) {
+			throw new IllegalStateException("No entity found in bdr archive");
+		}
+		byte[] persistenceFileContent = null;
+		try {
+			persistenceFileContent = getPersistenceFileContentFor(classNameList);
+		} catch (ParserConfigurationException e) {
+			throw new SBonitaRuntimeException(e);
+		} catch (SAXException e) {
+			throw new SBonitaRuntimeException(e);
+		} catch (IOException e) {
+			throw new SBonitaRuntimeException(e);
+		} catch (TransformerFactoryConfigurationError e) {
+			throw new SBonitaRuntimeException(e);
+		} catch (TransformerException e) {
+			throw new SBonitaRuntimeException(e);
+		}
+	}
 
-        InputStream is = JPABusinessDataRepositoryImpl.class.getResourceAsStream("persistence.xml");
-        Document document = documentBuilder.parse(is);
-        NodeList parentElement = document.getElementsByTagName("persistence-unit");
-        if (parentElement.getLength() > 0) {
-            Node node = parentElement.item(0);
-            for (String className : classNames) {
-                Element classNode = document.createElement("class");
-                classNode.setTextContent(className);
-                node.appendChild(classNode);
-            }
-        }
-        Transformer tf = TransformerFactory.newInstance().newTransformer();
-        tf.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-        tf.setOutputProperty(OutputKeys.INDENT, "yes");
-        ByteArrayOutputStream out = null;
-        try {
-            out = new ByteArrayOutputStream();
-            tf.transform(new DOMSource(document), new StreamResult(out));
-            byte[] byteArray = out.toByteArray();
-            return byteArray;
-        } finally {
-            if (out != null) {
-                out.close();
-            }
-        }
+	protected byte[] getPersistenceFileContentFor(final List<String> classNames) throws IOException, TransformerException, ParserConfigurationException, SAXException {
+		PersistenceUnitBuilder builder = new PersistenceUnitBuilder();
+		for(String classname : classNames){
+			builder.addClass(classname);
+		}
+		return IOUtil.toByteArray(builder.done());
+	}
 
-    }
+	@Override
+	public void start() {
+		final Map<String, Object> configOverrides = new HashMap<String, Object>();
+		configOverrides.put("hibernate.ejb.resource_scanner", InactiveScanner.class.getName());
+		entityManagerFactory = Persistence.createEntityManagerFactory("BDR", configOverrides);
+	}
 
-    @Override
-    public void start() {
-        final Map<String, Object> configOverrides = new HashMap<String, Object>();
-        configOverrides.put("hibernate.ejb.resource_scanner", InactiveScanner.class.getName());
-        entityManagerFactory = Persistence.createEntityManagerFactory("BDR", configOverrides);
-    }
+	@Override
+	public void stop() {
+		if (entityManagerFactory != null) {
+			entityManagerFactory.close();
+			entityManagerFactory = null;
+		}
+	}
 
-    @Override
-    public void stop() {
-        if (entityManagerFactory != null) {
-            entityManagerFactory.close();
-            entityManagerFactory = null;
-        }
-    }
+	@Override
+	public <T> T find(final Class<T> entityClass, final Serializable primaryKey) throws BusinessDataNotFoundException {
+		final EntityManager em = getEntityManager();
+		final T entity = em.find(entityClass, primaryKey);
+		if (entity == null) {
+			throw new BusinessDataNotFoundException("Impossible to get data with id: " + primaryKey);
+		}
+		return entity;
+	}
 
-    @Override
-    public <T> T find(final Class<T> entityClass, final Serializable primaryKey) throws BusinessDataNotFoundException {
-        final EntityManager em = getEntityManager();
-        final T entity = em.find(entityClass, primaryKey);
-        if (entity == null) {
-            throw new BusinessDataNotFoundException("Impossible to get data with id: " + primaryKey);
-        }
-        return entity;
-    }
+	@Override
+	public <T> T find(final Class<T> resultClass, final String qlString, final Map<String, Object> parameters) throws BusinessDataNotFoundException,
+	NonUniqueResultException {
+		final EntityManager em = getEntityManager();
+		final TypedQuery<T> query = em.createQuery(qlString, resultClass);
+		if (parameters != null) {
+			for (final Entry<String, Object> parameter : parameters.entrySet()) {
+				query.setParameter(parameter.getKey(), parameter.getValue());
+			}
+		}
+		try {
+			return query.getSingleResult();
+		} catch (final javax.persistence.NonUniqueResultException nure) {
+			throw new NonUniqueResultException(nure);
+		} catch (final NoResultException nre) {
+			throw new BusinessDataNotFoundException("Impossible to get data using query: " + qlString + " and parameters: " + parameters, nre);
+		}
+	}
 
-    @Override
-    public <T> T find(final Class<T> resultClass, final String qlString, final Map<String, Object> parameters) throws BusinessDataNotFoundException,
-            NonUniqueResultException {
-        final EntityManager em = getEntityManager();
-        final TypedQuery<T> query = em.createQuery(qlString, resultClass);
-        if (parameters != null) {
-            for (final Entry<String, Object> parameter : parameters.entrySet()) {
-                query.setParameter(parameter.getKey(), parameter.getValue());
-            }
-        }
-        try {
-            return query.getSingleResult();
-        } catch (final javax.persistence.NonUniqueResultException nure) {
-            throw new NonUniqueResultException(nure);
-        } catch (final NoResultException nre) {
-            throw new BusinessDataNotFoundException("Impossible to get data using query: " + qlString + " and parameters: " + parameters, nre);
-        }
-    }
+	private EntityManager getEntityManager() {
+		if (entityManagerFactory == null) {
+			throw new IllegalStateException("The BDR is not started");
+		}
+		final EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.joinTransaction();
+		return entityManager;
+	}
 
-    private EntityManager getEntityManager() {
-        if (entityManagerFactory == null) {
-            throw new IllegalStateException("The BDR is not started");
-        }
-        final EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.joinTransaction();
-        return entityManager;
-    }
-
-    @Override
-    public void persist(final Object entity) {
-        if (entity == null) {
-            return;
-        }
-        final EntityManager em = getEntityManager();
-        em.persist(entity);
-    }
+	@Override
+	public void persist(final Object entity) {
+		if (entity == null) {
+			return;
+		}
+		final EntityManager em = getEntityManager();
+		em.persist(entity);
+	}
 
 }
---
+++ services/bonita-business-data/bonita-business-data-impl/src/main/java/com/bonitasoft/engine/business/data/impl/PersistenceUnitBuilder.java
@@ -0,0 +1,104 @@
+/**
+ * Copyright (C) 2013 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.bonitasoft.engine.business.data.impl;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.bonitasoft.engine.commons.exceptions.SBonitaRuntimeException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * @author Romain Bioteau
+ *
+ */
+public class PersistenceUnitBuilder {
+
+	private Document document;
+	private Set<String> classes = new HashSet<String>();
+
+	public PersistenceUnitBuilder(){
+		document = initializeDefaultPersistenceDocument();
+	}
+
+	protected Document initializeDefaultPersistenceDocument() {
+		final DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		documentBuilderFactory.setValidating(false);
+		DocumentBuilder documentBuilder = null;
+		try {
+			documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			throw new SBonitaRuntimeException(e);
+		}
+		InputStream is = null ;
+		try{
+			is = JPABusinessDataRepositoryImpl.class.getResourceAsStream("persistence.xml");
+			return documentBuilder.parse(is);
+		} catch (SAXException e) {
+			throw new SBonitaRuntimeException(e);
+		} catch (IOException e) {
+			throw new SBonitaRuntimeException(e);
+		}finally{
+			if(is != null){
+				try {
+					is.close();
+				} catch (IOException e) {
+					throw new SBonitaRuntimeException(e);
+				}
+			}
+		}
+	}
+
+	public Document done() throws ParserConfigurationException, SAXException, IOException {
+		insertClasses();
+		return document;
+	}
+
+	protected void insertClasses() {
+		Node persistenceUnitNode = getPersistenceUnitNode();
+		for(String classname : classes){
+			Element classNode = document.createElement("class");
+			classNode.setTextContent(classname);
+			persistenceUnitNode.appendChild(classNode);
+		}
+
+	}
+
+	private Node getPersistenceUnitNode() {
+		NodeList parentElement = document.getElementsByTagName("persistence-unit");
+		if (parentElement.getLength() > 0) {
+			return parentElement.item(0);
+		}
+		return null;
+	}
+
+	public PersistenceUnitBuilder addClass(String classname) {
+		classes.add(classname);
+		return this;
+	}
+
+}
