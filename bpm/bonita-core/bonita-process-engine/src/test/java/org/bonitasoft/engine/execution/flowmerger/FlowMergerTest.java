461b00255d4355e1560c5420f7752ecb1ca0f215
Elias Ricken de Medeiros <elias.ricken@bonitasoft.com>
2013-12-16 17:29:46 +0100

    BS-2663, refactor flow merge algorithm

---
+++ bpm/bonita-core/bonita-process-engine/src/test/java/org/bonitasoft/engine/execution/flowmerger/FlowMergerTest.java
@@ -0,0 +1,210 @@
+/**
+ * Copyright (C) 2013 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
+package org.bonitasoft.engine.execution.flowmerger;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doReturn;
+
+import org.bonitasoft.engine.commons.exceptions.SObjectNotFoundException;
+import org.bonitasoft.engine.commons.exceptions.SObjectReadException;
+import org.bonitasoft.engine.core.process.definition.model.SFlowNodeDefinition;
+import org.bonitasoft.engine.core.process.definition.model.SGatewayDefinition;
+import org.bonitasoft.engine.core.process.instance.model.SFlowNodeInstance;
+import org.bonitasoft.engine.core.process.instance.model.SProcessInstance;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+
+
+/**
+ * @author Elias Ricken de Medeiros
+ *
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class FlowMergerTest {
+    
+    @Mock
+    SFlowNodeInstance child;
+    
+    @Mock
+    private SProcessInstance sProcessInstance;
+
+    @Mock
+    private SFlowNodeDefinition flowNode;
+    
+    @Mock
+    private SGatewayDefinition gateway;
+    
+    @Mock
+    private FlowNodeTransitionsWrapper transitionsDescriptor;
+    
+    @Mock
+    private TokenProvider tokenProvider;
+    
+    @Mock
+    private SFlowNodeWrapper flowNodeWrapper;
+
+    @Mock
+    private SFlowNodeWrapper transitionDependentFlowNodeWrapper;
+    
+    private FlowMerger flowNodeMerger;
+    
+    private FlowMerger transitionDependentFlowNodeMerger;
+    
+    @Before
+    public void setUp() throws SObjectReadException, SObjectNotFoundException {
+        flowNodeMerger = new FlowMerger(flowNodeWrapper, transitionsDescriptor, tokenProvider);
+        transitionDependentFlowNodeMerger = new FlowMerger(transitionDependentFlowNodeWrapper, transitionsDescriptor, tokenProvider);
+        doReturn(false).when(transitionDependentFlowNodeWrapper).isNull();
+        doReturn(false).when(transitionDependentFlowNodeWrapper).isBoundaryEvent();
+        doReturn(false).when(transitionDependentFlowNodeWrapper).isExclusive();
+    }
+    
+    @Test
+    public void should_not__create_token_if_flowNode_is_null() throws SObjectReadException, SObjectNotFoundException {
+        doReturn(true).when(flowNodeWrapper).isNull();
+        assertFalse(flowNodeMerger.mustCreateToken());
+    }
+
+    @Test
+    public void should_not_create_token_if_it_is_a_boundary_event() throws SObjectReadException, SObjectNotFoundException {
+        doReturn(true).when(flowNodeWrapper).isBoundaryEvent();
+        
+        assertFalse(flowNodeMerger.mustCreateToken());
+    }
+
+    @Test
+    public void should_not_create_token_if_it_is_exclusive() throws SObjectReadException, SObjectNotFoundException {
+        doReturn(true).when(flowNodeWrapper).isExclusive();
+        
+        assertFalse(flowNodeMerger.mustCreateToken());
+    }
+
+    @Test
+    public void should_not_create_token_if_is_last_flow_node() throws SObjectReadException, SObjectNotFoundException {
+        doReturn(true).when(transitionsDescriptor).isLastFlowNode();
+        
+        assertFalse(transitionDependentFlowNodeMerger.mustCreateToken());
+    }
+
+    @Test
+    public void should_not_create_token_if_dont_have_multiple_outgoing_transitions() throws SObjectReadException, SObjectNotFoundException {
+        doReturn(false).when(transitionsDescriptor).isLastFlowNode();
+        doReturn(false).when(transitionsDescriptor).hasMultipleOutgoingTransitions();
+        
+        assertFalse(transitionDependentFlowNodeMerger.mustCreateToken());
+    }
+
+    @Test
+    public void should_create_token_if_depend_on_transitons_and_has_multiple_outgoing_transitions() throws SObjectReadException, SObjectNotFoundException {
+        doReturn(false).when(transitionsDescriptor).isLastFlowNode();
+        doReturn(true).when(transitionsDescriptor).hasMultipleOutgoingTransitions();
+        
+        assertTrue(transitionDependentFlowNodeMerger.mustCreateToken());
+    }
+    
+    @Test
+    public void should_not_consume_token_if_flowNode_is_null() throws SObjectReadException, SObjectNotFoundException {
+        doReturn(true).when(flowNodeWrapper).isNull();
+        assertFalse(flowNodeMerger.mustConsumeInputToken());
+    }
+    
+    @Test
+    public void should_not_consume_token_if_it_is_a_boundary_event() throws SObjectReadException, SObjectNotFoundException {
+        doReturn(true).when(flowNodeWrapper).isBoundaryEvent();
+        
+        assertFalse(flowNodeMerger.mustConsumeInputToken());
+    }
+    
+    @Test
+    public void should_not_consume_token_if_it_is_exclusive() throws SObjectReadException, SObjectNotFoundException {
+        doReturn(true).when(flowNodeWrapper).isExclusive();
+        
+        assertFalse(flowNodeMerger.mustConsumeInputToken());
+    }
+    
+    @Test
+    public void should_not_consume_token_if_is_last_flow_node() throws SObjectReadException, SObjectNotFoundException {
+        doReturn(true).when(transitionsDescriptor).isLastFlowNode();
+        
+        assertFalse(transitionDependentFlowNodeMerger.mustConsumeInputToken());
+    }
+
+    @Test
+    public void should_not_consume_token_if_doesnt_have_multiple_incoming_transitions() throws SObjectReadException, SObjectNotFoundException {
+        doReturn(false).when(transitionsDescriptor).isLastFlowNode();
+        doReturn(false).when(transitionsDescriptor).hasMultipleIncomingTransitions();
+        
+        assertFalse(transitionDependentFlowNodeMerger.mustConsumeInputToken());
+    }
+
+    @Test
+    public void should_consume_token_if_has_multiple_incoming_transitions_and_is_parallel_or_inclusive() throws SObjectReadException, SObjectNotFoundException {
+        doReturn(false).when(transitionsDescriptor).isLastFlowNode();
+        doReturn(true).when(transitionsDescriptor).hasMultipleIncomingTransitions();
+        doReturn(true).when(transitionDependentFlowNodeWrapper).isParalleleOrInclusive();
+        
+        assertTrue(transitionDependentFlowNodeMerger.mustConsumeInputToken());
+    }
+
+    @Test
+    public void should_not_consume_token_if_has_multiple_incoming_transitions_is_not_parallel_or_inclusive() throws SObjectReadException, SObjectNotFoundException {
+        doReturn(false).when(transitionsDescriptor).isLastFlowNode();
+        doReturn(true).when(transitionsDescriptor).hasMultipleIncomingTransitions();
+        doReturn(false).when(transitionDependentFlowNodeWrapper).isParalleleOrInclusive();
+        
+        assertFalse(transitionDependentFlowNodeMerger.mustConsumeInputToken());
+    }
+    
+    
+    @Test
+    public void is_not_implicite_end_if_flowNode_is_null() throws SObjectReadException, SObjectNotFoundException {
+        doReturn(true).when(flowNodeWrapper).isNull();
+        assertFalse(flowNodeMerger.isImplicitEnd());
+    }
+
+    @Test
+    public void is_not_implicite_end_if_it_is_a_boundary_event() throws SObjectReadException, SObjectNotFoundException {
+        doReturn(true).when(flowNodeWrapper).isBoundaryEvent();
+        
+        assertFalse(flowNodeMerger.isImplicitEnd());
+    }
+
+    @Test
+    public void is_implicite_end_if_is_last_flow_node() throws SObjectReadException, SObjectNotFoundException {
+        doReturn(true).when(transitionsDescriptor).isLastFlowNode();
+        
+        assertTrue(flowNodeMerger.isImplicitEnd());
+    }
+
+    @Test
+    public void is_not_implicite_end_if_is_not_last_flow_node() throws SObjectReadException, SObjectNotFoundException {
+        doReturn(false).when(transitionsDescriptor).isLastFlowNode();
+        
+        assertFalse(flowNodeMerger.isImplicitEnd());
+    }
+    
+    @Test
+    public void return_token_info_supplied_by_tokenProvider() throws Exception {
+        TokenInfo tokenInfo = new TokenInfo(1L, 2L);
+        doReturn(tokenInfo).when(tokenProvider).getOutputTokenInfo();
+        assertEquals(tokenInfo, flowNodeMerger.getOutputTokenInfo());
+    }
+
+}
---
+++ bpm/bonita-core/bonita-process-engine/src/test/java/org/bonitasoft/engine/execution/flowmerger/SFlowNodeWrapperTest.java
@@ -0,0 +1,178 @@
+/**
+ * Copyright (C) 2013 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
+package org.bonitasoft.engine.execution.flowmerger;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doReturn;
+
+import org.bonitasoft.engine.commons.exceptions.SObjectNotFoundException;
+import org.bonitasoft.engine.commons.exceptions.SObjectReadException;
+import org.bonitasoft.engine.core.process.definition.model.SFlowNodeDefinition;
+import org.bonitasoft.engine.core.process.definition.model.SFlowNodeType;
+import org.bonitasoft.engine.core.process.definition.model.SGatewayDefinition;
+import org.bonitasoft.engine.core.process.definition.model.SGatewayType;
+import org.bonitasoft.engine.core.process.definition.model.event.SCatchEventDefinition;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+
+
+/**
+ * @author Elias Ricken de Medeiros
+ *
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class SFlowNodeWrapperTest {
+    
+    @Mock
+    private SFlowNodeDefinition flowNode;
+    
+    @Mock
+    private SGatewayDefinition gateway;
+
+    @Mock
+    private SCatchEventDefinition catchEvent;
+    
+    private SFlowNodeWrapper flowNodeWrapper;
+    
+    private SFlowNodeWrapper gatewayWrapper;
+
+    private SFlowNodeWrapper catchEventWrapper;
+
+    private SFlowNodeWrapper nullFlowNodewrapper;
+    
+    @Before
+    public void setUp() throws SObjectReadException, SObjectNotFoundException {
+        flowNodeWrapper = new SFlowNodeWrapper(flowNode);
+        gatewayWrapper = new SFlowNodeWrapper(gateway);
+        catchEventWrapper = new SFlowNodeWrapper(catchEvent);
+        nullFlowNodewrapper = new SFlowNodeWrapper(null);
+        
+        doReturn(SFlowNodeType.GATEWAY).when(gateway).getType();
+    }
+    
+    @Test
+    public void not_exclusive_if_flow_node_is_null() throws Exception {
+        assertFalse(nullFlowNodewrapper.isExclusive());
+    }
+
+    @Test
+    public void not_exclusive_if_not_gateway() throws Exception {
+        doReturn(SFlowNodeType.USER_TASK).when(flowNode).getType();
+        
+        assertFalse(flowNodeWrapper.isExclusive());
+    }
+
+    @Test
+    public void not_exclusive_if_parallel_gateway() throws Exception {
+        doReturn(SGatewayType.PARALLEL).when(gateway).getGatewayType();
+        
+        assertFalse(gatewayWrapper.isExclusive());
+    }
+
+    @Test
+    public void not_exclusive_if_inclusive_gateway() throws Exception {
+        doReturn(SGatewayType.INCLUSIVE).when(gateway).getGatewayType();
+        
+        assertFalse(gatewayWrapper.isExclusive());
+    }
+
+    @Test
+    public void exclusive_if_ecllusive_gateway() throws Exception {
+        doReturn(SGatewayType.EXCLUSIVE).when(gateway).getGatewayType();
+        
+        assertTrue(gatewayWrapper.isExclusive());
+    }
+    
+    @Test
+    public void not_parallelOrInclusive_if_flow_node_is_null() throws Exception {
+        assertFalse(nullFlowNodewrapper.isParalleleOrInclusive());
+    }
+
+    @Test
+    public void not_parallelOrInclusive_if_not_gateway() throws Exception {
+        doReturn(SFlowNodeType.USER_TASK).when(flowNode).getType();
+        
+        assertFalse(flowNodeWrapper.isParalleleOrInclusive());
+        
+    }
+
+    @Test
+    public void parallelOrInclusive_if_parallel_gateway() throws Exception {
+        doReturn(SGatewayType.PARALLEL).when(gateway).getGatewayType();
+        
+        assertTrue(gatewayWrapper.isParalleleOrInclusive());
+        
+    }
+
+    @Test
+    public void parallelOrInclusive_if_inclusive_gateway() throws Exception {
+        doReturn(SGatewayType.INCLUSIVE).when(gateway).getGatewayType();
+        
+        assertTrue(gatewayWrapper.isParalleleOrInclusive());
+        
+    }
+
+    @Test
+    public void not_parallelOrInclusive_if_exclusive_gateway() throws Exception {
+        doReturn(SGatewayType.EXCLUSIVE).when(gateway).getGatewayType();
+        
+        assertFalse(gatewayWrapper.isParalleleOrInclusive());
+        
+    }
+    
+    @Test
+    public void not_boundary_if_null() throws Exception {
+        assertFalse(nullFlowNodewrapper.isBoundaryEvent());
+    }
+    
+    @Test
+    public void boundary_if_is_a_boundary() throws Exception {
+        doReturn(SFlowNodeType.BOUNDARY_EVENT).when(flowNode).getType();
+        assertTrue(flowNodeWrapper.isBoundaryEvent());
+    }
+    
+    @Test
+    public void is_not_interrupting_if_null() throws Exception {
+        assertFalse(nullFlowNodewrapper.isInterrupting());
+    }
+
+    @Test
+    public void is_not_interrupting_if_not_catch_event() throws Exception {
+        assertFalse(flowNodeWrapper.isInterrupting());
+    }
+
+    @Test
+    public void is_interrupting_if_interrupting_catch_event() throws Exception {
+        doReturn(true).when(catchEvent).isInterrupting();
+        assertTrue(catchEventWrapper.isInterrupting());
+    }
+
+    @Test
+    public void is_not_interrupting_if_non_interrupting_catch_event() throws Exception {
+        doReturn(false).when(catchEvent).isInterrupting();
+        assertFalse(catchEventWrapper.isInterrupting());
+    }
+    
+    @Test
+    public void getFlowNode() throws Exception {
+        assertEquals(flowNode, flowNodeWrapper.getFlowNode());
+    }
+
+}
