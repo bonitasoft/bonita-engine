b254f1f6a8a09364a73afc4d0f9d5c400d3553b7
Céline Souchet <celine.souchet@bonitasoft.com>
2014-02-07 13:40:06 +0100

    Add javadoc on ActorMember

--- bpm/bonita-api/bonita-common-api/src/main/java/org/bonitasoft/engine/bpm/actor/ActorMember.java
+++ bpm/bonita-api/bonita-common-api/src/main/java/org/bonitasoft/engine/bpm/actor/ActorMember.java
@@ -15,18 +15,33 @@ package org.bonitasoft.engine.bpm.actor;
 
 import org.bonitasoft.engine.bpm.BonitaObject;
 
-
 /**
+ * It is used to map a actorInstance with the organisation.
+ * It's possible to map a actorInstance with a user, or a group, or a role, or a group and a role.
+ * 
  * @author Matthieu Chaffotte
+ * @author Celine Souchet
  */
 public interface ActorMember extends BonitaObject {
 
+    /**
+     * @return The identifier of the actor member
+     */
     long getId();
 
+    /**
+     * @return The identifier of the user
+     */
     long getUserId();
 
+    /**
+     * @return The identifier of the group
+     */
     long getGroupId();
 
+    /**
+     * @return The identifier of the role
+     */
     long getRoleId();
 
 }
