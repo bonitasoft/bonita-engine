aa65bcd6e3841180f573bf33a57deb432239ab94
Céline Souchet <celine.souchet@bonitasoft.com>
2014-02-13 17:24:36 +0100

    BS-6844 : Javadoc is missing description on packages and classes
    - Add javadoc for Actor

--- bpm/bonita-api/bonita-common-api/src/main/java/org/bonitasoft/engine/bpm/actor/ActorMappingExportException.java
+++ bpm/bonita-api/bonita-common-api/src/main/java/org/bonitasoft/engine/bpm/actor/ActorMappingExportException.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2012 BonitaSoft S.A.
+ * Copyright (C) 2012, 2014 BonitaSoft S.A.
  * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
  * This library is free software; you can redistribute it and/or modify it under the terms
  * of the GNU Lesser General Public License as published by the Free Software Foundation
@@ -16,7 +16,10 @@ package org.bonitasoft.engine.bpm.actor;
 import org.bonitasoft.engine.exception.ExecutionException;
 
 /**
+ * It's thrown when it's not possible to export the actor mappings.
+ * 
  * @author Matthieu Chaffotte
+ * @author Celine Souchet
  */
 public class ActorMappingExportException extends ExecutionException {
 
