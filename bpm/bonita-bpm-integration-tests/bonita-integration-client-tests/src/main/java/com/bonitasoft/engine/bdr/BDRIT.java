6294963c052561cc487ba3cc5dd30954ab83e1b9
Matthieu Chaffotte <matthieu.chaffotte@bonitasoft.com>
2014-02-18 17:13:27 +0100

    BS-7221: Add tests to check the behaviour of business objects in Groovy scripts and connectors

--- bpm/bonita-bpm-integration-tests/bonita-integration-client-tests/src/main/java/com/bonitasoft/engine/bdr/BDRIT.java
+++ bpm/bonita-bpm-integration-tests/bonita-integration-client-tests/src/main/java/com/bonitasoft/engine/bdr/BDRIT.java
@@ -1,5 +1,6 @@
 package com.bonitasoft.engine.bdr;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
@@ -7,12 +8,16 @@ import static org.junit.Assert.assertNull;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
 import org.apache.commons.io.IOUtils;
+import org.bonitasoft.engine.bpm.bar.BarResource;
+import org.bonitasoft.engine.bpm.bar.BusinessArchiveBuilder;
+import org.bonitasoft.engine.bpm.connector.ConnectorEvent;
 import org.bonitasoft.engine.bpm.flownode.HumanTaskInstance;
 import org.bonitasoft.engine.bpm.process.ProcessDefinition;
 import org.bonitasoft.engine.bpm.process.ProcessInstance;
@@ -165,8 +170,8 @@ public class BDRIT extends CommonAPISPTest {
 
         final ProcessDefinitionBuilderExt processDefinitionBuilder = new ProcessDefinitionBuilderExt().createNewInstance(
                 "shouldBeAbleToUpdateBusinessDataUsingJavaSetterOperation", "6.3-beta");
-        String businessDataName = "newBornBaby";
-        String newEmployeeFirstName = "Manon";
+        final String businessDataName = "newBornBaby";
+        final String newEmployeeFirstName = "Manon";
         processDefinitionBuilder.addBusinessData(businessDataName, EMPLOYEE_QUALIF_CLASSNAME, employeeExpression);
         processDefinitionBuilder.addActor(ACTOR_NAME);
         processDefinitionBuilder.addAutomaticTask("step1").addOperation(
@@ -176,19 +181,88 @@ public class BDRIT extends CommonAPISPTest {
         processDefinitionBuilder.addTransition("step1", "step2");
 
         final ProcessDefinition definition = deployAndEnableWithActor(processDefinitionBuilder.done(), ACTOR_NAME, matti);
-        long processInstanceId = getProcessAPI().startProcess(definition.getId()).getId();
+        final long processInstanceId = getProcessAPI().startProcess(definition.getId()).getId();
 
         waitForUserTask("step2", processInstanceId);
 
         // Let's check the updated firstName value by calling an expression:
-        Map<Expression, Map<String, Serializable>> expressions = new HashMap<Expression, Map<String, Serializable>>(1);
-        String expressionName = "retrieve_FirstName";
+        final Map<Expression, Map<String, Serializable>> expressions = new HashMap<Expression, Map<String, Serializable>>(1);
+        final String expressionName = "retrieve_FirstName";
         expressions.put(new ExpressionBuilder().createGroovyScriptExpression(expressionName, businessDataName + ".firstName", String.class.getName(),
                 new ExpressionBuilder().createBusinessDataExpression(businessDataName, EMPLOYEE_QUALIF_CLASSNAME)), null);
-        Map<String, Serializable> evaluatedExpressions = getProcessAPI().evaluateExpressionsOnProcessInstance(processInstanceId, expressions);
-        String returnedFirstName = (String) evaluatedExpressions.get(expressionName);
+        final Map<String, Serializable> evaluatedExpressions = getProcessAPI().evaluateExpressionsOnProcessInstance(processInstanceId, expressions);
+        final String returnedFirstName = (String) evaluatedExpressions.get(expressionName);
         assertEquals(newEmployeeFirstName, returnedFirstName);
 
         disableAndDeleteProcess(definition.getId());
     }
+
+    private ProcessDefinition buildProcess(final String taskName) throws BonitaException, IOException {
+
+        final Expression getEmployeeExpression = new ExpressionBuilder().createBusinessDataExpression("myEmployee", "org.bonita.pojo.Employee");
+
+        final Expression employeeExpression = new ExpressionBuilder()
+                .createGroovyScriptExpression("createNewEmployee",
+                        "import org.bonita.pojo.Employee; Employee e = new Employee(); e.firstName = 'John'; e.lastName = 'Doe'; return e;",
+                        "org.bonita.pojo.Employee");
+
+        final ProcessDefinitionBuilderExt processDefinitionBuilder = new ProcessDefinitionBuilderExt().createNewInstance("BizDataAndConnector", "1.0");
+        processDefinitionBuilder.addActor(ACTOR_NAME);
+        processDefinitionBuilder.addBusinessData("myEmployee", "org.bonita.pojo.Employee", employeeExpression);
+        processDefinitionBuilder
+                .addUserTask(taskName, ACTOR_NAME)
+                .addConnector("updateBusinessData", "com.bonitasoft.connector.BusinessDataUpdateConnector", "1.0", ConnectorEvent.ON_ENTER)
+                .addInput("bizData", getEmployeeExpression)
+                .addOutput(
+                        new OperationBuilder().createBusinessDataSetAttributeOperation("output1", "setFirstName", String.class.getName(),
+                                new ExpressionBuilder().createConstantStringExpression("Dana")));
+
+        final BusinessArchiveBuilder businessArchiveBuilder = new BusinessArchiveBuilder().createNewBusinessArchive().setProcessDefinition(
+                processDefinitionBuilder.done());
+        BarResource barResource = getResource("/com/bonitasoft/engine/bdr/BusinessDataUpdateConnector.impl", "BusinessDataUpdateConnector.impl");
+        businessArchiveBuilder.addConnectorImplementation(barResource);
+
+        barResource = buildBarResource(BusinessDataUpdateConnector.class, "BusinessDataUpdateConnector.jar");
+        businessArchiveBuilder.addClasspathResource(barResource);
+
+        final ProcessDefinition processDefinition = getProcessAPI().deploy(businessArchiveBuilder.done());
+        addMappingOfActorsForUser(ACTOR_NAME, matti.getId(), processDefinition);
+        getProcessAPI().enableProcess(processDefinition.getId());
+        return processDefinition;
+    }
+
+    private BarResource getResource(final String path, final String name) throws IOException {
+        final InputStream stream = BDRIT.class.getResourceAsStream(path);
+        assertNotNull(stream);
+        try {
+            final byte[] byteArray = IOUtils.toByteArray(stream);
+            return new BarResource(name, byteArray);
+        } finally {
+            stream.close();
+        }
+    }
+
+    @Test
+    @Ignore("Disabled until we support undeploy of a bdr, otherwise the following tests fail")
+    public void updateBusinessDataOutsideATransaction() throws Exception {
+        final String taskName = "step";
+
+        final ProcessDefinition definition = buildProcess(taskName);
+        final ProcessInstance instance = getProcessAPI().startProcess(definition.getId());
+        waitForUserTask(taskName, instance.getId());
+
+        final Object businessDataInstance = getProcessAPI().getBusinessDataInstance("myEmployee", instance.getId());
+        assertNotNull(businessDataInstance);
+
+        Method method = businessDataInstance.getClass().getMethod("getFirstName", null);
+        final String firstName = (String) method.invoke(businessDataInstance, null);
+        assertThat(firstName).isEqualTo("Dana");
+
+        method = businessDataInstance.getClass().getMethod("getLastName", null);
+        final String lastName = (String) method.invoke(businessDataInstance, null);
+        assertThat(lastName).isEqualTo("Hakkinen");
+
+        disableAndDeleteProcess(definition);
+    }
+
 }
---
+++ bpm/bonita-bpm-integration-tests/bonita-integration-client-tests/src/main/java/com/bonitasoft/engine/bdr/BusinessDataUpdateConnector.java
@@ -0,0 +1,27 @@
+package com.bonitasoft.engine.bdr;
+
+import java.lang.reflect.Method;
+
+import org.bonitasoft.engine.connector.AbstractConnector;
+import org.bonitasoft.engine.connector.ConnectorException;
+
+public class BusinessDataUpdateConnector extends AbstractConnector {
+
+    @Override
+    public void validateInputParameters() {
+
+    }
+
+    @Override
+    protected void executeBusinessLogic() throws ConnectorException {
+        final Object bizData = getInputParameter("bizData");
+        try {
+            final Method method = bizData.getClass().getMethod("setLastName", String.class);
+            method.invoke(bizData, "Hakkinen");
+            setOutputParameter("output1", bizData);
+        } catch (final Exception e) {
+            throw new ConnectorException(e);
+        }
+    }
+
+}
