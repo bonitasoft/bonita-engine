3a515d978980901a7320219bd7cb6a1c07400e44
Colin PUY <colin.puy@bonitasoft.com>
2014-02-24 11:24:35 +0100

    BS-7337, exception must contains compilation errors message

--- bpm/bonita-core/bonita-process-engine/src/main/java/com/bonitasoft/engine/bdm/compiler/JDTCompiler.java
+++ bpm/bonita-core/bonita-process-engine/src/main/java/com/bonitasoft/engine/bdm/compiler/JDTCompiler.java
@@ -2,6 +2,7 @@ package com.bonitasoft.engine.bdm.compiler;
 
 import static java.util.Arrays.asList;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -14,10 +15,7 @@ public class JDTCompiler {
 
     public void compile(Collection<File> filesToBeCompiled, File outputdirectory) throws CompilationException {
         String[] commandLine = buildCommandLineArguments(filesToBeCompiled, outputdirectory);
-        boolean succeeded = BatchCompiler.compile(commandLine, new PrintWriter(System.out), new PrintWriter(System.err), new DummyCompilationProgress());
-        if (!succeeded) {
-            throw new CompilationException("Compilation failed");
-        }
+        launchCompiler(commandLine);
     }
 
     private String[] buildCommandLineArguments(Collection<File> files, File outputdirectory) {
@@ -31,5 +29,27 @@ public class JDTCompiler {
 
     private List<String> outputDirectoryArguments(File outputdirectory) {
         return asList("-d", outputdirectory.getAbsolutePath());
+    }
+
+    private void launchCompiler(String[] commandLine) throws CompilationException {
+        PrintWriter outWriter = new PrintWriter(new ByteArrayOutputStream());
+        // closing outwriter since we don't want to see compilation out stream
+        outWriter.close();
+        
+        ByteArrayOutputStream errorStream = new ByteArrayOutputStream();
+        PrintWriter errorWriter = new PrintWriter(errorStream);
+        try {
+            compile(commandLine, outWriter, errorStream, errorWriter);
+        } finally {
+            // no need to close OutputStream, printWriter is doing it for us
+            if (errorWriter != null) errorWriter.close(); 
+        }
+    }
+
+    private void compile(String[] commandLine, PrintWriter outWriter, ByteArrayOutputStream errorStream, PrintWriter errorWriter) throws CompilationException {
+        boolean succeeded = BatchCompiler.compile(commandLine, outWriter, errorWriter, new DummyCompilationProgress());
+        if (!succeeded) {
+            throw new CompilationException(new String(errorStream.toByteArray()));
+        }
     }
 }
