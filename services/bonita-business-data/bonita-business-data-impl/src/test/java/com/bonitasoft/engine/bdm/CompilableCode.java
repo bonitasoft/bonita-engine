dac194d233ad7e81b484b1bc8792f00629ec13a3
Romain Bioteau <romain.bioteau@bonitasoft.com>
2014-02-24 15:56:24 +0100

    Add classpath to compiler

--- services/bonita-business-data/bonita-business-data-impl/src/test/java/com/bonitasoft/engine/bdm/CompilableCode.java
+++ services/bonita-business-data/bonita-business-data-impl/src/test/java/com/bonitasoft/engine/bdm/CompilableCode.java
@@ -3,6 +3,10 @@ package com.bonitasoft.engine.bdm;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.tools.JavaCompiler;
 import javax.tools.JavaFileObject;
@@ -18,8 +22,22 @@ public abstract class CompilableCode {
         final JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
         final StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
         final Iterable<? extends JavaFileObject> compUnits = fileManager.getJavaFileObjects(sourceFileToCompile);
-        final Boolean compiled = compiler.getTask(null, fileManager, null, null, null, compUnits).call();
+        final List<String> optionList = new ArrayList<String>();
+      
+        String javaxPersistencefilePath = findJarPath(javax.persistence.Basic.class);
+        String bdmEntityfilePath = findJarPath(Entity.class);
+        optionList.addAll(Arrays.asList("-classpath", javaxPersistencefilePath+":"+bdmEntityfilePath));
+        final Boolean compiled = compiler.getTask(null, fileManager, null, optionList, null, compUnits).call();
         assertThat(compiled).isTrue();
     }
+
+	private String findJarPath(Class<?> clazzToFind) {
+		URL jarURL = clazzToFind.getResource(clazzToFind.getSimpleName()+".class");
+        String jarPath = jarURL.getFile();
+        if(jarPath.indexOf("!") != -1){
+        	jarPath = jarPath.split("!")[0];
+        }
+		return jarPath;
+	}
 
 }
