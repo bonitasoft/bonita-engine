c14d0785604d92c7d53dcba7b9be3d2f2170fe48
Colin PUY <colin.puy@bonitasoft.com>
2014-03-05 14:42:57 +0100

    BS-1542, clean QuartzSchedulerExecutorTest

--- services/bonita-services-integration-tests/src/test/java/org/bonitasoft/engine/scheduler/impl/QuartzSchedulerExecutorTest.java
+++ services/bonita-services-integration-tests/src/test/java/org/bonitasoft/engine/scheduler/impl/QuartzSchedulerExecutorTest.java
@@ -48,70 +48,21 @@ public class QuartzSchedulerExecutorTest extends CommonServiceTest {
 
     @Before
     public void setUp() throws Exception {
-        tenant1 = PlatformUtil.createTenant(getTransactionService(), getPlatformService(), "tenant1", PlatformUtil.DEFAULT_CREATED_BY,
-                PlatformUtil.DEFAULT_TENANT_STATUS);
+        tenant1 = PlatformUtil.createTenant(
+                getTransactionService(), getPlatformService(), "tenant1", PlatformUtil.DEFAULT_CREATED_BY, PlatformUtil.DEFAULT_TENANT_STATUS);
+       
         TestUtil.startScheduler(schedulerService);
+        
         getTransactionService().begin();
         changeToDefaultTenant();
         getTransactionService().complete();
     }
 
-    @Override
     @After
-    public void tearDown() throws Exception {
-        TestUtil.closeTransactionIfOpen(getTransactionService());
+    public void stopScheduler() throws Exception {
         TestUtil.stopScheduler(schedulerService, getTransactionService());
         storage.clear();
         PlatformUtil.deleteTenant(getTransactionService(), getPlatformService(), tenant1);
-    }
-
-    /**
-     * @author Baptiste Mesta
-     */
-    private final class WaitForIncrementJobToHaveValue extends WaitFor {
-
-        private final int value;
-
-        /**
-         * @param timeout
-         * @param value
-         */
-        private WaitForIncrementJobToHaveValue(final int timeout, final int value) {
-            super(10, timeout);
-            this.value = value;
-        }
-
-        @Override
-        boolean check() {
-            return IncrementItselfJob.getValue() == value;
-        }
-    }
-
-    public abstract class WaitFor {
-
-        private final int timeout;
-
-        private final int repeatEach;
-
-        public WaitFor(final int repeatEach, final int timeout) {
-            assertTrue("timeout is not big enough", repeatEach < timeout);
-            this.repeatEach = repeatEach;
-            this.timeout = timeout;
-        }
-
-        public boolean waitFor() throws InterruptedException {
-            final long limit = new Date().getTime() + timeout;
-            while (new Date().getTime() < limit) {
-                Thread.sleep(repeatEach);
-                if (check()) {
-                    return true;
-                }
-            }
-            return check();
-        }
-
-        abstract boolean check();
-
     }
 
     @Test
