f9c75756c57be6c94e1babc4416469750dfc5cd6
Matthieu Chaffotte <matthieu.chaffotte@bonitasoft.com>
2014-03-06 10:20:18 +0100

    BS-7520: Restore tests

--- bpm/bonita-integration-tests/bonita-integration-local-tests/src/test/java/com/bonitasoft/engine/SlowExecutionLocalIntegrationTestsSP.java
+++ bpm/bonita-integration-tests/bonita-integration-local-tests/src/test/java/com/bonitasoft/engine/SlowExecutionLocalIntegrationTestsSP.java
@@ -10,39 +10,55 @@ package com.bonitasoft.engine;
 
 import org.bonitasoft.engine.BonitaSuiteRunner;
 import org.bonitasoft.engine.BonitaSuiteRunner.Initializer;
+import org.bonitasoft.engine.event.InterruptingTimerBoundaryEventTest;
+import org.bonitasoft.engine.event.MessageBoundaryEventTest;
+import org.bonitasoft.engine.event.MessageEventSubProcessTest;
+import org.bonitasoft.engine.event.MessageEventTest;
+import org.bonitasoft.engine.event.NonInterruptingTimerBoundaryEventTest;
+import org.bonitasoft.engine.event.TimerBoundaryEventTest;
+import org.bonitasoft.engine.event.TimerEventSubProcessTest;
+import org.bonitasoft.engine.event.TimerEventTest;
+import org.bonitasoft.engine.platform.PlatformLoginTest;
 import org.bonitasoft.engine.platform.PlatformTest;
+import org.bonitasoft.engine.test.BPMLocalSuiteTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.bonitasoft.engine.event.SPTimerBoundaryEventTest;
+import com.bonitasoft.engine.identity.SPIdentityTests;
+import com.bonitasoft.engine.platform.SPPlatformLoginTest;
+import com.bonitasoft.engine.platform.SPPlatformTest;
+import com.bonitasoft.engine.platform.SPProcessManagementTest;
+
 @RunWith(BonitaSuiteRunner.class)
 @SuiteClasses({
-        // TestShadesSP.class,
-        // // Same suites as in LocalIntegrationTestsSP below:
-        // BPMSPTests.class,
-        // BPMLocalSuiteTests.class,
-        // LocalLogTest.class,
-        // APIMethodSPTest.class,
-        // ConnectorExecutionTimeOutTest.class,
-        // ConnectorImplementationLocalSPTest.class,
-        //
-        // // Specific slow test suites below:
-        // NonInterruptingTimerBoundaryEventTest.class,
-        // InterruptingTimerBoundaryEventTest.class,
-        // TimerBoundaryEventTest.class,
-        // TimerEventTest.class,
-        // MessageEventTest.class,
-        // MessageBoundaryEventTest.class,
-        // TimerEventSubProcessTest.class,
-        // MessageEventSubProcessTest.class,
-        // PlatformLoginTest.class,
-        PlatformTest.class
+        TestShadesSP.class,
+        // Same suites as in LocalIntegrationTestsSP below:
+        BPMSPTests.class,
+        BPMLocalSuiteTests.class,
+        LocalLogTest.class,
+        APIMethodSPTest.class,
+        ConnectorExecutionTimeOutTest.class,
+        ConnectorImplementationLocalSPTest.class,
 
-// // SP specific slow test suites:
-// SPPlatformLoginTest.class,
-// SPPlatformTest.class,
-// SPTimerBoundaryEventTest.class,
-// SPIdentityTests.class,
-// SPProcessManagementTest.class
+        // Specific slow test suites below:
+        NonInterruptingTimerBoundaryEventTest.class,
+        InterruptingTimerBoundaryEventTest.class,
+        TimerBoundaryEventTest.class,
+        TimerEventTest.class,
+        MessageEventTest.class,
+        MessageBoundaryEventTest.class,
+        TimerEventSubProcessTest.class,
+        MessageEventSubProcessTest.class,
+        PlatformLoginTest.class,
+        PlatformTest.class,
+
+        // SP specific slow test suites:
+        SPPlatformLoginTest.class,
+        SPPlatformTest.class,
+        SPTimerBoundaryEventTest.class,
+        SPIdentityTests.class,
+        SPProcessManagementTest.class
 })
 @Initializer(TestsInitializerSP.class)
 public class SlowExecutionLocalIntegrationTestsSP extends LocalIntegrationTestsSP {
