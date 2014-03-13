e84ec6cca04f8c363268fb7ba7d5412a3609194b
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2014-03-13 15:33:03 +0100

    update sql script: rename urlToken to displayName

--- services/bonita-page/src/main/resources/org/bonitasoft/engine/page/impl/db/h2-createTables.sql
+++ services/bonita-page/src/main/resources/org/bonitasoft/engine/page/impl/db/h2-createTables.sql
@@ -2,12 +2,12 @@ CREATE TABLE page (
   tenantId BIGINT NOT NULL,
   id BIGINT NOT NULL,
   name VARCHAR(50) NOT NULL,
+  displayName VARCHAR(255) NOT NULL,
   description LONGVARCHAR,
   installationDate BIGINT NOT NULL,
   installedBy BIGINT NOT NULL,
   provided BOOLEAN,
   lastModificationDate BIGINT NOT NULL,
-  urlToken VARCHAR(50) NOT NULL,
   content LONGBLOB,
   UNIQUE (tenantId, name),
   PRIMARY KEY (tenantId, id)
