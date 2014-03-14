2ea677a239502d4517b9db153e67790311b6ce42
laurent leseigneur <laurent.leseigneur@bonitasoft.com>
2014-03-14 19:24:25 +0100

    BS-7593, fix merge issue

---
+++ bpm/bonita-core/bonita-process-engine/src/main/java/com/bonitasoft/engine/api/impl/transaction/reporting/DeleteReports.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (C) 2009, 2013 BonitaSoft S.A.
+ * BonitaSoft is a trademark of BonitaSoft SA.
+ * This software file is BONITASOFT CONFIDENTIAL. Not For Distribution.
+ * For commercial licensing information, contact:
+ * BonitaSoft, 32 rue Gustave Eiffel – 38000 Grenoble
+ * or BonitaSoft US, 51 Federal Street, Suite 305, San Francisco, CA 94107
+ *******************************************************************************/
+package com.bonitasoft.engine.api.impl.transaction.reporting;
+
+import java.util.List;
+
+import org.bonitasoft.engine.commons.exceptions.SBonitaException;
+import org.bonitasoft.engine.commons.transaction.TransactionContent;
+
+import com.bonitasoft.engine.core.reporting.ReportingService;
+import com.bonitasoft.engine.service.TenantServiceAccessor;
+
+/**
+ * @author Matthieu Chaffotte
+ */
+public class DeleteReports implements TransactionContent {
+
+    private final TenantServiceAccessor serviceAccessor;
+
+    private final List<Long> reportIds;
+
+    public DeleteReports(final TenantServiceAccessor serviceAccessor, final List<Long> reportIds) {
+        super();
+        this.serviceAccessor = serviceAccessor;
+        this.reportIds = reportIds;
+    }
+
+    @Override
+    public void execute() throws SBonitaException {
+        final ReportingService reportingService = serviceAccessor.getReportingService();
+        for (final long reportId : reportIds) {
+            reportingService.deleteReport(reportId);
+        }
+    }
+
+}
---
+++ bpm/bonita-core/bonita-process-engine/src/main/java/com/bonitasoft/engine/api/impl/transaction/reporting/GetReportContent.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (C) 2009, 2013 BonitaSoft S.A.
+ * BonitaSoft is a trademark of BonitaSoft SA.
+ * This software file is BONITASOFT CONFIDENTIAL. Not For Distribution.
+ * For commercial licensing information, contact:
+ * BonitaSoft, 32 rue Gustave Eiffel – 38000 Grenoble
+ * or BonitaSoft US, 51 Federal Street, Suite 305, San Francisco, CA 94107
+ *******************************************************************************/
+package com.bonitasoft.engine.api.impl.transaction.reporting;
+
+import org.bonitasoft.engine.commons.exceptions.SBonitaException;
+import org.bonitasoft.engine.commons.transaction.TransactionContentWithResult;
+
+import com.bonitasoft.engine.core.reporting.ReportingService;
+import com.bonitasoft.engine.service.TenantServiceAccessor;
+
+/**
+ * @author Matthieu Chaffotte
+ */
+public class GetReportContent implements TransactionContentWithResult<byte[]> {
+
+    private byte[] reportContent;
+
+    private final TenantServiceAccessor accessor;
+
+    private final long reportId;
+
+    public GetReportContent(final TenantServiceAccessor accessor, final long reportId) {
+        super();
+        this.accessor = accessor;
+        this.reportId = reportId;
+    }
+
+    @Override
+    public void execute() throws SBonitaException {
+        final ReportingService reportingService = accessor.getReportingService();
+        reportContent = reportingService.getReportContent(reportId);
+    }
+
+    @Override
+    public byte[] getResult() {
+        return reportContent;
+    }
+
+}
