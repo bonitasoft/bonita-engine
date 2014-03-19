85ef5460b697cf1b811539627ec1480f4e60c8a3
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2014-03-19 16:49:15 +0100

    BS-7731, thread were not stopped because of we never closed hibernate session factory
    Conflicts:
    	bpm/bonita-core/bonita-process-engine/src/main/java/com/bonitasoft/engine/api/impl/PlatformAPIExt.java

--- bpm/bonita-core/bonita-process-engine/src/main/java/com/bonitasoft/engine/api/impl/PlatformAPIExt.java
+++ bpm/bonita-core/bonita-process-engine/src/main/java/com/bonitasoft/engine/api/impl/PlatformAPIExt.java
@@ -304,12 +304,12 @@ public class PlatformAPIExt extends PlatformAPIImpl implements PlatformAPI {
 
             @Override
             public void deploy(String name, String description, byte[] screenShot, byte[] content) throws SBonitaException {
-            final ReportingService reportingService = tenantAccessor.getReportingService();
+                final ReportingService reportingService = tenantAccessor.getReportingService();
 		final SReportBuilder reportBuilder = BuilderFactory.get(SReportBuilderFactory.class).createNewInstance(name, /* system user */-1, true, description, screenShot);
-            final AddReport addReport = new AddReport(reportingService, reportBuilder.done(), content);
-            // Here we are already in a transaction, so we can call execute() directly:
-            addReport.execute();
-        }
+                final AddReport addReport = new AddReport(reportingService, reportBuilder.done(), content);
+                // Here we are already in a transaction, so we can call execute() directly:
+                addReport.execute();
+            }
         });
     }
 
@@ -338,6 +338,7 @@ public class PlatformAPIExt extends PlatformAPIImpl implements PlatformAPI {
             platformAccessor = ServiceAccessorFactory.getInstance().createPlatformServiceAccessor();
             final PlatformService platformService = platformAccessor.getPlatformService();
             final TransactionExecutor transactionExecutor = platformAccessor.getTransactionExecutor();
+            TechnicalLoggerService logger = platformAccessor.getTechnicalLoggerService();
 
             // delete tenant objects in database
             final TransactionContent transactionContentForTenantObjects = new DeleteTenantObjects(tenantId, platformService);
@@ -346,6 +347,12 @@ public class PlatformAPIExt extends PlatformAPIImpl implements PlatformAPI {
             // delete tenant in database
             final TransactionContent transactionContent = new DeleteTenant(tenantId, platformService);
             transactionExecutor.execute(transactionContent);
+
+            // stop tenant services and clear the spring context
+            TenantServiceAccessor tenantServiceAccessor = platformAccessor.getTenantServiceAccessor(tenantId);
+            stopServicesOfTenant(logger, tenantId, tenantServiceAccessor);
+            logger.log(getClass(), TechnicalLogSeverity.INFO, "Destroy tenant context of tenant " + tenantId);
+            tenantServiceAccessor.destroy();
 
             // delete tenant folder
             final String targetDir = BonitaHomeServer.getInstance().getTenantsFolder() + File.separator + tenantId;
