7c9993ccc2aaee154d2c1040c58feba7c460dbba
Romain Bioteau <romain.bioteau@bonitasoft.com>
2014-03-28 12:52:09 +0100

    BS-8133 Instantiate concrete class

--- bpm/bonita-core/bonita-process-engine/src/main/java/com/bonitasoft/engine/operation/UpdateBusinessDataOperationExecutorStrategy.java
+++ bpm/bonita-core/bonita-process-engine/src/main/java/com/bonitasoft/engine/operation/UpdateBusinessDataOperationExecutorStrategy.java
@@ -35,113 +35,125 @@ import com.bonitasoft.engine.core.process.instance.model.SRefBusinessDataInstanc
  */
 public class UpdateBusinessDataOperationExecutorStrategy implements OperationExecutorStrategy {
 
-    private final BusinessDataRepository businessDataRepository;
+	private static final String IMPL_SUFFIX = "Impl";
 
-    private final RefBusinessDataService refBusinessDataService;
+	private final BusinessDataRepository businessDataRepository;
 
-    private final FlowNodeInstanceService flowNodeInstanceService;
+	private final RefBusinessDataService refBusinessDataService;
 
-    public UpdateBusinessDataOperationExecutorStrategy(final BusinessDataRepository businessDataRepository,
-            final RefBusinessDataService refBusinessDataService, final FlowNodeInstanceService flowNodeInstanceService) {
-        super();
-        this.businessDataRepository = businessDataRepository;
-        this.refBusinessDataService = refBusinessDataService;
-        this.flowNodeInstanceService = flowNodeInstanceService;
-    }
+	private final FlowNodeInstanceService flowNodeInstanceService;
 
-    @Override
-    public Object getValue(final SOperation operation, final Object value, final long containerId, final String containerType,
-            final SExpressionContext expressionContext) throws SOperationExecutionException {
-        final String operator = getOperator(operation);
-        final String operatorParameterClassName = getOperatorParameterClassName(operation);
-        try {
-            final Object objectToInvokeJavaMethodOn = getBusinessDataObjectAndPutInContextIfNotAlready(containerId, containerType, expressionContext, operation
-                    .getLeftOperand().getName());
-            return new JavaMethodInvoker().invokeJavaMethod(operation.getRightOperand().getReturnType(), value, objectToInvokeJavaMethodOn, operator,
-                    operatorParameterClassName);
-        } catch (final Exception e) {
-            throw new SOperationExecutionException("Unable to evaluate operation " + operation, e);
-        }
-    }
+	public UpdateBusinessDataOperationExecutorStrategy(final BusinessDataRepository businessDataRepository,
+			final RefBusinessDataService refBusinessDataService, final FlowNodeInstanceService flowNodeInstanceService) {
+		super();
+		this.businessDataRepository = businessDataRepository;
+		this.refBusinessDataService = refBusinessDataService;
+		this.flowNodeInstanceService = flowNodeInstanceService;
+	}
 
-    protected String getOperatorParameterClassName(final SOperation operation) {
-        final String[] split = operation.getOperator().split(":", 2);
-        if (split.length > 1) {
-            return split[1];
-        }
-        return null;
-    }
+	@Override
+	public Object getValue(final SOperation operation, final Object value, final long containerId, final String containerType,
+			final SExpressionContext expressionContext) throws SOperationExecutionException {
+		final String operator = getOperator(operation);
+		final String operatorParameterClassName = getOperatorParameterClassName(operation);
+		try {
+			final Object objectToInvokeJavaMethodOn = getBusinessDataObjectAndPutInContextIfNotAlready(containerId, containerType, expressionContext, operation
+					.getLeftOperand().getName());
+			return new JavaMethodInvoker().invokeJavaMethod(operation.getRightOperand().getReturnType(), value, objectToInvokeJavaMethodOn, operator,
+					operatorParameterClassName);
+		} catch (final Exception e) {
+			throw new SOperationExecutionException("Unable to evaluate operation " + operation, e);
+		}
+	}
 
-    protected String getOperator(final SOperation operation) {
-        return operation.getOperator().split(":", 2)[0];
-    }
+	protected String getOperatorParameterClassName(final SOperation operation) {
+		final String[] split = operation.getOperator().split(":", 2);
+		if (split.length > 1) {
+			return split[1];
+		}
+		return null;
+	}
 
-    protected Object getBusinessDataObjectAndPutInContextIfNotAlready(final long containerId, final String containerType,
-            final SExpressionContext expressionContext, final String businessDataName) throws SOperationExecutionException, SFlowNodeNotFoundException,
-            SFlowNodeReadException {
-        final Map<String, Object> inputValues = expressionContext.getInputValues();
-        Object objectToInvokeJavaMethodOn = inputValues.get(businessDataName);
-        if (objectToInvokeJavaMethodOn == null) {
-            final long processInstanceId = flowNodeInstanceService.getProcessInstanceId(containerId, containerType);
-            objectToInvokeJavaMethodOn = getBusinessData(businessDataName, processInstanceId);
-            // put it in context for further reuse:
-            inputValues.put(businessDataName, objectToInvokeJavaMethodOn);
-        }
-        return objectToInvokeJavaMethodOn;
-    }
+	protected String getOperator(final SOperation operation) {
+		return operation.getOperator().split(":", 2)[0];
+	}
 
-    protected Object getBusinessData(final String bizDataName, final long processInstanceId) throws SOperationExecutionException {
-        try {
-            final SRefBusinessDataInstance refBusinessDataInstance = refBusinessDataService.getRefBusinessDataInstance(bizDataName, processInstanceId);
-            final Class<Entity> dataClass = (Class<Entity>) Thread.currentThread().getContextClassLoader()
-                    .loadClass(refBusinessDataInstance.getDataClassName());
-            final Long dataId = refBusinessDataInstance.getDataId();
-            if (dataId != null) {
-                return businessDataRepository.findById(dataClass, dataId);
-            } else {
-                return dataClass.newInstance();
-            }
-        } catch (final SBonitaReadException e) {
-            throw new SOperationExecutionException("Unable to retrieve business data instance with name " + bizDataName);
-        } catch (final Exception e) {
-            throw new SOperationExecutionException(e);
-        }
-    }
+	protected Object getBusinessDataObjectAndPutInContextIfNotAlready(final long containerId, final String containerType,
+			final SExpressionContext expressionContext, final String businessDataName) throws SOperationExecutionException, SFlowNodeNotFoundException,
+			SFlowNodeReadException {
+		final Map<String, Object> inputValues = expressionContext.getInputValues();
+		Object objectToInvokeJavaMethodOn = inputValues.get(businessDataName);
+		if (objectToInvokeJavaMethodOn == null) {
+			final long processInstanceId = flowNodeInstanceService.getProcessInstanceId(containerId, containerType);
+			objectToInvokeJavaMethodOn = getBusinessData(businessDataName, processInstanceId);
+			// put it in context for further reuse:
+			inputValues.put(businessDataName, objectToInvokeJavaMethodOn);
+		}
+		return objectToInvokeJavaMethodOn;
+	}
 
-    @Override
-    public void update(final SLeftOperand sLeftOperand, Object newValue, final long containerId, final String containerType)
-            throws SOperationExecutionException {
-        checkIsBusinessData(newValue);
-        try {
-            Entity businessData = businessDataRepository.merge((Entity) newValue);
-            SRefBusinessDataInstance refBusinessDataInstance = getRefBusinessDataInstance(sLeftOperand.getName(), containerId, containerType);
-            if (refBusinessDataInstance != null) {
-                refBusinessDataService.updateRefBusinessDataInstance(refBusinessDataInstance, businessData.getPersistenceId());
-            }
-        } catch (final SBonitaException e) {
-            throw new SOperationExecutionException(e);
-        }
-    }
+	@SuppressWarnings("unchecked")
+	protected Object getBusinessData(final String bizDataName, final long processInstanceId) throws SOperationExecutionException {
+		try {
+			final SRefBusinessDataInstance refBusinessDataInstance = refBusinessDataService.getRefBusinessDataInstance(bizDataName, processInstanceId);
+			String dataClassName = refBusinessDataInstance.getDataClassName();
+			final Class<Entity> dataClass = (Class<Entity>) Thread.currentThread().getContextClassLoader()
+					.loadClass(dataClassName);
+			final Long dataId = refBusinessDataInstance.getDataId();
+			if (dataId != null) {
+				return businessDataRepository.findById(dataClass, dataId);
+			} else {
+				Class<Entity> implClass = dataClass;
+				if(implClass.isInterface()){
+					implClass = (Class<Entity>) Thread.currentThread().getContextClassLoader()
+							.loadClass(dataClassName+IMPL_SUFFIX);
+				}
+				if(implClass.isInterface()){
+					throw new SOperationExecutionException("Unable to find the concrete implementation class for interface "+dataClass.getName());
+				}
+				return implClass.newInstance();
+			}
+		} catch (final SBonitaReadException e) {
+			throw new SOperationExecutionException("Unable to retrieve business data instance with name " + bizDataName);
+		} catch (final Exception e) {
+			throw new SOperationExecutionException(e);
+		}
+	}
 
-    private SRefBusinessDataInstance getRefBusinessDataInstance(final String businessDataName, final long containerId, final String containerType)
-            throws SFlowNodeNotFoundException, SFlowNodeReadException, SRefBusinessDataInstanceNotFoundException, SBonitaReadException {
-        long processInstanceId = flowNodeInstanceService.getProcessInstanceId(containerId, containerType);
-        return refBusinessDataService.getRefBusinessDataInstance(businessDataName, processInstanceId);
-    }
+	@Override
+	public void update(final SLeftOperand sLeftOperand, Object newValue, final long containerId, final String containerType)
+			throws SOperationExecutionException {
+		checkIsBusinessData(newValue);
+		try {
+			Entity businessData = businessDataRepository.merge((Entity) newValue);
+			SRefBusinessDataInstance refBusinessDataInstance = getRefBusinessDataInstance(sLeftOperand.getName(), containerId, containerType);
+			if (refBusinessDataInstance != null) {
+				refBusinessDataService.updateRefBusinessDataInstance(refBusinessDataInstance, businessData.getPersistenceId());
+			}
+		} catch (final SBonitaException e) {
+			throw new SOperationExecutionException(e);
+		}
+	}
 
-    private void checkIsBusinessData(Object newValue) throws SOperationExecutionException {
-        if (!(newValue instanceof Entity)) {
-            throw new SOperationExecutionException(new IllegalStateException(newValue.getClass().getName() + " must implements " + Entity.class.getName()));
-        }
-    }
+	private SRefBusinessDataInstance getRefBusinessDataInstance(final String businessDataName, final long containerId, final String containerType)
+			throws SFlowNodeNotFoundException, SFlowNodeReadException, SRefBusinessDataInstanceNotFoundException, SBonitaReadException {
+		long processInstanceId = flowNodeInstanceService.getProcessInstanceId(containerId, containerType);
+		return refBusinessDataService.getRefBusinessDataInstance(businessDataName, processInstanceId);
+	}
 
-    @Override
-    public String getOperationType() {
-        return OperatorType.BUSINESS_DATA_JAVA_SETTER.name();
-    }
+	private void checkIsBusinessData(Object newValue) throws SOperationExecutionException {
+		if (!(newValue instanceof Entity)) {
+			throw new SOperationExecutionException(new IllegalStateException(newValue.getClass().getName() + " must implements " + Entity.class.getName()));
+		}
+	}
 
-    @Override
-    public boolean shouldPerformUpdateAtEnd() {
-        return true;
-    }
+	@Override
+	public String getOperationType() {
+		return OperatorType.BUSINESS_DATA_JAVA_SETTER.name();
+	}
+
+	@Override
+	public boolean shouldPerformUpdateAtEnd() {
+		return true;
+	}
 }
