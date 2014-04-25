a9b191bf4a845f3755a8e767ec84b88f47bbfd90
Colin PUY <colin.puy@bonitasoft.com>
2014-04-25 15:58:00 +0200

    BS-2765, fix test

--- bpm/bonita-bpm-integration-tests/bonita-integration-client-tests/src/main/java/com/bonitasoft/engine/reporting/ReportingSQLValidityIT.java
+++ bpm/bonita-bpm-integration-tests/bonita-integration-client-tests/src/main/java/com/bonitasoft/engine/reporting/ReportingSQLValidityIT.java
@@ -1,6 +1,5 @@
 package com.bonitasoft.engine.reporting;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -13,8 +12,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import com.bonitasoft.engine.CommonAPISPTest;
 
@@ -26,31 +23,22 @@ import com.bonitasoft.engine.CommonAPISPTest;
 @RunWith(Parameterized.class)
 public class ReportingSQLValidityIT extends CommonAPISPTest {
 
-    @Parameters(name = "{1}")
+    @Parameters(name = "{0}")
     public static Collection<Object[]> params() throws URISyntaxException, IOException {
         ArrayList<Object[]> params = new ArrayList<Object[]>();
-        for (Resource resource : getSqlTestsFiles(params)) {
-            params.add(new Object[] { resource.getFile(), resource.getFile().getName() });
-        }
+        params.add(new Object[] { "AppName_test.sql" });
+        params.add(new Object[] { "CaseAvgTime_test.sql" });
+        params.add(new Object[] { "CaseHistory_test.sql" });
+        params.add(new Object[] { "CaseList_test.sql" });
+        params.add(new Object[] { "TaskList_test.sql" });
         return params;
     }
-
-    private static Resource[] getSqlTestsFiles(ArrayList<Object[]> list) throws URISyntaxException, IOException {
-        String reportingPackage = ReportingSQLValidityIT.class.getPackage().getName().replace(".", File.separator);
-        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        String locationPattern = "classpath*:" + reportingPackage + File.separator + "*_test.sql";
-        Resource[] resources = resolver.getResources(locationPattern);
-        if (resources.length == 0) {
-            throw new RuntimeException("No sql test files found");
-        }
-        return resources;
-    }
-
+    
     private String sql;
 
-    /* FileName is not used, only for testName printing */
-    public ReportingSQLValidityIT(File sqlFile, String fileName) throws IOException {
-        this.sql = IOUtils.toString(sqlFile.toURI());
+    public ReportingSQLValidityIT(String filename) throws IOException {
+        String sql = IOUtils.toString(ReportingSQLValidityIT.class.getResourceAsStream(filename));
+        this.sql = sql;
     }
 
     @Before
