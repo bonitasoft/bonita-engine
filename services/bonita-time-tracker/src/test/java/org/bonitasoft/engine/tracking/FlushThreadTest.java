5871582e8023bd7efb8e0d1db631a7dc0c5b266b
Charles Souillard <charles.souillard@bonitasoft.com>
2014-04-28 11:22:45 +0200

    fix wait time in FlusThreadTest

--- services/bonita-time-tracker/src/test/java/org/bonitasoft/engine/tracking/FlushThreadTest.java
+++ services/bonita-time-tracker/src/test/java/org/bonitasoft/engine/tracking/FlushThreadTest.java
@@ -15,14 +15,14 @@ public class FlushThreadTest {
     public void should_flush_thread_flush_until_interruption() throws Exception {
         final TimeTracker timeTracker = mock(TimeTracker.class);
         final TechnicalLoggerService logger = mock(TechnicalLoggerService.class);
-        final long flushIntervalInMilliSeconds = 1000;
+        final long flushIntervalInMilliSeconds = 10;
 
         final Clock clock = mock(Clock.class);
         when(clock.sleep(flushIntervalInMilliSeconds)).thenReturn(true).thenReturn(true).thenReturn(true).thenThrow(InterruptedException.class);
         final FlushThread flushThread = new FlushThread(clock, flushIntervalInMilliSeconds, timeTracker, logger);
         flushThread.start();
-
-        flushThread.join(flushIntervalInMilliSeconds * 60000);
+        // wait max 1 minute to not freeze CI in case of a bug
+        flushThread.join(60000);
         verify(timeTracker, times(3)).flush();
         verify(clock, times(4)).sleep(flushIntervalInMilliSeconds);
     }
