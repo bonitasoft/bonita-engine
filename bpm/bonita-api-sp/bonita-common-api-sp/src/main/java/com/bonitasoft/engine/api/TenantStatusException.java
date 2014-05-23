2f6939fa4f69ae6c1a68dea6e835f9cb9a3b9ac6
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2014-05-23 16:56:14 +0200

    BS-8852, TenantIsPausedException (name + message) is confusing on paused-only API method. Created TenantStatusException instead, for both types of error, with specific message

--- bpm/bonita-api-sp/bonita-common-api-sp/src/main/java/com/bonitasoft/engine/api/TenantStatusException.java
+++ bpm/bonita-api-sp/bonita-common-api-sp/src/main/java/com/bonitasoft/engine/api/TenantStatusException.java
@@ -11,17 +11,20 @@ package com.bonitasoft.engine.api;
 import org.bonitasoft.engine.exception.BonitaRuntimeException;
 
 /**
+ * Thrown when we try to access on a paused tenant an API method that cannot be called on a paused tenant, or when we try to access on a running tenant an API
+ * method that cannot be called on a running tenant.
+ * 
  * @author Emmanuel Duchastenier
  */
-public class TenantModeException extends BonitaRuntimeException {
+public class TenantStatusException extends BonitaRuntimeException {
 
     private static final long serialVersionUID = 1L;
 
     /**
      * @param message
-     *            the exception message
+     *        the exception message
      */
-    public TenantModeException(final String message) {
+    public TenantStatusException(final String message) {
         super(message);
     }
 
