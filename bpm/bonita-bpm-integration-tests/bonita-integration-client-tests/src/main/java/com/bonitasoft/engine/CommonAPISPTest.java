dfa53ba373dbde4b2b50d318ced37de1b1f3eb93
Aurelien Pupier <aurelien.pupier@bonitasoft.com>
2014-06-03 11:27:07 +0200

    When a test let the system in pause, we fail the test and resume the tenant

--- bpm/bonita-bpm-integration-tests/bonita-integration-client-tests/src/main/java/com/bonitasoft/engine/CommonAPISPTest.java
+++ bpm/bonita-bpm-integration-tests/bonita-integration-client-tests/src/main/java/com/bonitasoft/engine/CommonAPISPTest.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (C) 2009, 2013 BonitaSoft S.A.
- * BonitaSoft is a trademark of BonitaSoft SA.
+ * Copyright (C) 2009, 2014 Bonitasoft S.A.
+ * BonitaSoft is a trademark of Bonitasoft SA.
  * This software file is BONITASOFT CONFIDENTIAL. Not For Distribution.
  * For commercial licensing information, contact:
- * BonitaSoft, 32 rue Gustave Eiffel – 38000 Grenoble
- * or BonitaSoft US, 51 Federal Street, Suite 305, San Francisco, CA 94107
+ * Bonitasoft, 32 rue Gustave Eiffel – 38000 Grenoble
+ * or Bonitasoft US, 51 Federal Street, Suite 305, San Francisco, CA 94107
  *******************************************************************************/
 package com.bonitasoft.engine;
 
@@ -89,6 +89,10 @@ public abstract class CommonAPISPTest extends APITestSPUtil {
 
         for (final Tenant tenant : tenants) {
             loginOnTenantWithTechnicalLogger(tenant.getId());
+            if(getTenantManagementAPI().isPaused()){
+            	messages.add("Tenant was in paused state");
+            	getTenantManagementAPI().resume();
+            }
             messages.addAll(checkNoCommands());
             messages.addAll(checkNoUsers());
             messages.addAll(checkNoGroups());
