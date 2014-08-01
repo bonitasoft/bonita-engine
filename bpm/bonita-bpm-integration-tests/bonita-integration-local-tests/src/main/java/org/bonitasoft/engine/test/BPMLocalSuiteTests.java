80176cfce623eec9ae84e7697895cbbf27e06682
Elias Ricken de Medeiros <elias.ricken@bonitasoft.com>
2014-08-01 13:03:56 +0200

    BS-9437, move all 'boundary not triggered' tests to local integration tests, in this way we can check if the job was deleted instead of waiting.

--- bpm/bonita-bpm-integration-tests/bonita-integration-local-tests/src/main/java/org/bonitasoft/engine/test/BPMLocalSuiteTests.java
+++ bpm/bonita-bpm-integration-tests/bonita-integration-local-tests/src/main/java/org/bonitasoft/engine/test/BPMLocalSuiteTests.java
@@ -3,6 +3,7 @@ package org.bonitasoft.engine.test;
 import org.bonitasoft.engine.BonitaSuiteRunner;
 import org.bonitasoft.engine.BonitaSuiteRunner.Initializer;
 import org.bonitasoft.engine.TestsInitializer;
+import org.bonitasoft.engine.event.LocalInterruptingTimerBoundaryEventTest;
 import org.bonitasoft.engine.event.LocalTimerEventTest;
 import org.bonitasoft.engine.job.JobExecutionTest;
 import org.bonitasoft.engine.theme.ThemeTest;
@@ -11,15 +12,16 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(BonitaSuiteRunner.class)
 @SuiteClasses({
-        BPMLocalTest.class,
-        ConnectorExecutionsTestsLocal.class,
-        ProcessWithExpressionTestLocal.class,
-        ProcessArchiveTest.class,
-        LocalTimerEventTest.class,
-        DataInstanceIntegrationLocalTest.class,
-        GatewayExecutionLocalTest.class,
-        JobExecutionTest.class,
-        ThemeTest.class
+    BPMLocalTest.class,
+    ConnectorExecutionsTestsLocal.class,
+    ProcessWithExpressionTestLocal.class,
+    ProcessArchiveTest.class,
+    LocalTimerEventTest.class,
+    LocalInterruptingTimerBoundaryEventTest.class,
+    DataInstanceIntegrationLocalTest.class,
+    GatewayExecutionLocalTest.class,
+    JobExecutionTest.class,
+    ThemeTest.class
 })
 @Initializer(TestsInitializer.class)
 public class BPMLocalSuiteTests {
