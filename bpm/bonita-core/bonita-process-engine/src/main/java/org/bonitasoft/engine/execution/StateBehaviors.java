646a77c993da63191c7a22a5f6d6c7765e9a3509
Fabio <fabio.lombardi@bonitasoft.com>
2014-08-07 10:31:39 +0200

    BS-9294 [6.2.6] Comment gives task name instead of Display Name in the comments section

--- bpm/bonita-core/bonita-process-engine/src/main/java/org/bonitasoft/engine/execution/StateBehaviors.java
+++ bpm/bonita-core/bonita-process-engine/src/main/java/org/bonitasoft/engine/execution/StateBehaviors.java
@@ -329,7 +329,7 @@ public class StateBehaviors {
 
             final SUser user = identityService.getUser(userId);
             if (commentService.isCommentEnabled(SystemCommentType.STATE_CHANGE)) {
-                commentService.addSystemComment(flowNodeInstance.getRootContainerId(), "The task \"" + flowNodeInstance.getName() + "\" is now assigned to "
+                commentService.addSystemComment(flowNodeInstance.getRootContainerId(), "The task \"" + flowNodeInstance.getDisplayName() + "\" is now assigned to "
                         + user.getUserName());
             }
         }
