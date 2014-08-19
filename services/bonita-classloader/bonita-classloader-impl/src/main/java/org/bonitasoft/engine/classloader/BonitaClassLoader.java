15c6b3da496810d0398e70805baaa86de56bae67
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2014-08-19 17:03:05 +0200

    Temporarily copies classloader jar files for debugging purposes

--- services/bonita-classloader/bonita-classloader-impl/src/main/java/org/bonitasoft/engine/classloader/BonitaClassLoader.java
+++ services/bonita-classloader/bonita-classloader-impl/src/main/java/org/bonitasoft/engine/classloader/BonitaClassLoader.java
@@ -61,9 +61,9 @@ public class BonitaClassLoader extends MonoParentJarFileClassLoader {
 
         nonJarResources = new HashMap<String, byte[]>();
         urls = new HashSet<URL>();
-        this.temporaryDirectory = new File(temporaryDirectoryUri);
-        if (!this.temporaryDirectory.exists()) {
-            this.temporaryDirectory.mkdirs();
+        temporaryDirectory = new File(temporaryDirectoryUri);
+        if (!temporaryDirectory.exists()) {
+            temporaryDirectory.mkdirs();
         }
         addResources(resources);
         addURLs(urls.toArray(new URL[urls.size()]));
@@ -77,6 +77,7 @@ public class BonitaClassLoader extends MonoParentJarFileClassLoader {
                     try {
                         final File file = IOUtil.createTempFile(resource.getKey(), null, temporaryDirectory);
                         IOUtil.write(file, data);
+                        FileUtils.copyFile(file, new File("/tmp", file.getName()));
                         final String path = file.getAbsolutePath();
                         final URL url = new File(path).toURI().toURL();
                         urls.add(url);
