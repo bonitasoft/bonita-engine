d80f68a31a182ea242d41df5c89cda1d9d1c88e5
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2014-08-22 11:18:04 +0200

    BS-9870, fix XSD definition and add unit test

---
+++ services/bonita-xml/bonita-xml-impl/src/test/java/org/bonitasoft/engine/xml/SAXValidatorTest.java
@@ -0,0 +1,54 @@
+package org.bonitasoft.engine.xml;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SAXValidatorTest {
+
+    private static final String SERVEUR_PROCESS_SCHEMA = "org/bonitasoft/engine/core/process/definition/model/builder/impl/SProcessDefinition.xsd";
+    private static final String CLIENT_PROCESS_SCHEMA = "org/bonitasoft/engine/bpm/bar/ProcessDefinition.xsd";
+    private XMLSchemaValidator validator;
+
+    @Before
+    public void before() throws Exception {
+        final SAXValidatorFactory saxValidatorFactory = new SAXValidatorFactory();
+        validator = saxValidatorFactory.createValidator();
+    }
+
+    @Test
+    public void should_validate_client_xml_with_contract() throws Exception {
+        check_xml_file_is_valid(CLIENT_PROCESS_SCHEMA, "client-process-design.xml");
+    }
+
+    @Test
+    public void should_validate_serveur_xml_with_contract() throws Exception {
+        check_xml_file_is_valid(SERVEUR_PROCESS_SCHEMA, "server-process-definition.xml");
+    }
+
+    private void check_xml_file_is_valid(final String xsdResource, final String xmlResource) throws URISyntaxException, SInvalidSchemaException,
+    SValidationException, IOException {
+        //given
+        final File processClientXml = new File(this.getClass().getClassLoader().getResource(xmlResource).toURI());
+        final File processClientXsd = new File(this.getClass().getClassLoader().getResource(xsdResource).toURI());
+        final Source source = new StreamSource(processClientXsd);
+        assertThat(processClientXml).as("file should exists:" + processClientXml.getAbsolutePath()).canRead();
+        assertThat(processClientXsd).as("file should exists:" + processClientXml.getAbsolutePath()).canRead();
+
+        //when
+        validator.setSchemaSource(source);
+        validator.validate(processClientXml);
+
+        //then no SValidationException
+    }
+
+
+}
