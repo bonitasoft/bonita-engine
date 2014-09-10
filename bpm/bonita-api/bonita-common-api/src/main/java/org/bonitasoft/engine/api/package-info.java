a6746a004d4404b722b21c38d804c0adbf9c83f8
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2014-09-10 11:23:46 +0200

    change package.html to package-info.java

---
+++ bpm/bonita-api/bonita-common-api/src/main/java/org/bonitasoft/engine/api/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ *     <p>
+ *         The Bonita BPM project has a clear separation between the API the users should be interacting with and the actual implementation classes.
+ *         <br />
+ *         The public API exposes most of the features we believe "normal" users can safely use and should remain rather stable across releases.
+ *         <br />
+ *         Expert users can still access internal classes but should be aware that they should know what they are doing and that the internal API might still change in the future.
+ *     </p>
+ * 
+ */
+package org.bonitasoft.engine.api;
