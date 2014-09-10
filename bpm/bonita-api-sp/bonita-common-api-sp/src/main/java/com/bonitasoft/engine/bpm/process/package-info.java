322925c2d1af878195f9bbd8253d30487403d27c
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2014-09-10 11:23:56 +0200

    change package.html to package-info.java

---
+++ bpm/bonita-api-sp/bonita-common-api-sp/src/main/java/com/bonitasoft/engine/bpm/process/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ *     <p>
+ *         Package com.bonitasoft.engine.bpm.process gathers all model interfaces and exceptions related to process instance / definition objects manipulated
+ *         by process APIs, and specific to Bonita BPM Subscription Edition.
+ *     </p>
+ * 
+ */
+package com.bonitasoft.engine.bpm.process;
