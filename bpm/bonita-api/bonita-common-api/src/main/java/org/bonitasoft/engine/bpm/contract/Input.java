5f4003cd262f27af8954f907f692020ff83aa17f
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2014-09-11 17:55:54 +0200

    BS-10116, add toString for testing usage

--- bpm/bonita-api/bonita-common-api/src/main/java/org/bonitasoft/engine/bpm/contract/Input.java
+++ bpm/bonita-api/bonita-common-api/src/main/java/org/bonitasoft/engine/bpm/contract/Input.java
@@ -5,18 +5,23 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.bonitasoft.engine.bpm.contract;
 
 public class Input {
+
+    @Override
+    public String toString() {
+        return "Input [name=" + name + ", value=" + value + "]";
+    }
 
     private String name;
     private Object value;
@@ -25,8 +30,8 @@ public class Input {
     private Input() {
         // Empty private constructor for json serialization
     }
-    
-    public Input(String name, Object value) {
+
+    public Input(final String name, final Object value) {
         this.name = name;
         this.value = value;
     }
@@ -43,30 +48,37 @@ public class Input {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        result = prime * result + (name == null ? 0 : name.hashCode());
+        result = prime * result + (value == null ? 0 : value.hashCode());
         return result;
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
+    public boolean equals(final Object obj) {
+        if (this == obj) {
             return true;
-        if (obj == null)
+        }
+        if (obj == null) {
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (getClass() != obj.getClass()) {
             return false;
-        Input other = (Input) obj;
+        }
+        final Input other = (Input) obj;
         if (name == null) {
-            if (other.name != null)
+            if (other.name != null) {
                 return false;
-        } else if (!name.equals(other.name))
+            }
+        } else if (!name.equals(other.name)) {
             return false;
+        }
         if (value == null) {
-            if (other.value != null)
+            if (other.value != null) {
                 return false;
-        } else if (!value.equals(other.value))
+            }
+        } else if (!value.equals(other.value)) {
             return false;
+        }
         return true;
     }
 }
