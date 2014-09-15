dd7520f0523b1f8244a33c8f4d4587f11df6cf63
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2014-09-15 16:33:03 +0200

    BS-10195, fix missing import

--- bonita-integration-tests/bonita-integration-tests-client/src/main/java/org/bonitasoft/engine/activity/UserTaskContractTest.java
+++ bonita-integration-tests/bonita-integration-tests-client/src/main/java/org/bonitasoft/engine/activity/UserTaskContractTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.fail;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
