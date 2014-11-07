94627f4ea241a558e8df2efc7e9455f4b05b5c7d
Matthieu Chaffotte <matthieu.chaffotte@bonitasoft.com>
2014-11-07 15:59:11 +0100

    BS-10568, Update Command to support lazy mode

--- bpm/bonita-core/bonita-process-engine/src/main/java/com/bonitasoft/engine/command/GetBusinessDataByIdCommand.java
+++ bpm/bonita-core/bonita-process-engine/src/main/java/com/bonitasoft/engine/command/GetBusinessDataByIdCommand.java
@@ -8,20 +8,33 @@
  *******************************************************************************/
 package com.bonitasoft.engine.command;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.io.StringWriter;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.bonitasoft.engine.bpm.data.DataNotFoundException;
 import org.bonitasoft.engine.command.SCommandExecutionException;
 import org.bonitasoft.engine.command.SCommandParameterizationException;
 
+import com.bonitasoft.engine.api.rest.Link;
 import com.bonitasoft.engine.bdm.Entity;
 import com.bonitasoft.engine.business.data.BusinessDataRepository;
 import com.bonitasoft.engine.business.data.SBusinessDataNotFoundException;
 import com.bonitasoft.engine.service.TenantServiceAccessor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 /**
  * @author Matthieu Chaffotte
@@ -32,23 +45,40 @@ public class GetBusinessDataByIdCommand extends TenantCommand {
 
     public static final String BUSINESS_DATA_ID = "businessDataId";
 
+    public static final String BUSINESS_DATA_URI_PATTERN = "businessDataURIPattern";
+
+    public static final String BUSINESS_DATA_CHILD_NAME = "businessDataChildName";
+
+    private final ObjectMapper mapper;
+
+    public GetBusinessDataByIdCommand() {
+        mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+    }
+
+    protected GetBusinessDataByIdCommand(final ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
     @Override
     public Serializable execute(final Map<String, Serializable> parameters, final TenantServiceAccessor serviceAccessor)
             throws SCommandParameterizationException, SCommandExecutionException {
-        final BusinessDataRepository businessDataRepository = getBusinessDataRepository(serviceAccessor);
+        final BusinessDataRepository businessDataRepository = serviceAccessor.getBusinessDataRepository();
         final Long identifier = getLongMandadoryParameter(parameters, BUSINESS_DATA_ID);
         final String entityClassName = getStringMandadoryParameter(parameters, ENTITY_CLASS_NAME);
+        final String businessDataURIPattern = getStringMandadoryParameter(parameters, BUSINESS_DATA_URI_PATTERN);
         final Class<? extends Entity> entityClass = loadClass(entityClassName);
         try {
             final Entity entity = businessDataRepository.findById(entityClass, identifier);
-            return serializeResult(entity);
+            final String childName = getParameter(parameters, BUSINESS_DATA_CHILD_NAME);
+            if (childName != null && !childName.isEmpty()) {
+                return serializeChildResult(entity, childName, businessDataURIPattern);
+            } else {
+                return serializeResult(entity, businessDataURIPattern);
+            }
         } catch (final SBusinessDataNotFoundException sbdnfe) {
             throw new SCommandExecutionException(new DataNotFoundException(sbdnfe));
         }
-    }
-
-    protected BusinessDataRepository getBusinessDataRepository(final TenantServiceAccessor serviceAccessor) {
-        return serviceAccessor.getBusinessDataRepository();
     }
 
     @SuppressWarnings("unchecked")
@@ -60,14 +90,90 @@ public class GetBusinessDataByIdCommand extends TenantCommand {
         }
     }
 
-    private String serializeResult(final Serializable result) throws SCommandExecutionException {
-        final ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+    private String serializeResult(final Entity entity, final String businessDataURIPattern) throws SCommandExecutionException {
+        final List<Link> links = buildLinks(entity, businessDataURIPattern);
+        final JsonNode jsonNode = buildJsonNode(entity, links);
+        return serializeResult(jsonNode);
+    }
+
+    private JsonNode buildJsonNode(final Entity result, final List<Link> links) {
+        final JsonNode jsonNode = mapper.valueToTree(result);
+        if (!links.isEmpty()) {
+            final ArrayNode linksNode = ((ObjectNode) jsonNode).putArray("links");
+            for (final Link link : links) {
+                linksNode.addPOJO(link);
+            }
+        }
+        return jsonNode;
+    }
+
+    private List<Link> buildLinks(final Entity result, final String businessDataURIPattern) {
+        final List<Link> links = new ArrayList<Link>();
+        for (final Field entityField : result.getClass().getDeclaredFields()) {
+            if (entityField.getAnnotation(JsonIgnore.class) != null) {
+                final String uri = buildURI(result, businessDataURIPattern, entityField);
+                final Link link = new Link(entityField.getName(), uri);
+                links.add(link);
+            }
+        }
+        return links;
+    }
+
+    private String buildURI(final Entity result, final String businessDataURIPattern, final Field entityField) {
+        String uri = businessDataURIPattern.replace("{className}", result.getClass().getName());
+        uri = uri.replace("{id}", result.getPersistenceId().toString());
+        return uri.replace("{field}", entityField.getName());
+    }
+
+    private String serializeResult(final JsonNode jsonNode) throws SCommandExecutionException {
         try {
-            return mapper.writeValueAsString(result);
+            final StringWriter writer = new StringWriter();
+            mapper.writeValue(writer, jsonNode);
+            return writer.toString();
         } catch (final JsonProcessingException jpe) {
             throw new SCommandExecutionException(jpe);
+        } catch (final IOException ioe) {
+            throw new SCommandExecutionException(ioe);
         }
+    }
+
+    private String serializeChildResult(final Entity entity, final String fieldName, final String businessDataURIPattern) throws SCommandExecutionException {
+        final Method method;
+        final Object invoke;
+        try {
+            final String getterName = buildGetterMethodName(fieldName);
+            method = entity.getClass().getMethod(getterName);
+            invoke = method.invoke(entity);
+        } catch (final Exception e) {
+            throw new SCommandExecutionException(fieldName + " is not a valid attribute of entity " + entity.getClass(), e);
+        }
+
+        if (invoke instanceof Entity) {
+            return serializeResult((Entity) invoke, businessDataURIPattern);
+        } else {
+            if (invoke instanceof List) {
+            final Class<?> type = (Class<?>) ((ParameterizedType) method.getGenericReturnType()).getActualTypeArguments()[0];
+            if (Entity.class.isAssignableFrom(type)) {
+                final ArrayNode arrayNode = new ArrayNode(JsonNodeFactory.instance);
+                final List<Entity> entities = (List<Entity>) invoke;
+                for (final Entity entity2 : entities) {
+                    final List<Link> links = buildLinks(entity2, businessDataURIPattern);
+                    final JsonNode jsonNode = buildJsonNode(entity2, links);
+                    arrayNode.add(jsonNode);
+                }
+                return serializeResult(arrayNode);
+                }
+            }
+        }
+        throw new SCommandExecutionException("the type of " + fieldName + " must be an instance of either " + Entity.class.getName() + " or "
+                + List.class.getName() + " of " + Entity.class.getName());
+    }
+
+    private String buildGetterMethodName(final String fieldName) {
+        final StringBuilder builder = new StringBuilder("get");
+        builder.append(fieldName.substring(0, 1).toUpperCase());
+        builder.append(fieldName.substring(1));
+        return builder.toString();
     }
 
 }
--- bpm/bonita-core/bonita-process-engine/src/test/java/com/bonitasoft/engine/command/GetBusinessDataByIdCommandTest.java
+++ bpm/bonita-core/bonita-process-engine/src/test/java/com/bonitasoft/engine/command/GetBusinessDataByIdCommandTest.java
@@ -1,16 +1,30 @@
+/*******************************************************************************
+ * Copyright (C) 2014 Bonitasoft S.A.
+ * Bonitasoft is a trademark of Bonitasoft SA.
+ * This software file is BONITASOFT CONFIDENTIAL. Not For Distribution.
+ * For commercial licensing information, contact:
+ * Bonitasoft, 32 rue Gustave Eiffel 38000 Grenoble
+ * or Bonitasoft US, 51 Federal Street, Suite 305, San Francisco, CA 94107
+ *******************************************************************************/
 package com.bonitasoft.engine.command;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.io.Writer;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 import net.javacrumbs.jsonunit.assertj.JsonAssert;
 
+import org.apache.commons.io.IOUtils;
 import org.bonitasoft.engine.bpm.data.DataNotFoundException;
 import org.bonitasoft.engine.command.SCommandExecutionException;
 import org.bonitasoft.engine.command.SCommandParameterizationException;
@@ -18,18 +32,24 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.bonitasoft.engine.business.data.BusinessDataRepository;
 import com.bonitasoft.engine.business.data.SBusinessDataNotFoundException;
 import com.bonitasoft.engine.operation.pojo.Travel;
+import com.bonitasoft.engine.pojo.AddressBook;
+import com.bonitasoft.engine.pojo.Command;
+import com.bonitasoft.engine.pojo.CommandLine;
+import com.bonitasoft.engine.pojo.NameList;
+import com.bonitasoft.engine.pojo.Product;
 import com.bonitasoft.engine.service.TenantServiceAccessor;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GetBusinessDataByIdCommandTest {
 
-    @Spy
     private GetBusinessDataByIdCommand command;
 
     @Mock
@@ -40,7 +60,8 @@ public class GetBusinessDataByIdCommandTest {
 
     @Before
     public void setUp() throws Exception {
-        doReturn(bdrService).when(command).getBusinessDataRepository(tenantServiceAccessor);
+        command = new GetBusinessDataByIdCommand();
+        when(tenantServiceAccessor.getBusinessDataRepository()).thenReturn(bdrService);
     }
 
     @Test
@@ -48,6 +69,7 @@ public class GetBusinessDataByIdCommandTest {
         final Map<String, Serializable> parameters = new HashMap<String, Serializable>();
         parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_ID, 1983L);
         parameters.put(GetBusinessDataByIdCommand.ENTITY_CLASS_NAME, Travel.class.getName());
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_URI_PATTERN, "/businessdata/{className}/{id}/{field}");
         final Travel travel = new Travel();
         travel.setNbDays(45);
         when(bdrService.findById(Travel.class, 1983L)).thenReturn(travel);
@@ -61,6 +83,7 @@ public class GetBusinessDataByIdCommandTest {
         final Map<String, Serializable> parameters = new HashMap<String, Serializable>();
         parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_ID, 1983L);
         parameters.put(GetBusinessDataByIdCommand.ENTITY_CLASS_NAME, "com.bonitasoft.Employee");
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_URI_PATTERN, "/businessdata/{className}/{id}/{field}");
 
         command.execute(parameters, tenantServiceAccessor);
     }
@@ -70,6 +93,7 @@ public class GetBusinessDataByIdCommandTest {
         final Map<String, Serializable> parameters = new HashMap<String, Serializable>();
         parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_ID, 1983L);
         parameters.put(GetBusinessDataByIdCommand.ENTITY_CLASS_NAME, Travel.class.getName());
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_URI_PATTERN, "/businessdata/{className}/{id}/{field}");
         when(bdrService.findById(Travel.class, 1983L)).thenThrow(new SBusinessDataNotFoundException("not found"));
 
         try {
@@ -78,6 +102,152 @@ public class GetBusinessDataByIdCommandTest {
         } catch (final SCommandExecutionException scee) {
             assertThat(scee.getCause()).isInstanceOf(DataNotFoundException.class);
         }
+    }
+
+    @Test
+    public void should_get_the_business_data_in_lazymode_based_on_its_identifier() throws Exception {
+        final Map<String, Serializable> parameters = new HashMap<String, Serializable>();
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_ID, 1983L);
+        parameters.put(GetBusinessDataByIdCommand.ENTITY_CLASS_NAME, AddressBook.class.getName());
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_URI_PATTERN, "/businessdata/{className}/{id}/{field}");
+        final AddressBook addressBook = new AddressBook();
+        when(bdrService.findById(AddressBook.class, 1983L)).thenReturn(addressBook);
+
+        final String json = (String) command.execute(parameters, tenantServiceAccessor);
+
+        final String expectedJson = IOUtils.toString(GetBusinessDataByIdCommandTest.class.getResource("/rest/getProxyAddressBook.json"));
+        JsonAssert.assertThatJson(json).isEqualTo(expectedJson);
+    }
+
+    @Test
+    public void execute_should_return_the_list_child_of_an_entity() throws Exception {
+        final Map<String, Serializable> parameters = new HashMap<String, Serializable>();
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_ID, 1983L);
+        parameters.put(GetBusinessDataByIdCommand.ENTITY_CLASS_NAME, Command.class.getName());
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_URI_PATTERN, "/businessdata/{className}/{id}/{field}");
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_CHILD_NAME, "lines");
+        final Product pencil = new Product(687L, 1L, "Pencil");
+        final Product paper = new Product(688L, 12L, "Paper");
+        final Command command1 = new Command(1983L, 0L);
+        command1.addLine(new CommandLine(864L, 78L, pencil, 5));
+        command1.addLine(new CommandLine(964L, 7L, paper, 10));
+        when(bdrService.findById(Command.class, 1983L)).thenReturn(command1);
+
+        final String json = (String) command.execute(parameters, tenantServiceAccessor);
+
+        final String expectedJson = IOUtils.toString(GetBusinessDataByIdCommandTest.class.getResource("/rest/listOfCommandLines.json"));
+        JsonAssert.assertThatJson(json).isEqualTo(expectedJson);
+    }
+
+    @Test
+    public void execute_should_return_the_child_of_an_entity() throws Exception {
+        final Map<String, Serializable> parameters = new HashMap<String, Serializable>();
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_ID, 864L);
+        parameters.put(GetBusinessDataByIdCommand.ENTITY_CLASS_NAME, CommandLine.class.getName());
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_URI_PATTERN, "/businessdata/{className}/{id}/{field}");
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_CHILD_NAME, "product");
+        final Product pencil = new Product(687L, 1L, "Pencil");
+        final CommandLine commandLine = new CommandLine(864L, 78L, pencil, 5);
+        when(bdrService.findById(CommandLine.class, 864L)).thenReturn(commandLine);
+
+        final String json = (String) command.execute(parameters, tenantServiceAccessor);
+
+        final String expectedJson = IOUtils.toString(GetBusinessDataByIdCommandTest.class.getResource("/rest/getProductOfCommandLine.json"));
+        JsonAssert.assertThatJson(json).isEqualTo(expectedJson);
+    }
+
+    @Test
+    public void execute_should_return_the_entity_if_child_name_is_empty() throws Exception {
+        final Map<String, Serializable> parameters = new HashMap<String, Serializable>();
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_ID, 864L);
+        parameters.put(GetBusinessDataByIdCommand.ENTITY_CLASS_NAME, CommandLine.class.getName());
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_URI_PATTERN, "/businessdata/{className}/{id}/{field}");
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_CHILD_NAME, "");
+        final Product pencil = new Product(687L, 1L, "Pencil");
+        final CommandLine commandLine = new CommandLine(864L, 78L, pencil, 5);
+        when(bdrService.findById(CommandLine.class, 864L)).thenReturn(commandLine);
+
+        final String json = (String) command.execute(parameters, tenantServiceAccessor);
+
+        final String expectedJson = IOUtils.toString(GetBusinessDataByIdCommandTest.class.getResource("/rest/getCommandLine.json"));
+        JsonAssert.assertThatJson(json).isEqualTo(expectedJson);
+    }
+
+    @Test(expected = SCommandExecutionException.class)
+    public void execute_should_throw_an_exception_if_field_does_not_exist() throws Exception {
+        final Map<String, Serializable> parameters = new HashMap<String, Serializable>();
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_ID, 864L);
+        parameters.put(GetBusinessDataByIdCommand.ENTITY_CLASS_NAME, CommandLine.class.getName());
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_URI_PATTERN, "/businessdata/{className}/{id}/{field}");
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_CHILD_NAME, "address");
+        final Product pencil = new Product(687L, 1L, "Pencil");
+        final CommandLine commandLine = new CommandLine(864L, 78L, pencil, 5);
+        when(bdrService.findById(CommandLine.class, 864L)).thenReturn(commandLine);
+
+        command.execute(parameters, tenantServiceAccessor);
+    }
+
+    @Test(expected = SCommandExecutionException.class)
+    public void execute_should_throw_an_exception_if_field_is_not_an_entity_or_a_list_of_entity() throws Exception {
+        final Map<String, Serializable> parameters = new HashMap<String, Serializable>();
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_ID, 864L);
+        parameters.put(GetBusinessDataByIdCommand.ENTITY_CLASS_NAME, CommandLine.class.getName());
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_URI_PATTERN, "/businessdata/{className}/{id}/{field}");
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_CHILD_NAME, "persistenceVersion");
+        final Product pencil = new Product(687L, 1L, "Pencil");
+        final CommandLine commandLine = new CommandLine(864L, 78L, pencil, 5);
+        when(bdrService.findById(CommandLine.class, 864L)).thenReturn(commandLine);
+
+        command.execute(parameters, tenantServiceAccessor);
+    }
+
+    @Test(expected = SCommandExecutionException.class)
+    public void execute_should_throw_an_exception_if_a_parse_exception_occurs_during_serialization() throws Exception {
+        final ObjectMapper mapper = mock(ObjectMapper.class);
+        final GetBusinessDataByIdCommand command = new GetBusinessDataByIdCommand(mapper);
+
+        final Map<String, Serializable> parameters = new HashMap<String, Serializable>();
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_ID, 1983L);
+        parameters.put(GetBusinessDataByIdCommand.ENTITY_CLASS_NAME, Travel.class.getName());
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_URI_PATTERN, "/businessdata/{className}/{id}/{field}");
+        final Travel travel = new Travel();
+        travel.setNbDays(45);
+        when(bdrService.findById(Travel.class, 1983L)).thenReturn(travel);
+        doThrow(JsonProcessingException.class).when(mapper).writeValue(any(Writer.class), any(JsonNode.class));
+
+        command.execute(parameters, tenantServiceAccessor);
+    }
+
+    @Test(expected = SCommandExecutionException.class)
+    public void execute_should_throw_an_exception_if_an_io_exception_occurs_during_serialization() throws Exception {
+        final ObjectMapper mapper = mock(ObjectMapper.class);
+        final GetBusinessDataByIdCommand command = new GetBusinessDataByIdCommand(mapper);
+
+        final Map<String, Serializable> parameters = new HashMap<String, Serializable>();
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_ID, 1983L);
+        parameters.put(GetBusinessDataByIdCommand.ENTITY_CLASS_NAME, Travel.class.getName());
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_URI_PATTERN, "/businessdata/{className}/{id}/{field}");
+        final Travel travel = new Travel();
+        travel.setNbDays(45);
+        when(bdrService.findById(Travel.class, 1983L)).thenReturn(travel);
+        doThrow(IOException.class).when(mapper).writeValue(any(Writer.class), any(JsonNode.class));
+
+        command.execute(parameters, tenantServiceAccessor);
+    }
+
+    @Test(expected = SCommandExecutionException.class)
+    public void execute_should_throw_an_exception_if_field_is_a_list_but_not_of_entity() throws Exception {
+        final Map<String, Serializable> parameters = new HashMap<String, Serializable>();
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_ID, 864L);
+        parameters.put(GetBusinessDataByIdCommand.ENTITY_CLASS_NAME, NameList.class.getName());
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_URI_PATTERN, "/businessdata/{className}/{id}/{field}");
+        parameters.put(GetBusinessDataByIdCommand.BUSINESS_DATA_CHILD_NAME, "names");
+        final NameList nameList = new NameList();
+        nameList.setPersistenceId(864L);
+        nameList.setNames(Arrays.asList("Matti", "Akseli"));
+        when(bdrService.findById(NameList.class, 864L)).thenReturn(nameList);
+
+        command.execute(parameters, tenantServiceAccessor);
     }
 
 }
