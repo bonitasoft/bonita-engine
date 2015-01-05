5cef57d44efb1176600325b154cad78743b69163
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2015-01-05 12:08:38 +0100

    renamed some SP Platform tests

--- bonita-integration-tests-sp/bonita-integration-tests-client/src/main/java/com/bonitasoft/engine/platform/SPPlatformTest.java
+++ bonita-integration-tests-sp/bonita-integration-tests-client/src/main/java/com/bonitasoft/engine/platform/SPPlatformTest.java
@@ -317,12 +317,12 @@ public class SPPlatformTest {
     }
 
     @Test(expected = TenantNotFoundException.class)
-    public void activeNotExistTenant() throws BonitaException {
+    public void activateNotExistingTenantShouldThrowException() throws BonitaException {
         platformAPI.activateTenant(9999);
     }
 
     @Test(expected = TenantNotFoundException.class)
-    public void deactiveNotExistTenant() throws BonitaException {
+    public void deactiveNotExistingTenantShouldThrowException() throws BonitaException {
         platformAPI.deactiveTenant(9999);
     }
 
@@ -346,7 +346,7 @@ public class SPPlatformTest {
     }
 
     @Test(expected = DeletionException.class)
-    public void deleteNotExistTenant() throws BonitaException {
+    public void deleteNotExistingTenantShouldThrowException() throws BonitaException {
         platformAPI.deleteTenant(-3);
     }
 
