d06ff746c46d5ed3bef5be93bba146bec88c8217
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2015-01-30 17:26:10 +0100

    Updated copyright headers for all Subscription java files

--- bpm/bonita-core/bonita-process-engine/src/main/java/com/bonitasoft/engine/expression/ParameterExpressionExecutorStrategy.java
+++ bpm/bonita-core/bonita-process-engine/src/main/java/com/bonitasoft/engine/expression/ParameterExpressionExecutorStrategy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2009, 2013-2014 Bonitasoft S.A.
+ * Copyright (C) 2015 BonitaSoft S.A.
  * BonitaSoft is a trademark of BonitaSoft SA.
  * This software file is BONITASOFT CONFIDENTIAL. Not For Distribution.
  * For commercial licensing information, contact:
--- services/bonita-business-data/bonita-business-data-impl/src/test/java/com/company/pojo/ComplexInvoice.java
+++ services/bonita-business-data/bonita-business-data-impl/src/test/java/com/company/pojo/ComplexInvoice.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (C) 2014 Bonitasoft S.A.
- * Bonitasoft is a trademark of Bonitasoft SA.
+ * Copyright (C) 2015 BonitaSoft S.A.
+ * BonitaSoft is a trademark of BonitaSoft SA.
  * This software file is BONITASOFT CONFIDENTIAL. Not For Distribution.
  * For commercial licensing information, contact:
- * Bonitasoft, 32 rue Gustave Eiffel 38000 Grenoble
- * or Bonitasoft US, 51 Federal Street, Suite 305, San Francisco, CA 94107
+ * BonitaSoft, 32 rue Gustave Eiffel – 38000 Grenoble
+ * or BonitaSoft US, 51 Federal Street, Suite 305, San Francisco, CA 94107
  *******************************************************************************/
 package com.company.pojo;
 
