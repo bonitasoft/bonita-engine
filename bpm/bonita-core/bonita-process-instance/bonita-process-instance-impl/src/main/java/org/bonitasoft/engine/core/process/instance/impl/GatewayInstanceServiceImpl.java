3b20e6f476cffff337a8f97d3c9091f8fc382e6e
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2015-02-02 17:48:05 +0100

    test implem of new algorithme for inclusive gateways

--- bpm/bonita-core/bonita-process-instance/bonita-process-instance-impl/src/main/java/org/bonitasoft/engine/core/process/instance/impl/GatewayInstanceServiceImpl.java
+++ bpm/bonita-core/bonita-process-instance/bonita-process-instance-impl/src/main/java/org/bonitasoft/engine/core/process/instance/impl/GatewayInstanceServiceImpl.java
@@ -14,7 +14,9 @@
 package org.bonitasoft.engine.core.process.instance.impl;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -26,12 +28,14 @@ import org.bonitasoft.engine.core.process.definition.model.SFlowNodeDefinition;
 import org.bonitasoft.engine.core.process.definition.model.SGatewayDefinition;
 import org.bonitasoft.engine.core.process.definition.model.SProcessDefinition;
 import org.bonitasoft.engine.core.process.definition.model.STransitionDefinition;
+import org.bonitasoft.engine.core.process.instance.api.FlowNodeInstanceService;
 import org.bonitasoft.engine.core.process.instance.api.GatewayInstanceService;
 import org.bonitasoft.engine.core.process.instance.api.TokenService;
 import org.bonitasoft.engine.core.process.instance.api.exceptions.SGatewayCreationException;
 import org.bonitasoft.engine.core.process.instance.api.exceptions.SGatewayModificationException;
 import org.bonitasoft.engine.core.process.instance.api.exceptions.SGatewayNotFoundException;
 import org.bonitasoft.engine.core.process.instance.api.exceptions.SGatewayReadException;
+import org.bonitasoft.engine.core.process.instance.model.SFlowNodeInstance;
 import org.bonitasoft.engine.core.process.instance.model.SGatewayInstance;
 import org.bonitasoft.engine.core.process.instance.model.builder.SGatewayInstanceBuilderFactory;
 import org.bonitasoft.engine.core.process.instance.recorder.SelectDescriptorBuilder;
@@ -42,6 +46,9 @@ import org.bonitasoft.engine.events.model.SUpdateEvent;
 import org.bonitasoft.engine.events.model.builders.SEventBuilderFactory;
 import org.bonitasoft.engine.log.technical.TechnicalLogSeverity;
 import org.bonitasoft.engine.log.technical.TechnicalLoggerService;
+import org.bonitasoft.engine.persistence.FilterOption;
+import org.bonitasoft.engine.persistence.OrderByOption;
+import org.bonitasoft.engine.persistence.QueryOptions;
 import org.bonitasoft.engine.persistence.ReadPersistenceService;
 import org.bonitasoft.engine.persistence.SBonitaReadException;
 import org.bonitasoft.engine.persistence.SelectOneDescriptor;
@@ -58,6 +65,7 @@ import org.bonitasoft.engine.recorder.model.UpdateRecord;
  */
 public class GatewayInstanceServiceImpl implements GatewayInstanceService {
 
+    public static final Class<GatewayInstanceServiceImpl> TAG = GatewayInstanceServiceImpl.class;
     private final Recorder recorder;
 
     private final EventService eventService;
@@ -67,16 +75,18 @@ public class GatewayInstanceServiceImpl implements GatewayInstanceService {
     private final SGatewayInstanceBuilderFactory sGatewayInstanceBuilderFactory;
 
     private final TokenService tokenService;
+    private FlowNodeInstanceService flowNodeInstanceService;
 
     private final TechnicalLoggerService logger;
 
     public GatewayInstanceServiceImpl(final Recorder recorder, final EventService eventService, final ReadPersistenceService persistenceRead,
-            final TechnicalLoggerService logger, final TokenService tokenService) {
+            final TechnicalLoggerService logger, final TokenService tokenService, FlowNodeInstanceService flowNodeInstanceService) {
         this.recorder = recorder;
         this.eventService = eventService;
         this.persistenceRead = persistenceRead;
         this.logger = logger;
         this.tokenService = tokenService;
+        this.flowNodeInstanceService = flowNodeInstanceService;
         sGatewayInstanceBuilderFactory = BuilderFactory.get(SGatewayInstanceBuilderFactory.class);
     }
 
@@ -134,15 +144,135 @@ public class GatewayInstanceServiceImpl implements GatewayInstanceService {
     }
 
     private boolean inclusiveBehavior(final SProcessDefinition sDefinition, final SGatewayInstance gatewayInstance) throws SObjectReadException {
-        final SFlowNodeDefinition flowNode = sDefinition.getProcessContainer().getFlowNode(gatewayInstance.getFlowNodeDefinitionId());
-        if (flowNode.getIncomingTransitions().size() == 1) {
+
+        /*
+         * final SFlowNodeDefinition flowNode = sDefinition.getProcessContainer().getFlowNode(gatewayInstance.getFlowNodeDefinitionId());
+         * if (flowNode.getIncomingTransitions().size() == 1) {
+         * return true;
+         * }
+         * // get the token refId that hit the gate
+         * final Long tokenRefId = gatewayInstance.getTokenRefId();
+         * // if there is NO more token than the number of transitions that hit the gate merge is ok
+         * final int size = getHitByTransitionList(gatewayInstance).size();
+         * return tokenService.getNumberOfToken(gatewayInstance.getParentContainerId(), tokenRefId) <= size;
+         */
+        try {
+            logger.log(TAG, TechnicalLogSeverity.DEBUG,
+                    "Evaluate if gateway " + gatewayInstance.getName() + " of instance " + gatewayInstance.getRootProcessInstanceId() + " of definition "
+                            + sDefinition.getName() + " must be activated ");
+            return shouldFire(sDefinition, gatewayInstance);
+        } catch (SBonitaReadException e) {
+            throw new SObjectReadException(e);
+        }
+    }
+
+    private boolean shouldFire(SProcessDefinition sDefinition, SGatewayInstance gatewayInstance) throws SBonitaReadException {
+        //        IsEnabled(Workflow graph G, State s, Or-join A)
+        SFlowElementContainerDefinition processContainer = sDefinition.getProcessContainer();
+        SFlowNodeDefinition gatewayDefinition = processContainer.getFlowNode(gatewayInstance.getFlowNodeDefinitionId());
+        //        Red := {e | e is an incoming edge of A such that s(e) > 0}
+        List<String> hitByTransitionList = getHitByTransitionList(gatewayInstance);
+        List<STransitionDefinition> incomingWithTokens = new ArrayList<STransitionDefinition>();
+        List<STransitionDefinition> incomingWithoutTokens = new ArrayList<STransitionDefinition>();
+        List<STransitionDefinition> incomingTransitions = gatewayDefinition.getIncomingTransitions();
+
+        logger.log(TAG, TechnicalLogSeverity.DEBUG, "HitBys = "+gatewayInstance.getHitBys());
+        for (int i = 0; i < incomingTransitions.size(); i++) {
+            if (hitByTransitionList.contains(String.valueOf(i+1))) {
+                incomingWithTokens.add(incomingTransitions.get(i));
+            } else {
+                incomingWithoutTokens.add(incomingTransitions.get(i));
+            }
+        }
+        if (incomingWithTokens.size() == 0) {
+            logger.log(TAG, TechnicalLogSeverity.DEBUG, "Not transition with token on it");
             return true;
         }
-        // get the token refId that hit the gate
-        final Long tokenRefId = gatewayInstance.getTokenRefId();
-        // if there is NO more token than the number of transitions that hit the gate merge is ok
-        final int size = getHitByTransitionList(gatewayInstance).size();
-        return tokenService.getNumberOfToken(gatewayInstance.getParentContainerId(), tokenRefId) <= size;
+        List<STransitionDefinition> red = new ArrayList<STransitionDefinition>();
+        List<STransitionDefinition> green = new ArrayList<STransitionDefinition>();
+        //        while there exist edges e = (v1, v2) ∈ Red and e0 = (v3, v4) < Red such that v4 = v1 , A do
+        //            Red := Red ∪{e
+        addBackwardReachableTransitions(processContainer, gatewayDefinition, incomingWithTokens, red, Collections.<STransitionDefinition> emptyList());
+
+        //            Green := {e | e is an incoming edge of A such that s(e) = 0}
+        //            while there exist an edge e = (v1, v2) ∈ Green and e0 = (v3, v4) < (Green ∪ Red) such that
+        //            v4 = v1 , A do
+        //                Green := Green ∪{e
+        //            }
+        //        }
+        addBackwardReachableTransitions(processContainer, gatewayDefinition, incomingWithoutTokens, green, red);
+
+        logger.log(TAG, TechnicalLogSeverity.DEBUG, "Green transitions: " + green);
+        logger.log(TAG, TechnicalLogSeverity.DEBUG, "Red transitions: " + red);
+
+        //                return Green ∩{e | s(e) > 0} = ∅.
+        return !greenContainsToken(green, gatewayDefinition, gatewayInstance, processContainer);
+    }
+
+    private boolean greenContainsToken(List<STransitionDefinition> green, SFlowNodeDefinition gatewayDefinition, SGatewayInstance gatewayInstance,
+            SFlowElementContainerDefinition processContainer) throws SBonitaReadException {
+        List<SFlowNodeDefinition> sourceElements = new ArrayList<SFlowNodeDefinition>();
+        List<SFlowNodeDefinition> targetElements = new ArrayList<SFlowNodeDefinition>();
+
+        for (STransitionDefinition sTransitionDefinition : green) {
+            SFlowNodeDefinition source = processContainer.getFlowNode(sTransitionDefinition.getSource());
+            if (!source.equals(gatewayDefinition) && !sourceElements.contains(source)) {
+                sourceElements.add(source);
+            }
+            SFlowNodeDefinition target = processContainer.getFlowNode(sTransitionDefinition.getTarget());
+            if (!target.equals(gatewayDefinition) && !targetElements.contains(target)) {
+                targetElements.add(target);
+            }
+        }
+        for (SFlowNodeDefinition sourceElement : sourceElements) {
+            logger.log(TAG, TechnicalLogSeverity.DEBUG, "check if there is a token on source element of transition " + sourceElement.getName());
+            List<FilterOption> filters = new ArrayList<FilterOption>();
+            filters.add(new FilterOption(SFlowNodeInstance.class, "name", sourceElement.getName()));
+            filters.add(new FilterOption(SFlowNodeInstance.class, "parentContainerId", gatewayInstance.getParentContainerId()));
+            QueryOptions searchOptions = new QueryOptions(0, 20, Collections.<OrderByOption> emptyList(), filters, null);
+            List<SFlowNodeInstance> sFlowNodeInstances = flowNodeInstanceService.searchFlowNodeInstances(
+                    SFlowNodeInstance.class,
+                    searchOptions);
+            logger.log(TAG, TechnicalLogSeverity.DEBUG, (sFlowNodeInstances.isEmpty() ? "No" : "") + " flow node found for " + sourceElement.getName());
+            for (SFlowNodeInstance sFlowNodeInstance : sFlowNodeInstances) {
+                if (sFlowNodeInstance.isTerminal()) {
+                    logger.log(TAG, TechnicalLogSeverity.DEBUG, "flow node " + sFlowNodeInstance.getName() + " contain a token, gateway not merged");
+                    return true;
+                }
+            }
+        }
+        for (SFlowNodeDefinition targetElement : targetElements) {
+            logger.log(TAG, TechnicalLogSeverity.DEBUG, "check if there is a token on target element of transition " + targetElement.getName());
+            List<FilterOption> filters = new ArrayList<FilterOption>();
+            filters.add(new FilterOption(SFlowNodeInstance.class, "name", targetElement.getName()));
+            filters.add(new FilterOption(SFlowNodeInstance.class, "parentContainerId", gatewayInstance.getParentContainerId()));
+            QueryOptions searchOptions = new QueryOptions(0, 20, Collections.<OrderByOption> emptyList(), filters, null);
+            List<SFlowNodeInstance> sFlowNodeInstances = flowNodeInstanceService.searchFlowNodeInstances(
+                    SFlowNodeInstance.class,
+                    searchOptions);
+            logger.log(TAG, TechnicalLogSeverity.DEBUG, (sFlowNodeInstances.isEmpty() ? "No" : "") + " flow node found for " + targetElement.getName());
+            for (SFlowNodeInstance sFlowNodeInstance : sFlowNodeInstances) {
+                if (!sFlowNodeInstance.isTerminal()) {
+                    logger.log(TAG, TechnicalLogSeverity.DEBUG, "flow node " + sFlowNodeInstance.getName() + " contain a token, gateway not merged");
+                    return true;
+                }
+            }
+        }
+        logger.log(TAG, TechnicalLogSeverity.DEBUG, "No token to wait, gateway will fire");
+        return false;
+    }
+
+    private void addBackwardReachableTransitions(SFlowElementContainerDefinition processContainer, SFlowNodeDefinition gatewayDefinition,
+            List<STransitionDefinition> transitions, List<STransitionDefinition> backwardReachable, List<STransitionDefinition> notIn) {
+        for (STransitionDefinition sTransitionDefinition : transitions) {
+            if (!backwardReachable.contains(sTransitionDefinition) && !notIn.contains(sTransitionDefinition)) {
+                backwardReachable.add(sTransitionDefinition);
+                SFlowNodeDefinition flowNode = processContainer.getFlowNode(sTransitionDefinition.getSource());
+                if (!flowNode.equals(gatewayDefinition)) {
+                    addBackwardReachableTransitions(processContainer, gatewayDefinition, flowNode.getIncomingTransitions(), backwardReachable, notIn);
+                }
+            }
+        }
     }
 
     private boolean parallelBehavior(final SProcessDefinition sDefinition, final SGatewayInstance gatewayInstance) {
@@ -178,6 +308,8 @@ public class GatewayInstanceServiceImpl implements GatewayInstanceService {
 
     @Override
     public void hitTransition(final SGatewayInstance gatewayInstance, final long transitionIndex) throws SGatewayModificationException {
+        logger.log(TAG, TechnicalLogSeverity.DEBUG, "Hit gateway " + gatewayInstance.getName() + " of instance " + gatewayInstance.getRootProcessInstanceId()
+                + " with transition index " + transitionIndex);
         final String hitBys = gatewayInstance.getHitBys();
         String columnValue;
         if (hitBys == null || hitBys.isEmpty()) {
