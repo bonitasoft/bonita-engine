485181a685b8920aac49c40adc4f090853f1fda0
Matthieu Chaffotte <matthieu.chaffotte@bonitasoft.com>
2015-02-16 09:47:15 +0100

    BS-9938, use a unique constraint instead of a unique index

--- bpm/bonita-sql/src/main/resources/sql/oracle/createTables.sql
+++ bpm/bonita-sql/src/main/resources/sql/oracle/createTables.sql
@@ -6,7 +6,7 @@ CREATE TABLE contract_data (
   val BLOB
 );
 ALTER TABLE contract_data ADD CONSTRAINT pk_contract_data PRIMARY KEY (tenantid, id);
-CREATE UNIQUE INDEX idx_cd_scope_name ON contract_data (scopeId, name, tenantid);
+ALTER TABLE contract_data ADD CONSTRAINT uc_cd_scope_name UNIQUE (scopeId, name, tenantid);
 
 CREATE TABLE arch_contract_data (
   tenantid NUMBER(19, 0) NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE arch_contract_data (
   sourceObjectId NUMBER(19, 0) NOT NULL
 );
 ALTER TABLE arch_contract_data ADD CONSTRAINT pk_arch_contract_data PRIMARY KEY (tenantid, id);
-CREATE UNIQUE INDEX idx_acd_scope_name ON arch_contract_data (scopeId, name, tenantid);
+ALTER TABLE arch_contract_data ADD CONSTRAINT uc_acd_scope_name UNIQUE (scopeId, name, tenantid);
 
 CREATE TABLE actor (
   tenantid NUMBER(19, 0) NOT NULL,
