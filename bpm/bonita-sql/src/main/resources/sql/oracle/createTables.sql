e41da23579d951d9213bd03b830d1f03246a401b
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2015-03-18 10:47:40 +0100

    BS-12681, support contract data on all DB

--- bpm/bonita-sql/src/main/resources/sql/oracle/createTables.sql
+++ bpm/bonita-sql/src/main/resources/sql/oracle/createTables.sql
@@ -1,29 +1,26 @@
 CREATE TABLE contract_data (
   tenantid NUMBER(19, 0) NOT NULL,
   id NUMBER(19, 0) NOT NULL,
+  kind VARCHAR2(20 CHAR) NOT NULL,
+  scopeId NUMBER(19, 0) NOT NULL,
   name VARCHAR2(50 CHAR) NOT NULL,
   val BLOB
 );
 ALTER TABLE contract_data ADD CONSTRAINT pk_contract_data PRIMARY KEY (tenantid, id);
-
-CREATE TABLE task_contract_data (
-  tenantid NUMBER(19, 0) NOT NULL,
-  id NUMBER(19, 0) NOT NULL,
-  userTaskId NUMBER(19, 0) NOT NULL,
-);
-ALTER TABLE task_contract_data ADD CONSTRAINT pk_task_contract_data PRIMARY KEY (tenantid, id, userTaskId);
+ALTER TABLE contract_data ADD CONSTRAINT uc_cd_scope_name UNIQUE (kind, scopeId, name, tenantid);
 
 CREATE TABLE arch_contract_data (
   tenantid NUMBER(19, 0) NOT NULL,
   id NUMBER(19, 0) NOT NULL,
+  kind VARCHAR2(20 CHAR) NOT NULL,
   scopeId NUMBER(19, 0) NOT NULL,
   name VARCHAR2(50 CHAR) NOT NULL,
   val BLOB,
   archiveDate NUMBER(19, 0) NOT NULL,
   sourceObjectId NUMBER(19, 0) NOT NULL
 );
-ALTER TABLE arch_contract_data ADD CONSTRAINT pk_arch_contract_data PRIMARY KEY (tenantid, id);
-ALTER TABLE arch_contract_data ADD CONSTRAINT uc_acd_scope_name UNIQUE (scopeId, name, tenantid);
+ALTER TABLE arch_contract_data ADD CONSTRAINT pk_arch_contract_data PRIMARY KEY (tenantid, id, scopeId);
+ALTER TABLE arch_contract_data ADD CONSTRAINT uc_acd_scope_name UNIQUE (kind, scopeId, name, tenantid);
 
 CREATE TABLE actor (
   tenantid NUMBER(19, 0) NOT NULL,
