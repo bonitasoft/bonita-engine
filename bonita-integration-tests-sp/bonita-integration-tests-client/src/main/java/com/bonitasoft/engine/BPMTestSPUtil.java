1633860d97da262b697db21b10eed02029aff12d
Charles Souillard <charles.souillard@bonitasoft.com>
2015-03-30 18:32:36 +0200

    make sure the default tenant is also deleted

--- bonita-integration-tests-sp/bonita-integration-tests-client/src/main/java/com/bonitasoft/engine/BPMTestSPUtil.java
+++ bonita-integration-tests-sp/bonita-integration-tests-client/src/main/java/com/bonitasoft/engine/BPMTestSPUtil.java
@@ -99,15 +99,11 @@ public class BPMTestSPUtil {
         }
         for (final Tenant tenant : tenants) {
             try {
-                if (!tenant.isDefaultTenant()) {
-                    platformAPI.deactiveTenant(tenant.getId());
-                }
+                platformAPI.deactiveTenant(tenant.getId());
             } catch (final TenantDeactivationException tde) {
                 // Nothing to do
             }
-            if (!tenant.isDefaultTenant()) {
-                platformAPI.deleteTenant(tenant.getId());
-            }
+            platformAPI.deleteTenant(tenant.getId());
         }
         platformAPI.stopNode();
         platformAPI.cleanPlatform();
