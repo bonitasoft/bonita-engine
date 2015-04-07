a4d4e995851aea8bac34506aa15fc775fb871397
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2015-04-07 12:51:46 +0200

    BS-12938, add methods in builder

---
+++ bpm/bonita-api/bonita-common-api-impl/src/main/java/org/bonitasoft/engine/bpm/context/ContextEntryImpl.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (C) 2015 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
+
+package org.bonitasoft.engine.bpm.context;
+
+import java.util.Objects;
+
+import org.bonitasoft.engine.expression.Expression;
+
+/**
+ * @author Baptiste Mesta
+ */
+public class ContextEntryImpl implements ContextEntry {
+
+
+    private final String key;
+    private final Expression expression;
+
+    public ContextEntryImpl(String key, Expression expression) {
+        this.key = key;
+        this.expression = expression;
+    }
+
+    @Override
+    public String getKey() {
+        return key;
+    }
+
+    @Override
+    public Expression getExpression() {
+        return expression;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ContextEntryImpl)) return false;
+        ContextEntryImpl that = (ContextEntryImpl) o;
+        return Objects.equals(key, that.key) &&
+                Objects.equals(expression, that.expression);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, expression);
+    }
+
+    @Override
+    public String toString() {
+        return "ContextEntryImpl{" +
+                "key='" + key + '\'' +
+                ", expression='" + expression + '\'' +
+                '}';
+    }
+}
