c49e3995b9867670cc67db2315ad5bfc42a1aaaa
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2015-04-13 11:50:24 +0200

    fixed IT test due to refactoring with UNDEFINED form mapping

--- bonita-integration-tests/bonita-integration-tests-services-bpm/src/test/java/org/bonitasoft/engine/bpm/test/AllBPMTestsWithJNDI.java
+++ bonita-integration-tests/bonita-integration-tests-services-bpm/src/test/java/org/bonitasoft/engine/bpm/test/AllBPMTestsWithJNDI.java
@@ -18,7 +18,6 @@ import static org.bonitasoft.engine.bpm.CommonBPMServicesTest.getServicesBuilder
 import javax.naming.Context;
 
 import org.bonitasoft.engine.api.impl.PlatformAPIImpl;
-import org.bonitasoft.engine.core.form.impl.FormMappingServiceIT;
 import org.bonitasoft.engine.platform.PlatformService;
 import org.bonitasoft.engine.sessionaccessor.SessionAccessor;
 import org.bonitasoft.engine.test.util.TestUtil;
