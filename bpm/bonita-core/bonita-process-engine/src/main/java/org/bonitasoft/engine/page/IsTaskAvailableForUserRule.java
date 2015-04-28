b5d912f84cae0180ba950d334f89d9b7f049e04d
anthony <anthony.birembaut@bonitasoft.com>
2015-04-28 11:00:07 +0200

    BS-13115 - handle archived activities

--- bpm/bonita-core/bonita-process-engine/src/main/java/org/bonitasoft/engine/page/IsTaskAvailableForUserRule.java
+++ bpm/bonita-core/bonita-process-engine/src/main/java/org/bonitasoft/engine/page/IsTaskAvailableForUserRule.java
@@ -19,7 +19,11 @@ import java.util.Map;
 import org.bonitasoft.engine.commons.exceptions.SBonitaException;
 import org.bonitasoft.engine.commons.exceptions.SExecutionException;
 import org.bonitasoft.engine.core.process.instance.api.ActivityInstanceService;
+import org.bonitasoft.engine.core.process.instance.api.exceptions.SActivityInstanceNotFoundException;
+import org.bonitasoft.engine.core.process.instance.api.exceptions.SActivityReadException;
 import org.bonitasoft.engine.core.process.instance.model.SHumanTaskInstance;
+import org.bonitasoft.engine.core.process.instance.model.archive.SAHumanTaskInstance;
+import org.bonitasoft.engine.persistence.SBonitaReadException;
 import org.bonitasoft.engine.session.SessionService;
 import org.bonitasoft.engine.sessionaccessor.SessionAccessor;
 
@@ -55,15 +59,28 @@ public class IsTaskAvailableForUserRule implements AuthorizationRule {
             taskInstanceId = Long.parseLong(idParamValue[0]);
             try {
                 long userId = sessionService.getSession(sessionAccessor.getSessionId()).getUserId();
-                final SHumanTaskInstance humanTaskInstance = activityInstanceService.getHumanTaskInstance(taskInstanceId);
-                long assigneeId = humanTaskInstance.getAssigneeId();
-                if (assigneeId > 0) {
-                    return userId == assigneeId;
-                } else {
-                    return activityInstanceService.isTaskPendingForUser(taskInstanceId, userId);
-                }
+                return isTaskAvailableForOrExecutedByUser(taskInstanceId, userId);
             } catch (final SBonitaException e) {
                 throw new SExecutionException("Unable to figure out if the task " + taskInstanceId + " is available for the user.", e);
+            }
+        }
+    }
+
+    protected boolean isTaskAvailableForOrExecutedByUser(long taskInstanceId, long userId) throws SActivityReadException, SBonitaReadException, SActivityInstanceNotFoundException {
+        try {
+            final SHumanTaskInstance humanTaskInstance = activityInstanceService.getHumanTaskInstance(taskInstanceId);
+            long assigneeId = humanTaskInstance.getAssigneeId();
+            if (assigneeId > 0) {
+                return userId == assigneeId;
+            } else {
+                return activityInstanceService.isTaskPendingForUser(taskInstanceId, userId);
+            }
+        } catch (SActivityInstanceNotFoundException e) {
+            final SAHumanTaskInstance archivedHumanTaskInstance = activityInstanceService.getLastArchivedFlowNodeInstance(SAHumanTaskInstance.class, taskInstanceId);
+            if (archivedHumanTaskInstance != null) {
+                return userId == archivedHumanTaskInstance.getExecutedBy();
+            } else {
+                throw e;
             }
         }
     }
