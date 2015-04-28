5bc7e6a85e1acfea60850c77a4ef6c7b40b57e23
anthony <anthony.birembaut@bonitasoft.com>
2015-04-28 11:00:07 +0200

    BS-13115 - handle archived activities

--- bpm/bonita-core/bonita-process-engine/src/test/java/org/bonitasoft/engine/page/IsTaskAvailableForUserRuleTest.java
+++ bpm/bonita-core/bonita-process-engine/src/test/java/org/bonitasoft/engine/page/IsTaskAvailableForUserRuleTest.java
@@ -3,13 +3,17 @@ package org.bonitasoft.engine.page;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doThrow;
 
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.bonitasoft.engine.commons.exceptions.SExecutionException;
 import org.bonitasoft.engine.core.process.instance.api.ActivityInstanceService;
+import org.bonitasoft.engine.core.process.instance.api.exceptions.SActivityInstanceNotFoundException;
 import org.bonitasoft.engine.core.process.instance.model.SHumanTaskInstance;
+import org.bonitasoft.engine.core.process.instance.model.archive.SAHumanTaskInstance;
 import org.bonitasoft.engine.session.SessionService;
 import org.bonitasoft.engine.session.model.SSession;
 import org.bonitasoft.engine.sessionaccessor.SessionAccessor;
@@ -35,6 +39,9 @@ public class IsTaskAvailableForUserRuleTest {
     @Mock
     SHumanTaskInstance humanTaskInstance;
     
+    @Mock
+    SAHumanTaskInstance archivedHumanTaskInstance;
+    
     long userId = 2L;
     
     long taskInstanceId = 42L;
@@ -48,7 +55,6 @@ public class IsTaskAvailableForUserRuleTest {
         SSession session = mock(SSession.class);
         when(session.getUserId()).thenReturn(userId);
         when(sessionService.getSession(1L)).thenReturn(session);
-        when(activityInstanceService.getHumanTaskInstance(taskInstanceId)).thenReturn(humanTaskInstance);
     }
     
     @Test
@@ -58,6 +64,7 @@ public class IsTaskAvailableForUserRuleTest {
         queryParameters.put(URLAdapterConstants.ID_QUERY_PARAM, new String[] {Long.toString(taskInstanceId)});
         context.put(URLAdapterConstants.QUERY_PARAMETERS, (Serializable)queryParameters);
         when(humanTaskInstance.getAssigneeId()).thenReturn(userId);
+        when(activityInstanceService.getHumanTaskInstance(taskInstanceId)).thenReturn(humanTaskInstance);
         
         assertThat(isTaskAvailableForUserRule.isAllowed("key", context)).isTrue();
     }
@@ -69,6 +76,7 @@ public class IsTaskAvailableForUserRuleTest {
         queryParameters.put(URLAdapterConstants.ID_QUERY_PARAM, new String[] {Long.toString(taskInstanceId)});
         context.put(URLAdapterConstants.QUERY_PARAMETERS, (Serializable)queryParameters);
         when(humanTaskInstance.getAssigneeId()).thenReturn(0L);
+        when(activityInstanceService.getHumanTaskInstance(taskInstanceId)).thenReturn(humanTaskInstance);
         when(activityInstanceService.isTaskPendingForUser(taskInstanceId, userId)).thenReturn(true);
         
         assertThat(isTaskAvailableForUserRule.isAllowed("key", context)).isTrue();
@@ -81,6 +89,7 @@ public class IsTaskAvailableForUserRuleTest {
         queryParameters.put(URLAdapterConstants.ID_QUERY_PARAM, new String[] {Long.toString(taskInstanceId)});
         context.put(URLAdapterConstants.QUERY_PARAMETERS, (Serializable)queryParameters);
         when(humanTaskInstance.getAssigneeId()).thenReturn(5L);
+        when(activityInstanceService.getHumanTaskInstance(taskInstanceId)).thenReturn(humanTaskInstance);
         
         assertThat(isTaskAvailableForUserRule.isAllowed("key", context)).isFalse();
     }
@@ -92,8 +101,47 @@ public class IsTaskAvailableForUserRuleTest {
         queryParameters.put(URLAdapterConstants.ID_QUERY_PARAM, new String[] {Long.toString(taskInstanceId)});
         context.put(URLAdapterConstants.QUERY_PARAMETERS, (Serializable)queryParameters);
         when(humanTaskInstance.getAssigneeId()).thenReturn(0L);
+        when(activityInstanceService.getHumanTaskInstance(taskInstanceId)).thenReturn(humanTaskInstance);
         when(activityInstanceService.isTaskPendingForUser(taskInstanceId, userId)).thenReturn(false);
         
         assertThat(isTaskAvailableForUserRule.isAllowed("key", context)).isFalse();
+    }
+    
+    @Test
+    public void isAllowed_should_return_false_if_archived_task_performed_by_someone_else() throws Exception {
+        Map<String, Serializable> context = new HashMap<String, Serializable>();
+        final Map<String, String[]> queryParameters = new HashMap<String, String[]>();
+        queryParameters.put(URLAdapterConstants.ID_QUERY_PARAM, new String[] {Long.toString(taskInstanceId)});
+        context.put(URLAdapterConstants.QUERY_PARAMETERS, (Serializable)queryParameters);
+        doThrow(SActivityInstanceNotFoundException.class).when(activityInstanceService).getHumanTaskInstance(taskInstanceId);
+        when(archivedHumanTaskInstance.getExecutedBy()).thenReturn(5L);
+        when(activityInstanceService.getLastArchivedFlowNodeInstance(SAHumanTaskInstance.class, taskInstanceId)).thenReturn(archivedHumanTaskInstance);
+        
+        assertThat(isTaskAvailableForUserRule.isAllowed("key", context)).isFalse();
+    }
+    
+    @Test
+    public void isAllowed_should_return_true_if_archived_task_performed_by_user() throws Exception {
+        Map<String, Serializable> context = new HashMap<String, Serializable>();
+        final Map<String, String[]> queryParameters = new HashMap<String, String[]>();
+        queryParameters.put(URLAdapterConstants.ID_QUERY_PARAM, new String[] {Long.toString(taskInstanceId)});
+        context.put(URLAdapterConstants.QUERY_PARAMETERS, (Serializable)queryParameters);
+        doThrow(SActivityInstanceNotFoundException.class).when(activityInstanceService).getHumanTaskInstance(taskInstanceId);
+        when(archivedHumanTaskInstance.getExecutedBy()).thenReturn(userId);
+        when(activityInstanceService.getLastArchivedFlowNodeInstance(SAHumanTaskInstance.class, taskInstanceId)).thenReturn(archivedHumanTaskInstance);
+        
+        assertThat(isTaskAvailableForUserRule.isAllowed("key", context)).isTrue();
+    }
+    
+    @Test(expected=SExecutionException.class)
+    public void isAllowed_should_throw_exception_when_task_not_found() throws Exception {
+        Map<String, Serializable> context = new HashMap<String, Serializable>();
+        final Map<String, String[]> queryParameters = new HashMap<String, String[]>();
+        queryParameters.put(URLAdapterConstants.ID_QUERY_PARAM, new String[] {Long.toString(taskInstanceId)});
+        context.put(URLAdapterConstants.QUERY_PARAMETERS, (Serializable)queryParameters);
+        doThrow(SActivityInstanceNotFoundException.class).when(activityInstanceService).getHumanTaskInstance(taskInstanceId);
+        when(activityInstanceService.getLastArchivedFlowNodeInstance(SAHumanTaskInstance.class, taskInstanceId)).thenReturn(null);
+        
+        isTaskAvailableForUserRule.isAllowed("key", context);
     }
 }
