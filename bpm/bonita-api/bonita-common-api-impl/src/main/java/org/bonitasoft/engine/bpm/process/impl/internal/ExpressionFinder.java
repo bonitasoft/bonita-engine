05fb2f38aa4449d7f47d23c8f1df04443cdddfff
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2015-05-15 18:09:29 +0200

    BS-13282, small refactor on Visitor

--- bpm/bonita-api/bonita-common-api-impl/src/main/java/org/bonitasoft/engine/bpm/process/impl/internal/ExpressionFinder.java
+++ bpm/bonita-api/bonita-common-api-impl/src/main/java/org/bonitasoft/engine/bpm/process/impl/internal/ExpressionFinder.java
@@ -58,21 +58,8 @@ import org.bonitasoft.engine.operation.Operation;
  */
 public class ExpressionFinder implements ModelFinderVisitor<Expression> {
 
-    private Expression getExpressionWithId(Expression expression, long expressionDefinitionId) {
-        if (expression != null && expression.accept(this, expressionDefinitionId) != null) {
-            return expression;
-        }
-        return null;
-    }
-
     protected Expression getExpressionFromContainer(Container container, long expressionDefinitionId) {
-        if (container != null) {
-            final Expression expression = container.accept(this, expressionDefinitionId);
-            if (expression != null) {
-                return expression;
-            }
-        }
-        return null;
+        return container != null ? container.accept(this, expressionDefinitionId) : null;
     }
 
     protected Expression getExpressionFromOperationList(List<Operation> operations, long expressionDefinitionId) {
@@ -97,7 +84,7 @@ public class ExpressionFinder implements ModelFinderVisitor<Expression> {
     public Expression find(DesignProcessDefinition designProcessDefinition, long expressionDefinitionId) {
         for (int i = 1; i <= 5; i++) {
             final Expression expression = designProcessDefinition.getStringIndexValue(i);
-            if (getExpressionWithId(expression, expressionDefinitionId) != null) {
+            if (getExpressionFromContainer(expression, expressionDefinitionId) != null) {
                 return expression;
             }
         }
@@ -118,15 +105,15 @@ public class ExpressionFinder implements ModelFinderVisitor<Expression> {
         }
 
         Expression aExpression = flowNodeDefinition.getDisplayName();
-        if (getExpressionWithId(aExpression, expressionDefinitionId) != null) {
+        if (getExpressionFromContainer(aExpression, expressionDefinitionId) != null) {
             return aExpression;
         }
         aExpression = flowNodeDefinition.getDisplayDescription();
-        if (getExpressionWithId(aExpression, expressionDefinitionId) != null) {
+        if (getExpressionFromContainer(aExpression, expressionDefinitionId) != null) {
             return aExpression;
         }
         aExpression = flowNodeDefinition.getDisplayDescriptionAfterCompletion();
-        if (getExpressionWithId(aExpression, expressionDefinitionId) != null) {
+        if (getExpressionFromContainer(aExpression, expressionDefinitionId) != null) {
             return aExpression;
         }
 
@@ -220,7 +207,7 @@ public class ExpressionFinder implements ModelFinderVisitor<Expression> {
     @Override
     public Expression find(ActivityDefinition activityDefinition, long expressionDefinitionId) {
         final Expression displayName = activityDefinition.getDisplayName();
-        if (getExpressionWithId(displayName, expressionDefinitionId) != null) {
+        if (getExpressionFromContainer(displayName, expressionDefinitionId) != null) {
             return displayName;
         }
         final LoopCharacteristics loopCharacteristics = activityDefinition.getLoopCharacteristics();
@@ -264,7 +251,7 @@ public class ExpressionFinder implements ModelFinderVisitor<Expression> {
     @Override
     public Expression find(UserFilterDefinition userFilterDefinition, long expressionDefinitionId) {
         for (Expression expression : userFilterDefinition.getInputs().values()) {
-            if (getExpressionWithId(expression, expressionDefinitionId) != null) {
+            if (getExpressionFromContainer(expression, expressionDefinitionId) != null) {
                 return expression;
             }
         }
@@ -300,15 +287,15 @@ public class ExpressionFinder implements ModelFinderVisitor<Expression> {
     @Override
     public Expression find(CallActivityDefinition callActivityDefinition, long expressionDefinitionId) {
         final Expression callableElement = callActivityDefinition.getCallableElement();
-        if (getExpressionWithId(callableElement, expressionDefinitionId) != null) {
+        if (getExpressionFromContainer(callableElement, expressionDefinitionId) != null) {
             return callableElement;
         }
         final Expression callableElementVersion = callActivityDefinition.getCallableElementVersion();
-        if (getExpressionWithId(callableElementVersion, expressionDefinitionId) != null) {
+        if (getExpressionFromContainer(callableElementVersion, expressionDefinitionId) != null) {
             return callableElementVersion;
         }
         for (Expression expression : callActivityDefinition.getProcessStartContractInputs().values()) {
-            if (getExpressionWithId(expression, expressionDefinitionId) != null) {
+            if (getExpressionFromContainer(expression, expressionDefinitionId) != null) {
                 return expression;
             }
         }
@@ -325,31 +312,31 @@ public class ExpressionFinder implements ModelFinderVisitor<Expression> {
 
     @Override
     public Expression find(Operation operation, long expressionDefinitionId) {
-        return getExpressionWithId(operation.getRightOperand(), expressionDefinitionId);
+        return getExpressionFromContainer(operation.getRightOperand(), expressionDefinitionId);
     }
 
     @Override
     public Expression find(TransitionDefinition transition, long expressionDefinitionId) {
-        return getExpressionWithId(transition.getCondition(), expressionDefinitionId);
+        return getExpressionFromContainer(transition.getCondition(), expressionDefinitionId);
     }
 
     @Override
     public Expression find(BusinessDataDefinition businessDataDefinition, long expressionDefinitionId) {
-        return getExpressionWithId(businessDataDefinition.getDefaultValueExpression(), expressionDefinitionId);
+        return getExpressionFromContainer(businessDataDefinition.getDefaultValueExpression(), expressionDefinitionId);
     }
 
     @Override
     public Expression find(DataDefinition dataDefinition, long expressionDefinitionId) {
-        return getExpressionWithId(dataDefinition.getDefaultValueExpression(), expressionDefinitionId);
+        return getExpressionFromContainer(dataDefinition.getDefaultValueExpression(), expressionDefinitionId);
     }
 
     @Override
     public Expression find(CorrelationDefinition correlationDefinition, long expressionDefinitionId) {
-        final Expression keyExpression = getExpressionWithId(correlationDefinition.getKey(), expressionDefinitionId);
+        final Expression keyExpression = getExpressionFromContainer(correlationDefinition.getKey(), expressionDefinitionId);
         if (keyExpression != null) {
             return keyExpression;
         }
-        final Expression valueExpression = getExpressionWithId(correlationDefinition.getValue(), expressionDefinitionId);
+        final Expression valueExpression = getExpressionFromContainer(correlationDefinition.getValue(), expressionDefinitionId);
         if (valueExpression != null) {
             return valueExpression;
         }
@@ -367,11 +354,11 @@ public class ExpressionFinder implements ModelFinderVisitor<Expression> {
 
     @Override
     public Expression find(ThrowMessageEventTriggerDefinition throwMessageEventTriggerDefinition, long expressionDefinitionId) {
-        final Expression targetFlownode = getExpressionWithId(throwMessageEventTriggerDefinition.getTargetFlowNode(), expressionDefinitionId);
+        final Expression targetFlownode = getExpressionFromContainer(throwMessageEventTriggerDefinition.getTargetFlowNode(), expressionDefinitionId);
         if (targetFlownode != null) {
             return targetFlownode;
         }
-        final Expression targetProcess = getExpressionWithId(throwMessageEventTriggerDefinition.getTargetProcess(), expressionDefinitionId);
+        final Expression targetProcess = getExpressionFromContainer(throwMessageEventTriggerDefinition.getTargetProcess(), expressionDefinitionId);
         if (targetProcess != null) {
             return targetProcess;
         }
@@ -397,12 +384,12 @@ public class ExpressionFinder implements ModelFinderVisitor<Expression> {
 
     @Override
     public Expression find(TimerEventTriggerDefinition timerEventTriggerDefinition, long expressionDefinitionId) {
-        return getExpressionWithId(timerEventTriggerDefinition.getTimerExpression(), expressionDefinitionId);
+        return getExpressionFromContainer(timerEventTriggerDefinition.getTimerExpression(), expressionDefinitionId);
     }
 
     @Override
     public Expression find(ContextEntry contextEntry, long expressionDefinitionId) {
-        return getExpressionWithId(contextEntry.getExpression(), expressionDefinitionId);
+        return getExpressionFromContainer(contextEntry.getExpression(), expressionDefinitionId);
     }
 
     @Override
@@ -446,18 +433,18 @@ public class ExpressionFinder implements ModelFinderVisitor<Expression> {
 
     @Override
     public Expression find(DocumentDefinition documentDefinition, long expressionDefinitionId) {
-        return getExpressionWithId(documentDefinition.getInitialValue(), expressionDefinitionId);
+        return getExpressionFromContainer(documentDefinition.getInitialValue(), expressionDefinitionId);
     }
 
     @Override
     public Expression find(DocumentListDefinition documentListDefinition, long expressionDefinitionId) {
-        return getExpressionWithId(documentListDefinition.getExpression(), expressionDefinitionId);
+        return getExpressionFromContainer(documentListDefinition.getExpression(), expressionDefinitionId);
     }
 
     @Override
     public Expression find(ConnectorDefinition connectorDefinition, long expressionDefinitionId) {
         for (Expression expression : connectorDefinition.getInputs().values()) {
-            if (getExpressionWithId(expression, expressionDefinitionId) != null) {
+            if (getExpressionFromContainer(expression, expressionDefinitionId) != null) {
                 return expression;
             }
         }
@@ -471,11 +458,11 @@ public class ExpressionFinder implements ModelFinderVisitor<Expression> {
     @Override
     public Expression find(StandardLoopCharacteristics standardLoopCharacteristics, long expressionDefinitionId) {
         final Expression loopCondition = standardLoopCharacteristics.getLoopCondition();
-        if (getExpressionWithId(loopCondition, expressionDefinitionId) != null) {
+        if (getExpressionFromContainer(loopCondition, expressionDefinitionId) != null) {
             return loopCondition;
         }
         final Expression loopMax = standardLoopCharacteristics.getLoopMax();
-        if (getExpressionWithId(loopMax, expressionDefinitionId) != null) {
+        if (getExpressionFromContainer(loopMax, expressionDefinitionId) != null) {
             return loopMax;
         }
         return null;
@@ -484,11 +471,11 @@ public class ExpressionFinder implements ModelFinderVisitor<Expression> {
     @Override
     public Expression find(MultiInstanceLoopCharacteristics multiInstanceLoopCharacteristics, long expressionDefinitionId) {
         final Expression completionCondition = multiInstanceLoopCharacteristics.getCompletionCondition();
-        if (getExpressionWithId(completionCondition, expressionDefinitionId) != null) {
+        if (getExpressionFromContainer(completionCondition, expressionDefinitionId) != null) {
             return completionCondition;
         }
         final Expression loopCardinality = multiInstanceLoopCharacteristics.getLoopCardinality();
-        if (getExpressionWithId(loopCardinality, expressionDefinitionId) != null) {
+        if (getExpressionFromContainer(loopCardinality, expressionDefinitionId) != null) {
             return loopCardinality;
         }
         return null;
