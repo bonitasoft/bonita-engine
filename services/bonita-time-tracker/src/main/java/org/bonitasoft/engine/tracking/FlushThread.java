2cadbd3ce3648d940eec48e3daba467e86974e99
Pierre-Yves Monnet <pierre-yves.monnet@bonitasoft.com>
2015-05-29 12:08:59 -0700

    BOS formatting

--- services/bonita-time-tracker/src/main/java/org/bonitasoft/engine/tracking/FlushThread.java
+++ services/bonita-time-tracker/src/main/java/org/bonitasoft/engine/tracking/FlushThread.java
@@ -8,85 +8,94 @@ import org.bonitasoft.engine.log.technical.TechnicalLoggerService;
 
 public class FlushThread extends Thread {
 
-		private final TimeTracker timeTracker;
+    private final TimeTracker timeTracker;
 
-		private long flushIntervalInSeconds;
+    private long flushIntervalInSeconds;
 
-		private Date currentDate= null;
-		
-		private final Clock clock;
+    private Date currentDate = null;
 
-		private final TechnicalLoggerService logger;
+    private final Clock clock;
 
-		public boolean askStop;
+    private final TechnicalLoggerService logger;
 
-		private String msgState="";
-		public FlushThread(final Clock clock, final long flushIntervalInSeconds, final TimeTracker timeTracker, final TechnicalLoggerService logger) {
-				super("TimeTracker-FlushThread");
-				this.clock = clock;
-				this.logger = logger;
-				this.flushIntervalInSeconds = flushIntervalInSeconds;
-				this.timeTracker = timeTracker;
-		}
+    public boolean askStop;
 
-		public Date getLastFlushExecutionTime()
-		{
-				return currentDate;
-		}
-    public long getFlushIntervalInSeconds()
-    {
-    		return flushIntervalInSeconds;
+    private String msgState = "";
+
+    public FlushThread(final Clock clock, final long flushIntervalInSeconds,
+            final TimeTracker timeTracker, final TechnicalLoggerService logger) {
+        super("TimeTracker-FlushThread");
+        this.clock = clock;
+        this.logger = logger;
+        this.flushIntervalInSeconds = flushIntervalInSeconds;
+        this.timeTracker = timeTracker;
     }
-    
-    public void setFlushIntervalInSeconds( long flushIntervalInSeconds)
-    {
-    		this.flushIntervalInSeconds = flushIntervalInSeconds;
-    }
-    public String getMsgState()
-    {
-    		return msgState;
-    }
-    
-		@Override
-		public void run() {
-				
-				msgState="Started;";
-				if (this.logger.isLoggable(getClass(), TechnicalLogSeverity.INFO)) {
-						logger.log(getClass(), TechnicalLogSeverity.INFO, "Starting " + this.getName() + "...");
-				}
-				SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-				askStop = false;
-				while (true) {
-						msgState +="Sleep "+flushIntervalInSeconds+" s;";
-						try {
-								clock.sleep(flushIntervalInSeconds * 1000);
-						} catch (InterruptedException e) {
-								break;
-						}
-						currentDate = new Date();
-						msgState=""; // reset
-						msgState +="Wakeup at "+sdf.format(currentDate)+" ;";
-						
-						try {
-								this.timeTracker.flush();
-						} catch (Exception e) {
-								if (this.logger.isLoggable(getClass(), TechnicalLogSeverity.WARNING)) {
-										this.logger.log(getClass(), TechnicalLogSeverity.WARNING, "Exception caught while flushing: " + e.getMessage(), e);
-								}
-								msgState +="Error flush:"+e.toString()+" ;";
 
-						}
-						if (askStop) {
-								msgState +="AskStop;";
-								if (this.logger.isLoggable(getClass(), TechnicalLogSeverity.INFO)) {
-										logger.log(getClass(), TechnicalLogSeverity.INFO, this.getName() + " ASK STOP.");
-								}
-								return;
-						}
-				}
-				if (this.logger.isLoggable(getClass(), TechnicalLogSeverity.INFO)) {
-						logger.log(getClass(), TechnicalLogSeverity.INFO, this.getName() + " stopped.");
-				}
-		}
+    public Date getLastFlushExecutionTime() {
+        return currentDate;
+    }
+
+    public long getFlushIntervalInSeconds() {
+        return flushIntervalInSeconds;
+    }
+
+    public void setFlushIntervalInSeconds(long flushIntervalInSeconds) {
+        this.flushIntervalInSeconds = flushIntervalInSeconds;
+    }
+
+    public String getMsgState() {
+        return msgState;
+    }
+
+    @Override
+    public void run() {
+
+        msgState = "Started;";
+        if (this.logger.isLoggable(getClass(), TechnicalLogSeverity.INFO)) {
+            logger.log(getClass(), TechnicalLogSeverity.INFO, "Starting "
+                    + this.getName() + "...");
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        askStop = false;
+        while (true) {
+            msgState += "Sleep " + flushIntervalInSeconds + " s;";
+            try {
+                clock.sleep(flushIntervalInSeconds * 1000);
+            } catch (InterruptedException e) {
+                break;
+            }
+            currentDate = new Date();
+            msgState = ""; // reset
+            msgState += "Wakeup at " + sdf.format(currentDate) + " ;";
+
+            try {
+                this.timeTracker.flush();
+            } catch (Exception e) {
+                if (this.logger.isLoggable(getClass(),
+                        TechnicalLogSeverity.WARNING)) {
+                    this.logger.log(
+                            getClass(),
+                            TechnicalLogSeverity.WARNING,
+                            "Exception caught while flushing: "
+                                    + e.getMessage(), e);
+                }
+                msgState += "Error flush:" + e.toString() + " ;";
+
+            }
+            if (askStop) {
+                msgState += "AskStop;";
+                if (this.logger.isLoggable(getClass(),
+                        TechnicalLogSeverity.INFO)) {
+                    logger.log(getClass(), TechnicalLogSeverity.INFO,
+                            this.getName() + " ASK STOP.");
+                }
+                return;
+            }
+        }
+        if (this.logger.isLoggable(getClass(), TechnicalLogSeverity.INFO)) {
+            logger.log(getClass(), TechnicalLogSeverity.INFO, this.getName()
+                    + " stopped.");
+        }
+    }
 
 }
