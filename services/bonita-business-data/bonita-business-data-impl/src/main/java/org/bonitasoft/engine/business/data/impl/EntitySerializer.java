98f084f9957de5fecf2d73a101e3027db22ebaac
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2015-06-12 15:23:53 +0200

    BS-13648, add string representation if number can overflow 2^53 and be truncated by javascript

--- services/bonita-business-data/bonita-business-data-impl/src/main/java/org/bonitasoft/engine/business/data/impl/EntitySerializer.java
+++ services/bonita-business-data/bonita-business-data-impl/src/main/java/org/bonitasoft/engine/business/data/impl/EntitySerializer.java
@@ -16,23 +16,26 @@ package org.bonitasoft.engine.business.data.impl;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import javassist.util.proxy.MethodHandler;
-
-import org.bonitasoft.engine.commons.ClassReflector;
-
-import org.bonitasoft.engine.bdm.Entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import javassist.util.proxy.MethodHandler;
+import org.bonitasoft.engine.bdm.Entity;
+import org.bonitasoft.engine.commons.ClassReflector;
 
 public class EntitySerializer extends JsonSerializer<Entity> {
 
+    public static final String STRING_SUFFIX = "_string";
     private String patternURI;
 
     public void setPatternURI(final String patternURI) {
@@ -45,6 +48,15 @@ public class EntitySerializer extends JsonSerializer<Entity> {
         return uri.replace("{field}", entityField.getName());
     }
 
+    private Set<String> numberTypes;
+
+    public EntitySerializer() {
+        numberTypes = new HashSet<>();
+        numberTypes.add(Long.class.getCanonicalName());
+        numberTypes.add(Float.class.getCanonicalName());
+        numberTypes.add(Double.class.getCanonicalName());
+    }
+
     @Override
     public void serialize(final Entity value, final JsonGenerator jgen, final SerializerProvider provider) throws IOException, JsonProcessingException {
         final Class<? extends Entity> valueClass = value.getClass();
@@ -53,26 +65,31 @@ public class EntitySerializer extends JsonSerializer<Entity> {
         final List<Link> links = new ArrayList<Link>();
         for (final Field field : valueClass.getDeclaredFields()) {
             final Class<?> fieldType = field.getType();
-            if (fieldType.equals(MethodHandler.class)) {
+            if (shouldSkipField(fieldType)) {
                 continue;
             }
-            if (field.isAnnotationPresent(JsonIgnore.class)) {
+            if (shouldReplaceFieldByLink(field)) {
                 links.add(new Link(field.getName(), buildURI(value, patternURI, field)));
+                continue;
             }
-            else {
-                try {
-                    Method declaredMethod;
-                    final String getterName = ClassReflector.getGetterName(field.getName(), fieldType);
-                    declaredMethod = valueClass.getDeclaredMethod(getterName);
-                    final Object invoke = declaredMethod.invoke(value);
-                    jgen.writeObjectField(field.getName(), invoke);
-                } catch (final NoSuchMethodException e) {
-                    // nothing to do
+            try {
+                Method declaredMethod;
+                final String getterName = ClassReflector.getGetterName(field.getName(), fieldType);
+                declaredMethod = valueClass.getDeclaredMethod(getterName);
+                final Object invoke = declaredMethod.invoke(value);
+                jgen.writeObjectField(field.getName(), invoke);
+                if (invoke != null && shouldAddStringRepresentationForNumber(field)) {
+                    jgen.writeObjectField(field.getName().concat(STRING_SUFFIX), invoke.toString());
                 }
-                catch (final Exception e) {
-                    throw new JsonGenerationException(e);
+                if (invoke != null && shouldAddStringRepresentationForNumberList(field)) {
+                    jgen.writeObjectField(field.getName().concat(STRING_SUFFIX), convertToStringList((List) invoke));
                 }
+            } catch (final NoSuchMethodException e) {
+                // nothing to do
+            } catch (final Exception e) {
+                throw new JsonGenerationException(e);
             }
+
         }
         if (!links.isEmpty()) {
             jgen.writeArrayFieldStart("links");
@@ -82,6 +99,35 @@ public class EntitySerializer extends JsonSerializer<Entity> {
             jgen.writeEndArray();
         }
         jgen.writeEndObject();
+    }
+
+    private List<String> convertToStringList(List numberList) {
+        ArrayList<String> strings = new ArrayList<>();
+        for (Object item : numberList) {
+            strings.add(item.toString());
+        }
+        return strings;
+    }
+
+    private boolean shouldAddStringRepresentationForNumberList(Field field) {
+        if (field.getType().equals(List.class)) {
+            ParameterizedType parameterizedType = (ParameterizedType) field.getGenericType();
+            Type type = parameterizedType.getActualTypeArguments()[0];
+            return numberTypes.contains(((Class) type).getCanonicalName());
+        }
+        return false;
+    }
+
+    private boolean shouldAddStringRepresentationForNumber(Field field) {
+        return numberTypes.contains(field.getType().getCanonicalName());
+    }
+
+    protected boolean shouldReplaceFieldByLink(Field field) {
+        return field.isAnnotationPresent(JsonIgnore.class);
+    }
+
+    protected boolean shouldSkipField(Class<?> fieldType) {
+        return fieldType.equals(MethodHandler.class);
     }
 
     @Override
