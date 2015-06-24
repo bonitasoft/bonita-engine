41a62af26a1ba1fb8486e317f92319eaa6f49eab
Elias Ricken de Medeiros <elias.ricken@bonitasoft.com>
2015-06-24 18:31:22 +0200

    fix default theme name

--- bonita-integration-tests-sp/bonita-integration-tests-client/src/main/java/com/bonitasoft/engine/business/application/ApplicationAPIApplicationPageIT.java
+++ bonita-integration-tests-sp/bonita-integration-tests-client/src/main/java/com/bonitasoft/engine/business/application/ApplicationAPIApplicationPageIT.java
@@ -355,8 +355,8 @@ public class ApplicationAPIApplicationPageIT extends TestWithCustomPage {
         List<String> allPagesForProfile2 = getSubscriptionApplicationAPI().getAllPagesForProfile(profile2.getId());
 
         //then
-        assertThat(allPagesForProfile1).isEqualTo(Arrays.asList("custompage_defaultlayout", "custompage_defaulttheme", "custompage_page1","custompage_page2","custompage_page3"));
-        assertThat(allPagesForProfile2).isEqualTo(Arrays.asList("custompage_defaultlayout", "custompage_defaulttheme", "custompage_page4"));
+        assertThat(allPagesForProfile1).isEqualTo(Arrays.asList("custompage_bootstrapdefaulttheme", "custompage_defaultlayout", "custompage_page1","custompage_page2","custompage_page3"));
+        assertThat(allPagesForProfile2).isEqualTo(Arrays.asList("custompage_bootstrapdefaulttheme", "custompage_defaultlayout", "custompage_page4"));
 
         //clean
         getSubscriptionApplicationAPI().deleteApplication(app1.getId());
