32f2a6e7c2646a2962a4d3c91d646bcb4ed8487a
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2015-08-25 14:54:48 +0200

    add recommended indexes on quartz tables

--- bpm/bonita-sql/src/main/resources/sql/mysql/createQuartzTables.sql
+++ bpm/bonita-sql/src/main/resources/sql/mysql/createQuartzTables.sql
@@ -236,4 +236,28 @@ ALTER TABLE QRTZ_TRIGGERS ADD
     JOB_GROUP
   );
   
-COMMIT;
+
+
+create index idx_qrtz_t_nf_time on QRTZ_TRIGGERS(NEXT_FIRE_TIME);
+
+create index idx_qrtz_t_state on QRTZ_TRIGGERS(TRIGGER_STATE);
+
+create index idx_qrtz_t_nf_st on QRTZ_TRIGGERS(TRIGGER_STATE,NEXT_FIRE_TIME);
+
+create index idx_qrtz_ft_trig_name on QRTZ_FIRED_TRIGGERS(TRIGGER_NAME);
+
+create index idx_qrtz_ft_trig_group on QRTZ_FIRED_TRIGGERS(TRIGGER_GROUP);
+
+create index idx_qrtz_ft_trig_n_g on QRTZ_FIRED_TRIGGERS(TRIGGER_NAME,TRIGGER_GROUP);
+
+create index idx_qrtz_ft_trig_inst_name on QRTZ_FIRED_TRIGGERS(INSTANCE_NAME);
+
+create index idx_qrtz_ft_job_name on QRTZ_FIRED_TRIGGERS(JOB_NAME);
+
+create index idx_qrtz_ft_job_group on QRTZ_FIRED_TRIGGERS(JOB_GROUP);
+
+create index idx_qrtz_t_nf_time_misfire on QRTZ_TRIGGERS(MISFIRE_INSTR,NEXT_FIRE_TIME);
+
+create index idx_qrtz_t_nf_st_misfire on QRTZ_TRIGGERS(MISFIRE_INSTR,NEXT_FIRE_TIME,TRIGGER_STATE);
+
+create index idx_qrtz_t_nf_st_misfire_grp on QRTZ_TRIGGERS(MISFIRE_INSTR,NEXT_FIRE_TIME,TRIGGER_GROUP,TRIGGER_STATE);
--- bpm/bonita-sql/src/main/resources/sql/sqlserver/createQuartzTables.sql
+++ bpm/bonita-sql/src/main/resources/sql/sqlserver/createQuartzTables.sql
@@ -343,3 +343,41 @@ ALTER TABLE [dbo].[QRTZ_TRIGGERS] ADD
     [JOB_GROUP]
   )
 GO
+
+
+
+create index idx_qrtz_t_nf_time on qrtz_triggers(NEXT_FIRE_TIME)
+GO
+
+create index idx_qrtz_t_state on qrtz_triggers(TRIGGER_STATE)
+GO
+
+create index idx_qrtz_t_nf_st on qrtz_triggers(TRIGGER_STATE,NEXT_FIRE_TIME)
+GO
+
+create index idx_qrtz_ft_trig_name on qrtz_fired_triggers(TRIGGER_NAME)
+GO
+
+create index idx_qrtz_ft_trig_group on qrtz_fired_triggers(TRIGGER_GROUP)
+GO
+
+create index idx_qrtz_ft_trig_n_g on qrtz_fired_triggers(TRIGGER_NAME,TRIGGER_GROUP)
+GO
+
+create index idx_qrtz_ft_trig_inst_name on qrtz_fired_triggers(INSTANCE_NAME)
+GO
+
+create index idx_qrtz_ft_job_name on qrtz_fired_triggers(JOB_NAME)
+GO
+
+create index idx_qrtz_ft_job_group on qrtz_fired_triggers(JOB_GROUP)
+GO
+
+create index idx_qrtz_t_nf_time_misfire on qrtz_triggers(MISFIRE_INSTR,NEXT_FIRE_TIME)
+GO
+
+create index idx_qrtz_t_nf_st_misfire on qrtz_triggers(MISFIRE_INSTR,NEXT_FIRE_TIME,TRIGGER_STATE)
+GO
+
+create index idx_qrtz_t_nf_st_misfire_grp on qrtz_triggers(MISFIRE_INSTR,NEXT_FIRE_TIME,TRIGGER_GROUP,TRIGGER_STATE)
+GO
