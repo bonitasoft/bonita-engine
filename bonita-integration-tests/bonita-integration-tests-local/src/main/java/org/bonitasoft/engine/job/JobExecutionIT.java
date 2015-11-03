94fcf9bf9045922a762187766dc1f077b935b302
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2015-11-03 12:52:00 +0100

    Fixed previous commit on test clean-up

--- bonita-integration-tests/bonita-integration-tests-local/src/main/java/org/bonitasoft/engine/job/JobExecutionIT.java
+++ bonita-integration-tests/bonita-integration-tests-local/src/main/java/org/bonitasoft/engine/job/JobExecutionIT.java
@@ -55,6 +55,7 @@ public class JobExecutionIT extends CommonAPILocalIT {
     public void before() throws Exception {
         loginOnDefaultTenantWithDefaultTechnicalUser();
 
+        createUser(USERNAME, PASSWORD);
         logoutOnTenant();
         loginOnDefaultTenantWith(USERNAME, PASSWORD);
         setSessionInfo(getSession());
