076104d2bf7b4980b61f0f16ca9b78deaff36dbf
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2016-02-24 18:05:14 +0100

    fix (BS-14961) StaleObjectStateException under thrad concurrency
    fixes [BS-14961](https://bonitasoft.atlassian.net/browse/BS-14921)
    
    * close entityManager after transaction completion
    * configure hibernate session context at thread level

---
+++ services/bonita-business-data/bonita-business-data-impl/src/test/java/org/bonitasoft/engine/business/data/impl/RemoveEntityManagerSynchronizationTest.java
@@ -0,0 +1,55 @@
+package org.bonitasoft.engine.business.data.impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import javax.persistence.EntityManager;
+
+import org.bonitasoft.engine.transaction.TransactionState;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * @author Laurent Leseigneur
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class RemoveEntityManagerSynchronizationTest {
+
+    @Mock
+    EntityManager entityManager;
+
+    @Test
+    public void should_keep_entityManager_open_before_commit() throws Exception {
+        //given
+        ThreadLocal<EntityManager> localManager = new ThreadLocal<>();
+        localManager.set(entityManager);
+        RemoveEntityManagerSynchronization removeEntityManagerSynchronization = new RemoveEntityManagerSynchronization(localManager);
+
+        //when
+        removeEntityManagerSynchronization.beforeCommit();
+
+        //then
+        verify(entityManager, times(0)).close();
+        assertThat(localManager.get()).as("should remove entity manager").isNotNull();
+
+    }
+
+    @Test
+    public void should_close_entityManager_after_transaction_completion() throws Exception {
+        //given
+        ThreadLocal<EntityManager> localManager = new ThreadLocal<>();
+        localManager.set(entityManager);
+        RemoveEntityManagerSynchronization removeEntityManagerSynchronization = new RemoveEntityManagerSynchronization(localManager);
+
+        //when
+        removeEntityManagerSynchronization.afterCompletion(TransactionState.ACTIVE);
+
+        //then
+        verify(entityManager).close();
+        assertThat(localManager.get()).as("should remove entity manager").isNull();
+
+    }
+}
