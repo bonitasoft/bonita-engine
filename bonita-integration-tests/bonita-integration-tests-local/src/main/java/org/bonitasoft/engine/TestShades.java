aa0c7996f61fee47c148bb821f02d3880e742792
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2016-03-18 17:49:33 +0100

    fix(platform_tests): fix bundle api call outside of tx
    Bundles used an API call that was outside a transaction. Restored it + fixed SQL resources dependencies

--- bonita-integration-tests/bonita-integration-tests-local/src/main/java/org/bonitasoft/engine/TestShades.java
+++ bonita-integration-tests/bonita-integration-tests-local/src/main/java/org/bonitasoft/engine/TestShades.java
@@ -86,6 +86,7 @@ public class TestShades {
         outputOfMaven = outputOfMaven.replaceAll(ORG_BONITASOFT_PLATFORM + ":platform-setup", "");
         outputOfMaven = outputOfMaven.replaceAll(ORG_BONITASOFT_PLATFORM + ":platform-common", ""); // FIXME: remove this once this module has been merged !!!
         outputOfMaven = outputOfMaven.replaceAll(ORG_BONITASOFT_PLATFORM + ":platform-configuration", ""); // FIXME: remove this once this module has been merged !!!
+        outputOfMaven = outputOfMaven.replaceAll(ORG_BONITASOFT_PLATFORM + ":platform-sql-resources", ""); // FIXME: remove this once this module has been merged !!!
         return outputOfMaven;
     }
 
