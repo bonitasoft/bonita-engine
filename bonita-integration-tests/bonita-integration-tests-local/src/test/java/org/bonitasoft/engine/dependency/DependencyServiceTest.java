722a6ee0beefd6f512be2c5c3ae6c16ac2a3190c
educhastenier <emmanuel.duchastenier@bonitasoft.com>
2016-05-10 10:25:55 +0200

    refactor(dependency) factorise and simplify code (#185)
    factorise code and use only the createMappedDependency of the dependency
    service to create dependencies

--- bonita-integration-tests/bonita-integration-tests-local/src/test/java/org/bonitasoft/engine/dependency/DependencyServiceTest.java
+++ bonita-integration-tests/bonita-integration-tests-local/src/test/java/org/bonitasoft/engine/dependency/DependencyServiceTest.java
@@ -13,18 +13,16 @@
  **/
 package org.bonitasoft.engine.dependency;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.bonitasoft.engine.bpm.CommonBPMServicesTest;
-import org.bonitasoft.engine.builder.BuilderFactory;
 import org.bonitasoft.engine.dependency.model.SDependency;
 import org.bonitasoft.engine.dependency.model.ScopeType;
-import org.bonitasoft.engine.dependency.model.builder.SDependencyBuilderFactory;
-import org.bonitasoft.engine.recorder.model.EntityUpdateDescriptor;
 import org.junit.Test;
 
 /**
@@ -42,38 +40,26 @@ public class DependencyServiceTest extends CommonBPMServicesTest {
 
     private final String defaultFileName = "dfv.cu";
 
-    private final byte[] defaultValue = new byte[] { 12, 33 };
-
-    private SDependency buildDefaultDependency() {
-        return BuilderFactory.get(SDependencyBuilderFactory.class)
-                .createNewInstance(defaultName, 2L, ScopeType.PROCESS, defaultFileName, defaultValue).done();
-    }
+    private final byte[] defaultValue = new byte[]{12, 33};
 
     @Test
-    public void testLifeCyle() throws Exception {
+    public void testLifeCycle() throws Exception {
         getTransactionService().begin();
 
-        final SDependency dependency = buildDefaultDependency();
-        dependencyService.createDependency(dependency);
+        SDependency mappedDependency = dependencyService.createMappedDependency(defaultName, defaultValue, defaultFileName, 2L, ScopeType.PROCESS);
 
-        assertEquals(dependency, dependencyService.getDependency(dependency.getId()));
+        List<Long> dependencyIds = dependencyService.getDependencyIds(2L, ScopeType.PROCESS, 0, 1);
 
-        final EntityUpdateDescriptor descriptor = new EntityUpdateDescriptor();
-        descriptor.addField(BuilderFactory.get(SDependencyBuilderFactory.class).getDescriptionKey(), "updated description");
-        descriptor.addField(BuilderFactory.get(SDependencyBuilderFactory.class).getFileNameKey(), "updated filename");
+        assertThat(mappedDependency.getFileName()).isEqualTo(defaultFileName);
+        assertThat(mappedDependency.getName()).isEqualTo("2_" + defaultName);
+        assertThat(mappedDependency.getValue()).isEqualTo(defaultValue);
+        assertThat(dependencyIds.get(0)).isEqualTo(mappedDependency.getId());
 
-        dependencyService.updateDependency(dependency, descriptor);
-
-        final SDependency updatedDependency = dependencyService.getDependency(dependency.getId());
-
-        assertEquals("updated description", updatedDependency.getDescription());
-        assertEquals("updated filename", updatedDependency.getFileName());
-
-        dependencyService.deleteDependency(dependency.getId());
+        dependencyService.deleteDependencies(2L, ScopeType.PROCESS);
 
         try {
-            dependencyService.getDependency(dependency.getId());
-            fail("dependency with id: " + dependency.getId() + " must not be found!");
+            dependencyService.getDependency(mappedDependency.getId());
+            fail("dependency with id: " + mappedDependency.getId() + " must not be found!");
         } catch (final SDependencyNotFoundException e) {
             // OK
         }
@@ -85,14 +71,9 @@ public class DependencyServiceTest extends CommonBPMServicesTest {
     public void testDeleteAllDependencies() throws Exception {
         getTransactionService().begin();
 
-        final SDependency dependency = buildDefaultDependency();
-        dependencyService.createDependency(dependency);
-
+        SDependency mappedDependency = dependencyService.createMappedDependency(defaultName, defaultValue, defaultFileName, 2L, ScopeType.PROCESS);
         dependencyService.deleteAllDependencies();
-
-        List<Long> ids = new ArrayList<Long>();
-        ids.add(dependency.getId());
-        assertEquals(0, dependencyService.getDependencies(ids).size());
+        assertEquals(0, dependencyService.getDependencies(Collections.singleton(mappedDependency.getId())).size());
 
         getTransactionService().complete();
     }
