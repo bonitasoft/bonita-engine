04b1c9956b407f7c849eb1b71428f2267f731c55
Baptiste Mesta <baptiste.mesta@gmail.com>
2016-05-12 17:03:15 +0200

    feat(paas_ready): change CLI behavior to push from known folder (#191)
    * feat(paas_ready): change CLI behavior to push from known folder
    
    now pushing on init from setup/platform_conf/initial
    pulling into setup/platform_conf/current
    pushing (after init) from setup/platform_conf/current
    
    Closes[BS-15300](https://bonitasoft.atlassian.net/browse/BS-15300)
    
    * fix(setup): fix setup.sh execution:
    * ensure deletion of directory platform_conf/current/ is recursive
    * support launch of script from any directory
    * fixed e2e SH tests according to the change of structure
    
    * fix(setup): fix SP assembly which lost the licenses folder
    
    * PR review feedback

--- bpm/bonita-api/bonita-server-api-http/src/main/java/org/bonitasoft/engine/api/internal/servlet/EngineInitializerListener.java
+++ bpm/bonita-api/bonita-server-api-http/src/main/java/org/bonitasoft/engine/api/internal/servlet/EngineInitializerListener.java
@@ -40,7 +40,7 @@ public class EngineInitializerListener implements ServletContextListener {
     @Override
     public void contextInitialized(final ServletContextEvent arg0) {
         try {
-            new PlatformSetup().setup(); // init tables and default configuration
+            new PlatformSetup().init(); // init tables and default configuration
             new EngineInitializer(PlatformTenantManager.getInstance(), new EngineInitializerProperties()).initializeEngine();
         } catch (final Exception e) {
             LOGGER.log(Level.SEVERE, "Error while initializing the Engine", e);
