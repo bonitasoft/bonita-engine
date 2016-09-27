f85c5e52dc743dec8df0a0cd14de779a2ded093b
Baptiste Mesta <baptiste.mesta@gmail.com>
2016-09-27 04:30:04 -0700

    tr(profiles): replace custom xml serialization with jaxb (#322)
    * tr(profiles): replace custom xml serialization with jaxb
    * make index not mandatory
    * use xs:all in membership

---
+++ bpm/bonita-core/bonita-process-engine/src/main/java/org/bonitasoft/engine/profile/impl/package-info.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C) 2016 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
+
+/**
+ * <p>
+ * Contains Actor mapping model classes.
+ * </p>
+ */
+//
+@XmlSchema(namespace = "http://www.bonitasoft.org/ns/profile/6.1", elementFormDefault = XmlNsForm.UNSET, xmlns = {
+        @javax.xml.bind.annotation.XmlNs(namespaceURI = "http://www.bonitasoft.org/ns/profile/6.1", prefix = "profiles") })
+package org.bonitasoft.engine.profile.impl;
+
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
