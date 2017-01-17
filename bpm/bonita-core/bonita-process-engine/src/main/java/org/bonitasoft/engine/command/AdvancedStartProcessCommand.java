00b2f6ccdca1c939ca3d14b15efc71d9462bcbda
Baptiste Mesta <baptiste.mesta@gmail.com>
2017-01-17 09:40:18 +0100

    feat(contract): add support for contract in adv start command (#441)
    the MultipleStartPointsProcessCommand was not supporting contract input,
    add support for that. Please note that only process contract input is
    useful for the command, not activity because the user task contract are
    set when executing the task that is in ready
    
    Closes [BS-16118](https://bonitasoft.atlassian.net/browse/BS-16118)

--- bpm/bonita-core/bonita-process-engine/src/main/java/org/bonitasoft/engine/command/AdvancedStartProcessCommand.java
+++ bpm/bonita-core/bonita-process-engine/src/main/java/org/bonitasoft/engine/command/AdvancedStartProcessCommand.java
@@ -25,16 +25,16 @@ import java.util.Map;
  * Example: {@code commandAPI.execute("advancedStartProcessCommand", parameters)}</p>
  * Parameters:
  * <ul>
- *      <li> started_by: the user id (long) is used as the process starter. It's a mandatory parameter.</li>
- *      <li> process_definition_id: the process definition id (long) identifies the process to start. It's a mandatory parameter.</li>
- *      <li> activity_name: the name of the activity (String) where the process will start the execution. It's a mandatory parameter.</li>
- *      <li> operations: the operations (ArrayList<Operation>) are executed when the process starts (set variables and documents). It's an optional parameter.</li>
- *      <li> context: the context (HashMap<String, Serializable>) is used during operations execution. It's an optional parameter.</li>
+ * <li> started_by: the user id (long) is used as the process starter. It's a mandatory parameter.</li>
+ * <li> process_definition_id: the process definition id (long) identifies the process to start. It's a mandatory parameter.</li>
+ * <li> activity_name: the name of the activity (String) where the process will start the execution. It's a mandatory parameter.</li>
+ * <li> operations: the operations (ArrayList<Operation>) are executed when the process starts (set variables and documents). It's an optional parameter.</li>
+ * <li> context: the context (HashMap<String, Serializable>) is used during operations execution. It's an optional parameter.</li>
  * </ul>
  * Limitations:
  * <ul>
- *      <li> It is not possible to start the execution of a process from a gateway, a boundary event or an event sub-process</li>
- *      <li> The process must be started when there is only one active branch. Otherwise use {@code MultipleStartPointsProcessCommand}</li>
+ * <li> It is not possible to start the execution of a process from a gateway, a boundary event or an event sub-process</li>
+ * <li> The process must be started when there is only one active branch. Otherwise use {@code MultipleStartPointsProcessCommand}</li>
  * </ul>
  * Example:
  * start -> step1 -> gateway1 -> (step2 || step3) -> gateway2 -> step4 -> end
