7d07fba7ca34241a14ef2cc050d865a596c07e02
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2017-04-04 17:45:56 +0200

    tr(recorder): simplify code and remove useless trace
    traces methods are pretty useless in the way that there is no context
    information.
    it only makes the code unreadable.
    This in a first pull request before making the recorder service create
    the update/delete/insert event it fire

--- services/bonita-recorder/bonita-recorder-impl/src/main/java/org/bonitasoft/engine/recorder/impl/RecorderImpl.java
+++ services/bonita-recorder/bonita-recorder-impl/src/main/java/org/bonitasoft/engine/recorder/impl/RecorderImpl.java
@@ -15,11 +15,10 @@ package org.bonitasoft.engine.recorder.impl;
 
 import java.util.List;
 
-import org.bonitasoft.engine.commons.LogUtil;
 import org.bonitasoft.engine.events.EventService;
-import org.bonitasoft.engine.events.model.SFireEventException;
 import org.bonitasoft.engine.events.model.SDeleteEvent;
 import org.bonitasoft.engine.events.model.SEvent;
+import org.bonitasoft.engine.events.model.SFireEventException;
 import org.bonitasoft.engine.events.model.SInsertEvent;
 import org.bonitasoft.engine.events.model.SUpdateEvent;
 import org.bonitasoft.engine.log.technical.TechnicalLogSeverity;
@@ -47,7 +46,8 @@ public class RecorderImpl implements Recorder {
 
     private final EventService eventService;
 
-    public RecorderImpl(final PersistenceService persistenceService, final TechnicalLoggerService logger, final EventService eventService) {
+    public RecorderImpl(final PersistenceService persistenceService, final TechnicalLoggerService logger,
+            final EventService eventService) {
         this.persistenceService = persistenceService;
         this.logger = logger;
         this.eventService = eventService;
@@ -55,125 +55,74 @@ public class RecorderImpl implements Recorder {
 
     @Override
     public void recordInsert(final InsertRecord record, final SInsertEvent insertEvent) throws SRecorderException {
-        final String methodName = "recordInsert";
-
-        traceBeforeMethod(methodName);
         try {
             persistenceService.insert(record.getEntity());
             fireEvent(insertEvent);
-            traceAfterMethod(methodName);
-        } catch (final SFireEventException e) {
-            logFireEventExceptionAndThrowSRecorderException(e, methodName);
         } catch (final Exception e) {
-            traceException(methodName, e);
+            logExceptionsFromHandlers(e);
             throw new SRecorderException(e);
         }
     }
 
     @Override
-    public void recordBatchInsert(final BatchInsertRecord record, final SInsertEvent insertEvent) throws SRecorderException {
-        final String methodName = "recordBatchInsert";
-
-        traceBeforeMethod(methodName);
+    public void recordBatchInsert(final BatchInsertRecord record, final SInsertEvent insertEvent)
+            throws SRecorderException {
         try {
             persistenceService.insertInBatch(record.getEntity());
             fireEvent(insertEvent);
-            traceAfterMethod(methodName);
-        } catch (final SFireEventException e) {
-            logFireEventExceptionAndThrowSRecorderException(e, methodName);
         } catch (final Exception e) {
-            traceException(methodName, e);
+            logExceptionsFromHandlers(e);
             throw new SRecorderException(e);
         }
     }
 
     @Override
     public void recordDelete(final DeleteRecord record, final SDeleteEvent deleteEvent) throws SRecorderException {
-        final String methodName = "recordDelete";
-
-        traceBeforeMethod(methodName);
         try {
             persistenceService.delete(record.getEntity());
             fireEvent(deleteEvent);
-            traceAfterMethod(methodName);
-        } catch (final SFireEventException e) {
-            logFireEventExceptionAndThrowSRecorderException(e, methodName);
         } catch (final Exception e) {
-            traceException(methodName, e);
+            logExceptionsFromHandlers(e);
             throw new SRecorderException(e);
         }
     }
 
     @Override
     public void recordDeleteAll(final DeleteAllRecord record) throws SRecorderException {
-        final String methodName = "recordDeleteAll";
-
-        traceBeforeMethod(methodName);
         try {
             persistenceService.deleteByTenant(record.getEntityClass(), record.getFilters());
-            traceAfterMethod(methodName);
         } catch (final Exception e) {
-            traceException(methodName, e);
+            logExceptionsFromHandlers(e);
             throw new SRecorderException(e);
         }
     }
 
     @Override
     public void recordUpdate(final UpdateRecord record, final SUpdateEvent updateEvent) throws SRecorderException {
-        String methodName = "recordUpdate";
-
-        traceBeforeMethod(methodName);
         final UpdateDescriptor desc = UpdateDescriptor.buildSetFields(record.getEntity(), record.getFields());
         try {
             persistenceService.update(desc);
             fireEvent(updateEvent);
-            traceAfterMethod(methodName);
-        } catch (final SFireEventException e) {
-            logFireEventExceptionAndThrowSRecorderException(e, methodName);
         } catch (final Exception e) {
-            traceException(methodName, e);
-            // FIXME what to do if some handlers fail?
+            logExceptionsFromHandlers(e);
             throw new SRecorderException(e);
         }
     }
 
-    protected void logFireEventExceptionAndThrowSRecorderException(final SFireEventException e, String methodName) throws SRecorderException {
-        final List<Exception> handlerExceptions = e.getHandlerExceptions();
-        for (Exception exception : handlerExceptions) {
-            traceException(methodName, exception);
+    private void logExceptionsFromHandlers(final Exception e)
+            throws SRecorderException {
+        if (!(e instanceof SFireEventException)) {
+            return;
         }
-        throw new SRecorderException(e);
+        final List<Exception> handlerExceptions = ((SFireEventException) e).getHandlerExceptions();
+        for (Exception exception : handlerExceptions) {
+            logger.log(this.getClass(), TechnicalLogSeverity.TRACE, "error while executing handler", exception);
+        }
     }
 
     private void fireEvent(final SEvent evt) throws SFireEventException {
         if (evt != null) {
             eventService.fireEvent(evt);
-        }
-    }
-
-    private boolean isTraceLoggable() {
-        return logger.isLoggable(this.getClass(), TechnicalLogSeverity.TRACE);
-    }
-
-    private void logTrace(final String text) {
-        logger.log(this.getClass(), TechnicalLogSeverity.TRACE, text);
-    }
-
-    private void traceException(final String methodName, final Exception e) {
-        if (isTraceLoggable()) {
-            logTrace(LogUtil.getLogOnExceptionMethod(this.getClass(), methodName, e));
-        }
-    }
-
-    private void traceAfterMethod(final String methodName) {
-        if (isTraceLoggable()) {
-            logTrace(LogUtil.getLogAfterMethod(this.getClass(), methodName));
-        }
-    }
-
-    private void traceBeforeMethod(final String methodName) {
-        if (isTraceLoggable()) {
-            logTrace(LogUtil.getLogBeforeMethod(this.getClass(), methodName));
         }
     }
 }
