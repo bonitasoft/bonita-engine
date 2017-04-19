adae512d1129a3df857a83e0cfe51d943ff08377
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2017-04-19 10:47:29 +0200

    fix(test): wrong assertion on expected sort
     * mysql and postgres order by sort "_" after a-z
     * check expected size but not expected order that may differ
    
     Closes [BS-16187](https://bonitasoft.atlassian.net/browse/BS-16187)

--- bonita-integration-tests/bonita-integration-tests-client/src/main/java/org/bonitasoft/engine/search/SearchActivityInstanceIT.java
+++ bonita-integration-tests/bonita-integration-tests-client/src/main/java/org/bonitasoft/engine/search/SearchActivityInstanceIT.java
@@ -14,7 +14,6 @@
 package org.bonitasoft.engine.search;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.tuple;
 import static org.bonitasoft.engine.test.TestStates.SKIPPED;
 import static org.junit.Assert.*;
 
@@ -1224,7 +1223,7 @@ public class SearchActivityInstanceIT extends TestWithUser {
         final SearchResult<HumanTaskInstance> searchHumanTaskInstancesWithEscapeCharacter = getProcessAPI().searchHumanTaskInstances(builder.done());
         assertEquals(3, searchHumanTaskInstancesWithEscapeCharacter.getCount());
         List<HumanTaskInstance> tasks = searchHumanTaskInstancesWithEscapeCharacter.getResult();
-        assertThat(tasks).extracting("name").containsExactly("step#1_b", "step#1_c", "step#1a");
+        assertThat(tasks).extracting("name").containsOnly("step#1_b", "step#1_c", "step#1a");
 
         builder = new SearchOptionsBuilder(0, 10);
         builder.sort(HumanTaskInstanceSearchDescriptor.NAME, Order.ASC);
@@ -1232,7 +1231,7 @@ public class SearchActivityInstanceIT extends TestWithUser {
         final SearchResult<HumanTaskInstance> searchHumanTaskInstancesWithUnderscoreCharacter = getProcessAPI().searchHumanTaskInstances(builder.done());
         assertEquals(2, searchHumanTaskInstancesWithUnderscoreCharacter.getCount());
         tasks = searchHumanTaskInstancesWithUnderscoreCharacter.getResult();
-        assertThat(tasks).extracting("name").containsExactly("step#1_b", "step#1_c");
+        assertThat(tasks).extracting("name").containsOnly("step#1_b", "step#1_c");
 
         builder = new SearchOptionsBuilder(0, 10);
         builder.sort(HumanTaskInstanceSearchDescriptor.NAME, Order.ASC);
@@ -1240,7 +1239,7 @@ public class SearchActivityInstanceIT extends TestWithUser {
         final SearchResult<HumanTaskInstance> searchHumanTaskInstancesWithPercentageCharacter = getProcessAPI().searchHumanTaskInstances(builder.done());
         assertEquals(2, searchHumanTaskInstancesWithPercentageCharacter.getCount());
         tasks = searchHumanTaskInstancesWithPercentageCharacter.getResult();
-        assertThat(tasks).extracting("name").containsExactly("%step#2", "%step#4_a");
+        assertThat(tasks).extracting("name").containsOnly("%step#2", "%step#4_a");
 
         builder = new SearchOptionsBuilder(0, 10);
         builder.sort(HumanTaskInstanceSearchDescriptor.NAME, Order.ASC);
