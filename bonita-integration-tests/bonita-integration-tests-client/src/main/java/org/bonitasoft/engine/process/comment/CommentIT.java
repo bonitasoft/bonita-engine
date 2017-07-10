180f509b54b16ff8225d3b6981cf60b71194ecbd
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2017-07-10 10:13:42 +0200

    chore(tests): stabilize and merge tests on comments (#684)
    * add search order to make test more stable
    * merge 3 tests in 1 to save the test setup time

--- bonita-integration-tests/bonita-integration-tests-client/src/main/java/org/bonitasoft/engine/process/comment/CommentIT.java
+++ bonita-integration-tests/bonita-integration-tests-client/src/main/java/org/bonitasoft/engine/process/comment/CommentIT.java
@@ -16,9 +16,7 @@ package org.bonitasoft.engine.process.comment;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.bonitasoft.engine.TestWithUser;
@@ -30,29 +28,15 @@ import org.bonitasoft.engine.bpm.process.DesignProcessDefinition;
 import org.bonitasoft.engine.bpm.process.ProcessDefinition;
 import org.bonitasoft.engine.bpm.process.ProcessInstance;
 import org.bonitasoft.engine.bpm.process.impl.ProcessDefinitionBuilder;
+import org.bonitasoft.engine.search.Order;
 import org.bonitasoft.engine.search.SearchOptionsBuilder;
 import org.bonitasoft.engine.search.SearchResult;
-import org.bonitasoft.engine.test.BuildTestUtil;
 import org.junit.Test;
 
 public class CommentIT extends TestWithUser {
 
     @Test
-    public void addComment() throws Exception {
-        final DesignProcessDefinition designProcessDefinition = BuildTestUtil.buildProcessDefinitionWithHumanAndAutomaticSteps(Arrays.asList("step1", "step2"),
-                Arrays.asList(true, true));
-
-        final ProcessDefinition processDefinition = deployAndEnableProcessWithActor(designProcessDefinition, ACTOR_NAME, user);
-        final ProcessInstance pi0 = getProcessAPI().startProcess(processDefinition.getId());
-        final String commentContent = "abc";
-        final Comment comment = getProcessAPI().addProcessComment(pi0.getId(), commentContent);
-        assertNotNull(comment);
-        assertEquals(commentContent, comment.getContent());
-        disableAndDeleteProcess(processDefinition);
-    }
-
-    @Test
-    public void should_be_able_to_search_comments() throws Exception {
+    public void should_be_able_to_add_search_and_archive_comments() throws Exception {
         final ProcessDefinitionBuilder processBuilder = new ProcessDefinitionBuilder().createNewInstance(PROCESS_NAME, PROCESS_VERSION);
         processBuilder.addActor(ACTOR_NAME);
         String taskName = "userTask1";
@@ -64,18 +48,30 @@ public class CommentIT extends TestWithUser {
         final List<Comment> originalComments = getProcessAPI().searchComments(
                 new SearchOptionsBuilder(0, 100).filter(SearchCommentsDescriptor.PROCESS_INSTANCE_ID, pi0.getId()).done()).getResult();
         // add comments
-        getProcessAPI().addProcessComment(pi0.getId(), "added comment 0");
+        final String commentContent = "added comment 0";
+        final Comment comment = getProcessAPI().addProcessComment(pi0.getId(), commentContent);
+        assertNotNull(comment);
+        assertEquals(commentContent, comment.getContent());
         getProcessAPI().addProcessComment(pi0.getId(), "added comment 1");
         Comment comment2 = getProcessAPI().addProcessComment(pi0.getId(), "added comment 2");
         // get comments again.
         final List<Comment> commentsAfterAdd = getProcessAPI().searchComments(
-                new SearchOptionsBuilder(0, 100).filter(SearchCommentsDescriptor.PROCESS_INSTANCE_ID, pi0.getId()).done()).getResult();
+                new SearchOptionsBuilder(0, 100)
+                        .filter(SearchCommentsDescriptor.PROCESS_INSTANCE_ID, pi0.getId())
+                        .sort(SearchCommentsDescriptor.POSTDATE, Order.ASC)
+                        .done())
+                .getResult();
         assertThat(commentsAfterAdd).as("should have 3 more comments").hasSize(originalComments.size() + 3);
         assertThat(commentsAfterAdd.get(1).getContent()).isEqualTo("added comment 1");
 
         // Archive comments
         waitForUserTaskAndExecuteIt(pi0, taskName, user);
         waitForProcessToFinish(pi0);
+
+        final SearchResult<ArchivedComment> searchArchivedComments = getProcessAPI().searchArchivedComments(
+                new SearchOptionsBuilder(0, 10).filter(ArchivedCommentsSearchDescriptor.PROCESS_INSTANCE_ID, pi0.getId()).done());
+        assertThat(searchArchivedComments.getCount()).as("At least 3 comments should have been retrieved (+ possible automatic comments)")
+                .isGreaterThanOrEqualTo(3);
 
         // ensure we can retrieve archived comments using the SOURCE_OBJECT_ID field
         final SearchResult<ArchivedComment> searchArchivedCommentsBySourceObjectId = getProcessAPI().searchArchivedComments(
@@ -92,24 +88,4 @@ public class CommentIT extends TestWithUser {
         disableAndDeleteProcess(processDefinition);
     }
 
-    @Test
-    public void allCommentsAreArchived() throws Exception {
-        final ProcessDefinitionBuilder processBuilder = new ProcessDefinitionBuilder().createNewInstance("allCommentsAreArchived", "0.2");
-        processBuilder.addActor(ACTOR_NAME);
-        final String activityName = "maTache";
-        final DesignProcessDefinition designProcessDefinition = processBuilder.addUserTask(activityName, ACTOR_NAME).getProcess();
-        final ProcessDefinition processDefinition = deployAndEnableProcessWithActor(designProcessDefinition, ACTOR_NAME, user);
-
-        final ProcessInstance pi = getProcessAPI().startProcess(processDefinition.getId());
-        for (int i = 0; i < 21; i++) {
-            getProcessAPI().addProcessComment(pi.getId(), "myComment_" + i);
-        }
-        waitForUserTaskAndExecuteIt(pi, activityName, user);
-        waitForProcessToFinish(pi);
-        final SearchResult<ArchivedComment> searchArchivedComments = getProcessAPI().searchArchivedComments(
-                new SearchOptionsBuilder(0, 30).filter(ArchivedCommentsSearchDescriptor.PROCESS_INSTANCE_ID, pi.getId()).done());
-        assertTrue("At least 21 comments should have been retrieved (+ possible automatic comments)", searchArchivedComments.getCount() >= 21);
-
-        disableAndDeleteProcess(processDefinition);
-    }
 }
