a136df5af7081bb9de44f0931a4464b6083549d7
Thomas Bouffard <thomas.bouffard@bonitasoft.com>
2017-10-12 10:55:28 +0200

    chore(test): shade test displays maven logs live (#774)
    * chore(test): shade test displays maven logs live
    
    Previously, the command may take a while to retrieve dependencies. There
    was no timeout and we did not see the maven logs live to understand what
    was going on.
    
    Now, the maven logs are displayed as far as they are available. This is
    done using slf4j.
    
    In addition, a timeout has been set when running the command to avoid
    the test to hang forever.
    
    * Only perform one maven command and do it offline
    
    Artifacts are supposed to have been built before running the test, so
    they are already in the local repository and we can run offline.

--- bonita-integration-tests/bonita-integration-tests-local/src/main/java/org/bonitasoft/engine/TestShades.java
+++ bonita-integration-tests/bonita-integration-tests-local/src/main/java/org/bonitasoft/engine/TestShades.java
@@ -13,67 +13,96 @@
  **/
 package org.bonitasoft.engine;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.SystemUtils;
 import org.bonitasoft.engine.io.IOUtil;
 import org.bonitasoft.engine.test.BPMLocalIT;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TestShades {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestShades.class);
 
     private static final String ORG_BONITASOFT_ENGINE = "org.bonitasoft.engine";
     protected static final String ORG_BONITASOFT_PLATFORM = "org.bonitasoft.platform";
 
     @Test
-    public void testShades() throws IOException {
+    public void testShades() throws Exception {
+        String mvn = getMavenExecutable();
+        LOG.info("Used mvn path: {}", mvn);
+
         String version = BPMLocalIT.getBonitaVersion();
+        LOG.info("Detected Bonita version: {}", version);
 
+        File workingDirectory = new File("target", "shadeTester");
+        workingDirectory.delete();
+        FileUtils.forceMkdir(workingDirectory);
+        File pomFile = new File(workingDirectory, "pom.xml");
+        LOG.info("Building {}", pomFile.getAbsolutePath());
+        IOUtil.writeContentToFile(generatePomContent(version), pomFile);
 
-        String thePom = getPom(version);
-        File file = new File("shadeTester");
-        file.mkdir();
-        String outputOfMaven;
-        try {
-            File file2 = new File(file, "pom.xml");
-            IOUtil.writeContentToFile(thePom, file2);
-            String mvn = getMavenExecutable();
-            System.out.println("mvn path used: " + mvn);
-            System.out.println("bonita version detected: " + version);
-            System.out.println("building " + file2.getAbsolutePath());
-            System.out.println("Run mvn in " + file.getAbsolutePath());
-            String localRepository = System.getProperty("localRepository");
-            String jvmProperties = "";
-            if (localRepository != null) {
-                System.out.println("Using overridden localRepository: " + localRepository);
-                jvmProperties += " -Dmaven.repo.local=" + localRepository;
-            }
-            String command = mvn + " dependency:tree" + jvmProperties;
-            System.out.println("executing "+command);
-            Process exec = Runtime.getRuntime().exec(command, null, file);
-            outputOfMaven = getOutputOfProcess(exec);
-            System.out.println(outputOfMaven);
-        } finally {
-            IOUtil.deleteDir(file);
+        String localRepository = System.getProperty("localRepository");
+        String jvmProperties = "";
+        if (localRepository != null) {
+            LOG.info("Using overridden localRepository: {}", localRepository);
+            jvmProperties += " -Dmaven.repo.local=" + localRepository;
         }
-        assertTrue("build was not successfull", outputOfMaven.contains("BUILD SUCCESS"));
-        outputOfMaven = outputOfMaven.replaceAll("\n?.*Downloading.*\n", "");
-        outputOfMaven = outputOfMaven.replaceAll("\n?.*Downloaded.*\n", "");
-        outputOfMaven = removedIgnoredBonitaDeps(outputOfMaven);
-        if (outputOfMaven.contains("bonitasoft")) {
+
+        LOG.info("Get the dependency tree"); // they are supposed to have been built before running the test
+        final String mvnCommand = mvn + " -o dependency:tree" + jvmProperties;
+        String mavenOutput = executeCommand(mvnCommand, workingDirectory, 1);
+        LOG.debug("Maven output: {}", mavenOutput);
+
+        assertThat(mavenOutput).as("Maven output").contains("BUILD SUCCESS");
+        mavenOutput = mavenOutput.replaceAll("\n?.*Downloading.*\n", "");
+        mavenOutput = mavenOutput.replaceAll("\n?.*Downloaded.*\n", "");
+        mavenOutput = removedIgnoredBonitaDeps(mavenOutput);
+        if (mavenOutput.contains("bonitasoft")) {
             String str = "bonitasoft";
-            int indexOf = outputOfMaven.indexOf(str);
-            String part1 = outputOfMaven.substring(indexOf - 50, indexOf - 1);
-            String part2 = outputOfMaven.substring(indexOf - 1, indexOf + str.length());
-            String part3 = outputOfMaven.substring(indexOf + str.length(), indexOf + str.length() + 50);
+            int indexOf = mavenOutput.indexOf(str);
+            String part1 = mavenOutput.substring(indexOf - 50, indexOf - 1);
+            String part2 = mavenOutput.substring(indexOf - 1, indexOf + str.length());
+            String part3 = mavenOutput.substring(indexOf + str.length(), indexOf + str.length() + 50);
             fail("the dependency tree contains other modules than server/client/common: \"" + part1 + " =====>>>>>" + part2 + " <<<<<=====" + part3);
         }
+    }
 
+    private static String executeCommand(String command, File workingDirectory, long timeoutInMinutes)
+            throws InterruptedException, IOException {
+        LOG.info("Running command {} in {}", command, workingDirectory.getAbsolutePath());
+
+        Process process = Runtime.getRuntime().exec(command, null, workingDirectory);
+        InputStream processInputStream = process.getInputStream();
+        StreamGobbler outputGobbler = new StreamGobbler(processInputStream, "OUTPUT");
+        outputGobbler.start();
+        InputStream processErrorStream = process.getErrorStream();
+        StreamGobbler errorGobbler = new StreamGobbler(processErrorStream, "ERROR");
+        errorGobbler.start();
+
+        LOG.info("Waiting at most {} minutes", timeoutInMinutes);
+        boolean exitNormally = process.waitFor(timeoutInMinutes, TimeUnit.MINUTES);
+        LOG.info("Exit normally?: {}", exitNormally);
+
+        IOUtils.closeQuietly(processInputStream);
+        IOUtils.closeQuietly(processErrorStream);
+
+        outputGobbler.join();
+        errorGobbler.join();
+        return outputGobbler.getOutputAsString();
     }
 
     private String getMavenExecutable() throws IOException {
@@ -83,37 +112,23 @@ public class TestShades {
         } else {
             return pathToRoot + File.separator + "mvnw";
         }
-
     }
 
     protected String getPathToRoot() {
-        File file = new File("pom.xml");
-        return file.getAbsoluteFile().getParentFile().getParentFile().getParentFile().getAbsolutePath();
+        File currentPom = new File("pom.xml");
+        return currentPom.getAbsoluteFile().getParentFile().getParentFile().getParentFile().getAbsolutePath();
     }
 
-    private String getOutputOfProcess(Process exec) throws IOException {
-        InputStream inputStream = exec.getInputStream();
-        exec.getOutputStream().close();
-        InputStream errorStream = exec.getErrorStream();
-        System.err.println(IOUtil.read(errorStream));
-        errorStream.close();
-
-        String outputOfMaven;
-        outputOfMaven = IOUtil.read(inputStream);
-        inputStream.close();
-        return outputOfMaven;
+    protected String removedIgnoredBonitaDeps(final String mavenOutput) {
+        String filteredOutput = mavenOutput.replaceAll(ORG_BONITASOFT_ENGINE + ":bonita-server", "");
+        filteredOutput = filteredOutput.replaceAll(ORG_BONITASOFT_ENGINE + ":bonita-client", "");
+        filteredOutput = filteredOutput.replaceAll(ORG_BONITASOFT_ENGINE + ":bonita-common", "");
+        filteredOutput = filteredOutput.replaceAll(ORG_BONITASOFT_PLATFORM + ":platform-setup", "");
+        filteredOutput = filteredOutput.replaceAll(ORG_BONITASOFT_PLATFORM + ":platform-resources", "");
+        return filteredOutput;
     }
 
-    protected String removedIgnoredBonitaDeps(String outputOfMaven) {
-        outputOfMaven = outputOfMaven.replaceAll(ORG_BONITASOFT_ENGINE + ":bonita-server", "");
-        outputOfMaven = outputOfMaven.replaceAll(ORG_BONITASOFT_ENGINE + ":bonita-client", "");
-        outputOfMaven = outputOfMaven.replaceAll(ORG_BONITASOFT_ENGINE + ":bonita-common", "");
-        outputOfMaven = outputOfMaven.replaceAll(ORG_BONITASOFT_PLATFORM + ":platform-setup", "");
-        outputOfMaven = outputOfMaven.replaceAll(ORG_BONITASOFT_PLATFORM + ":platform-resources", "");
-        return outputOfMaven;
-    }
-
-    private String getPom(final String version) {
+    private String generatePomContent(final String version) {
         String pom = "<project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n";
         pom += "    xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">\n";
         pom += "<modelVersion>4.0.0</modelVersion>\n";
@@ -122,15 +137,15 @@ public class TestShades {
         pom += "<version>0.0.1-SNAPSHOT</version>\n";
         pom += "<dependencies>\n";
         pom += generateDependencies(version);
-        pom += "    </dependencies>\n";
-        pom += "</project>    \n";
+        pom += "</dependencies>\n";
+        pom += "</project>\n";
         return pom;
     }
 
     protected String generateDependencies(final String version) {
-        String pom2 = generateDependency("bonita-server", ORG_BONITASOFT_ENGINE, version);
-        pom2 += generateDependency("bonita-client", ORG_BONITASOFT_ENGINE, version);
-        return pom2;
+        String dependencies = generateDependency("bonita-server", ORG_BONITASOFT_ENGINE, version);
+        dependencies += generateDependency("bonita-client", ORG_BONITASOFT_ENGINE, version);
+        return dependencies;
     }
 
     protected String generateDependency(final String artifactId, final String groupId, final String version) {
@@ -141,6 +156,42 @@ public class TestShades {
         dep += "</dependency>\n";
 
         return dep;
+    }
+
+    private static class StreamGobbler extends Thread {
+
+        private static final Logger LOG = LoggerFactory.getLogger(StreamGobbler.class);
+
+        private InputStream is;
+        private String type;
+
+        private String ouput;
+
+        public StreamGobbler(InputStream is, String type) {
+            this.is = is;
+            this.type = type;
+        }
+
+        public void run() {
+            try (StringWriter outputWriter = new StringWriter()) {
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr);
+                String line;
+                while ((line = br.readLine()) != null) {
+                    LOG.info("{} > {}", type, line);
+                    outputWriter.append(line);
+                }
+                outputWriter.flush();
+                ouput = outputWriter.toString();
+            } catch (Exception e) {
+                LOG.error(type + " >", e);
+            }
+        }
+
+        public String getOutputAsString() {
+            return ouput;
+        }
+
     }
 
 }
