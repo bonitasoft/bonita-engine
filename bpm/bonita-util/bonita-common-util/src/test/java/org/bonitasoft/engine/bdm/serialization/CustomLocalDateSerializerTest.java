d1587502804b7bb713a9342e620774ed0e9e520a
Thomas Bouffard <thomas.bouffard@bonitasoft.com>
2017-11-07 14:48:01 +0100

    refactor(bdm): remove adherence to jackson in deserializer code (#790)
    Remove the jackson exception from the deserializer method signature
    - to reduce adherence
    - because they are redundant: they extends IOException which is also declared in the method signatures
    - Use non deprecated MockitoJUnitRunner
    - Remove unused constructors

--- bpm/bonita-util/bonita-common-util/src/test/java/org/bonitasoft/engine/bdm/serialization/CustomLocalDateSerializerTest.java
+++ bpm/bonita-util/bonita-common-util/src/test/java/org/bonitasoft/engine/bdm/serialization/CustomLocalDateSerializerTest.java
@@ -21,7 +21,7 @@ import java.time.LocalDate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 
