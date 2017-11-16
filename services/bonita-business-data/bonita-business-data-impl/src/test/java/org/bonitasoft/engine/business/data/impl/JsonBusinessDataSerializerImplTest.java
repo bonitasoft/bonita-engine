1766578b96dd607bc0ecf83bbc16fd2cd99b6313
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2017-11-16 17:28:02 +0100

    tr(tests): replace json-unit-assertj by json-unit-fluent
    json-unit-assertj is not maintained anymore, in favor to json-unit-fluent:
    * developed by the same team
    * it has the same syntax.
    
    It will allows to use specific utilitary methods on Json unit test.
    
    Closes [BS-17556](https://bonitasoft.atlassian.net/browse/BS-17556)

--- services/bonita-business-data/bonita-business-data-impl/src/test/java/org/bonitasoft/engine/business/data/impl/JsonBusinessDataSerializerImplTest.java
+++ services/bonita-business-data/bonita-business-data-impl/src/test/java/org/bonitasoft/engine/business/data/impl/JsonBusinessDataSerializerImplTest.java
@@ -13,7 +13,7 @@
  **/
 package org.bonitasoft.engine.business.data.impl;
 
-import static net.javacrumbs.jsonunit.assertj.JsonAssert.assertThatJson;
+import static net.javacrumbs.jsonunit.fluent.JsonFluentAssert.assertThatJson;
 import static org.mockito.Mockito.mock;
 
 import java.io.IOException;
