e65f55f9f7244fe89ccce6765467a58af0a82546
Thomas Bouffard <thomas.bouffard@bonitasoft.com>
2017-11-28 16:34:54 +0100

    api(cleanup): simplify code and fix warnings
    - move all related xml conversion code in a dedicated class + wrap internal
    serialization exception into a bonita runtime exception to hide. This
    allow better separation of concerns, make the code cleaner and easier to
    test
    - use multi-catch, try-with-resource and
    - make some private methods private
    - simplify methods signature (exceptions)
    
    Relates to [BS-17624](https://bonitasoft.atlassian.net/browse/BS-17624)

--- bpm/bonita-api/bonita-client-api/src/main/java/org/bonitasoft/engine/api/HTTPServerAPI.java
+++ bpm/bonita-api/bonita-client-api/src/main/java/org/bonitasoft/engine/api/HTTPServerAPI.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2015 BonitaSoft S.A.
+ * Copyright (C) 2015-2018 BonitaSoft S.A.
  * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
  * This library is free software; you can redistribute it and/or modify it under the terms
  * of the GNU Lesser General Public License as published by the Free Software Foundation
@@ -19,9 +19,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
 import java.lang.reflect.UndeclaredThrowableException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
@@ -43,16 +40,12 @@ import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.PoolingClientConnectionManager;
 import org.apache.http.message.BasicNameValuePair;
+import org.bonitasoft.engine.api.impl.XmlConverter;
 import org.bonitasoft.engine.api.internal.ServerAPI;
 import org.bonitasoft.engine.api.internal.ServerWrappedException;
 import org.bonitasoft.engine.bpm.bar.BusinessArchive;
-import org.bonitasoft.engine.exception.BonitaRuntimeException;
 import org.bonitasoft.engine.exception.StackTraceTransformer;
 import org.bonitasoft.engine.http.BonitaResponseHandler;
-
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.mapper.CannotResolveClassException;
-import com.thoughtworks.xstream.security.AnyTypePermission;
 
 /**
  * @author Baptiste Mesta
@@ -64,7 +57,10 @@ public class HTTPServerAPI implements ServerAPI {
 
     private static final long serialVersionUID = -3375874140999200702L;
 
+    private static final Logger LOGGER = Logger.getLogger(HTTPServerAPI.class.getName());
+
     private static final String UTF_8 = "UTF-8";
+    private static final Charset UTF8 = Charset.forName("UTF-8");
 
     private static final String CLASS_NAME_PARAMETERS = "classNameParameters";
 
@@ -80,6 +76,7 @@ public class HTTPServerAPI implements ServerAPI {
 
     private static final String SERVER_API = "/serverAPI/";
 
+    // package-private for testing purpose
     static final String SERVER_URL = "server.url";
 
     private static final String BASIC_AUTHENTICATION_ACTIVE = "basicAuthentication.active";
@@ -88,36 +85,27 @@ public class HTTPServerAPI implements ServerAPI {
 
     private static final String BASIC_AUTHENTICATION_PASSWORD = "basicAuthentication.password";
 
+    // package-private for testing purpose
     static final String APPLICATION_NAME = "application.name";
 
-    private static final Logger LOGGER = Logger.getLogger(HTTPServerAPI.class.getName());
+    private final String serverUrl;
 
-    private String serverUrl = null;
+    private final String applicationName;
 
-    private String applicationName = null;
+    private final boolean basicAuthenticationActive;
 
-    private boolean basicAuthenticationActive = false;
+    private final String basicAuthenticationUserName;
 
-    private String basicAuthenticationUserName = null;
-
-    private String basicAuthenticationPassword = null;
+    private final String basicAuthenticationPassword;
 
     private static DefaultHttpClient httpclient;
 
-    private static final XStream XSTREAM;
-
-    private static final Charset UTF8 = Charset.forName("UTF-8");
-
     private static final ResponseHandler<String> RESPONSE_HANDLER = new BonitaResponseHandler();
 
-    static {
-        XSTREAM = new XStream();
-        XStream.setupDefaultSecurity(XSTREAM);
-        XSTREAM.addPermission(AnyTypePermission.ANY);
-        XSTREAM.registerConverter(new BonitaStackTraceElementConverter(), XStream.PRIORITY_VERY_HIGH);
-    }
+    private final XmlConverter xmlConverter;
 
     public HTTPServerAPI(final Map<String, String> parameters) {
+        xmlConverter = new XmlConverter();
         // initialize httpclient in the constructor to avoid incompatibility when running tests:
         // java.security.NoSuchAlgorithmException: class configured for SSLContext: sun.security.ssl.SSLContextImpl$TLS10Context not a SSLContext
         if (httpclient == null) {
@@ -135,8 +123,8 @@ public class HTTPServerAPI implements ServerAPI {
             final List<String> classNameParameters, final Object[] parametersValues) throws ServerWrappedException {
         String response = null;
         try {
-            response = executeHttpPost(options, apiInterfaceName, methodName, classNameParameters, parametersValues, XSTREAM);
-            return checkInvokeMethodReturn(response, XSTREAM);
+            response = executeHttpPost(options, apiInterfaceName, methodName, classNameParameters, parametersValues);
+            return checkInvokeMethodReturn(response);
         } catch (final UndeclaredThrowableException e) {
             if (LOGGER.isLoggable(Level.FINE)) {
                 LOGGER.log(Level.FINE, e.getMessage(), e);
@@ -149,11 +137,11 @@ public class HTTPServerAPI implements ServerAPI {
         }
     }
 
-    //package-private for testing purpose
-    Object checkInvokeMethodReturn(final String response, final XStream xstream) throws Throwable {
+    // package-private for testing purpose
+    Object checkInvokeMethodReturn(final String response) throws Throwable {
         Object invokeMethodReturn = null;
         if (response != null && !response.isEmpty() && !"null".equals(response)) {
-            invokeMethodReturn = fromXML(response, xstream);
+            invokeMethodReturn = xmlConverter.fromXML(response);
             if (invokeMethodReturn instanceof Throwable) {
                 throw (Throwable) invokeMethodReturn;
             }
@@ -161,11 +149,11 @@ public class HTTPServerAPI implements ServerAPI {
         return invokeMethodReturn;
     }
 
-    //package-private for testing purpose
+    // package-private for testing purpose
     String executeHttpPost(final Map<String, Serializable> options, final String apiInterfaceName, final String methodName,
-            final List<String> classNameParameters, final Object[] parametersValues, final XStream xstream) throws UnsupportedEncodingException, IOException,
-            ClientProtocolException {
-        final HttpPost httpost = createHttpPost(options, apiInterfaceName, methodName, classNameParameters, parametersValues, xstream);
+            final List<String> classNameParameters, final Object[] parametersValues) throws IOException {
+        final HttpPost httpost = createHttpPost(options, apiInterfaceName, methodName, classNameParameters,
+                parametersValues);
         try {
             return httpclient.execute(httpost, RESPONSE_HANDLER);
         } catch (final ClientProtocolException e) {
@@ -177,8 +165,8 @@ public class HTTPServerAPI implements ServerAPI {
     }
 
     private final HttpPost createHttpPost(final Map<String, Serializable> options, final String apiInterfaceName, final String methodName,
-            final List<String> classNameParameters, final Object[] parametersValues, final XStream xstream) throws UnsupportedEncodingException, IOException {
-        final HttpEntity httpEntity = buildEntity(options, classNameParameters, parametersValues, xstream);
+            final List<String> classNameParameters, final Object[] parametersValues) throws IOException {
+        final HttpEntity httpEntity = buildEntity(options, classNameParameters, parametersValues);
         final StringBuilder sBuilder = new StringBuilder(serverUrl);
         sBuilder.append(SLASH).append(applicationName).append(SERVER_API).append(apiInterfaceName).append(SLASH).append(methodName);
         final HttpPost httpost = new HttpPost(sBuilder.toString());
@@ -196,17 +184,18 @@ public class HTTPServerAPI implements ServerAPI {
         return httpost;
     }
 
-    final HttpEntity buildEntity(final Map<String, Serializable> options, final List<String> classNameParameters, final Object[] parametersValues,
-            final XStream xstream) throws UnsupportedEncodingException, IOException {
+    // package-private for testing purpose
+    final HttpEntity buildEntity(final Map<String, Serializable> options, final List<String> classNameParameters,
+            final Object[] parametersValues) throws IOException {
         final HttpEntity httpEntity;
         /*
          * if we have a business archive we use multipart to have the business archive attached as a binary content (it can be big)
          */
         if (classNameParameters.contains(BusinessArchive.class.getName()) || classNameParameters.contains(byte[].class.getName())) {
-            final List<Object> bytearrayParameters = new ArrayList<Object>();
+            final List<Object> bytearrayParameters = new ArrayList<>();
             final MultipartEntity entity = new MultipartEntity(null, null, UTF8);
-            entity.addPart(OPTIONS, new StringBody(toXML(options, xstream), UTF8));
-            entity.addPart(CLASS_NAME_PARAMETERS, new StringBody(toXML(classNameParameters, xstream), UTF8));
+            entity.addPart(OPTIONS, new StringBody(xmlConverter.toXML(options), UTF8));
+            entity.addPart(CLASS_NAME_PARAMETERS, new StringBody(xmlConverter.toXML(classNameParameters), UTF8));
             for (int i = 0; i < parametersValues.length; i++) {
                 final Object parameterValue = parametersValues[i];
                 if (parameterValue instanceof BusinessArchive || parameterValue instanceof byte[]) {
@@ -214,7 +203,7 @@ public class HTTPServerAPI implements ServerAPI {
                     bytearrayParameters.add(parameterValue);
                 }
             }
-            entity.addPart(PARAMETERS_VALUES, new StringBody(toXML(parametersValues, xstream), UTF8));
+            entity.addPart(PARAMETERS_VALUES, new StringBody(xmlConverter.toXML(parametersValues), UTF8));
             int i = 0;
             for (final Object object : bytearrayParameters) {
                 entity.addPart(BINARY_PARAMETER + i, new ByteArrayBody(serialize(object), BINARY_PARAMETER + i));
@@ -222,10 +211,10 @@ public class HTTPServerAPI implements ServerAPI {
             }
             httpEntity = entity;
         } else {
-            final List<NameValuePair> nvps = new ArrayList<NameValuePair>();
-            nvps.add(new BasicNameValuePair(OPTIONS, toXML(options, xstream)));
-            nvps.add(new BasicNameValuePair(CLASS_NAME_PARAMETERS, toXML(classNameParameters, xstream)));
-            nvps.add(new BasicNameValuePair(PARAMETERS_VALUES, toXML(parametersValues, xstream)));
+            final List<NameValuePair> nvps = new ArrayList<>();
+            nvps.add(new BasicNameValuePair(OPTIONS, xmlConverter.toXML(options)));
+            nvps.add(new BasicNameValuePair(CLASS_NAME_PARAMETERS, xmlConverter.toXML(classNameParameters)));
+            nvps.add(new BasicNameValuePair(PARAMETERS_VALUES, xmlConverter.toXML(parametersValues)));
             httpEntity = new UrlEncodedFormEntity(nvps, UTF_8);
         }
         return httpEntity;
@@ -242,37 +231,6 @@ public class HTTPServerAPI implements ServerAPI {
         final ByteArrayInputStream b = new ByteArrayInputStream(bytes);
         final ObjectInputStream o = new ObjectInputStream(b);
         return o.readObject();
-    }
-
-    private String toXML(final Object options, final XStream xstream) throws IOException {
-        final StringWriter stringWriter = new StringWriter();
-        final ObjectOutputStream out = xstream.createObjectOutputStream(stringWriter);
-        out.writeObject(options);
-        out.close();
-        return stringWriter.toString();
-    }
-
-    @SuppressWarnings("unchecked")
-    private <T> T fromXML(final String object, final XStream xstream) {
-        final StringReader xmlReader = new StringReader(object);
-        ObjectInputStream in = null;
-        try {
-            in = xstream.createObjectInputStream(xmlReader);
-            try {
-                return (T) in.readObject();
-            } catch (final IOException e) {
-                throw new BonitaRuntimeException("unable to deserialize object " + object, e);
-            } catch (final ClassNotFoundException e) {
-                throw new BonitaRuntimeException("unable to deserialize object " + object, e);
-            } catch (final CannotResolveClassException e) {
-                throw new BonitaRuntimeException("unable to deserialize object " + object, e);
-            } finally {
-                in.close();
-                xmlReader.close();
-            }
-        } catch (final IOException e) {
-            throw new BonitaRuntimeException("unable to deserialize object " + object, e);
-        }
     }
 
 }
---
+++ bpm/bonita-api/bonita-client-api/src/main/java/org/bonitasoft/engine/api/impl/XmlConverter.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) 2018 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
+package org.bonitasoft.engine.api.impl;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import org.bonitasoft.engine.api.BonitaStackTraceElementConverter;
+import org.bonitasoft.engine.exception.BonitaRuntimeException;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.security.AnyTypePermission;
+
+public class XmlConverter {
+
+    private static final XStream XSTREAM;
+    static {
+        XSTREAM = new XStream();
+        XStream.setupDefaultSecurity(XSTREAM);
+        XSTREAM.addPermission(AnyTypePermission.ANY);
+        XSTREAM.registerConverter(new BonitaStackTraceElementConverter(), XStream.PRIORITY_VERY_HIGH);
+    }
+
+    public String toXML(final Object object) throws IOException {
+        final StringWriter stringWriter = new StringWriter();
+        try (final ObjectOutputStream out = XSTREAM.createObjectOutputStream(stringWriter)){
+            out.writeObject(object);
+        }
+        return stringWriter.toString();
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T fromXML(final String object) {
+        try (final StringReader xmlReader = new StringReader(object);
+                final ObjectInputStream in = XSTREAM.createObjectInputStream(xmlReader)) {
+            return (T) in.readObject();
+        } catch (final ClassNotFoundException | IOException | RuntimeException e) {
+            throw new BonitaRuntimeException("Unable to deserialize object " + object, e);
+        }
+    }
+}
