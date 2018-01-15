256f3b48dac39f9b228b17c5467bd2d7bf42ced4
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2018-01-15 11:32:05 +0100

    Revert "tr(setup): avoid double setup initialization"
    This reverts commit 568a73da6c1781a7d0e33f62eef443b9e1d2906b.

--- platform/platform-setup/src/main/java/org/bonitasoft/platform/setup/PlatformSetupAccessor.java
+++ platform/platform-setup/src/main/java/org/bonitasoft/platform/setup/PlatformSetupAccessor.java
@@ -34,18 +34,13 @@ import org.springframework.transaction.support.TransactionTemplate;
  */
 public class PlatformSetupAccessor {
 
-    private static PlatformSetup _INSTANCE;
-
     public static PlatformSetup getPlatformSetup() throws NamingException {
-        if (_INSTANCE == null) {
-            _INSTANCE = createPlatformSetup();
-        }
-        return _INSTANCE;
-    }
-
-    private static PlatformSetup createPlatformSetup() throws NamingException {
         final DataSource dataSource = lookupDataSource();
         String dbVendor = System.getProperty("sysprop.bonita.db.vendor");
+        return getPlatformSetup(dataSource, dbVendor);
+    }
+
+    public static PlatformSetup getPlatformSetup(DataSource dataSource, String dbVendor) {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
         final DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager(dataSource);
         TransactionTemplate transactionTemplate = new TransactionTemplate(dataSourceTransactionManager);
