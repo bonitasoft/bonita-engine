7c2455fa40a88e5f67b491fd4c7fba4b23420889
danila_mazour <danila.mazour@bonitasoft.com>
2018-03-15 14:41:04 +0100

    fix(BDM validation): Add BDM multiple aggregation validation ref to itself (#904)
    When an object references itself in multiple aggregation in the BDM, it generates an exeption during deployment.
    I added a verification to prevent it.
    
    Closes [BS-17679](https://bonitasoft.atlassian.net/browse/BS-17679)

---
+++ bpm/bonita-util/bonita-common-util/src/main/java/org/bonitasoft/engine/bdm/validator/rule/MultipleAggregationToItselfValidationRule.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2018 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ */
+
+package org.bonitasoft.engine.bdm.validator.rule;
+
+import static org.bonitasoft.engine.bdm.model.field.RelationField.Type.AGGREGATION;
+
+import java.util.List;
+
+import org.bonitasoft.engine.bdm.model.BusinessObject;
+import org.bonitasoft.engine.bdm.model.BusinessObjectModel;
+import org.bonitasoft.engine.bdm.model.field.Field;
+import org.bonitasoft.engine.bdm.model.field.RelationField;
+import org.bonitasoft.engine.bdm.validator.ValidationStatus;
+
+/**
+ * @author Danila Mazour
+ */
+public class MultipleAggregationToItselfValidationRule extends ValidationRule<BusinessObjectModel, ValidationStatus> {
+
+    public MultipleAggregationToItselfValidationRule() {
+        super(BusinessObjectModel.class);
+    }
+
+    @Override
+    protected ValidationStatus validate(BusinessObjectModel bom) {
+        ValidationStatus validationStatus = new ValidationStatus();
+        for (BusinessObject bo : bom.getBusinessObjects()) {
+            List<Field> boFields = bo.getFields();
+            for (Field boField : boFields) {
+                if (boField instanceof RelationField) {
+                    RelationField relationField = (RelationField) boField;
+                    String fieldReferenceQualifiedName = relationField.getReference().getQualifiedName();
+                    String boQualifiedName = bo.getQualifiedName();
+                    if (relationField.getType() == AGGREGATION && fieldReferenceQualifiedName.equals(boQualifiedName) && relationField.isCollection()) {
+                        validationStatus.addError("The object " + boQualifiedName
+                                + " is referencing itself in a multiple aggregation relation.");
+                    }
+                }
+            }
+        }
+        return validationStatus;
+    }
+}
