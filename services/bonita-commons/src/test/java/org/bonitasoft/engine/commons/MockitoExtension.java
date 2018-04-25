b977e93d1c09a3a8275e8018923e07a35216abc0
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2018-04-25 14:28:36 +0200

    tr(junit 5): integrate Junit 5 + provide samples
    * Integrate Junit 5 new test dependencies
    * Make both Junit 4 & 5 coexit on the same modules
    * Provide migrated test examples
    
    For new modules where new or existing tests are written in Junit 5, just add the following dependency:
    <dependency>
    	<groupId>org.junit.jupiter</groupId>
    	<artifactId>junit-jupiter-api</artifactId>
    </dependency>
    
    If junit 4 has been completely removed from a module, simply remove:
    <dependency>
        <groupId>junit</groupId>
        <artifactId>junit</artifactId>
    </dependency>

---
+++ services/bonita-commons/src/test/java/org/bonitasoft/engine/commons/MockitoExtension.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (C) 2018 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
+package org.bonitasoft.engine.commons;
+
+import static org.mockito.Mockito.mock;
+
+import java.lang.reflect.Parameter;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolver;
+import org.junit.jupiter.api.extension.TestInstancePostProcessor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class MockitoExtension implements TestInstancePostProcessor, ParameterResolver {
+
+    @Override
+    public void postProcessTestInstance(Object testInstance, ExtensionContext context) {
+        MockitoAnnotations.initMocks(testInstance);
+    }
+
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
+        return parameterContext.getParameter().isAnnotationPresent(Mock.class);
+    }
+
+    @Override
+    public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
+        return getMock(parameterContext.getParameter(), extensionContext);
+    }
+
+    private Object getMock(Parameter parameter, ExtensionContext extensionContext) {
+        Class<?> mockType = parameter.getType();
+        ExtensionContext.Store mocks = extensionContext
+                .getStore(ExtensionContext.Namespace.create(MockitoExtension.class, mockType));
+        String mockName = getMockName(parameter);
+
+        if (mockName != null) {
+            return mocks.getOrComputeIfAbsent(mockName, key -> mock(mockType, mockName));
+        } else {
+            return mocks.getOrComputeIfAbsent(mockType.getCanonicalName(), key -> mock(mockType));
+        }
+    }
+
+    private String getMockName(Parameter parameter) {
+        String explicitMockName = parameter.getAnnotation(Mock.class).name().trim();
+        if (!explicitMockName.isEmpty()) {
+            return explicitMockName;
+        } else if (parameter.isNamePresent()) {
+            return parameter.getName();
+        }
+        return null;
+    }
+}
--- services/bonita-commons/src/test/java/org/bonitasoft/engine/commons/TypeConverterUtilTest.java
+++ services/bonita-commons/src/test/java/org/bonitasoft/engine/commons/TypeConverterUtilTest.java
@@ -15,6 +15,7 @@
 package org.bonitasoft.engine.commons;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -22,22 +23,17 @@ import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.Date;
 
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author Emmanuel Duchastenier
  */
-public class TypeConverterUtilTest {
-
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
+class TypeConverterUtilTest {
 
     private final TypeConverterUtil typeConverterUtil = new TypeConverterUtil(null);
 
     @Test
-    public void convertToType_should_convert_to_LocalDate() throws Exception {
+    void convertToType_should_convert_to_LocalDate() {
         final Object date = typeConverterUtil.convertToType(LocalDate.class, "2014-07-14");
 
         // then:
@@ -45,7 +41,7 @@ public class TypeConverterUtilTest {
     }
 
     @Test
-    public void getDefaultType_should_truncate_extra_LocalDate_string_characters() throws Exception {
+    void getDefaultType_should_truncate_extra_LocalDate_string_characters() {
         final Object date = typeConverterUtil.convertToType(LocalDate.class, "2015-07-14TOTO12:00");
 
         // then:
@@ -53,25 +49,21 @@ public class TypeConverterUtilTest {
     }
 
     @Test
-    public void getDefaultType_should_gracefully_handle_null_values() throws Exception {
-        // then:
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("unable to parse");
-
-        // when
-        typeConverterUtil.convertToType(Date.class, null);
+    void getDefaultType_should_gracefully_handle_null_values() {
+        assertThrows(IllegalArgumentException.class, () -> typeConverterUtil.convertToType(Date.class, null),
+                "unable to parse");
     }
 
     @Test
-    public void convertToType_should_convert_to_LocalDateTime() throws Exception {
+    void convertToType_should_convert_to_LocalDateTime() {
         final Object date = typeConverterUtil.convertToType(LocalDateTime.class, "2014-07-14T17:42:01");
 
         // then:
-        assertThat(date).isEqualTo(LocalDateTime.of(2014, 7, 14, 17, 42, 01));
+        assertThat(date).isEqualTo(LocalDateTime.of(2014, 7, 14, 17, 42, 1));
     }
 
     @Test
-    public void getDefaultType_should_truncate_extra_LocalDateTime_string_characters() throws Exception {
+    void getDefaultType_should_truncate_extra_LocalDateTime_string_characters() {
         final Object date = typeConverterUtil.convertToType(LocalDateTime.class, "2015-07-14T12:00:07+03:00");
 
         // then:
@@ -79,7 +71,7 @@ public class TypeConverterUtilTest {
     }
 
     @Test
-    public void convertToType_should_convert_to_OffsetDateTime() throws Exception {
+    void convertToType_should_convert_to_OffsetDateTime() {
         final Object date = typeConverterUtil.convertToType(OffsetDateTime.class, "2014-07-14T17:42:01Z");
 
         // then:
@@ -87,42 +79,30 @@ public class TypeConverterUtilTest {
     }
 
     @Test
-    public void getDefaultType_should_throw_exception_for_wrong_date_format() throws Exception {
-        // then:
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("unable to parse");
-
-        // when:
-        typeConverterUtil.convertToType(OffsetDateTime.class, "2015-07-14T12:00:07+03:00:17:15:123.000");
+    void getDefaultType_should_throw_exception_for_wrong_date_format() {
+        assertThrows(IllegalArgumentException.class,
+                () -> typeConverterUtil.convertToType(OffsetDateTime.class, "2015-07-14T12:00:07+03:00:17:15:123.000"),
+                "unable to parse");
     }
 
     @Test
-    public void getDefaultType_should_throw_exception_for_wrong_LocalDate_type() throws Exception {
-        // then:
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("unable to parse");
-
-        // when:
-        typeConverterUtil.convertToType(LocalDate.class, 225L);
+    void getDefaultType_should_throw_exception_for_wrong_LocalDate_type() {
+        assertThrows(IllegalArgumentException.class,
+                () -> typeConverterUtil.convertToType(LocalDate.class, 225L),
+                "unable to parse");
     }
 
     @Test
-    public void getDefaultType_should_throw_exception_for_wrong_LocalDateTime_type() throws Exception {
-        // then:
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("unable to parse");
-
-        // when:
-        typeConverterUtil.convertToType(LocalDateTime.class, new Date());
+    void getDefaultType_should_throw_exception_for_wrong_LocalDateTime_type() {
+        assertThrows(IllegalArgumentException.class,
+                () -> typeConverterUtil.convertToType(LocalDateTime.class, new Date()),
+                "unable to parse");
     }
 
     @Test
-    public void getDefaultType_should_throw_exception_for_wrong_OffsetDateTime_type() throws Exception {
-        // then:
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("unable to parse");
-
-        // when:
-        typeConverterUtil.convertToType(OffsetDateTime.class, (byte) 0);
+    void getDefaultType_should_throw_exception_for_wrong_OffsetDateTime_type() {
+        assertThrows(IllegalArgumentException.class,
+                () -> typeConverterUtil.convertToType(OffsetDateTime.class, (byte) 0),
+                "unable to parse");
     }
 }
