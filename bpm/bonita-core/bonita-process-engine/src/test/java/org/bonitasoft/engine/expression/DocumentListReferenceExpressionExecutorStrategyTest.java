d4eac9b3aa19db42c51c13fabaded8b08d42069c
Thomas Bouffard <thomas.bouffard@bonitasoft.com>
2018-10-04 11:51:01 +0200

    fix(doc list operation): apply changes from the same transaction (#1045)
    * fix(doc list operation): apply changes from the same transaction
    
    Fix the following use case:
      - an operation assigns a value to a document list 1
      - a subsequent operation uses the updated document list 1 to
      assign another document list 2
    
    Before the fix, the document list 2 didn't get the updated value of document
    list 1 but the one prior to the update.
    
    Covers [BS-18741](https://bonitasoft.atlassian.net/browse/BS-18741)

--- bpm/bonita-core/bonita-process-engine/src/test/java/org/bonitasoft/engine/expression/DocumentListReferenceExpressionExecutorStrategyTest.java
+++ bpm/bonita-core/bonita-process-engine/src/test/java/org/bonitasoft/engine/expression/DocumentListReferenceExpressionExecutorStrategyTest.java
@@ -13,15 +13,20 @@
  **/
 package org.bonitasoft.engine.expression;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
+import static org.bonitasoft.engine.expression.ContainerState.ACTIVE;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.bonitasoft.engine.bpm.document.Document;
+import org.bonitasoft.engine.bpm.document.impl.DocumentImpl;
 import org.bonitasoft.engine.core.document.api.DocumentService;
 import org.bonitasoft.engine.core.document.model.SMappedDocument;
 import org.bonitasoft.engine.core.process.definition.ProcessDefinitionService;
@@ -30,13 +35,13 @@ import org.bonitasoft.engine.core.process.definition.model.SDocumentListDefiniti
 import org.bonitasoft.engine.core.process.definition.model.SFlowElementContainerDefinition;
 import org.bonitasoft.engine.core.process.definition.model.SProcessDefinition;
 import org.bonitasoft.engine.core.process.definition.model.impl.SDocumentListDefinitionImpl;
-import org.bonitasoft.engine.core.process.instance.api.FlowNodeInstanceService;
+import org.bonitasoft.engine.core.process.instance.api.ActivityInstanceService;
 import org.bonitasoft.engine.core.process.instance.api.ProcessInstanceService;
 import org.bonitasoft.engine.core.process.instance.api.exceptions.SProcessInstanceNotFoundException;
 import org.bonitasoft.engine.core.process.instance.api.exceptions.SProcessInstanceReadException;
 import org.bonitasoft.engine.core.process.instance.model.SProcessInstance;
+import org.bonitasoft.engine.expression.model.impl.SExpressionImpl;
 import org.bonitasoft.engine.persistence.SBonitaReadException;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -49,47 +54,77 @@ public class DocumentListReferenceExpressionExecutorStrategyTest {
     @Mock
     private DocumentService documentService;
     @Mock
-    private FlowNodeInstanceService flowNodeInstanceService;
+    private ActivityInstanceService activityInstanceService;
     @Mock
     private ProcessInstanceService processInstanceService;
     @Mock
     private ProcessDefinitionService processDefinitionService;
 
-    @Mock
-    private SProcessInstance processInstance;
-    @Mock
-    private SProcessDefinition processDefinition;
-    @Mock
-    private SFlowElementContainerDefinition flowElementContainerDefinition;
-
     @InjectMocks
-    private DocumentListReferenceExpressionExecutorStrategy documentListReferenceExpressionExecutorStrategy;
+    private DocumentListReferenceExpressionExecutorStrategy strategy;
 
-    @Before
-    public void setUp() {
+    @Test
+    public void evaluate_result_should_retrieve_documents_list_when_it_is_not_already_in_the_context() throws Exception {
+        //given:
+        final Map<String, Object> context = new HashMap<>();
+        context.put("containerId", 123L);
+        context.put("containerType", "PROCESS_INSTANCE");
+
+        final DocumentListReferenceExpressionExecutorStrategy spiedStrategy = spy(strategy);
+        final List<DocumentImpl> expectedDocumentList = singletonList(new DocumentImpl());
+        doReturn(expectedDocumentList).when(spiedStrategy).getDocumentList(anyLong(), anyString(), eq(null));
+
+        //when:
+        final List<Object> result = spiedStrategy.evaluate(asList(expressionForDocumentList()), context, null, ACTIVE);
+
+        //then:
+        assertThat(result).hasSize(1).containsOnly(expectedDocumentList);
+        verifyZeroInteractions(activityInstanceService);
     }
+
+    @Test
+    public void evaluate_should_directly_return_the_documents_list_when_it_is_already_in_the_context() throws Exception {
+        //given:
+        final Map<String, Object> context = new HashMap<>();
+        final List<DocumentImpl> documentListInContext = singletonList(new DocumentImpl());
+        final String docListName = "docListPreviouslyPutInContext";
+        context.put(docListName, documentListInContext);
+
+        context.put("containerId", 365498L);
+        context.put("containerType", "PROCESS_INSTANCE");
+
+        //when:
+        final Object result = strategy.evaluate(expressionForDocumentList(docListName), context, null, ACTIVE);
+
+        //then:
+        assertThat(result).isSameAs(documentListInContext);
+        verifyZeroInteractions(documentService);
+    }
+
+    // =================================================================================================================
+    // implementation detail methods tests
+    // =================================================================================================================
 
     @Test
     public void should_getDocument_return_empty() throws Exception {
         //given
-        doReturn(Collections.emptyList()).when(documentService).getDocumentList("theList", 45l, 0, 100);
+        doReturn(emptyList()).when(documentService).getDocumentList("theList", 45l, 0, 100);
         initDefinition("theList");
 
         //when
-        final List<Document> theList = documentListReferenceExpressionExecutorStrategy.getDocumentList(45l, "theList", null);
+        final List<Document> theList = strategy.getDocumentList(45l, "theList", null);
 
         assertThat(theList).isEmpty();
-
     }
 
     @Test
     public void should_getDocument_return_null() throws Exception {
         //given
-        doReturn(Collections.emptyList()).when(documentService).getDocumentList("theList", 45l, 0, 100);
+        doReturn(emptyList()).when(documentService).getDocumentList("theList", 45l, 0, 100);
         initDefinition("notTheList");
 
         //when
-        final List<Document> theList = documentListReferenceExpressionExecutorStrategy.getDocumentList(45l, "theList", null);
+        final List<Document> theList = strategy.getDocumentList(45l, "theList", null);
 
         assertThat(theList).isNull();
     }
@@ -97,30 +132,51 @@ public class DocumentListReferenceExpressionExecutorStrategyTest {
     @Test
     public void should_getDocument_return_theList() throws Exception {
         //given
-        final List<SMappedDocument> toBeReturned = Collections.singletonList(mock(SMappedDocument.class));
-        doReturn(toBeReturned).when(documentService).getDocumentList("theList", 45l, 0, 100);
+        doReturn(singletonList(mock(SMappedDocument.class))).when(documentService).getDocumentList("theList", 45l, 0,
+                100);
 
         //when
-        final List<Document> theList = documentListReferenceExpressionExecutorStrategy.getDocumentList(45l, "theList", null);
+        final List<Document> theList = strategy.getDocumentList(45l, "theList", null);
 
         assertThat(theList).hasSize(1);
     }
 
+    // =================================================================================================================
+    // UTILS
+    // =================================================================================================================
+
     private void initDefinition(final String... names) throws SProcessInstanceNotFoundException, SProcessInstanceReadException,
             SProcessDefinitionNotFoundException,
             SBonitaReadException {
-        doReturn(processInstance).when(processInstanceService).getProcessInstance(45l);
+        final SProcessInstance processInstance = mock(SProcessInstance.class);
         doReturn(154l).when(processInstance).getProcessDefinitionId();
-        doReturn(processDefinition).when(processDefinitionService).getProcessDefinition(154l);
+        doReturn(processInstance).when(processInstanceService).getProcessInstance(45l);
+
+        final SProcessDefinition processDefinition = mock(SProcessDefinition.class);
+        final SFlowElementContainerDefinition flowElementContainerDefinition = mock(SFlowElementContainerDefinition.class);
         doReturn(flowElementContainerDefinition).when(processDefinition).getProcessContainer();
         doReturn(createListOfDocumentListDefinition(names)).when(flowElementContainerDefinition).getDocumentListDefinitions();
+        doReturn(processDefinition).when(processDefinitionService).getProcessDefinition(154l);
     }
 
-    private List<SDocumentListDefinition> createListOfDocumentListDefinition(final String... names) {
-        final List<SDocumentListDefinition> list = new ArrayList<SDocumentListDefinition>();
+    private static List<SDocumentListDefinition> createListOfDocumentListDefinition(final String... names) {
+        final List<SDocumentListDefinition> list = new ArrayList<>();
         for (final String name : names) {
             list.add(new SDocumentListDefinitionImpl(name));
         }
         return list;
     }
+
+    private static SExpressionImpl expressionForDocumentList(final String content) {
+        final SExpressionImpl expression = new SExpressionImpl();
+        expression.setContent(content);
+        expression.setReturnType(List.class.getName());
+        expression.setExpressionType(ExpressionType.TYPE_DOCUMENT_LIST.name());
+        return expression;
+    }
+
+    private static SExpressionImpl expressionForDocumentList() {
+        return expressionForDocumentList("a_name_i_dont_care_about");
+    }
+
 }
