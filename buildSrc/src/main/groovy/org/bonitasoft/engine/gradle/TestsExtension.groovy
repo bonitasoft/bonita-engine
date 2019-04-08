177444d0d829e13cd439ee9e363242875cbd3217
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2019-04-08 14:54:13 +0200

    defect(u tests): run ALL Unit tests for real

--- buildSrc/src/main/groovy/org/bonitasoft/engine/gradle/TestsExtension.groovy
+++ buildSrc/src/main/groovy/org/bonitasoft/engine/gradle/TestsExtension.groovy
@@ -1,7 +1,7 @@
 package org.bonitasoft.engine.gradle
 
 class TestsExtension {
-    String testPattern = "**/Test.class"
+    String testPattern = "**/*Test.class"
     String integrationTestsPattern = "**/*IT.class"
     String integrationTestsSuite
     String slowTestsSuite
