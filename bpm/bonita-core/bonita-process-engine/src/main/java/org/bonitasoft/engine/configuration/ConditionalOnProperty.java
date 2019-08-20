fb8c3f8eb58a2af31566bf9676dc46185142e250
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2019-08-20 16:59:33 +0200

    feat(metrics): add optional jvm metrics (#1390)
    activate them using properties:
    * org.bonitasoft.engine.monitoring.metrics.jvm.memory.enable=true
    * org.bonitasoft.engine.monitoring.metrics.jvm.threads.enable=true
    * org.bonitasoft.engine.monitoring.metrics.jvm.gc.enable=true
    
    Relates to [BR-176](https://bonitasoft.atlassian.net/browse/BR-176)

--- bpm/bonita-core/bonita-process-engine/src/main/java/org/bonitasoft/engine/configuration/ConditionalOnProperty.java
+++ bpm/bonita-core/bonita-process-engine/src/main/java/org/bonitasoft/engine/configuration/ConditionalOnProperty.java
@@ -13,12 +13,19 @@
  **/
 package org.bonitasoft.engine.configuration;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import org.springframework.context.annotation.Conditional;
 
 /**
  * @author Danila Mazour
  */
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE, ElementType.METHOD })
 @Conditional(OnPropertyCondition.class)
 public @interface ConditionalOnProperty {
 
