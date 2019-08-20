9eaf946d10d80c2bb9a62da4d06323a2a01b7d85
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2019-08-20 16:52:46 +0200

    fix(test): fix non-deterministic test with async behaviour
    LoggingMeterRegistryTest.should_print_readable_logs() could fail as logs
     were not necessarily written down synchronously with the log 'get and
     assert' in the test

--- bpm/bonita-core/bonita-process-engine/src/test/java/org/bonitasoft/engine/configuration/LoggingMeterRegistryTest.java
+++ bpm/bonita-core/bonita-process-engine/src/test/java/org/bonitasoft/engine/configuration/LoggingMeterRegistryTest.java
@@ -1,30 +1,31 @@
 package org.bonitasoft.engine.configuration;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 
 import java.time.Duration;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import io.micrometer.core.instrument.Clock;
-import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.Tags;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Tags;
+
 public class LoggingMeterRegistryTest {
 
     @Rule
-    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog().muteForSuccessfulTests();
 
     public static final Logger logger = LoggerFactory.getLogger(LoggingMeterRegistryTest.class);
 
     private LoggingMeterRegistry loggingMeterRegistry = new LoggingMeterRegistry(
             k -> k.equals("logging.step") ? Duration.ofMillis(10).toString() : null,
-            Clock.SYSTEM
-    );
+            Clock.SYSTEM);
 
     @Test
     public void should_print_readable_logs() throws Exception {
@@ -40,14 +41,11 @@ public class LoggingMeterRegistryTest {
             counter.increment(3);
             Thread.sleep(10);
         }
-        String log = systemOutRule.getLog();
-
-        assertThat(log)
+        await().until(() -> assertThat(systemOutRule.getLog())
                 .contains("my.counter {tenant=12}")
                 .contains("my.gauge value=")
                 .contains("my.gauge2 {t1=1, t2=2} value=")
-                .contains("throughput=");
-
+                .contains("throughput="));
 
     }
 
