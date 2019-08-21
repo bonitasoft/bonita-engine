0125cea666b9f97f0946d69e6a1e8bbb9eebb1f7
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2019-08-21 11:42:40 +0200

    refactor(deployer): clean-up Deployer code (#1393)
    * renamed package projectdeployer -> application.deployer
    * remove useless code (profiles, orga, bdm, access control)
    * improve log messages to better track what's going on

---
+++ bpm/bonita-core/bonita-process-engine/src/main/java/org/bonitasoft/engine/api/impl/application/deployer/detector/artifact/DetectedPage.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C) 2019 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
+package org.bonitasoft.engine.api.impl.application.deployer.detector.artifact;
+
+import org.bonitasoft.engine.api.impl.application.deployer.descriptor.DeploymentDescriptor.DeploymentDescriptorBuilder;
+import org.bonitasoft.engine.api.impl.application.deployer.model.Page;
+
+public class DetectedPage extends DefaultDetectedArtifact {
+
+    public DetectedPage(String fileName) {
+        super(fileName);
+    }
+
+    @Override
+    public void build(DeploymentDescriptorBuilder builder) {
+        LOGGER.info("Found page file: '{}'.", fileName);
+        builder.page(new Page().setFile(fileName));
+    }
+}
---
+++ bpm/bonita-core/bonita-process-engine/src/test/java/org/bonitasoft/engine/api/impl/application/deployer/detector/LayoutDetectorTest.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (C) 2019 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
+package org.bonitasoft.engine.api.impl.application.deployer.detector;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.bonitasoft.engine.io.FileAndContentUtils.file;
+import static org.bonitasoft.engine.io.FileAndContentUtils.zip;
+
+import java.io.File;
+import java.nio.file.Files;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+/**
+ * @author Emmanuel Duchastenier
+ */
+public class LayoutDetectorTest {
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
+    @Test
+    public void isCompliant_should_reject_layout_if_contentType_is_not_layout() throws Exception {
+        // given:
+        final LayoutDetector detector = new LayoutDetector();
+        byte[] zip = zip(
+                file("page.properties", "contentType=toto"),
+                file("resources/index.html", "anything"));
+        final File file = tempFolder.newFile("myLayout.zip");
+        Files.write(file.toPath(), zip);
+
+        // when:
+        final boolean compliant = detector.isCompliant(file);
+
+        // then:
+        assertThat(compliant).isFalse();
+    }
+
+    @Test
+    public void isCompliant_should_reject_layout_if_index_page_not_present() throws Exception {
+        // given:
+        final LayoutDetector detector = new LayoutDetector();
+        byte[] zip = zip(
+                file("page.properties", "contentType=layout"),
+                file("resources/toto.html", "anything"));
+        final File file = tempFolder.newFile("myLayout.zip");
+        Files.write(file.toPath(), zip);
+
+        // when:
+        final boolean compliant = detector.isCompliant(file);
+
+        // then:
+        assertThat(compliant).isFalse();
+    }
+
+    @Test
+    public void isCompliant_should_accept_valid_layout() throws Exception {
+        // given:
+        final File file = new File(this.getClass().getResource("/layout.zip").toURI());
+
+        // when:
+        final boolean compliant = new LayoutDetector().isCompliant(file);
+
+        // then:
+        assertThat(compliant).isTrue();
+    }
+}
