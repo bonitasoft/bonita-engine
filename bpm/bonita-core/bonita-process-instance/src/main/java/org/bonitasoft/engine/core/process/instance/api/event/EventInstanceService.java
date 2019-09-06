c032e3c39576994e10a90b579a2912845cc77480
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2019-09-06 12:18:10 +0200

    fix(bpm): delete event trigger instance on interrupt and and deleteProcess (#1406)
    When executing normally, event trigger instances were deleted. But when
    the process was deleted directly or when a boundary was interrupted, we
    were removing the timer job but not the associated event trigger instances.
    
    I checked everywhere we delete timer job: it was the only place were the
     deletion of event trigger instances were missing.
    
    We should handle timer jobs differently in the future and remove those
    event trigger instance objects (replace by job list/updating)
    
    Closes [BS-15900](https://bonitasoft.atlassian.net/browse/BS-15900)

--- bpm/bonita-core/bonita-process-instance/src/main/java/org/bonitasoft/engine/core/process/instance/api/event/EventInstanceService.java
+++ bpm/bonita-core/bonita-process-instance/src/main/java/org/bonitasoft/engine/core/process/instance/api/event/EventInstanceService.java
@@ -150,6 +150,8 @@ public interface EventInstanceService extends FlowNodeInstanceService {
      */
     void deleteEventTriggerInstance(STimerEventTriggerInstance eventTriggerInstance) throws SEventTriggerInstanceDeletionException;
 
+    void deleteEventTriggerInstanceOfFlowNode(long flowNodeInstanceId) throws SBonitaReadException, SEventTriggerInstanceDeletionException;
+
     /**
      * @param flowNodeInstance
      * @throws SWaitingEventModificationException
