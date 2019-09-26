57d71278072ec23e8f18eb48a2539950e9160ae3
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2019-09-26 14:26:13 +0200

    tr(secu): update Jackson Databind version (#1434)
    from 2.9.8 to 2.9.10 for security reasons

--- buildSrc/src/main/groovy/Deps.groovy
+++ buildSrc/src/main/groovy/Deps.groovy
@@ -16,7 +16,7 @@ class Deps {
     public static String slf4jVersion = "1.7.25"
     public static String h2Version = "1.4.199"
     public static String hibernateVersion = "4.3.11.Final"
-    public static String jacksonDataBindingVersion = "2.9.8"
+    public static String jacksonDataBindingVersion = "2.9.10"
     public static String javaxJtaVersion = "1.1"
     public static String groovyAllVersion = "2.4.16"
     public static String javassistVersion = "3.18.1-GA"
