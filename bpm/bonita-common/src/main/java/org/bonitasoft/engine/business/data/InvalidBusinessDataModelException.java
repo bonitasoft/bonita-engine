60f21120dea5c8323e8745102433aff24f7b8632
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2019-11-18 16:48:54 +0100

    fix(bdm): throw the right exception when deploying invalid bdm.zip (#1511)
    The exception was present api side, but internally that was not the right
    exception thrown
    
    Closes [BR-14](https://bonitasoft.atlassian.net/browse/BR-14)

--- bpm/bonita-common/src/main/java/org/bonitasoft/engine/business/data/InvalidBusinessDataModelException.java
+++ bpm/bonita-common/src/main/java/org/bonitasoft/engine/business/data/InvalidBusinessDataModelException.java
@@ -33,4 +33,11 @@ public class InvalidBusinessDataModelException extends BusinessDataRepositoryExc
         super(cause);
     }
 
+    public InvalidBusinessDataModelException(String message, Exception cause) {
+        super(message, cause);
+    }
+
+    public InvalidBusinessDataModelException(String message) {
+        super(message);
+    }
 }
