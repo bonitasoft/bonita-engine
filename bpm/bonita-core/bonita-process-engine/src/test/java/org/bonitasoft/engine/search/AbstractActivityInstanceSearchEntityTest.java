30986e1b5548e6b16449cb31c460e6923cfce40d
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2019-11-27 10:35:22 +0100

    chore(formatting): fix license headers (#1507)
    Uses the bonita-formatting plugin to re-format all headers.

--- bpm/bonita-core/bonita-process-engine/src/test/java/org/bonitasoft/engine/search/AbstractActivityInstanceSearchEntityTest.java
+++ bpm/bonita-core/bonita-process-engine/src/test/java/org/bonitasoft/engine/search/AbstractActivityInstanceSearchEntityTest.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2015 Bonitasoft S.A.
+/**
+ * Copyright (C) 2019 Bonitasoft S.A.
  * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
  * This library is free software; you can redistribute it and/or modify it under the terms
  * of the GNU Lesser General Public License as published by the Free Software Foundation
@@ -9,8 +9,8 @@
  * See the GNU Lesser General Public License for more details.
  * You should have received a copy of the GNU Lesser General Public License along with this
  * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
- * Floor, Boston, MA 02110-1301, US.
- */
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
 package org.bonitasoft.engine.search;
 
 import static org.assertj.core.api.Assertions.assertThat;
