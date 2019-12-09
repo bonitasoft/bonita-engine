436f9cd5f7282d70595707e4df6187d220da51be
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2019-12-09 17:42:15 +0100

    refactor(jpa): use JPA annotation for SPlatform and STenant objects (#1545)
    Closes [BR-312](https://bonitasoft.atlassian.net/browse/BR-312)

---
+++ bonita-integration-tests/bonita-query-tests/src/test/java/org/bonitasoft/engine/platform/model/impl/PlatformTest.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (C) 2019 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
+package org.bonitasoft.engine.platform.model.impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+import static org.bonitasoft.engine.commons.Pair.pair;
+
+import java.util.Map;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.bonitasoft.engine.persistence.PersistentObject;
+import org.bonitasoft.engine.platform.model.SPlatform;
+import org.bonitasoft.engine.platform.model.STenant;
+import org.bonitasoft.engine.test.persistence.repository.PlatformRepository;
+import org.bonitasoft.engine.test.persistence.repository.TestRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = {"/testContext.xml"})
+@Transactional
+public class PlatformTest {
+    @Inject
+    private PlatformRepository repository;
+    @Inject
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    public void should_save_and_get_SPlatform() {
+        SPlatform platform = repository.add(SPlatform.builder()
+                .initialVersion("5.9.0")
+                .previousVersion("7.8.0")
+                .version("7.10.0")
+                .created(345L)
+                .information("some infos XYZ")
+                .createdBy("The almighty")
+                .build());
+        repository.flush();
+
+        PersistentObject platformFromQuery = repository.selectOneOnPlatform("getPlatform");
+        Map<String, Object> platformAsMap = jdbcTemplate.queryForMap("SELECT * FROM platform");
+
+        assertThat(platformFromQuery).isEqualTo(platform);
+        assertThat(platformAsMap).containsOnly(
+                entry("ID", platform.getId()),
+                entry("CREATED", 345L),
+                entry("CREATEDBY", "The almighty"),
+                entry("INITIALVERSION", "5.9.0"),
+                entry("PREVIOUSVERSION", "7.8.0"),
+                entry("VERSION", "7.10.0"),
+                entry("INFORMATION", "some infos XYZ"));
+    }
+
+
+    @Test
+    public void should_save_and_get_STenant() {
+        STenant tenant = repository.add(STenant.builder()
+                .created(456L)
+                .createdBy("The almighty")
+                .defaultTenant(true)
+                .description("A tenant created to do things")
+                .name("MyTenant")
+                .status("OK")
+                .iconName("toto.png")
+                .iconPath("path/toto.png")
+                .build());
+        repository.flush();
+
+        PersistentObject tenantFromQuery = repository.selectOneOnPlatform("getTenantByName", pair("name", "MyTenant"));
+        Map<String, Object> tenantAsMap = jdbcTemplate.queryForMap("SELECT * FROM tenant");
+
+        assertThat(tenantFromQuery).isEqualTo(tenant);
+        assertThat(tenantAsMap).containsOnly(
+                entry("ID", tenant.getId()),
+                entry("CREATED", 456L),
+                entry("CREATEDBY", "The almighty"),
+                entry("DESCRIPTION", "A tenant created to do things"),
+                entry("DEFAULTTENANT", true),
+                entry("ICONNAME", "toto.png"),
+                entry("ICONPATH", "path/toto.png"),
+                entry("STATUS", "OK"),
+                entry("NAME", "MyTenant"));
+    }
+
+}
