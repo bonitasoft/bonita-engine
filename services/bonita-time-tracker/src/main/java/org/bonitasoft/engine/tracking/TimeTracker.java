f4ee72f4d770ac8ea2bfb53f856d5d035fbfcfd8
Pierre-Yves-Monnet <pierre-yves.monnet@laposte.net>
2020-04-06 16:08:12 -0700

    set getFlushEventListeners as public,  add public void addFlushEventListener()

--- services/bonita-time-tracker/src/main/java/org/bonitasoft/engine/tracking/TimeTracker.java
+++ services/bonita-time-tracker/src/main/java/org/bonitasoft/engine/tracking/TimeTracker.java
@@ -92,7 +92,10 @@ public class TimeTracker implements TenantLifecycleService {
         log(TechnicalLogSeverity.INFO, getStatus());
     }
 
-    List<FlushEventListener> getActiveFlushEventListeners() {
+    /**
+     * get the list of Active Listener
+     */
+   public List<FlushEventListener> getActiveFlushEventListeners() {
         final List<FlushEventListener> active = new ArrayList<>();
         for (final FlushEventListener flushEventListener : this.flushEventListeners.values()) {
             if (flushEventListener.isActive()) {
@@ -101,6 +104,12 @@ public class TimeTracker implements TenantLifecycleService {
         }
         return active;
     }
+   /**
+    * return all Event Listeners, active or not
+    */
+   public List<FlushEventListener> getFlushEventListeners() {
+       return new ArrayList(this.flushEventListeners.values());
+   }
 
     /**
      * reference a new flushEventListener. The key of the reference is the flushEventListener.name().
