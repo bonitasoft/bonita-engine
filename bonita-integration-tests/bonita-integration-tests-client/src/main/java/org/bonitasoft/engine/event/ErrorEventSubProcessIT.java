c3c50f8c2a4c0247f4f7c92daf829f870dfe3b61
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2020-09-08 14:42:44 +0200

    fix(event): fix cancel of process when interrupted by error event (#1734)
    When we cancel a process that is aborted by an event sub process, the cancellation might re-try to trigger the event subprocess.
    
    We do not trigger error events when cancelling process instance using API anymore.
    
    In addition there was a class cast exception in the related activity state `EndingCallActivityExceptionStateImpl.java`
    
    Relates to [BR-477](https://bonitasoft.atlassian.net/browse/BR-477)
    Relates to [BS-19528](https://bonitasoft.atlassian.net/browse/BS-19528)

--- bonita-integration-tests/bonita-integration-tests-client/src/main/java/org/bonitasoft/engine/event/ErrorEventSubProcessIT.java
+++ bonita-integration-tests/bonita-integration-tests-client/src/main/java/org/bonitasoft/engine/event/ErrorEventSubProcessIT.java
@@ -189,7 +189,7 @@ public class ErrorEventSubProcessIT extends AbstractWaitingEventIT {
     }
 
     @Test
-    public void eventSubProcesWithDataAndRootProcessWithNoData() throws Exception {
+    public void eventSubProcessWithDataAndRootProcessWithNoData() throws Exception {
         final String rootUserTaskName = "step1";
         final String subProcUserTaskName = "subStep";
         final String dataName = "content";
@@ -299,7 +299,7 @@ public class ErrorEventSubProcessIT extends AbstractWaitingEventIT {
     }
 
     @Test
-    public void should_catch_error_in_event_subprocess_when_process_is_disabled() throws Exception {
+    public void should_catch_error_in_event_subprocess_when_process_is_disabled_and_then_cancel_it() throws Exception {
         //given: a process with error event subprocess catching an error
         ProcessDefinition parent = deployAndEnableProcessWithActor(new ProcessDefinitionBuilder()
                 .createNewInstance("Parent process with error event subprocess", "1.0")
@@ -337,12 +337,14 @@ public class ErrorEventSubProcessIT extends AbstractWaitingEventIT {
         getProcessAPI().assignAndExecuteUserTask(user.getId(), task, Collections.emptyMap());
 
         //then: the error event sub process should be triggered
-        long eventSubProcessTask = waitForUserTask("eventSubProcessTask");
-        getProcessAPI().assignAndExecuteUserTask(user.getId(), eventSubProcessTask, Collections.emptyMap());
-        waitForProcessToBeInState(processInstance, ProcessInstanceState.ABORTED);
+        waitForUserTask("eventSubProcessTask");
+
+        // then: we should be able to cancel that instance
+        getProcessAPI().cancelProcessInstance(processInstance.getId());
+
+        waitForProcessToBeInState(processInstance, ProcessInstanceState.CANCELLED);
 
         // to allow proper clean-up:
         getProcessAPI().enableProcess(parent.getId());
     }
-
 }
