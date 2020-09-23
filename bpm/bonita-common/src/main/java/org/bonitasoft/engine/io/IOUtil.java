7975b430e1fdb4851fdf0b82b5392ce23ec83832
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2020-09-23 09:05:24 +0200

    chore(deps): upgrade apache commons versions
    commons-io to 2.8.0
    commons-lang to 3.11
    commons-dbcp2 to 2.5.0
    commons-cli to 1.4
    
    There was a signature change that required code change, also due to a change in commons-lang in a toString method, a test was changed to avoid it to fail.

--- bpm/bonita-common/src/main/java/org/bonitasoft/engine/io/IOUtil.java
+++ bpm/bonita-common/src/main/java/org/bonitasoft/engine/io/IOUtil.java
@@ -218,11 +218,7 @@ public class IOUtil {
 
         mkdirs(tmpDir);
 
-        try {
-            // to initialize internal class FilenameUtils. Otherwise it cannot load the class as the shutdown is in progress:
-            FileUtils.isSymlink(tmpDir);
-        } catch (IOException ignored) {
-        }
+        FileUtils.isSymlink(tmpDir);
 
         try {
 
