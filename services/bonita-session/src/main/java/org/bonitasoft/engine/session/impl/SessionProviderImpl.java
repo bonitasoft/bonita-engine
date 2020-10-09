18f2607f57b833346a5571f3706a097d6700f701
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2020-10-09 09:08:34 +0200

    refactor(session): cleanup code of Session provider (#1799)
    Removed unused item + set sessions map as static final

--- services/bonita-session/src/main/java/org/bonitasoft/engine/session/impl/SessionProviderImpl.java
+++ services/bonita-session/src/main/java/org/bonitasoft/engine/session/impl/SessionProviderImpl.java
@@ -16,20 +16,16 @@ package org.bonitasoft.engine.session.impl;
 import java.util.HashMap;
 import java.util.Map;
 
-import lombok.extern.slf4j.Slf4j;
 import org.bonitasoft.engine.session.SessionProvider;
 import org.bonitasoft.engine.session.model.SSession;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConditionalOnSingleCandidate(SessionProvider.class)
-@Slf4j
 public final class SessionProviderImpl extends AbstractSessionProvider {
 
-    static Map<Long, SSession> sessions = new HashMap<Long, SSession>();
-    private ApplicationContext applicationContext;
+    private static final Map<Long, SSession> sessions = new HashMap<>();
 
     @Override
     protected Map<Long, SSession> getSessions() {
