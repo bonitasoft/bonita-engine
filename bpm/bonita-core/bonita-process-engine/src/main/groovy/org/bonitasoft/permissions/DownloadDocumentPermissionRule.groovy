c1567f678dd7546345c856f2895e634d4a4c8a7a
abirembaut <anthony.birembaut@bonitasoft.com>
2020-12-08 08:41:57 +0100

    fix(DownloadDocumentPermissionRule): support contentStorageId parameter (#1881)
    document download servlet also works with a contentStorageId parameter only.
    This use case was not covered by the dynamic check rule
    
    Covers [BR-559](https://bonitasoft.atlassian.net/browse/BR-559)

--- bpm/bonita-core/bonita-process-engine/src/main/groovy/org/bonitasoft/permissions/DownloadDocumentPermissionRule.groovy
+++ bpm/bonita-core/bonita-process-engine/src/main/groovy/org/bonitasoft/permissions/DownloadDocumentPermissionRule.groovy
@@ -25,6 +25,10 @@ import org.bonitasoft.engine.api.permission.PermissionRule
 import org.bonitasoft.engine.exception.BonitaException
 import org.bonitasoft.engine.exception.NotFoundException
 import org.bonitasoft.engine.session.APISession
+import org.bonitasoft.engine.bpm.document.DocumentsSearchDescriptor
+import org.bonitasoft.engine.bpm.document.DocumentNotFoundException
+import org.bonitasoft.engine.bpm.document.ArchivedDocumentNotFoundException
+import org.bonitasoft.engine.search.SearchOptionsBuilder
 
 /**
  *
@@ -44,34 +48,84 @@ class DownloadDocumentPermissionRule implements PermissionRule {
 
     public static final String DOCUMENT_ID_PARAM = "document"
 
+    public static final String CONTENT_STORAGE_ID_PARAM = "contentStorageId"
+
+    public static final String FILE_NAME_PARAM = "fileName"
+
     @Override
     public boolean isAllowed(APISession apiSession, APICallContext apiCallContext, APIAccessor apiAccessor, Logger logger) {
         long currentUserId = apiSession.getUserId()
 
         if (apiCallContext.isGET()) {
+            def processInstanceId = null
+            def contentStorageId = apiCallContext.getParameters().get(CONTENT_STORAGE_ID_PARAM)
+            def fileName = apiCallContext.getParameters().get(FILE_NAME_PARAM)
             def documentId = apiCallContext.getParameters().get(DOCUMENT_ID_PARAM)
-            if (documentId != null) {
-                return checkMethodWithResourceId(documentId, apiAccessor, currentUserId)
+            // Download servlets requires either contentStorageId + fileName parameters or a document parameter
+            if (fileName != null && contentStorageId != null && contentStorageId.length > 0) {
+                processInstanceId = getProcessInstanceIdFromContentStorageId(contentStorageId[0], apiAccessor, logger)
+            } else if (documentId != null && documentId.length > 0) {
+                processInstanceId = getProcessInstanceIdFromDocumentId(documentId[0], apiAccessor, logger)
+            }
+            if (processInstanceId != -1l) {
+                return checkInvolvement(processInstanceId, apiAccessor, currentUserId)
             }
             return true
         }
-
         return false
     }
 
-    private boolean checkMethodWithResourceId(String resourceId, APIAccessor apiAccessor, long currentUserId) {
+    private long getProcessInstanceIdFromDocumentId(String documentIdStr, APIAccessor apiAccessor, Logger logger) {
+        def processInstanceId = -1l
         def processAPI = apiAccessor.getProcessAPI()
         try {
-            long documentId = Long.valueOf(resourceId)
-            def processInstanceId = processAPI.getDocument(documentId).getProcessInstanceId()
-            return isInvolved(processAPI, currentUserId, processInstanceId) ||
-                    isSupervisor(processAPI, currentUserId, processInstanceId)
+            long documentId = Long.valueOf(documentIdStr)
+            try {
+                processInstanceId = processAPI.getDocument(documentId).getProcessInstanceId()
+            } catch (DocumentNotFoundException dnfe) {
+                processInstanceId = processAPI.getArchivedVersionOfProcessDocument(documentId).getProcessInstanceId()
+            }
+        }
+        catch (ArchivedDocumentNotFoundException e) {
+            logger.debug("No document or archived document found with Id " + documentIdStr)
         }
         catch (NumberFormatException e) {
-            return true
+            logger.debug("documentId " + documentIdStr + " is not a number")
         }
+        return processInstanceId
     }
 
+    private long getProcessInstanceIdFromContentStorageId(String contentStorageIdStr, APIAccessor apiAccessor, Logger logger) {
+        def processInstanceId = -1l
+        def processAPI = apiAccessor.getProcessAPI()
+        try {
+            long contentStorageId = Long.valueOf(contentStorageIdStr)
+            SearchOptionsBuilder builder = new SearchOptionsBuilder(0, 1)
+            builder.filter(DocumentsSearchDescriptor.CONTENT_STORAGE_ID, contentStorageId)
+            def documentSearchResult = processAPI.searchDocuments(builder.done())
+            if (documentSearchResult.getCount() > 0) {
+                processInstanceId = documentSearchResult.getResult().get(0).getProcessInstanceId()
+            } else {
+                def archivedDocumentSearchResult = processAPI.searchArchivedDocuments(builder.done())
+                if (archivedDocumentSearchResult.getCount() > 0) {
+                    processInstanceId = archivedDocumentSearchResult.getResult().get(0).getProcessInstanceId()
+                }
+            }
+            if (processInstanceId == -1l) {
+                logger.debug("No document or archived document found for contentStorageId " + contentStorageIdStr)
+            }
+        }
+        catch (NumberFormatException e) {
+            logger.debug("contentStorageId is not a number")
+        }
+        return processInstanceId
+    }
+
+    private boolean checkInvolvement(long processInstanceId, APIAccessor apiAccessor, long currentUserId) {
+        def processAPI = apiAccessor.getProcessAPI()
+        return isInvolved(processAPI, currentUserId, processInstanceId) ||
+                isSupervisor(processAPI, currentUserId, processInstanceId)
+    }
 
     private boolean isInvolved(ProcessAPI processAPI, long currentUserId, long processInstanceId) {
         try {
