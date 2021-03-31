738fa81f85f75458047e9a19ebd4235d012b742e
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2021-03-31 15:24:42 +0200

    fix(bdm): override hibernate schema generator to ignore case in FKs (#1964)
    Fixes [RUNTIME-154](https://bonitasoft.atlassian.net/browse/RUNTIME-154)

---
+++ services/bonita-business-data/bonita-business-data-impl/src/main/java/org/bonitasoft/engine/business/data/impl/hibernatepatch/GroupedSchemaMigratorImpl.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (C) 2021 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
+package org.bonitasoft.engine.business.data.impl.hibernatepatch;
+
+import java.util.Set;
+
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.boot.model.relational.Namespace;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.engine.jdbc.internal.Formatter;
+import org.hibernate.mapping.Table;
+import org.hibernate.tool.schema.extract.spi.DatabaseInformation;
+import org.hibernate.tool.schema.extract.spi.NameSpaceTablesInformation;
+import org.hibernate.tool.schema.extract.spi.TableInformation;
+import org.hibernate.tool.schema.internal.exec.GenerationTarget;
+import org.hibernate.tool.schema.spi.ExecutionOptions;
+import org.hibernate.tool.schema.spi.SchemaFilter;
+
+/**
+ * @author Andrea Boriero
+ *         This implementation executes a single
+ *         {@link java.sql.DatabaseMetaData#getTables(String, String, String, String[])} call
+ *         to retrieve all the database table in order to determine if all the {@link javax.persistence.Entity} have a
+ *         mapped database tables.
+ */
+public class GroupedSchemaMigratorImpl extends AbstractSchemaMigrator {
+
+    public GroupedSchemaMigratorImpl(
+            HibernateSchemaManagementTool tool,
+            SchemaFilter schemaFilter) {
+        super(tool, schemaFilter);
+    }
+
+    @Override
+    protected NameSpaceTablesInformation performTablesMigration(
+            Metadata metadata,
+            DatabaseInformation existingDatabase,
+            ExecutionOptions options,
+            Dialect dialect,
+            Formatter formatter,
+            Set<String> exportIdentifiers,
+            boolean tryToCreateCatalogs,
+            boolean tryToCreateSchemas,
+            Set<Identifier> exportedCatalogs,
+            Namespace namespace, GenerationTarget[] targets) {
+        final NameSpaceTablesInformation tablesInformation = new NameSpaceTablesInformation(
+                metadata.getDatabase().getJdbcEnvironment().getIdentifierHelper());
+
+        if (schemaFilter.includeNamespace(namespace)) {
+            createSchemaAndCatalog(
+                    existingDatabase,
+                    options,
+                    dialect,
+                    formatter,
+                    tryToCreateCatalogs,
+                    tryToCreateSchemas,
+                    exportedCatalogs,
+                    namespace,
+                    targets);
+            final NameSpaceTablesInformation tables = existingDatabase.getTablesInformation(namespace);
+            for (Table table : namespace.getTables()) {
+                if (schemaFilter.includeTable(table) && table.isPhysicalTable()) {
+                    checkExportIdentifier(table, exportIdentifiers);
+                    final TableInformation tableInformation = tables.getTableInformation(table);
+                    if (tableInformation == null) {
+                        createTable(table, dialect, metadata, formatter, options, targets);
+                    } else if (tableInformation.isPhysicalTable()) {
+                        tablesInformation.addTableInformation(tableInformation);
+                        migrateTable(table, tableInformation, dialect, metadata, formatter, options, targets);
+                    }
+                }
+            }
+
+            for (Table table : namespace.getTables()) {
+                if (schemaFilter.includeTable(table) && table.isPhysicalTable()) {
+                    final TableInformation tableInformation = tablesInformation.getTableInformation(table);
+                    if (tableInformation == null || tableInformation.isPhysicalTable()) {
+                        applyIndexes(table, tableInformation, dialect, metadata, formatter, options, targets);
+                        applyUniqueKeys(table, tableInformation, dialect, metadata, formatter, options, targets);
+                    }
+                }
+            }
+        }
+        return tablesInformation;
+    }
+}
