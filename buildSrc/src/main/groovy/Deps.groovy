8ae5d6bb2105384f593e6e41e2e71b6dd4c70c0a
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2021-06-24 13:46:09 +0200

    chore(dependencies): upgrade javassist to 3.27.0-GA (#2018)
    This is not the last version but it is the version that hibernate `5.4.32.Final` (next version we will integrate) uses (downgraded by us)
    
    Relates to [RUNTIME-315](https://bonitasoft.atlassian.net/browse/RUNTIME-315)

--- buildSrc/src/main/groovy/Deps.groovy
+++ buildSrc/src/main/groovy/Deps.groovy
@@ -18,7 +18,7 @@ class Deps {
     public static String javaxJtaVersion = "1.1"
     public static String javaxServletVersion = "3.0.1"
     public static String groovyAllVersion = "2.4.20"
-    public static String javassistVersion = "3.18.1-GA"
+    public static String javassistVersion = "3.27.0-GA" //version used by hibernate 5.4.32.Final
     public static String httpComponentsVersion = "4.5.12"
     public static String xstreamVersion = "1.4.17"
     public static String ehcacheVersion = "2.10.6"
