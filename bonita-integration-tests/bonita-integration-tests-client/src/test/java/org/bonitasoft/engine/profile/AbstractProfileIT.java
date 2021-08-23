6bbad617948d0c267eb1b59d1a52a9cf6d434899
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2021-08-23 17:23:13 +0200

    feat(commands): remove profile commands (#2072)
    They were used only in tests.
    
    + remove unused code from ProfileMemberUtils (server side)
    
    Relates to [RUNTIME-412](https://bonitasoft.atlassian.net/browse/RUNTIME-412)

--- bonita-integration-tests/bonita-integration-tests-client/src/test/java/org/bonitasoft/engine/profile/AbstractProfileIT.java
+++ bonita-integration-tests/bonita-integration-tests-client/src/test/java/org/bonitasoft/engine/profile/AbstractProfileIT.java
@@ -13,39 +13,19 @@
  **/
 package org.bonitasoft.engine.profile;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.InputStream;
-import java.io.Serializable;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.commons.io.IOUtils;
 import org.bonitasoft.engine.TestWithTechnicalUser;
+import org.bonitasoft.engine.exception.SearchException;
 import org.bonitasoft.engine.identity.Group;
 import org.bonitasoft.engine.identity.Role;
 import org.bonitasoft.engine.identity.User;
-import org.bonitasoft.engine.search.Order;
 import org.bonitasoft.engine.search.SearchOptionsBuilder;
-import org.bonitasoft.engine.test.APITestUtil;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
 
 /**
  * @author Baptiste Mesta
  */
 public abstract class AbstractProfileIT extends TestWithTechnicalUser {
-
-    protected static final String IMPORT_PROFILES_CMD = "importProfilesCommand";
-
-    protected static final String EXPORT_DEFAULT_PROFILES_CMD = "exportDefaultProfilesCommand";
-
-    protected static final int ADMIN_PROFILE_ENTRY_COUNT = 24;
-
-    protected static final int USER_PROFILE_ENTRY_COUNT = 17;
 
     protected Long adminProfileId;
 
@@ -91,26 +71,23 @@ public abstract class AbstractProfileIT extends TestWithTechnicalUser {
         role2 = createRole("role2");
         role3 = createRole("role3");
 
-        // Restoring up default profiles before tests:
-        final InputStream xmlStream = AbstractProfileIT.class.getResourceAsStream("AllProfiles.xml");
-        final byte[] xmlContent = IOUtils.toByteArray(xmlStream);
-        xmlStream.close();
-        final Map<String, Serializable> importParameters = new HashMap<String, Serializable>(1);
-        importParameters.put("xmlContent", xmlContent);
-        getCommandAPI().execute(IMPORT_PROFILES_CMD, importParameters);
-
         // search for the newly created profile IDs:
-        final SearchOptionsBuilder builder = new SearchOptionsBuilder(0, Integer.MAX_VALUE)
-                .sort(ProfileSearchDescriptor.NAME, Order.ASC);
-        final List<Profile> profiles = getProfileAPI().searchProfiles(builder.done()).getResult();
-        assertEquals(4, profiles.size());
-        for (final Profile map : profiles) {
-            if ("Administrator".equals(map.getName())) {
-                adminProfileId = map.getId();
-            } else if ("User".equals(map.getName())) {
-                userProfileId = map.getId();
-            }
-        }
+        adminProfileId = getProfileByName("Administrator").getId();
+        userProfileId = getProfileByName("User").getId();
+
+        getProfileAPI().createProfileMember(userProfileId, user1.getId(), null, null);
+        getProfileAPI().createProfileMember(userProfileId, null, group1.getId(), null);
+        getProfileAPI().createProfileMember(adminProfileId, user1.getId(), null, null);
+        getProfileAPI().createProfileMember(adminProfileId, null, group1.getId(), role2.getId());
+        getProfileAPI().createProfileMember(adminProfileId, null, group2.getId(), role2.getId());
+        getProfileAPI().createProfileMember(adminProfileId, null, null, role1.getId());
+        getProfileAPI().createProfileMember(adminProfileId, null, null, role2.getId());
+
+    }
+
+    private Profile getProfileByName(String name) throws SearchException {
+        return getProfileAPI().searchProfiles(new SearchOptionsBuilder(0, 1).filter("name", name).done()).getResult()
+                .get(0);
     }
 
     @Override
@@ -119,28 +96,7 @@ public abstract class AbstractProfileIT extends TestWithTechnicalUser {
         deleteUsers(user1, user2, user3, user4, user5);
         deleteGroups(group1, group2, group3);
         deleteRoles(role1, role2, role3);
-
-        // Clean profiles
-        final InputStream xmlStream = AbstractProfileIT.class.getResourceAsStream("CleanProfiles.xml");
-        final byte[] xmlContent = IOUtils.toByteArray(xmlStream);
-        xmlStream.close();
-        final Map<String, Serializable> importParameters = new HashMap<String, Serializable>(1);
-        importParameters.put("xmlContent", xmlContent);
-        getCommandAPI().execute(IMPORT_PROFILES_CMD, importParameters);
-
         super.after();
-    }
-
-    @AfterClass
-    public static void restorDefaultProfiles() throws Exception {
-        final APITestUtil testUtil = new APITestUtil();
-        testUtil.loginOnDefaultTenantWithDefaultTechnicalUser();
-        final byte[] xmlContent = (byte[]) testUtil.getCommandAPI().execute(EXPORT_DEFAULT_PROFILES_CMD,
-                Collections.<String, Serializable> emptyMap());
-        final Map<String, Serializable> importParameters = new HashMap<String, Serializable>(1);
-        importParameters.put("xmlContent", xmlContent);
-        testUtil.getCommandAPI().execute(IMPORT_PROFILES_CMD, importParameters);
-        testUtil.logoutOnTenant();
     }
 
 }
--- bpm/bonita-external/src/main/java/org/bonitasoft/engine/external/identitymapping/ProfileMemberUtils.java
+++ bpm/bonita-external/src/main/java/org/bonitasoft/engine/external/identitymapping/ProfileMemberUtils.java
@@ -13,13 +13,6 @@
  **/
 package org.bonitasoft.engine.external.identitymapping;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import org.bonitasoft.engine.profile.model.SProfileMember;
-
 /**
  * @author Julien Mege
  * @author Elias Ricken de Medeiros
@@ -33,69 +26,5 @@ public class ProfileMemberUtils {
     public static final String GROUP_ID = "groupId";
 
     public static final String ROLE_ID = "roleId";
-
-    public static final String PROFILE_MEMBER_ID = "profileMemberId";
-
-    public static final String DISPLAY_NAME_PART1 = "displayNamePart1";
-
-    public static final String DISPLAY_NAME_PART2 = "displayNamePart2";
-
-    public static final String DISPLAY_NAME_PART3 = "displayNamePart3";
-
-    public static final String ROLE_AND_GROUP_TYPE = "roleAndGroup";
-
-    public static final String ROLE_TYPE = "role";
-
-    public static final String GROUP_TYPE = "group";
-
-    public static final String USER_TYPE = "user";
-
-    /**
-     * Search command parameters
-     */
-    public static final String PROFILE_MEMBER_SEARCH_INDEX = "fromIndex";
-
-    public static final String PROFILE_MEMBER_SEARCH_NUMBER = "numberOfProfiles";
-
-    public static final String PROFILE_MEMBER_SEARCH_FIELD = "field";
-
-    public static final String PROFILE_MEMBER_SEARCH_ORDER = "order";
-
-    public static final String PROFILE_MEMBER_SEARCH_OPTIONS_KEY = "searchOptions";
-
-    public static final String PROFILE_MEMBER_TYPE = "memberType";
-
-    /**
-     * Query Suffix
-     */
-    public static final String USER_SUFFIX = "ForUser";
-
-    public static final String GROUP_SUFFIX = "ForGroup";
-
-    public static final String ROLE_SUFFIX = "ForRole";
-
-    public static final String ROLE_AND_GROUP_SUFFIX = "ForRoleAndGroup";
-
-    public static HashMap<String, Serializable> memberAsProfileMembersMap(final SProfileMember profileMember) {
-        final HashMap<String, Serializable> profileMemeber = new HashMap<String, Serializable>();
-        profileMemeber.put(PROFILE_ID, profileMember.getProfileId());
-        profileMemeber.put(USER_ID, profileMember.getUserId());
-        profileMemeber.put(GROUP_ID, profileMember.getGroupId());
-        profileMemeber.put(ROLE_ID, profileMember.getRoleId());
-        profileMemeber.put(PROFILE_MEMBER_ID, profileMember.getId());
-        profileMemeber.put(DISPLAY_NAME_PART1, profileMember.getDisplayNamePart1());
-        profileMemeber.put(DISPLAY_NAME_PART2, profileMember.getDisplayNamePart2());
-        profileMemeber.put(DISPLAY_NAME_PART3, profileMember.getDisplayNamePart3());
-        return profileMemeber;
-    }
-
-    public static List<HashMap<String, Serializable>> membersAsProfileMembersMapList(
-            final List<SProfileMember> members) {
-        final ArrayList<HashMap<String, Serializable>> profileMemberMaps = new ArrayList<HashMap<String, Serializable>>();
-        for (final SProfileMember profileMember : members) {
-            profileMemberMaps.add(memberAsProfileMembersMap(profileMember));
-        }
-        return profileMemberMaps;
-    }
 
 }
