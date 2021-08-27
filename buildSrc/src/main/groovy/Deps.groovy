45296dafe65b0884b51ab776fa252b9a29b652d5
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2021-08-27 16:42:19 +0200

    fix(classloader): remove usage of xbean's classloader (#2075)
    We had a fork on the `JarFileClassLoader` of Xbeans. It looks like it had some issues that were fixed in the JVM's `URLClassLoader`. see https://bugs.openjdk.java.net/browse/JDK-6832540
    
    We do not rely anymore on features provided by the `JarFileClassLoader`.
    
    Replace the parent class of our `BonitaClassLoader` from that classloader to `URLClassLoader`.
    
    We might be able to remove some additional handling of non-jar resources as well.
    
    Closes to [RUNTIME-417](https://bonitasoft.atlassian.net/browse/RUNTIME-417)

--- buildSrc/src/main/groovy/Deps.groovy
+++ buildSrc/src/main/groovy/Deps.groovy
@@ -38,7 +38,6 @@ class Deps {
     public static String narayanaVersion = "5.10.6.Final"
     public static String jaxbVersion = "2.3.1"
     public static String logbackVersion = "1.2.3"
-    public static String xbeanClassloaderVersion = "3.7"
     public static String javaxAnnotationsVersion = "1.3.2"
 
     // extensions versions
