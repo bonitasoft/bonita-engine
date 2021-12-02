e40d38026a6af7c8181291d0df8705e8de7e74b6
abirembaut <anthony.birembaut@bonitasoft.com>
2021-12-02 10:01:37 +0100

    fix(dependencies): Update logback version (#2133)
    Covers [RUNTIME-695](https://bonitasoft.atlassian.net/browse/RUNTIME-695)

--- buildSrc/src/main/groovy/Deps.groovy
+++ buildSrc/src/main/groovy/Deps.groovy
@@ -37,7 +37,7 @@ class Deps {
     public static String narayanaVersion = "5.10.6.Final"
     public static String jbossLoggingVersion = "3.1.3.GA"
     public static String jaxbVersion = "2.3.1"
-    public static String logbackVersion = "1.2.3"
+    public static String logbackVersion = "1.2.7"
     public static String xbeanClassloaderVersion = "3.7"
 
     public static String junit4Version = "4.13.2"
