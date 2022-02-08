4f16d6af3f5dcd0a156da0064dca79d8606abe4d
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2022-02-08 15:29:19 +0100

    feat(configurability): make XA tx timeout easily configurable (#2169)
    in Tomcat bundle
    
    Closes [RUNTIME-841](https://bonitasoft.atlassian.net/browse/RUNTIME-841)

--- platform/platform-setup/src/main/standalone/tomcat-templates/setenv.bat
+++ platform/platform-setup/src/main/standalone/tomcat-templates/setenv.bat
@@ -20,6 +20,9 @@ set BDM_DB_OPTS="-Dsysprop.bonita.bdm.db.vendor=h2"
 rem Arjuna (JTA service added to Tomcat and required by Bonita Engine for transaction management)
 set ARJUNA_OPTS="-Dcom.arjuna.ats.arjuna.common.propertiesFile=%CATALINA_HOME%\conf\jbossts-properties.xml"
 
+rem use env variable BONITA_RUNTIME_TRANSACTION_XATIMEOUT=180 to override default XA Transaction timeout (in seconds):
+if not "%BONITA_RUNTIME_TRANSACTION_XATIMEOUT%" == "" (set XA_TIMEOUT_OPTS="-Dbonita.runtime.transaction.xa-timeout=%BONITA_RUNTIME_TRANSACTION_XATIMEOUT%")
+
 rem Optional JAAS configuration. Usually used when delegating authentication to LDAP / Active Directory server
 rem set SECURITY_OPTS="-Djava.security.auth.login.config=%CATALINA_HOME%\conf\jaas-standard.cfg"
 
@@ -27,12 +30,9 @@ rem Optional JMX remote access Configuration. Used to enable remote JMX agent in
 if "%JMX_REMOTE_ACCESS%" == "true" (set JMX_REMOTE_ACCESS_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9000 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=true -Dcom.sun.management.jmxremote.password.file=%CATALINA_HOME%\conf\jmxremote.password -Dcom.sun.management.jmxremote.access.file=%CATALINA_HOME%\conf\jmxremote.access")
 
 rem Pass the JVM system properties to Tomcat JVM using CATALINA_OPTS variable
-set CATALINA_OPTS=%CATALINA_OPTS% %LOG_CONF_FILE_PATH% %PLATFORM_SETUP% %H2_DATABASE_DIR% %DB_OPTS% %BDM_DB_OPTS% %ARJUNA_OPTS% %INCIDENT_LOG_DIR% %JMX_REMOTE_ACCESS_OPTS% -Dfile.encoding=UTF-8 -Xshare:auto -Xms1024m -Xmx1024m -XX:+HeapDumpOnOutOfMemoryError
+set CATALINA_OPTS=%CATALINA_OPTS% %LOG_CONF_FILE_PATH% %PLATFORM_SETUP% %XA_TIMEOUT_OPTS% %H2_DATABASE_DIR% %DB_OPTS% %BDM_DB_OPTS% %ARJUNA_OPTS% %INCIDENT_LOG_DIR% %JMX_REMOTE_ACCESS_OPTS% -Dfile.encoding=UTF-8 -Xshare:auto -Xms1024m -Xmx1024m -XX:+HeapDumpOnOutOfMemoryError
 
 set CATALINA_PID=%CATALINA_BASE%\catalina.pid
 
 @rem extra lib at Tomcat startup
 set CLASSPATH="%CATALINA_HOME%\lib\ext\*"
-
-rem Uncomment the line below to add any custom JVM System property, like the example to increase the XA transaction timeout:
-rem set JAVA_OPTS="-Dbonita.runtime.transaction.xa-timeout=180"
