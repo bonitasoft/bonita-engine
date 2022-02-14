5730db7dc7b6dc7f092f2c1a472e34a1fe97593a
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2022-02-14 14:31:42 +0100

    feat(log4j2): update setEnv to allow using of log4j2 in bonita bundle (#2171)
    Enable `formatMsgNoLookups` to mitigate [CVE-2021-44228](https://github.com/advisories/GHSA-jfh8-c2jp-5v3q) with custom client's code

--- platform/platform-setup/src/test/resources/tomcat_conf/setup/tomcat-templates/setenv.bat
+++ platform/platform-setup/src/test/resources/tomcat_conf/setup/tomcat-templates/setenv.bat
@@ -1,28 +1,4 @@
-@echo on
+rem This is a test file, no need to put the full content here
 
-rem Set some JVM system properties required by Bonita
-
-set PLATFORM_SETUP="-Dorg.bonitasoft.platform.setup.folder=%CATALINA_HOME%\..\setup"
-set H2_DATABASE_DIR="-Dorg.bonitasoft.h2.database.dir=%CATALINA_HOME%\..\h2_database"
-set INCIDENT_LOG_DIR="-Dorg.bonitasoft.engine.incident.folder=%CATALINA_HOME%\logs"
-
-rem Define the RDMBS vendor use by Bonita Engine to store data. Valid values are: h2, postgres, sqlserver, oracle, mysql.
 set DB_OPTS="-Dsysprop.bonita.db.vendor=h2"
-
-rem Define the RDMBS vendor use by Bonita Engine to store Business Data. Valid values are: h2, postgres, sqlserver, oracle, mysql.
-rem If you use different DB engines by tenants, please update directly bonita-tenant-community-custom.properties
 set BDM_DB_OPTS="-Dsysprop.bonita.bdm.db.vendor=h2"
-
-rem Arjuna (JTA service added to Tomcat and required by Bonita Engine for transaction management)
-set ARJUNA_OPTS="-Dcom.arjuna.ats.arjuna.common.propertiesFile=%CATALINA_HOME%\conf\jbossts-properties.xml"
-
-rem Optional JAAS configuration. Usually used when delegating authentication to LDAP / Active Directory server
-rem set SECURITY_OPTS="-Djava.security.auth.login.config=%CATALINA_HOME%\conf\jaas-standard.cfg"
-
-rem Pass the JVM system properties to Tomcat JVM using CATALINA_OPTS variable
-set CATALINA_OPTS=%CATALINA_OPTS% %PLATFORM_SETUP% %H2_DATABASE_DIR% %DB_OPTS% %BDM_DB_OPTS% %ARJUNA_OPTS% %INCIDENT_LOG_DIR% -Dfile.encoding=UTF-8 -Xshare:auto -Xms1024m -Xmx1024m -XX:+HeapDumpOnOutOfMemoryError
-
-set CATALINA_PID=%CATALINA_BASE%\catalina.pid
-
-@rem extra lib at Tomcat startup (for instance bonita juli extensions)
-set CLASSPATH="%CATALINA_HOME%\lib\ext\*"
