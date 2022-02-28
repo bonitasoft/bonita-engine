1f15e723530345e6393fc43190b943adb6830c49
danila_mazour <danila.mazour@bonitasoft.com>
2022-02-28 11:09:09 +0100

    fix(postgres): update Postgres version to 42.3.3 (#2189)
    Fixes CVE.

--- buildSrc/src/main/groovy/Deps.groovy
+++ buildSrc/src/main/groovy/Deps.groovy
@@ -34,7 +34,7 @@ class Deps {
     public static String mysqlVersion = "8.0.14"
     public static String mssqlVersion = "7.2.1.jre8"
     public static String oracleVersion = "19.3.0.0"
-    public static String postgresqlVersion = "42.2.5"
+    public static String postgresqlVersion = "42.3.3"
     public static String lombokVersion = "1.18.4"
 
     public static String naranayaVersion = "5.9.3.Final"
