04069c456e8cd4db7a6f9313856bd1942e2b0f64
abirembaut <anthony.birembaut@bonitasoft.com>
2022-03-06 20:16:13 +0100

    chore(dependencies): update mysql driver version (#2198)
    update to 8.028 to fix CVE-2022-21363

--- buildSrc/src/main/groovy/Deps.groovy
+++ buildSrc/src/main/groovy/Deps.groovy
@@ -31,7 +31,7 @@ class Deps {
     public static String quartzVersion = "2.3.2"
     public static String micrometerVersion = "1.3.2"
 
-    public static String mysqlVersion = "8.0.14"
+    public static String mysqlVersion = "8.0.28"
     public static String mssqlVersion = "7.2.1.jre8"
     public static String oracleVersion = "19.3.0.0"
     public static String postgresqlVersion = "42.3.3"
