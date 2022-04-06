48ff0fd792318c5ffd4fc6f6ae20856399556301
Haroun El Alami <haroun.elalami@bonitasoft.com>
2022-04-06 09:44:03 +0200

    fix(deps): replace javax servlet by jakarta servlet (#2218)
    Covers [RUNTIME-798](https://bonitasoft.atlassian.net/browse/RUNTIME-798)

--- buildSrc/src/main/groovy/Deps.groovy
+++ buildSrc/src/main/groovy/Deps.groovy
@@ -16,7 +16,7 @@ class Deps {
     public static String hibernateVersion = "5.4.32.Final"
     public static String jacksonDataBindingVersion = "2.12.6"
     public static String jakartaTransactionVersion = "1.3.3"
-    public static String javaxServletVersion = "4.0.1"
+    public static String jakartaServletVersion = "4.0.4"
     // The groovy version must be in synch with the runtime-bom artifact
     public static String groovyVersion = "3.0.9"
     public static String javassistVersion = "3.27.0-GA" //version used by hibernate 5.4.32.Final
