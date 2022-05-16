cdeda60e25b764af257dc7c65cafa2446053121a
Pascal GARCIA <pascal.garcia@bonitasoft.com>
2022-05-16 15:23:09 +0200

    chore(hibernate): bump hibernate version (#2246)
    * bump hibernate version to prepare update of h2( v2)
    * add adr  0011-hibernate5.4_to_5.6_impacts_analyses.md
    * add javaassist dependencies to allow bdm client generation with studio
    
    
    **Possible Impacts Identified :** 
    
    [migration-5.5](https://github.com/hibernate/hibernate-orm/blob/5.5/migration-guide.adoc
    ) => no impact identified
    [migration-5.6](https://github.com/hibernate/hibernate-orm/blob/5.6/migration-guide.adoc
    ) => only one thing could be have an impact is [Changes to the DDL type for CLOB ](https://github.com/hibernate/hibernate-orm/blob/5.6/migration-guide.adoc#changes-to-the-ddl-type-for-clob-in-postgresql81dialect-and-its-subclasses) but obviously they have no impact.
    
    **I validated it :**
    **From Scatch :**
    *  run bonita 7.14 with postgres database
    *  run a process using bdm with field type text maps as CLOB in bdm client.
    * display the case details of process running before and the bdm datas is well retrieve
    **=> The process works fine*
    
    **Migration :** 
    *  run bonita 7.14 with postgres database
    *  run a process using bdm with field type text maps as CLOB in bdm client.
    *  migrate database to 7.15 
    * display the case details of process running before and the bdm datas is well retrieve
    *  run a process using bdm with field type text maps as CLOB in bdm client.
    
    **=> The process still working well and old bdm datas are still accessible**
    
    
    Closes [RUNTIME-1009](https://bonitasoft.atlassian.net/browse/RUNTIME-1009)
    Closes [RUNTIME-863](https://bonitasoft.atlassian.net/browse/RUNTIME-863)

--- services/bonita-business-data/bonita-business-data-generator/src/test/java/org/bonitasoft/engine/business/data/generator/client/ClientBDMJarBuilderTest.java
+++ services/bonita-business-data/bonita-business-data-generator/src/test/java/org/bonitasoft/engine/business/data/generator/client/ClientBDMJarBuilderTest.java
@@ -18,7 +18,6 @@ import static org.mockito.Mockito.verify;
 import java.io.File;
 
 import org.apache.commons.io.FileUtils;
-import org.bonitasoft.engine.bdm.dao.client.resources.BusinessObjectDeserializer;
 import org.bonitasoft.engine.business.data.generator.BOMBuilder;
 import org.bonitasoft.engine.io.IOUtils;
 import org.junit.Before;
@@ -48,7 +47,7 @@ public class ClientBDMJarBuilderTest {
 
         clientBDMJarBuilder.addSourceFilesToDirectory(BOMBuilder.aBOM().build(), directory);
 
-        verify(resourcesLoader).copyJavaFilesToDirectory(BusinessObjectDeserializer.class.getPackage().getName(),
+        verify(resourcesLoader).copyJavaFilesToDirectory("org.bonitasoft.engine.bdm.dao.client.resources",
                 directory);
         FileUtils.deleteDirectory(directory);
     }
