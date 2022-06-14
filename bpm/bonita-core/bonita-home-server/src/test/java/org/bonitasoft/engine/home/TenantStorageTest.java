3d0b2c4866298c80fd1322a7e0589996c8fa0848
Pascal GARCIA <pascal.garcia@bonitasoft.com>
2022-06-14 15:17:41 +0200

    fix(incidents): add possibility to filter incident logs using marker (#2280)
    **issue**: since the migration to log4j2, the incident logs are not more logged on a specific file.
    **fix**: add slf4j marker on incident log to allow to define a new log4j2 appender that will log all logs marked with INCIDENT to a specific file.
    **also** remove no more needed getIncidentFileHandler method
    
    Closes [RUNTIME-1018](https://bonitasoft.atlassian.net/browse/RUNTIME-1018)

--- bpm/bonita-core/bonita-home-server/src/test/java/org/bonitasoft/engine/home/TenantStorageTest.java
+++ bpm/bonita-core/bonita-home-server/src/test/java/org/bonitasoft/engine/home/TenantStorageTest.java
@@ -14,18 +14,12 @@
 package org.bonitasoft.engine.home;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.bonitasoft.engine.home.TenantStorage.INCIDENTS_LOG_FILENAME;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
 import java.io.File;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.contrib.java.lang.system.ClearSystemProperties;
 import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.mockito.Spy;
@@ -36,10 +30,6 @@ import org.mockito.junit.MockitoJUnitRunner;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class TenantStorageTest {
-
-    @Rule
-    public final ClearSystemProperties bonitaClientHome = new ClearSystemProperties(
-            TenantStorage.INCIDENT_LOG_FOLDER_PROPERTY);
 
     @Rule
     public TemporaryFolder temporaryFolder = new TemporaryFolder();;
@@ -56,36 +46,5 @@ public class TenantStorageTest {
 
         //then
         assertThat(profileMD5).as("should retrieve file").exists().hasBinaryContent("md5".getBytes());
-    }
-
-    @Test
-    public void getIncidentFileHandler_should_use_temp_folder_if_property_not_set() throws Exception {
-        //given
-        final String expectedIncidentFileAbsolutePath = Paths
-                .get(tenantStorage.getTenantTempFolder(14L).getFile().getAbsolutePath())
-                .resolve(INCIDENTS_LOG_FILENAME).toString();
-
-        // when
-        tenantStorage.getIncidentFileHandler(14L);
-
-        verify(tenantStorage).getFileHandler(expectedIncidentFileAbsolutePath);
-    }
-
-    @Test
-    public void getIncidentFileHandler_should_use_incident_property_if_set() throws Exception {
-        //given
-        final String newFolder = temporaryFolder.newFolder().getAbsolutePath();
-        System.setProperty(TenantStorage.INCIDENT_LOG_FOLDER_PROPERTY, newFolder);
-        final long tenantId = 14L;
-        final String expectedIncidentFileAbsolutePath = Paths.get(newFolder).resolve("tenants").resolve("" + tenantId)
-                .resolve(INCIDENTS_LOG_FILENAME)
-                .toString();
-
-        // when
-        tenantStorage.getIncidentFileHandler(tenantId);
-
-        //then:
-        verify(tenantStorage).getFileHandler(expectedIncidentFileAbsolutePath);
-        verify(tenantStorage, times(0)).getTenantTempFolder(anyLong());
     }
 }
