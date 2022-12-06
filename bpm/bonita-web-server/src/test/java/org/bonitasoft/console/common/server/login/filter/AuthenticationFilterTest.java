010f4c9ae12e78238c2b146ac1db91cee9107b3d
bonita-ci <hudson.mailer@bonitasoft.com>
2022-12-06 17:19:44 +0100

    fix(Platform session): avoid HTTP session invalidation (#2411)
    Modifications reported from **bonita-web-sp**. See https://github.com/bonitasoft/bonita-web-sp/commit/9b9fbe68f63c02cbe50e78b2c76339de9b6e5a71

--- bpm/bonita-web-server/src/test/java/org/bonitasoft/console/common/server/login/filter/AuthenticationFilterTest.java
+++ bpm/bonita-web-server/src/test/java/org/bonitasoft/console/common/server/login/filter/AuthenticationFilterTest.java
@@ -37,6 +37,8 @@ import org.assertj.core.api.Condition;
 import org.bonitasoft.console.common.server.auth.AuthenticationManager;
 import org.bonitasoft.console.common.server.login.HttpServletRequestAccessor;
 import org.bonitasoft.console.common.server.login.utils.RedirectUrl;
+import org.bonitasoft.console.common.server.utils.SessionUtil;
+import org.bonitasoft.engine.session.APISession;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -56,6 +58,9 @@ public class AuthenticationFilterTest {
 
     @Mock
     private HttpSession httpSession;
+
+    @Mock
+    private APISession apiSession;
 
     @Mock
     private FilterConfig filterConfig;
@@ -133,6 +138,22 @@ public class AuthenticationFilterTest {
 
         verify(firstFailingRule, never()).proceedWithRequest(chain, httpRequest, httpResponse);
         verify(secondFailingRule, never()).proceedWithRequest(chain, httpRequest, httpResponse);
+        verify(authenticationFilter).cleanHttpSession(httpSession);
+        verify(httpResponse).sendRedirect(anyString());
+    }
+
+    @Test
+    public void testIfSessionIsNotInvalidatedWhenCallingPlatformAPIWithAPISession() throws Exception {
+        AuthenticationRule failingRule = spy(createFailingRule());
+        authenticationFilter.addRule(failingRule);
+        when(httpRequest.getServletPath()).thenReturn("/API");
+        when(httpRequest.getPathInfo()).thenReturn("/platform/license");
+        when(httpRequest.getContextPath()).thenReturn("/bonita");
+        when(httpSession.getAttribute(SessionUtil.API_SESSION_PARAM_KEY)).thenReturn(apiSession);
+
+        authenticationFilter.doAuthenticationFiltering(request, httpResponse, chain);
+
+        verify(authenticationFilter, never()).cleanHttpSession(httpSession);
         verify(httpResponse).sendRedirect(anyString());
     }
 
