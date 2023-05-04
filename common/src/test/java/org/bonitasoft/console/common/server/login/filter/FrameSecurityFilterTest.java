90ebff75b2c88a4d931d7d18dc57d469660f9c94
bonita-ci <hudson.mailer@bonitasoft.com>
2023-05-04 18:27:01 +0200

    fix(frame security): allow usage of Content-Security-Policy header (#2553)
    cf. commit bonitasoft/bonita-web-sp@a08451b and bonita-web-sp@984c0fc
    * support Content-Security-Policy header in order to enforce clickjacking protection
    * continue supporting X-Frame-Options header for backward compatibility although it is now only partially supported by browsers
    * add possibility to configure headers value through property to avoid having to update web.xml
    
    Covers [RUNTIME-1688](https://bonitasoft.atlassian.net/browse/RUNTIME-1688)
    
    Co-authored-by: Adrien Kantcheff <adrien.kantcheff@bonitasoft.com>

---
+++ common/src/test/java/org/bonitasoft/console/common/server/login/filter/FrameSecurityFilterTest.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (C) 2023 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
+package org.bonitasoft.console.common.server.login.filter;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.bonitasoft.console.common.server.preferences.properties.SecurityProperties;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FrameSecurityFilterTest {
+
+    @Mock
+    private HttpServletRequest httpRequest;
+
+    @Mock
+    private HttpServletResponse httpResponse;
+    
+    @Mock
+    private FilterConfig filterConfig;
+    
+    @Mock
+    private FilterChain chain;
+    
+    @Mock
+    private ServletContext servletContext;
+    
+    @Mock
+    private SecurityProperties securityProperties;
+    
+    @Spy
+    FrameSecurityFilter frameSecurityFilter;
+    
+    @Before
+    public void setUp() throws Exception {
+        initMocks(this);
+        doReturn(securityProperties).when(frameSecurityFilter).getSecurityProperties();
+        when(servletContext.getContextPath()).thenReturn("");
+        when(filterConfig.getServletContext()).thenReturn(servletContext);
+    }
+    
+    @Test
+    public void should_add_default_headers() throws Exception {
+        
+        frameSecurityFilter.init(filterConfig);
+        
+        frameSecurityFilter.proceedWithFiltering(httpRequest, httpResponse, chain);
+        
+        verify(httpResponse).setHeader(FrameSecurityFilter.X_FRAME_OPTIONS_HEADER, FrameSecurityFilter.X_FRAME_OPTIONS_HEADER_DEFAULT);
+        verify(httpResponse).setHeader(FrameSecurityFilter.CONTENT_SECURITY_POLICY_HEADER, FrameSecurityFilter.CONTENT_SECURITY_POLICY_HEADER_DEFAULT);
+    }
+    
+    @Test
+    public void should_add_configured_headers() throws Exception {
+        
+        doReturn("DENY").when(securityProperties).getXFrameOptionsHeader();
+        doReturn("frame-ancestors 'none';").when(securityProperties).getContentSecurityPolicyHeader();
+        frameSecurityFilter.init(filterConfig);
+        
+        frameSecurityFilter.proceedWithFiltering(httpRequest, httpResponse, chain);
+        
+        verify(httpResponse).setHeader(FrameSecurityFilter.X_FRAME_OPTIONS_HEADER, "DENY");
+        verify(httpResponse).setHeader(FrameSecurityFilter.CONTENT_SECURITY_POLICY_HEADER, "frame-ancestors 'none';");
+    }
+}
