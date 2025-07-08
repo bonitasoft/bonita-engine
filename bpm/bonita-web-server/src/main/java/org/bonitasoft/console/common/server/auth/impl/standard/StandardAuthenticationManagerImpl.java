bonitasoft/bonita-engine

commit 108bc5b82387252beacf81baa01c0e62403a989a

feat(login page): make login page configurable with variable (#3440)
* apply the same mechanism as for authproviders page to the login page

Relates to [PROD-431](https://bonitasoft.atlassian.net/browse/PROD-431)

bpm/bonita-web-server/src/main/java/org/bonitasoft/console/common/server/auth/impl/standard/StandardAuthenticationManagerImpl.java
@@ -21,9 +21,12 @@ import javax.servlet.ServletException;
 
 import org.bonitasoft.console.common.server.auth.AuthenticationFailedException;
 import org.bonitasoft.console.common.server.auth.AuthenticationManager;
+import org.bonitasoft.console.common.server.auth.AuthenticationManagerProperties;
 import org.bonitasoft.console.common.server.login.HttpServletRequestAccessor;
 import org.bonitasoft.console.common.server.login.credentials.Credentials;
 import org.bonitasoft.console.common.server.utils.LocaleUtils;
+import org.bonitasoft.console.common.server.utils.UrlBuilder;
+import org.bonitasoft.engine.properties.StringProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,16 +43,14 @@ public class StandardAuthenticationManagerImpl implements AuthenticationManager 
     @Override
     public String getLoginPageURL(final HttpServletRequestAccessor request, final String redirectURL)
             throws ServletException {
-        final StringBuilder url = new StringBuilder();
-        String context = request.asHttpServletRequest().getContextPath();
-        url.append(context).append(AuthenticationManager.LOGIN_PAGE).append("?");
+        final UrlBuilder loginURL = new UrlBuilder(getLoginPage(request));
         //adds the locale to the login URL if it is set in the requested URL
         String localeFromRequestedURL = LocaleUtils.getLocaleFromRequestURL(request.asHttpServletRequest());
         if (localeFromRequestedURL != null) {
-            url.append(LocaleUtils.PORTAL_LOCALE_PARAM).append("=").append(localeFromRequestedURL).append("&");
+            loginURL.appendParameter(LocaleUtils.PORTAL_LOCALE_PARAM, localeFromRequestedURL);
         }
-        url.append(AuthenticationManager.REDIRECT_URL).append("=").append(redirectURL);
-        return url.toString();
+        loginURL.appendParameter(AuthenticationManager.REDIRECT_URL, redirectURL);
+        return loginURL.build();
     }
 
     @Override
@@ -67,5 +68,26 @@ public class StandardAuthenticationManagerImpl implements AuthenticationManager 
     public String getLogoutPageURL(final HttpServletRequestAccessor request, final String redirectURL)
             throws ServletException {
         return null;
+    }
+
+    protected String getLoginPage(HttpServletRequestAccessor requestAccessor) {
+        StringProperty loginPage = new StringProperty("External Login URL",
+                AuthenticationManager.BONITA_RUNTIME_AUTHENTICATION_LOGIN_URL_VAR,
+                getAuthenticationProperty(
+                        AuthenticationManager.BONITA_RUNTIME_AUTHENTICATION_LOGIN_URL_VAR,
+                        getDefaultLoginPage(requestAccessor)));
+        return loginPage.getValue();
+    }
+
+    protected String getDefaultLoginPage(HttpServletRequestAccessor requestAccessor) {
+        final StringBuilder url = new StringBuilder();
+        String context = requestAccessor.asHttpServletRequest().getContextPath();
+        url.append(context).append(LOGIN_PAGE);
+        return url.toString();
+    }
+
+    protected String getAuthenticationProperty(String propertyName, String defaultValue) {
+        String propertyValue = AuthenticationManagerProperties.getProperties().getTenantProperty(propertyName);
+        return propertyValue != null ? propertyValue : defaultValue;
     }
 }

bpm/bonita-web-server/src/test/java/org/bonitasoft/console/common/server/auth/impl/standard/StandardAuthenticationManagerImplTest.java
@@ -41,7 +41,7 @@ public class StandardAuthenticationManagerImplTest {
 
     @Test
     public void testGetSimpleLoginpageURL() throws Exception {
-        String redirectUrl = "%2Fapps%2FappDirectoryBonita";
+        String redirectUrl = "/apps/appDirectoryBonita";
 
         String loginURL = standardLoginManagerImpl.getLoginPageURL(requestAccessor, redirectUrl);
 
@@ -50,7 +50,7 @@ public class StandardAuthenticationManagerImplTest {
 
     @Test
     public void testGetLoginpageURLWithLocale() throws Exception {
-        String redirectUrl = "%2Fapps%2FappDirectoryBonita";
+        String redirectUrl = "/apps/appDirectoryBonita";
         request.setParameter("_l", "es");
 
         String loginURL = standardLoginManagerImpl.getLoginPageURL(requestAccessor, redirectUrl);
@@ -60,7 +60,7 @@ public class StandardAuthenticationManagerImplTest {
 
     @Test
     public void testGetLoginpageURLFromPortal() throws Exception {
-        String redirectUrl = "%2Fapps%2FappDirectoryBonita";
+        String redirectUrl = "/apps/appDirectoryBonita";
         request.setServletPath("/portal/");
 
         String loginURL = standardLoginManagerImpl.getLoginPageURL(requestAccessor, redirectUrl);

bpm/bonita-web-server/src/main/java/org/bonitasoft/console/common/server/auth/AuthenticationManager.java
@@ -44,9 +44,14 @@ public interface AuthenticationManager {
     String REDIRECT_AFTER_LOGIN_PARAM_NAME = "redirect";
 
     /**
-     * the URL of the default login page
+     * the URL of the default login page. Only used when @BONITA_RUNTIME_AUTHENTICATION_LOGIN_URL_VAR is not set
      */
     String LOGIN_PAGE = "/login.jsp";
+
+    /*
+     * The system variable or property name for the login page URL
+     */
+    String BONITA_RUNTIME_AUTHENTICATION_LOGIN_URL_VAR = "bonita.runtime.authentication.login.url";
 
     /**
      * The default redirect URL.

bpm/bonita-web-server/src/main/java/org/bonitasoft/console/common/server/login/servlet/LoginServlet.java
@@ -24,7 +24,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.bonitasoft.console.common.server.auth.AuthenticationFailedException;
 import org.bonitasoft.console.common.server.auth.AuthenticationManager;
+import org.bonitasoft.console.common.server.auth.AuthenticationManagerFactory;
 import org.bonitasoft.console.common.server.auth.AuthenticationManagerNotFoundException;
+import org.bonitasoft.console.common.server.login.HttpServletRequestAccessor;
 import org.bonitasoft.console.common.server.login.LoginFailedException;
 import org.bonitasoft.console.common.server.login.LoginManager;
 import org.bonitasoft.console.common.server.login.utils.RedirectUrlBuilder;
@@ -60,11 +62,6 @@ public class LoginServlet extends HttpServlet {
      * login fail message
      */
     protected static final String LOGIN_FAIL_MESSAGE = "loginFailMessage";
-
-    /**
-     * the URL param for the login page
-     */
-    protected static final String LOGIN_URL_PARAM_NAME = "loginUrl";
 
     /*
      * System property to allow login with GET from the development suite
@@ -123,14 +120,25 @@ public class LoginServlet extends HttpServlet {
             doLogin(request, response);
             final APISession apiSession = (APISession) request.getSession()
                     .getAttribute(SessionUtil.API_SESSION_PARAM_KEY);
-            // if there a redirect=true or a redirectURL parameter in the request do nothing (API login), otherwise, redirect (Portal login)
+            // if there is no redirect=true or redirectURL parameter in the request do nothing (API login), otherwise, redirect (Portal login)
             if (redirectAfterLogin) {
                 if (apiSession.isTechnicalUser() || hasProfile(apiSession)) {
                     response.sendRedirect(createRedirectUrl(redirectURL, locale));
                 } else {
-                    request.setAttribute(LOGIN_FAIL_MESSAGE, "noProfileForUser");
-                    getServletContext().getRequestDispatcher(AuthenticationManager.LOGIN_PAGE).forward(request,
-                            response);
+                    String loginURL = AuthenticationManagerFactory.getAuthenticationManager()
+                            .getLoginPageURL(new HttpServletRequestAccessor(request), redirectURL);
+                    if (loginURL.startsWith(request.getContextPath() + AuthenticationManager.LOGIN_PAGE)) {
+                        request.setAttribute(LOGIN_FAIL_MESSAGE, "noProfileForUser");
+                        getServletContext().getRequestDispatcher(AuthenticationManager.LOGIN_PAGE).forward(request,
+                                response);
+                    } else {
+                        // if the login page is not the default jsp but an external URL the forward cannot work.
+                        // just respond with a 401
+                        if (LOGGER.isDebugEnabled()) {
+                            LOGGER.debug("No profiles for user");
+                        }
+                        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED, "noProfileForUser");
+                    }
                 }
             } else {
                 LocaleUtils.addOrReplaceLocaleCookieResponse(response, locale);
@@ -161,29 +169,37 @@ public class LoginServlet extends HttpServlet {
     }
 
     private void handleException(final HttpServletRequest request, final HttpServletResponse response,
-            final boolean redirectAfterLogin,
-            final Exception e, final String locale) throws ServletException {
+            final boolean redirectAfterLogin, final Exception exception, final String locale) throws ServletException {
         // if there a redirect=false attribute in the request do nothing (API login), otherwise, redirect (Portal login)
         if (redirectAfterLogin) {
             try {
-                request.setAttribute(LOGIN_FAIL_MESSAGE, LOGIN_FAIL_MESSAGE);
-                String loginURL = request.getParameter(LOGIN_URL_PARAM_NAME);
+                String loginURL = request.getParameter(AuthenticationManager.LOGIN_URL_PARAM_NAME);
                 if (loginURL == null) {
-                    loginURL = AuthenticationManager.LOGIN_PAGE;
-                    getServletContext().getRequestDispatcher(loginURL).forward(request, response);
+                    final String redirectURL = getRedirectUrl(request, redirectAfterLogin);
+                    loginURL = AuthenticationManagerFactory.getAuthenticationManager()
+                            .getLoginPageURL(new HttpServletRequestAccessor(request), redirectURL);
                 } else {
-                    getServletContext().getRequestDispatcher(createRedirectUrl(loginURL, locale)).forward(request,
+                    loginURL = createRedirectUrl(loginURL, locale);
+                }
+                if (loginURL.startsWith(request.getContextPath() + AuthenticationManager.LOGIN_PAGE)) {
+                    request.setAttribute(LOGIN_FAIL_MESSAGE, LOGIN_FAIL_MESSAGE);
+                    getServletContext().getRequestDispatcher(AuthenticationManager.LOGIN_PAGE).forward(request,
                             response);
+                } else {
+                    // if the login page is not the default jsp but an external URL the forward cannot work.
+                    // just respond with a 401
+                    if (LOGGER.isDebugEnabled()) {
+                        LOGGER.debug(exception.getMessage());
+                    }
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED, LOGIN_FAIL_MESSAGE);
                 }
-            } catch (final Exception e1) {
-                if (LOGGER.isErrorEnabled()) {
-                    LOGGER.error(e1.getMessage());
-                }
-                throw new ServletException(e1);
+            } catch (final Exception e) {
+                LOGGER.error(e.getMessage());
+                throw new ServletException(e);
             }
         } else {
             if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug(e.getMessage());
+                LOGGER.debug(exception.getMessage());
             }
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         }

bpm/bonita-web-server/src/test/java/org/bonitasoft/console/common/server/login/servlet/LoginServletTest.java
@@ -263,6 +263,7 @@ public class LoginServletTest {
         final LoginManager loginManager = mock(LoginManager.class);
         final ServletContext servletContext = mock(ServletContext.class);
         RequestDispatcher requestDispatcher = mock(RequestDispatcher.class);
+        doReturn("/bonita").when(req).getContextPath();
         doReturn("true").when(req).getParameter(AuthenticationManager.REDIRECT_AFTER_LOGIN_PARAM_NAME);
         doReturn("anyurl").when(req).getParameter(AuthenticationManager.REDIRECT_URL);
         doReturn(httpSession).when(req).getSession();

commit a06067aeae5d5900963b0ef995a43bb000499f8c

fix(login page): only compute login page URL encoding once (#3465)
Fix double encoding of redirectUrl (already encoded By the class LoginUrl)

This PR fixes a defect introduced in #3440

bpm/bonita-web-server/src/main/java/org/bonitasoft/console/common/server/auth/impl/standard/StandardAuthenticationManagerImpl.java
@@ -14,6 +14,8 @@
 package org.bonitasoft.console.common.server.auth.impl.standard;
 
 import java.io.Serializable;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.Map;
 
@@ -49,7 +51,10 @@ public class StandardAuthenticationManagerImpl implements AuthenticationManager 
         if (localeFromRequestedURL != null) {
             loginURL.appendParameter(LocaleUtils.PORTAL_LOCALE_PARAM, localeFromRequestedURL);
         }
-        loginURL.appendParameter(AuthenticationManager.REDIRECT_URL, redirectURL);
+        //Decodes the redirect URL if it is encoded because UrlBuilder already encodes it (avoid double encoding)
+        //since this method is part of a public interface, we cannot change the calling code to pass decoded redirectURL
+        String decodedRedirectURL = URLDecoder.decode(redirectURL, StandardCharsets.UTF_8);
+        loginURL.appendParameter(AuthenticationManager.REDIRECT_URL, decodedRedirectURL);
         return loginURL.build();
     }
 

bpm/bonita-web-server/src/test/java/org/bonitasoft/console/common/server/auth/impl/standard/StandardAuthenticationManagerImplTest.java
@@ -41,7 +41,7 @@ public class StandardAuthenticationManagerImplTest {
 
     @Test
     public void testGetSimpleLoginpageURL() throws Exception {
-        String redirectUrl = "/apps/appDirectoryBonita";
+        String redirectUrl = "%2Fapps%2FappDirectoryBonita";
 
         String loginURL = standardLoginManagerImpl.getLoginPageURL(requestAccessor, redirectUrl);
 
@@ -50,7 +50,7 @@ public class StandardAuthenticationManagerImplTest {
 
     @Test
     public void testGetLoginpageURLWithLocale() throws Exception {
-        String redirectUrl = "/apps/appDirectoryBonita";
+        String redirectUrl = "%2Fapps%2FappDirectoryBonita";
         request.setParameter("_l", "es");
 
         String loginURL = standardLoginManagerImpl.getLoginPageURL(requestAccessor, redirectUrl);
@@ -60,7 +60,7 @@ public class StandardAuthenticationManagerImplTest {
 
     @Test
     public void testGetLoginpageURLFromPortal() throws Exception {
-        String redirectUrl = "/apps/appDirectoryBonita";
+        String redirectUrl = "%2Fapps%2FappDirectoryBonita";
         request.setServletPath("/portal/");
 
         String loginURL = standardLoginManagerImpl.getLoginPageURL(requestAccessor, redirectUrl);