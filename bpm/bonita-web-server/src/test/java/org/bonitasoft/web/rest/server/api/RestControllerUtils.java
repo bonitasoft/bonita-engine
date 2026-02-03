bonitasoft/bonita-engine

commit d2c88312a4c7561def814099c8c434208c411f7e

refactor(bpm): convert TimerEventTriggerResource REST API to Spring MVC (#3686)
Convert TimerEventTriggerResource from Restlet to Spring MVC framework.

Changes:
- Create TimerEventTriggerController with GET PUT
- Add Spring MVC servlet mapping in web.xml
- Remove Restlet route from application class
- Remove Finder from FinderFactory
- Delete old Restlet Resource and Finder classes
- Create TimerEventTriggerControllerTest with AbstractControllerTest
- Delete old Restlet test

The endpoint maintains the same API contract:
- GET /API/bpm/timerEventTrigger
- PUT /API/bpm/timerEventTrigger

bpm/bonita-web-server/src/main/java/org/bonitasoft/web/rest/server/api/bpm/flownode/TimerEventTriggerController.java
@@ -1,0 +1,91 @@
+/**
+ * Copyright (C) 2026 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
+package org.bonitasoft.web.rest.server.api.bpm.flownode;
+
+import static org.bonitasoft.web.rest.server.APIPaginationUtils.buildContentRange;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.bonitasoft.engine.bpm.flownode.TimerEventTriggerInstance;
+import org.bonitasoft.engine.exception.BonitaException;
+import org.bonitasoft.engine.search.SearchOptionsBuilder;
+import org.bonitasoft.engine.search.SearchResult;
+import org.bonitasoft.web.rest.server.api.AbstractRESTController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Spring MVC Controller for Timer Event Triggers.
+ * Converted from Restlet to Spring MVC.
+ *
+ * @author Emmanuel Duchastenier
+ */
+@RestController
+@RequestMapping("/API/bpm/timerEventTrigger")
+public class TimerEventTriggerController extends AbstractRESTController {
+
+    /**
+     * Search timer event triggers for a given case.
+     *
+     * @param caseId the case ID to search timers for
+     * @param page the page number (p parameter)
+     * @param count the number of results per page (c parameter)
+     * @param httpSession the HTTP session
+     * @return the list of timer event triggers with Content-Range header
+     * @throws BonitaException if an error occurs
+     */
+    @GetMapping
+    public ResponseEntity<List<TimerEventTriggerInstance>> searchTimerEventTriggers(
+            @RequestParam(value = "caseId") Long caseId,
+            @RequestParam(value = "p", defaultValue = "0") int page,
+            @RequestParam(value = "c", defaultValue = "10") int count,
+            HttpSession httpSession) throws BonitaException {
+
+        final SearchResult<TimerEventTriggerInstance> searchResult = getProcessAPI(httpSession)
+                .searchTimerEventTriggerInstances(caseId, new SearchOptionsBuilder(page * count, count).done());
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_RANGE,
+                        buildContentRange(page, searchResult.getResult().size(), searchResult.getCount()))
+                .body(searchResult.getResult());
+    }
+
+    /**
+     * Update the execution date of a timer event trigger.
+     *
+     * @param id the timer event trigger instance ID
+     * @param trigger the timer event trigger data containing the new execution date
+     * @param httpSession the HTTP session
+     * @return the updated timer event trigger
+     * @throws BonitaException if an error occurs
+     */
+    @PutMapping("/{id}")
+    public TimerEventTrigger updateTimerEventTrigger(@PathVariable Long id, @RequestBody TimerEventTrigger trigger,
+            HttpSession httpSession) throws BonitaException {
+        final Date executionDate = new Date(trigger.getExecutionDate());
+        final Date updatedDate = getProcessAPI(httpSession).updateExecutionDateOfTimerEventTriggerInstance(id,
+                executionDate);
+        return new TimerEventTrigger(updatedDate.getTime());
+    }
+}

commit bbaa63ddf6d69e400c8b27cd13ba9345ca07aecd

fix(bpm): configure Jackson serializers for Spring MVC message converters (#3692)
## Summary

Fixes a regression introduced in #3686 where the `_string` suffix fields
for numeric IDs were missing from the JSON response after converting
TimerEventTrigger REST API from Restlet to Spring MVC.

**Root cause:** The `@EnableWebMvc` annotation conflicts with `extends
WebMvcConfigurationSupport`. When both are present, Spring imports
`DelegatingWebMvcConfiguration` which overrides our custom
configuration, preventing `configureMessageConverters()` from being
called.

**Fix:**
- Remove `@EnableWebMvc` annotation (redundant when extending
`WebMvcConfigurationSupport` directly)
- Override `configureMessageConverters()` to register custom Bonita
Jackson serializers
- Add `BonitaJacksonModuleProvider` to share Jackson configuration
between production and tests

## Test plan

- [x] Unit tests pass (`TimerEventTriggerControllerTest`)
- [x] All bonita-web-server tests pass (1260 tests)
- [x] Manual test confirms `_string` fields are now present in JSON
response

bpm/bonita-web-server/src/main/java/org/bonitasoft/web/rest/server/SpringWebConfiguration.java
@@ -13,23 +13,29 @@
  **/
 package org.bonitasoft.web.rest.server;
 
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.bonitasoft.web.rest.server.utils.BonitaJacksonModuleProvider;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.accept.ContentNegotiationManager;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import org.springframework.web.servlet.resource.ResourceUrlProvider;
 
 @Configuration
-@EnableWebMvc
+// Note: Do NOT use @EnableWebMvc when extending WebMvcConfigurationSupport directly
 @ComponentScan({ "org.bonitasoft.web.rest.server.api", "com.bonitasoft.web.rest.server.api" })
 public class SpringWebConfiguration extends WebMvcConfigurationSupport {
 
     @Bean
+    @Override
     public RequestMappingHandlerMapping requestMappingHandlerMapping(
             @Qualifier("mvcContentNegotiationManager") ContentNegotiationManager contentNegotiationManager,
             @Qualifier("mvcConversionService") FormattingConversionService conversionService,
@@ -38,5 +44,25 @@ public class SpringWebConfiguration extends WebMvcConfigurationSupport {
                 conversionService, resourceUrlProvider);
         handlerMapping.setAlwaysUseFullPath(true);
         return handlerMapping;
+    }
+
+    /**
+     * Configures HTTP message converters with custom Bonita Jackson serializer.
+     * This method is called by Spring during context initialization to set up
+     * the message converters for REST API serialization/deserialization.
+     */
+    @Override
+    protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // First, create a custom Jackson converter with Bonita serializers
+        // for backward compatibility with Restlet JSON format (_string suffix fields)
+        ObjectMapper objectMapper = new ObjectMapper();
+        BonitaJacksonModuleProvider.configureObjectMapper(objectMapper);
+        MappingJackson2HttpMessageConverter bonitaConverter = new MappingJackson2HttpMessageConverter(objectMapper);
+
+        // Add our custom converter first (highest priority)
+        converters.add(bonitaConverter);
+
+        // Then add all default converters
+        addDefaultHttpMessageConverters(converters);
     }
 }

bpm/bonita-web-server/src/main/java/org/bonitasoft/web/rest/server/api/bpm/flownode/TimerEventTrigger.java
@@ -13,23 +13,5 @@
  **/
 package org.bonitasoft.web.rest.server.api.bpm.flownode;
 
-public class TimerEventTrigger {
-
-    private Long executionDate = null;
-
-    public TimerEventTrigger() {
-    }
-
-    public TimerEventTrigger(final long executionDate) {
-        this.executionDate = executionDate;
-    }
-
-    public void setExecutionDate(final Long executionDate) {
-        this.executionDate = executionDate;
-    }
-
-    public Long getExecutionDate() {
-        return executionDate;
-    }
-
+public record TimerEventTrigger(Long executionDate) {
 }

bpm/bonita-web-server/src/main/java/org/bonitasoft/web/rest/server/api/bpm/flownode/TimerEventTriggerController.java
@@ -65,10 +65,19 @@ public class TimerEventTriggerController extends AbstractRESTController {
         final SearchResult<TimerEventTriggerInstance> searchResult = getProcessAPI(httpSession)
                 .searchTimerEventTriggerInstances(caseId, new SearchOptionsBuilder(page * count, count).done());
 
+        final List<TimerEventTriggerInstance> results = searchResult.getResult();
+
+        // Return 204 No Content for empty results
+        if (results.isEmpty()) {
+            return ResponseEntity.noContent()
+                    .header(HttpHeaders.CONTENT_RANGE, buildContentRange(0, 0, 0))
+                    .build();
+        }
+
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_RANGE,
-                        buildContentRange(page, searchResult.getResult().size(), searchResult.getCount()))
-                .body(searchResult.getResult());
+                        buildContentRange(page, results.size(), searchResult.getCount()))
+                .body(results);
     }
 
     /**
@@ -83,7 +92,7 @@ public class TimerEventTriggerController extends AbstractRESTController {
     @PutMapping("/{id}")
     public TimerEventTrigger updateTimerEventTrigger(@PathVariable Long id, @RequestBody TimerEventTrigger trigger,
             HttpSession httpSession) throws BonitaException {
-        final Date executionDate = new Date(trigger.getExecutionDate());
+        final Date executionDate = new Date(trigger.executionDate());
         final Date updatedDate = getProcessAPI(httpSession).updateExecutionDateOfTimerEventTriggerInstance(id,
                 executionDate);
         return new TimerEventTrigger(updatedDate.getTime());

bpm/bonita-web-server/src/main/java/org/bonitasoft/web/rest/server/utils/BonitaJacksonModuleProvider.java
@@ -1,0 +1,79 @@
+/**
+ * Copyright (C) 2026 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
+package org.bonitasoft.web.rest.server.utils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.bonitasoft.engine.bdm.serialization.CustomLocalDateDeserializer;
+import org.bonitasoft.engine.bdm.serialization.CustomLocalDateSerializer;
+import org.bonitasoft.engine.bdm.serialization.CustomLocalDateTimeDeserializer;
+import org.bonitasoft.engine.bdm.serialization.CustomLocalDateTimeSerializer;
+import org.bonitasoft.engine.bdm.serialization.CustomOffsetDateTimeDeserializer;
+import org.bonitasoft.engine.bdm.serialization.CustomOffsetDateTimeSerializer;
+
+/**
+ * Provides Jackson module configuration for Bonita REST APIs.
+ * <p>
+ * This ensures backward compatibility with the Restlet JSON format, including:
+ * <ul>
+ * <li>_string suffix fields for numeric IDs (for JavaScript precision)</li>
+ * <li>Custom date/time serialization</li>
+ * </ul>
+ * <p>
+ * Used by both Spring MVC configuration and test infrastructure.
+ */
+public final class BonitaJacksonModuleProvider {
+
+    private BonitaJacksonModuleProvider() {
+        // Utility class
+    }
+
+    /**
+     * Creates a Jackson module with Bonita custom serializers.
+     * Same configuration as BonitaJacksonConverter for Restlet.
+     *
+     * @return the configured Jackson module
+     */
+    public static Module createBonitaModule() {
+        SimpleModule bonitaModule = new SimpleModule("BonitaModule");
+
+        // Add custom serializers for _string suffix fields
+        bonitaModule.addSerializer(new DataInstanceSerializer());
+        bonitaModule.addSerializer(new TimerEventTriggerInstanceSerializer());
+
+        // Date/time serializers and deserializers
+        bonitaModule.addDeserializer(LocalDate.class, new CustomLocalDateDeserializer());
+        bonitaModule.addDeserializer(LocalDateTime.class, new CustomLocalDateTimeDeserializer());
+        bonitaModule.addDeserializer(OffsetDateTime.class, new CustomOffsetDateTimeDeserializer());
+        bonitaModule.addSerializer(LocalDate.class, new CustomLocalDateSerializer());
+        bonitaModule.addSerializer(LocalDateTime.class, new CustomLocalDateTimeSerializer());
+        bonitaModule.addSerializer(OffsetDateTime.class, new CustomOffsetDateTimeSerializer());
+
+        return bonitaModule;
+    }
+
+    /**
+     * Configures the given ObjectMapper with Bonita custom serializers.
+     *
+     * @param mapper the ObjectMapper to configure
+     */
+    public static void configureObjectMapper(ObjectMapper mapper) {
+        mapper.registerModule(createBonitaModule());
+    }
+}

bpm/bonita-web-server/src/test/java/org/bonitasoft/web/rest/server/api/RestControllerUtils.java
@@ -16,26 +16,72 @@ package org.bonitasoft.web.rest.server.api;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 
+import java.util.List;
 import java.util.Map;
 
 import org.bonitasoft.console.common.server.utils.SessionUtil;
 import org.bonitasoft.engine.session.APISession;
+import org.bonitasoft.web.rest.server.utils.BonitaJacksonModuleProvider;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 /**
  * Test-purpose utility class to initialize MockMvc for REST controllers with session attributes.
  */
 public class RestControllerUtils {
 
+    /**
+     * Cached default HTTP message converters configured with Bonita custom serializers.
+     */
+    private static final List<HttpMessageConverter<?>> MESSAGE_CONVERTERS = createConfiguredConverters();
+
     public static MockMvc initMockMvcWithSessionAttributes(AbstractRESTController controller,
             Map<String, Object> sessionAttributes, APISession apiSession) {
         doReturn(apiSession).when(controller).getApiSession(any());
         sessionAttributes.put(SessionUtil.API_SESSION_PARAM_KEY, apiSession);
-        return MockMvcBuilders
+
+        StandaloneMockMvcBuilder builder = MockMvcBuilders
                 .standaloneSetup(controller)
-                .setControllerAdvice(new SpringRestResponseEntityExceptionHandler()) // so that generic exceptions are also handled
-                .build();
+                .setControllerAdvice(new SpringRestResponseEntityExceptionHandler());
+
+        // Configure Jackson converter with Bonita custom serializers while keeping other default converters
+        configureJacksonConverter(builder);
+
+        return builder.build();
+    }
+
+    /**
+     * Configures the Jackson message converter with Bonita custom serializers.
+     * Uses setMessageConverters to add our configured converter while keeping defaults.
+     */
+    private static void configureJacksonConverter(StandaloneMockMvcBuilder builder) {
+        builder.setMessageConverters(MESSAGE_CONVERTERS.toArray(new HttpMessageConverter<?>[0]));
+    }
+
+    /**
+     * Creates HTTP message converters configured with Bonita custom serializers.
+     */
+    private static List<HttpMessageConverter<?>> createConfiguredConverters() {
+        List<HttpMessageConverter<?>> converters = new WebMvcConfigurationSupport() {
+
+            public List<HttpMessageConverter<?>> getDefaultConverters() {
+                return getMessageConverters();
+            }
+        }.getDefaultConverters();
+
+        // Configure the Jackson converter with Bonita custom serializers
+        for (HttpMessageConverter<?> converter : converters) {
+            if (converter instanceof MappingJackson2HttpMessageConverter jacksonConverter) {
+                BonitaJacksonModuleProvider.configureObjectMapper(jacksonConverter.getObjectMapper());
+                break;
+            }
+        }
+
+        return converters;
     }
 
 }

bpm/bonita-web-server/src/test/java/org/bonitasoft/web/rest/server/api/bpm/flownode/TimerEventTriggerControllerTest.java
@@ -53,11 +53,12 @@ class TimerEventTriggerControllerTest extends AbstractControllerTest<TimerEventT
     void should_search_timer_event_triggers_for_given_case() throws Exception {
         // Given
         final long caseId = 123L;
+        final long triggerId = 42L;
+        final long eventInstanceId = 100L;
 
         when(processAPI.searchTimerEventTriggerInstances(anyLong(), any(SearchOptions.class)))
-                .thenReturn(new SearchResultImpl<>(2,
-                        List.of(new TimerEventTriggerInstanceImpl(1L, 1L, "timer1", new Date()),
-                                new TimerEventTriggerInstanceImpl(2L, 1L, "timer2", new Date()))));
+                .thenReturn(new SearchResultImpl<>(1,
+                        List.of(new TimerEventTriggerInstanceImpl(triggerId, eventInstanceId, "timer1", new Date()))));
 
         // When & Then
         mockMvc.perform(get("/API/bpm/timerEventTrigger")
@@ -67,11 +68,43 @@ class TimerEventTriggerControllerTest extends AbstractControllerTest<TimerEventT
                 .sessionAttrs(sessionAttributes)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.CONTENT_RANGE, "0-2/2"))
+                .andExpect(header().string(HttpHeaders.CONTENT_RANGE, "0-1/1"))
                 .andExpect(jsonPath("$").isArray())
-                .andExpect(jsonPath("$.length()").value(2));
+                .andExpect(jsonPath("$.length()").value(1))
+                // Verify _string fields are present (backward compatibility with Restlet)
+                .andExpect(jsonPath("$[0].id").value(triggerId))
+                .andExpect(jsonPath("$[0].id_string").value(String.valueOf(triggerId)))
+                .andExpect(jsonPath("$[0].eventInstanceId").value(eventInstanceId))
+                .andExpect(jsonPath("$[0].eventInstanceId_string").value(String.valueOf(eventInstanceId)))
+                .andExpect(jsonPath("$[0].eventInstanceName").value("timer1"));
 
         verify(processAPI).searchTimerEventTriggerInstances(eq(caseId), any(SearchOptions.class));
+    }
+
+    @Test
+    void should_return_string_fields_for_numeric_ids() throws Exception {
+        // Given - test with large IDs that could lose precision in JavaScript
+        final long caseId = 123L;
+        final long triggerId = 9007199254740993L; // > Number.MAX_SAFE_INTEGER
+        final long eventInstanceId = 9007199254740994L;
+
+        when(processAPI.searchTimerEventTriggerInstances(anyLong(), any(SearchOptions.class)))
+                .thenReturn(new SearchResultImpl<>(1,
+                        List.of(new TimerEventTriggerInstanceImpl(triggerId, eventInstanceId, "bigTimer",
+                                new Date()))));
+
+        // When & Then - _string fields preserve precision for JavaScript clients
+        mockMvc.perform(get("/API/bpm/timerEventTrigger")
+                .param("caseId", String.valueOf(caseId))
+                .param("p", "0")
+                .param("c", "10")
+                .sessionAttrs(sessionAttributes)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(triggerId))
+                .andExpect(jsonPath("$[0].id_string").value("9007199254740993"))
+                .andExpect(jsonPath("$[0].eventInstanceId").value(eventInstanceId))
+                .andExpect(jsonPath("$[0].eventInstanceId_string").value("9007199254740994"));
     }
 
     @Test
@@ -153,46 +186,41 @@ class TimerEventTriggerControllerTest extends AbstractControllerTest<TimerEventT
     }
 
     @Test
-    void should_return_empty_list_when_no_timer_triggers_found() throws Exception {
+    void should_return_no_content_when_no_timer_triggers_found() throws Exception {
         // Given
         final long caseId = 123L;
 
         when(processAPI.searchTimerEventTriggerInstances(anyLong(), any(SearchOptions.class)))
                 .thenReturn(new SearchResultImpl<>(0, List.of()));
 
-        // When & Then
+        // When & Then - 204 No Content has no body
         mockMvc.perform(get("/API/bpm/timerEventTrigger")
                 .param("caseId", String.valueOf(caseId))
                 .param("p", "0")
                 .param("c", "10")
                 .sessionAttrs(sessionAttributes)
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.CONTENT_RANGE, "0-0/0"))
-                .andExpect(jsonPath("$").isArray())
-                .andExpect(jsonPath("$.length()").value(0));
+                .andExpect(status().isNoContent())
+                .andExpect(header().string(HttpHeaders.CONTENT_RANGE, "0-0/0"));
     }
 
     @Test
-    void should_return_empty_list_when_page_out_of_bounds() throws Exception {
+    void should_return_no_content_when_page_out_of_bounds() throws Exception {
         // Given
         final long caseId = 123L;
 
         when(processAPI.searchTimerEventTriggerInstances(anyLong(), any(SearchOptions.class)))
                 .thenReturn(new SearchResultImpl<>(2, List.of())); // Total 2, but empty results for this page
 
-        // When & Then
-        // Content-Range format: page-countOnCurrentPage/total
+        // When & Then - 204 No Content has no body
         mockMvc.perform(get("/API/bpm/timerEventTrigger")
                 .param("caseId", String.valueOf(caseId))
                 .param("p", "10") // Page 10 when only 2 results exist
                 .param("c", "10")
                 .sessionAttrs(sessionAttributes)
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.CONTENT_RANGE, "10-0/2"))
-                .andExpect(jsonPath("$").isArray())
-                .andExpect(jsonPath("$.length()").value(0));
+                .andExpect(status().isNoContent())
+                .andExpect(header().string(HttpHeaders.CONTENT_RANGE, "0-0/0"));
     }
 
     @Test